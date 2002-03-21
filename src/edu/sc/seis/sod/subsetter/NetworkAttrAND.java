7bd4bd3cb78a6c7a669be66c98fd2fb54bfc9ddb
telukutl <none@none>
2002-03-21 11:11:46 -0500

    [svn r1360] initial version
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/NetworkAttrAND.java
@@ -0,0 +1,39 @@
+package edu.sc.seis.sod.subsetter;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.*;
+
+/**
+ * NetworkAttrAND.java
+ *
+ *
+ * Created: Thu Mar 14 14:02:33 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class NetworkAttrAND 
+    extends LogicalSubsetter 
+    implements NetworkAttrSubsetter {
+    
+    public NetworkAttrAND (Element config) throws ConfigurationException {
+	super(config);
+    }
+
+    public boolean accept(NetworkAttr e,  CookieJar cookies) {
+	Iterator it = filterList.iterator();
+	if (it.hasNext()) {
+	    NetworkAttrSubsetter filter = (NetworkAttrSubsetter)it.next();
+	    if ( filter.accept(e, cookies)) {
+		return false;
+	    }
+	}
+	return false;
+    }
+
+}// NetworkAttrAND
---
+++ src/edu/sc/seis/sod/subsetter/NetworkIDOR.java
@@ -0,0 +1,39 @@
+package edu.sc.seis.sod.subsetter;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.*;
+
+/**
+ * NetworkIDOR.java
+ *
+ *
+ * Created: Thu Mar 14 14:02:33 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class NetworkIDOR 
+    extends LogicalSubsetter 
+    implements NetworkIdSubsetter {
+    
+    public NetworkIDOR (Element config) throws ConfigurationException {
+	super(config);
+    }
+
+    public boolean accept(NetworkId e,  CookieJar cookies) {
+	Iterator it = filterList.iterator();
+	if (it.hasNext()) {
+	    NetworkIdSubsetter filter = (NetworkIdSubsetter)it.next();
+	    if ( filter.accept(e, cookies)) {
+		return false;
+	    }
+	}
+	return false;
+    }
+
+}// NetworkIDOR
