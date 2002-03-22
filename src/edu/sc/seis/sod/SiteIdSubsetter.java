5ff3399341644d449368ca8da0e60eb7e164f043
telukutl <none@none>
2002-03-22 00:41:12 -0500

    [svn r1362] *** empty log message ***
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/SiteIdSubsetter.java
@@ -0,0 +1,20 @@
+package edu.sc.seis.sod;
+
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+
+/**
+ * SiteIdSubsetter.java
+ *
+ *
+ * Created: Thu Dec 13 17:06:22 2001
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public interface SiteIdSubsetter {
+
+    public boolean accept(SiteId id, CookieJar cookies);
+    
+}// SiteIdSubsetter
---
+++ src/edu/sc/seis/sod/SiteSubsetter.java
@@ -0,0 +1,20 @@
+package edu.sc.seis.sod;
+
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+
+/**
+ * SiteSubsetter.java
+ *
+ *
+ * Created: Thu Dec 13 17:05:33 2001
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public interface SiteSubsetter {
+
+    public boolean accept(Site station, CookieJar cookies);
+    
+}// SiteSubsetter
