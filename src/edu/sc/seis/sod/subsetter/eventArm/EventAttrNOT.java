26e68bb06e7f2fe43d7cc7935b988c4c9eb858a6
telukutl <none@none>
2002-03-22 11:07:41 -0500

    [svn r1367] after moving from subsetter to subsetter/eventArm
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/eventArm/EventAttrNOT.java
@@ -0,0 +1,39 @@
+package edu.sc.seis.sod.subsetter;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.event.*;
+import edu.iris.Fissures.*;
+
+/**
+ * EventAttrNOT.java
+ *
+ *
+ * Created: Thu Mar 14 14:02:33 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class EventAttrNOT 
+    extends LogicalSubsetter 
+    implements EventAttrSubsetter {
+    
+    public EventAttrNOT (Element config) throws ConfigurationException {
+	super(config);
+    }
+
+    public boolean accept(EventAttr e,  CookieJar cookies) {
+	Iterator it = filterList.iterator();
+	if (it.hasNext()) {
+	    EventAttrSubsetter filter = (EventAttrSubsetter)it.next();
+	    if ( filter.accept(e, cookies)) {
+		return false;
+	    }
+	}
+	return false;
+    }
+
+}// EventAttrNOT
