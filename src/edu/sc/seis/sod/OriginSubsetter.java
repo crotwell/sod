a24e7f6177b9c4fe02aea1fc9b8a3ee3e8753889
telukutl <none@none>
2002-04-02 13:33:38 -0500

    [svn r1398] *** empty log message ***
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/OriginSubsetter.java
@@ -0,0 +1,18 @@
+package edu.sc.seis.sod;
+
+import edu.iris.Fissures.IfEvent.*;
+
+/**
+ * OriginSubsetter.java
+ *
+ *
+ * Created: Tue Apr  2 13:32:13 2002
+ *
+ * @author <a href="mailto:telukutl@piglet">Srinivasa Telukutla</a>
+ * @version
+ */
+
+public interface OriginSubsetter extends Subsetter{
+    public boolean accept(Origin origin, CookieJar cookies);
+    
+}// OriginSubsetter
