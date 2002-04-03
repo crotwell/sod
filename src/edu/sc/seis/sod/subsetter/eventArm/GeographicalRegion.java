d1d6dd9a92e330217aee31e4e95d1c11c05418f9
telukutl <none@none>
2002-04-03 12:16:17 -0500

    [svn r1400] *** empty log message ***
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/eventArm/GeographicalRegion.java
@@ -0,0 +1,26 @@
+package edu.sc.seis.sod.subsetter.eventArm;
+
+import edu.sc.seis.sod.*;
+import edu.sc.seis.sod.subsetter.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.event.*;
+import edu.iris.Fissures.*;
+
+/**
+ * GeographicalRegion.java
+ *
+ *
+ * Created: Tue Mar 19 13:42:28 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class GeographicalRegion  extends FlinnEngdahlRegion{
+    public GeographicalRegion (Element config){
+	super(config);
+    }
+    
+}// GeographicalRegion
