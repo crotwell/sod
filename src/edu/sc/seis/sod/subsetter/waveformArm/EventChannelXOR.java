02d7e0658a47a7098e521edc2adb38d7cc060eb8
telukutl <none@none>
2002-04-08 15:48:18 -0400

    [svn r1412] initial version
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/waveformArm/EventChannelXOR.java
@@ -0,0 +1,32 @@
+package edu.sc.seis.sod.subsetter.waveFormArm;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.*;
+
+
+public class EventChannelXOR 
+    extends  WaveFormLogicalSubsetter 
+    implements EventChannelSubsetter {
+    
+    public EventChannelXOR (Element config) throws ConfigurationException {
+	super(config);
+    }
+
+    public boolean accept(EventAccessOperations o, Channel channel,  CookieJar cookies) {
+	Iterator it = filterList.iterator();
+	while (it.hasNext()) {
+	    EventChannelSubsetter filter = (EventChannelSubsetter)it.next();
+	    if (!filter.accept(o, channel, cookies)) {
+		return false;
+	    }
+	}
+	return true;
+    }
+
+}// EventChannelXOR
