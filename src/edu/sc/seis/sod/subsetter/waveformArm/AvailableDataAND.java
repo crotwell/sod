ec76348be78e947ba0a7c2d274ee117bb6b2eada
telukutl <none@none>
2002-04-08 15:30:42 -0400

    [svn r1410] *** empty log message ***
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/waveformArm/AvailableDataAND.java
@@ -0,0 +1,32 @@
+package edu.sc.seis.sod.subsetter.waveFormArm;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.*;
+
+
+public class AvailableDataAND 
+    extends  WaveFormLogicalSubsetter 
+    implements AvailableDataSubsetter {
+    
+    public AvailableDataAND (Element config) throws ConfigurationException {
+	super(config);
+    }
+
+    public boolean accept(EventAccessOperations o, Station station,  CookieJar cookies) {
+	Iterator it = filterList.iterator();
+	while (it.hasNext()) {
+	    AvailableDataSubsetter filter = (AvailableDataSubsetter)it.next();
+	    if (!filter.accept(o, station, cookies)) {
+		return false;
+	    }
+	}
+	return true;
+    }
+
+}// AvailableDataAND
