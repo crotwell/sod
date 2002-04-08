c9c8b5eb06ff9d5d3fb04638d5941bc8310a8c74
telukutl <none@none>
2002-04-08 16:38:17 -0400

    [svn r1415] *** empty log message ***
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/waveformArm/EventStationDistance.java
@@ -0,0 +1,35 @@
+package edu.sc.seis.sod.subsetter.waveFormArm;
+
+import edu.sc.seis.sod.*;
+import edu.sc.seis.sod.subsetter.*;
+
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.event.*;
+import edu.iris.Fissures.IfNetwork.*;
+import edu.iris.Fissures.network.*;
+
+import edu.iris.Fissures.*;
+
+import org.w3c.dom.*;
+
+/**
+ * EventStationDistance.java
+ *
+ *
+ * Created: Mon Apr  8 16:32:56 2002
+ *
+ * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * @version
+ */
+
+public class EventStationDistance extends DistanceRange implements EventStationSubsetter {
+    public EventStationDistance (Element config){
+	super(config);
+    }
+    
+    public boolean accept(EventAccessOperations eventAccess, Station station, CookieJar cookies) {
+
+	return true;
+    }
+
+}// EventStationDistance
