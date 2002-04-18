3a2dd8389d367153b3df8b65b37a1699d8e1a337
telukutl <none@none>
2002-04-18 11:44:47 -0400

    [svn r1498] *** empty log message ***
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/eventArm/NullEventAttrSubsetter.java
@@ -0,0 +1,42 @@
+package edu.sc.seis.sod.subsetter.eventArm;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.event.*;
+import edu.iris.Fissures.*;
+
+/**
+ * NullEventAttrSubsetter.java
+ *
+ *
+ * Created: Thu Mar 14 14:02:33 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class NullEventAttrSubsetter implements EventAttrSubsetter {
+    
+    /**
+     * Creates a new <code>NullEventAttrSubsetter</code> instance.
+     *
+     * @param config an <code>Element</code> value
+     * @exception ConfigurationException if an error occurs
+     */
+    public NullEventAttrSubsetter () {
+    }
+
+    /**
+     * Describe <code>accept</code> method here.
+     *
+     * @param e an <code>EventAttr</code> value
+     * @param cookies a <code>CookieJar</code> value
+     * @return a <code>boolean</code> value
+     */
+    public boolean accept(EventAttr e,  CookieJar cookies) {
+	return true;
+    }
+
+}// NullEventAttrSubsetter
---
+++ src/edu/sc/seis/sod/subsetter/eventArm/NullOriginSubsetter.java
@@ -0,0 +1,45 @@
+package edu.sc.seis.sod.subsetter.eventArm;
+
+import edu.sc.seis.sod.*;
+import java.util.*;
+import org.w3c.dom.*;
+import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.event.*;
+import edu.iris.Fissures.*;
+
+/**
+ * NullOriginSubsetter.java
+ *
+ *
+ * Created: Thu Mar 14 14:02:33 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class NullOriginSubsetter
+    implements OriginSubsetter {
+    
+    /**
+     * Creates a new <code>NullOriginSubsetter</code> instance.
+     *
+     * @param config an <code>Element</code> value
+     * @exception ConfigurationException if an error occurs
+     */
+    public NullOriginSubsetter () {
+    }
+
+    /**
+     * Describe <code>accept</code> method here.
+     *
+     * @param event an <code>EventAccessOperations</code> value
+     * @param e an <code>Origin</code> value
+     * @param cookies a <code>CookieJar</code> value
+     * @return a <code>boolean</code> value
+     * @exception Exception if an error occurs
+     */
+    public boolean accept(EventAccessOperations event, Origin e,  CookieJar cookies) throws Exception{
+	return true;
+    }
+
+}// NullOriginSubsetter
