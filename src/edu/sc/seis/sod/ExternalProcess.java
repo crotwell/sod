33508b7a5a8ef62cb6cb2585d025581c2098c4db
crotwell <none@none>
2002-05-08 17:00:19 -0400

    [svn r1601] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/ExternalProcess.java
@@ -0,0 +1,37 @@
+package edu.sc.seis.sod;
+
+import org.w3c.dom.*;
+
+/**
+ * ExternalProcess.java
+ *
+ *
+ * Created: Fri Apr 12 12:03:11 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public abstract class ExternalProcess implements Process {
+    public ExternalProcess (Element config) throws ConfigurationException {
+	NodeList children = config.getChildNodes();
+	Node node;
+	for (int i=0; i < children.getLength(); i++) {
+	    node = children.item(i);
+	    if (node instanceof Element) {
+		Element subElement = (Element)node;
+		String tagName = subElement.getTagName();
+		if (tagName.equals("classname")) {
+		    classname = SodUtil.getText(subElement);
+		}
+	    }
+	}
+	SodElement se = SodUtil.loadExternal(config, classname);
+	process = (Process)se;
+    }
+    
+    Process process;
+
+    String classname;
+    
+}// ExternalProcess
