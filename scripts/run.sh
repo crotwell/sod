795f2a93b48186689f823a5a1559f241da435111
crotwell <none@none>
2002-06-04 15:45:22 -0400

    [svn r1781] add taup, log4j
    --HG--
    branch : sod

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,8 +5,10 @@ OBNAMING=../../OrbacusSignedJars/dist/OBNaming.jar
 FISSURESUTIL=../../fissuresUtil/dist/lib/fissuresUtil.jar
 FISSURESIMPL=../../fissures/dist/lib/FissuresImpl.jar
 FISSURESIDL=../../fissures/dist/lib/FissuresIDL.jar
+TAUP=../../TauP/taup.jar
 XERCES=../../Xerces/xercesImpl.jar
 XMLAPI=../../Xerces/xmlParserAPIs.jar
+LOG4J=../../jakarta-log4j-1.1.3/dist/lib/log4j.jar
 
 
-java -cp ../build:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${OB}:${OBNAMING}:${XERCES}:${XMLAPI} edu.sc.seis.sod.Start $*
+java -cp ../build:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${OB}:${OBNAMING}:${TAUP}:${XERCES}:${XMLAPI}:${LOG4J} edu.sc.seis.sod.Start $*
