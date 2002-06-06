91066351fc254b68fe9874e597b3ae2d7b7462fb
crotwell <none@none>
2002-06-06 11:53:15 -0400

    [svn r1812] change to use filename if present, or stdout if not, need to add filename to configureation
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventArm/PrintLineEventProcess.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/PrintLineEventProcess.java
@@ -20,7 +20,6 @@ public class PrintLineEventProcess implements EventArmProcess {
      * @param config an <code>Element</code> value
      */
     public PrintLineEventProcess (Element config){
-	
     }
 
     /**
@@ -30,15 +29,36 @@ public class PrintLineEventProcess implements EventArmProcess {
      * @param cookies a <code>CookieJar</code> value
      */
     public void process(EventAccessOperations event, CookieJar cookies) {
+	String eventStr = 
+	    regions.getRegionName(event.get_attributes().region);
 	try {
-	    FileWriter fwriter = new FileWriter("_my_event_temp_", true);
-	    BufferedWriter bwriter = new BufferedWriter(fwriter);
-	    bwriter.write(event.get_attributes().name, 0, event.get_attributes().name.length());
-	    bwriter.newLine();
-	    bwriter.close();
-	} catch(Exception e) {
+	    eventStr+=" "+event.get_preferred_origin().origin_time.date_time;
+	} catch (NoPreferredOrigin e) {
+	    eventStr+=" No Pref Origin!";
+	} // end of try-catch
+	
+
+	if (filename != null) {
+	    try {
+		FileWriter fwriter = new FileWriter("_my_event_temp_", true);
+		BufferedWriter bwriter = new BufferedWriter(fwriter);
+		bwriter.write(eventStr, 0, eventStr.length());
+		bwriter.newLine();
+		bwriter.close();
+	    } catch(Exception e) {
 	    
-	    System.out.println("Exception caught while writing to file in PrintLineChannelProcess");
-	}
+		System.out.println("Exception caught while writing to file in PrintLineChannelProcess");
+	    }
+	} else {
+
+	    System.out.println(eventStr);
+	} // end of else
+	
     }
+
+    protected String filename = null;
+
+    protected static edu.sc.seis.fissuresUtil.display.ParseRegions regions 
+	= new edu.sc.seis.fissuresUtil.display.ParseRegions();;
+
 }// PrintLineEventProcess
