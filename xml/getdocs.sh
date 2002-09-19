a7d0fed787db4c51a920596849a56dbe6f3af786
crotwell <none@none>
2002-09-19 17:07:06 -0400

    [svn r2598] initial
    --HG--
    branch : sod

---
+++ xml/getdocs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+XALANDIR=/Users/crotwell/Developement/xalan-j_2_4_0/bin 
+
+java -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalan.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar:${XALANDIR}/xsltc.jar org.apache.xalan.xslt.Process -in utilities.xsd -out sod_docs.html -xsl getdocs.xsl
+
+echo Done!
+
+
