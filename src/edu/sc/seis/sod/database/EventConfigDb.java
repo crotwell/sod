ad51a445a77fbb778c0a5566c23bf94b59e33d03
telukutl <none@none>
2002-09-28 13:14:56 -0400

    [svn r2633] switched the implementation from sleepycat to hsqldb
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/database/EventConfigDb.java
+++ src/edu/sc/seis/sod/database/EventConfigDb.java
@@ -3,9 +3,11 @@ package edu.sc.seis.sod.database;
 
 import edu.iris.Fissures.model.*;
 
-import com.sleepycat.db.*;
 import java.io.*;
 import java.util.*;
+import java.sql.*;
+
+import org.hsqldb.*;
 
 
 /**
@@ -20,9 +22,32 @@ import java.util.*;
 
 public class EventConfigDb {
     public EventConfigDb (){
-	
+	init();
     }
     
+    private void init() {
+	try {
+	    String driverName = "org.hsqldb.jdbcDriver";
+	    Class.forName(driverName).newInstance();
+	    Connection connection = DriverManager.getConnection("jdbc:hsqldb:myhsqldb", "sa", "");
+	    Statement stmt = connection.createStatement();
+	    try {
+		stmt.executeUpdate("CREATE TABLE timeconfig "+
+				   "( time timestamp)");
+	    } catch(SQLException sqle) {
+		System.out.println("Table timeconfig  is already created");
+	    }
+	    setTimeStmt = connection.prepareStatement(" INSERT into timeconfig "+
+						      " VALUES(?) ");
+	    updateTimeStmt = connection.prepareStatement(" UPDATE timeconfig set time = ? ");
+	    
+	    getTimeStmt = connection.prepareStatement(" SELECT time from timeconfig");
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+	
+
+    }
     public EventConfigDb(edu.iris.Fissures.Time time, 
 			 String serverDNS,
 			 String serverName) {
@@ -35,24 +60,19 @@ public class EventConfigDb {
 
     
     public void setTime(edu.iris.Fissures.Time time) {
-	open();
 	try {
-	    Dbt key = new Dbt();
-	    String keyStr = new String("time");
-	    key.set_data(keyStr.getBytes());
-	    key.set_size(keyStr.length());
-
-	    Dbt data = new Dbt();
-	    byte[] bytes = getBytes(time);
-	    data.set_data(bytes);
-	    data.set_size(bytes.length);
-	
-	    table.put(null, key, data, Db.DB_APPEND);
-	    table.close(0);
-	} catch(Exception e) {
-	    e.printStackTrace();
+	    if(getTime() == null) {
+		MicroSecondDate ms = new MicroSecondDate(time);
+		setTimeStmt.setTimestamp(1, ms.getTimestamp());
+		setTimeStmt.executeUpdate();
+	    } else {
+		updateTime(time);
+	    }
+	} catch(SQLException sqle) {
+	    sqle.printStackTrace();
 	}
     }
+    
 
     public void setServerDNS(String serverDNS) {
 	this.serverDNS = serverDNS;
@@ -65,19 +85,15 @@ public class EventConfigDb {
 
 
     public edu.iris.Fissures.Time getTime() {
-	open();
 	try {
-	    Dbt data = new Dbt();
-	    Dbt key = new Dbt();
-	    Dbc dbc = table.cursor(null, 0);
-	    dbc.get(key, data, Db.DB_NEXT);
-	    Object obj = getObject(data.get_data());
-	    dbc.close();
-	    table.close(0);
-	    
-	    return (edu.iris.Fissures.Time)obj;
-	} catch(Exception e) {
-	    e.printStackTrace();
+	    ResultSet rs = getTimeStmt.executeQuery();
+	    if(rs.next()) {
+		MicroSecondDate ms = new MicroSecondDate(rs.getTimestamp("time"));
+		return ms.getFissuresTime();
+	    }
+	    return null;
+	} catch(SQLException sqle) {
+	    sqle.printStackTrace();
 	    return null;
 	}
     }
@@ -90,22 +106,19 @@ public class EventConfigDb {
 	calendar.roll(Calendar.DAY_OF_MONTH, 1);
 	microSecondDate = new MicroSecondDate(calendar.getTime());
 	time = microSecondDate.getFissuresTime();
-	open();
-	try {
-	    Dbt data = new Dbt();
-	    Dbt key = new Dbt();
-	    Dbc dbc = table.cursor(null, 0);
-	    dbc.get(key, data, Db.DB_NEXT);
-	    dbc.del(0);
-	    dbc.close();
-	    table.close(0);
-	} catch(Exception e) {
-	    e.printStackTrace();
-	}
-
-	setTime(time);
+	updateTime(time);
     }
 
+    private void updateTime(edu.iris.Fissures.Time time) {
+	try {
+	    MicroSecondDate ms = new MicroSecondDate(time);
+	    updateTimeStmt.setTimestamp(1, ms.getTimestamp());
+	    updateTimeStmt.executeUpdate();
+	} catch(SQLException sqle) {
+	    sqle.printStackTrace();
+	}
+    }
+    
     public String getServerDNS() {
 	return this.serverDNS;
     }
@@ -145,25 +158,14 @@ public class EventConfigDb {
 	}
     }
 
-    
-    private void open() {
-
-	try {
-	    String fileName = "eventconfig.db";
-	    table = new Db(null, 0);
-	    table.set_error_stream(System.err);
-	    table.set_errpfx("EVENT CONFIG");
-	    table.open(fileName, null, Db.DB_RECNO, Db.DB_CREATE, 0644);
-	} catch(Exception e) {
-	    e.printStackTrace();
-	}
-    }
-
-    private Db table = null;
-
     private String serverDNS;
 
     private String serverName;
 
+    private PreparedStatement setTimeStmt;
+    
+    private PreparedStatement updateTimeStmt;
+
+    private PreparedStatement getTimeStmt;
    
 }// EventConfigDb
