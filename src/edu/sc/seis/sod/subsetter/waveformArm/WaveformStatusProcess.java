f445dc4d48b4a962455cdd91f1e842517a78e796
crotwell <none@none>
2002-10-28 09:13:37 -0500

    [svn r2831] changed to interface
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/waveformArm/WaveformStatusProcess.java
+++ src/edu/sc/seis/sod/subsetter/waveformArm/WaveformStatusProcess.java
@@ -8,7 +8,6 @@ import edu.iris.Fissures.IfNetwork.*;
 import edu.iris.Fissures.network.*;
 
 import org.w3c.dom.*;
-import java.io.*;
 
 
 /**
@@ -21,110 +20,38 @@ import java.io.*;
  * @version
  */
 
-public class WaveformStatusProcess implements Subsetter{
-    public WaveformStatusProcess (Element config){
-	fileName = SodUtil.getNestedText(config);
-// 	try {
-// 	    FileWriter fw = new FileWriter(fileName);
-// 	    bw = new BufferedWriter(fw);
-// 	    writeHeader();
-// 	} catch(Exception e) {
-// 	    e.printStackTrace();
-// 	}
-    }
+public interface WaveformStatusProcess extends Subsetter{
 
-    private void writeHeader() {
-	String header = "<html><head><title>sodReport</title></head><body>";
-	write(header);
-	//	newLine();
-	
-    }
+    public void begin(EventAccessOperations eventAccess);
 
-    private void writeTail() {
-	String str = "</body></html>";
-	write(str);
-    }
+    public void begin(EventAccessOperations eventAccess, 
+		      NetworkAccess networkAccess);
 
-    private void startTable() {
-	String str = new String("<table>");
-	write(str);
-	//bw.newLine();
-    }
-
-    private void endTable() {
-	String str = new String("</table>");
-	write(str);
-	//bw.newLine();
-    }
-
-    private void write(String str) {
-	try {
-	    FileWriter fw = new FileWriter(fileName, true);
-	    BufferedWriter bw = new BufferedWriter(fw);
-	    bw.write(str, 0, str.length());
-	    bw.write("<br><br>");
-	    bw.newLine();
-	    bw.close();
-	    fw.close();
-	} catch(Exception e) {
-	    e.printStackTrace();
-	}
-    }
-    public void begin(EventAccessOperations eventAccess) {
-	totalEvents++;
-	EventAttr eventAttr = eventAccess.get_attributes();
-	startTable();
-	write("<tr> Processing Event "+eventAttr.name+"</tr>");
-    }
-    public void begin(EventAccessOperations eventAccess, NetworkAccess networkAccess) {
-	NetworkAttr networkAttr = networkAccess.get_attributes();
-	write("<tr> Processing Network "+networkAttr.name+"</tr>");
-	//bw.newLine();
-    }
-
-    public void begin(EventAccessOperations eventAccess, Station station) {
-// 	write("<tr> Processing Station "+station.get_code()+"</tr>");
-    }
+    public void begin(EventAccessOperations eventAccess, 
+		      Station station);
     
-    public void begin(EventAccessOperations eventAccess, Site site) {
-// 	write("<tr> Processing Site "+site.get_code()+"</tr>");
-    }
+    public void begin(EventAccessOperations eventAccess, 
+		      Site site);
     
-    public void begin(EventAccessOperations eventAccess, Channel channel) {
-       write("<tr> Processing Channel "+ChannelIdUtil.toString(channel.get_id())+"</tr>");
-    }
+    public void begin(EventAccessOperations eventAccess, 
+		      Channel channel);
 
-    public void end(EventAccessOperations eventAccess, Channel channel, Status status, String reason) {
-	write("<tr> Done with Channel "+ChannelIdUtil.toString(channel.get_id())+" Status: "+status.toString()+
-		 "Reason: "+reason+" </tr>");
-    }
+    public void end(EventAccessOperations eventAccess, 
+		    Channel channel, 
+		    Status status, 
+		    String reason);
     
-    public void end(EventAccessOperations eventAccess, Site site) {
-// 	write("<tr> Done with Site "+site.get_code()+"</tr>");
-    }
+    public void end(EventAccessOperations eventAccess, 
+		    Site site);
     
-    public void end(EventAccessOperations eventAccess, Station station) {
-	//	write("<tr> Done with Station "+station.get_code()+"</tr>");
-    }
+    public void end(EventAccessOperations eventAccess, 
+		    Station station);
 
-    public void end(EventAccessOperations eventAccess, NetworkAccess networkAccess) {
-	//	write("<tr> Processing Station "+station.get_code()+"</tr>");
-	NetworkAttr networkAttr = networkAccess.get_attributes();
-	write("<tr> Done with Network "+networkAttr.name+"</tr>");
-    }
+    public void end(EventAccessOperations eventAccess, 
+		    NetworkAccess networkAccess);
 
-    public void end(EventAccessOperations eventAccess) {
-	EventAttr eventAttr = eventAccess.get_attributes();
-	write("<tr> Done with Event "+eventAttr.name+"</tr>");
-	endTable();
-    }
+    public void end(EventAccessOperations eventAccess);
 
-    public void closeProcessing() {
-	write("<h1> Total number of events processed are "+totalEvents+"</h1>");
-	writeTail();
-    }
+    public void closeProcessing();
 
-    private String fileName = new String("status.html");
-    private int totalEvents = 0;
-  
 }// WaveformStatusProcess
