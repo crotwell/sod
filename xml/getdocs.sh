401b42bcfca1af50ab766d26d72e161a9dc23ce1
crotwell <none@none>
2002-11-12 21:35:23 -0500

    [svn r2900] use botclasspath since xalan2.2 is in java1.4
    --HG--
    branch : sod

--- xml/getdocs.sh
+++ xml/getdocs.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-XALANDIR=/Users/crotwell/Developement/xalan-j_2_4_0/bin 
+XALANDIR=/Users/crotwell/External/xalan-j_2_4_1/bin 
 
-java -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalan.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar:${XALANDIR}/xsltc.jar org.apache.xalan.xslt.Process -in sod.xsd -out sod_docs.html -xsl getdocs.xsl
+java -Xbootclasspath/p:${XALANDIR}/xalan.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar:${XALANDIR}/xsltc.jar org.apache.xalan.xslt.EnvironmentCheck
+
+
+java -Xbootclasspath/p:${XALANDIR}/xalan.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar:${XALANDIR}/xsltc.jar org.apache.xalan.xslt.Process -in ../../src/edu/sc/seis/sod/data/sod.xsd -out sod_docs.html -xsl getdocs.xsl 
 
 echo Done!
 
