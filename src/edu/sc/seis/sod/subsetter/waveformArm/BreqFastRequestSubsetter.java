31cd574b9dab4288c7313075385e6afc985c5a41
crotwell <none@none>
2003-03-19 16:43:25 -0500

    [svn r3512] initial, this uses BreqFastAvailableData to do the actual work
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/waveformArm/BreqFastRequestSubsetter.java
@@ -0,0 +1,47 @@
+package edu.sc.seis.sod.subsetter.waveFormArm;
+
+import edu.sc.seis.sod.*;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import org.w3c.dom.*;
+
+/**
+ * BreqFastRequestSubsetter.java
+ *
+ *
+ * Created: Wed Mar 19 14:07:16 2003
+ *
+ * @author <a href="mailto:crotwell@owl.seis.sc.edu">Philip Crotwell</a>
+ * @version 1.0
+ */
+public class BreqFastRequestSubsetter 
+    implements RequestSubsetter {
+
+    public BreqFastRequestSubsetter(Element config) 
+        throws ConfigurationException 
+    {
+        breqfast = new BreqFastAvailableData(config);
+    }
+    
+    public boolean accept(EventAccessOperations event, 
+                          NetworkAccess network, 
+                          Channel channel, 
+                          RequestFilter[] request,
+                          CookieJar cookies) 
+        throws Exception 
+    {
+        breqfast.accept(event, 
+                        network, 
+                        channel, 
+                        request, 
+                        request,
+                        cookies);
+
+        return true;
+    }
+
+    BreqFastAvailableData breqfast;
+
+} // BreqFastRequestGenerator
