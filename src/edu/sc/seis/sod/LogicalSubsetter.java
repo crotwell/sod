eca6bc724d5720769b12230b895754ec998d307b
crotwell <none@none>
2003-03-21 14:36:34 -0500

    [svn r3526] do not log empty text nodes
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/LogicalSubsetter.java
+++ src/edu/sc/seis/sod/LogicalSubsetter.java
@@ -24,7 +24,7 @@ public abstract class LogicalSubsetter implements Subsetter {
      * @exception ConfigurationException if an error occurs
      */
     public LogicalSubsetter (Element config) throws ConfigurationException {
-	processConfig(config);
+        processConfig(config);
     }
     
     /**
@@ -34,32 +34,36 @@ public abstract class LogicalSubsetter implements Subsetter {
      * @exception ConfigurationException if an error occurs
      */
     protected void processConfig(Element config) throws ConfigurationException{
-	NodeList children = config.getChildNodes();
-	Node node;
-	Class[] constructorArgTypes = new Class[1];
-	constructorArgTypes[0] = Element.class;
+        NodeList children = config.getChildNodes();
+        Node node;
+        Class[] constructorArgTypes = new Class[1];
+        constructorArgTypes[0] = Element.class;
 
-	for (int i=0; i<children.getLength(); i++) {
-	    node = children.item(i);
-	    if (node instanceof Element) {
-		Element subElement = (Element)node;
-		if (subElement.getTagName().equals("description")) {
-		    // skip description element
-		    continue;
-		}
+        for (int i=0; i<children.getLength(); i++) {
+            node = children.item(i);
+            if (node instanceof Element) {
+                Element subElement = (Element)node;
+                if (subElement.getTagName().equals("description")) {
+                    // skip description element
+                    continue;
+                }
 		
-		Object obj = SodUtil.load(subElement, getPackageName());
-		if (obj instanceof Subsetter) {
-		    filterList.add((Subsetter)obj);
-		} else {
-		    logger.error("obj not instance of Subsetter "+obj);
-		} // end of else
+                Object obj = SodUtil.load(subElement, getPackageName());
+                if (obj instanceof Subsetter) {
+                    filterList.add((Subsetter)obj);
+                } else {
+                    logger.error("obj not instance of Subsetter "+obj);
+                } // end of else
 		
-	    } else {
-		logger.error("node not instance of Element "+node);
-	    } // end of else
+            } else {
+                if ( node instanceof TextNode && node.getValue().trim().equals("")) {
+                    // don't worry about empty text
+                } else {
+                    logger.error("node not instance of Element "+node);
+                } // end of else
+            } // end of else
 	    
-	} // end of for (int i=0; i<children.getSize(); i++)
+        } // end of for (int i=0; i<children.getSize(); i++)
 	
     }
 
@@ -80,6 +84,6 @@ public abstract class LogicalSubsetter implements Subsetter {
 
 
     static Category logger = 
-	Category.getInstance(LogicalSubsetter.class.getName());
+        Category.getInstance(LogicalSubsetter.class.getName());
 
 }// LogicalSubsetter
