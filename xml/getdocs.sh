488a73d2dd139a132764496e5343a65a11d6f298
crotwell <none@none>
2003-03-28 16:46:03 -0500

    [svn r3589] fix paths
    --HG--
    branch : sod

--- xml/getdocs.sh
+++ xml/getdocs.sh
@@ -2,10 +2,10 @@
 
 XALANDIR=/Users/crotwell/External/xalan-j_2_4_1/bin 
 
-java -Xbootclasspath/p:${XALANDIR}/xalan.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar:${XALANDIR}/xsltc.jar org.apache.xalan.xslt.EnvironmentCheck
+java -Xbootclasspath/p:${XALANDIR}/xalan.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar:${XALANDIR}/xsltc.jar -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar org.apache.xalan.xslt.EnvironmentCheck
 
 
-java -Xbootclasspath/p:${XALANDIR}/xalan.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar:${XALANDIR}/xsltc.jar org.apache.xalan.xslt.Process -in ../../src/edu/sc/seis/sod/data/sod.xsd -out sod_docs.html -xsl getdocs.xsl 
+java -Xbootclasspath/p:${XALANDIR}/xalan.jar:${XALANDIR}/xercesImpl.jar:${XALANDIR}/xml-apis.jar:${XALANDIR}/xsltc.jar -cp ${XALANDIR}/BCEL.jar:${XALANDIR}/JLex.jar:${XALANDIR}/bsf.jar:${XALANDIR}/java_cup.jar:${XALANDIR}/regexp.jar:${XALANDIR}/runtime.jar:${XALANDIR}/xalansamples.jar:${XALANDIR}/xalanservlet.jar org.apache.xalan.xslt.Process -in ../src/edu/sc/seis/sod/data/sod.xsd -out sod_docs.html -xsl getdocs.xsl 
 
 echo Done!
 
