8102e449baeb7d8b7f62553ebda637e5e6f82212
crotwell <none@none>
2003-10-07 12:46:48 -0400

    [svn r6023] remove bad try-catch, reindent
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/WaveFormArmThread.java
+++ src/edu/sc/seis/sod/WaveFormArmThread.java
@@ -29,33 +29,32 @@ import org.apache.log4j.*;
  */
 
 public class WaveFormArmThread extends SodExceptionSource implements Runnable{
-    public WaveFormArmThread (EventDbObject eventAccess, 
-			      EventStationSubsetter eventStationSubsetter,
-			      LocalSeismogramArm localSeismogramArm,
-			      NetworkDbObject networkAccess,
-			      ChannelDbObject[] successfulChannels, 
-			      WaveFormArm parent, 
-			      SodExceptionListener sodExceptionListener){
-	this.eventAccess = eventAccess;
-	this.networkAccess = networkAccess;
-	this.eventStationSubsetter = eventStationSubsetter;
-	this.localSeismogramArm = localSeismogramArm;
-	this.networkArm = networkArm;
-	this.successfulChannels = successfulChannels;
-	this.parent = parent;
-	addSodExceptionListener(sodExceptionListener);
+    public WaveFormArmThread (EventDbObject eventAccess,
+                  EventStationSubsetter eventStationSubsetter,
+                  LocalSeismogramArm localSeismogramArm,
+                  NetworkDbObject networkAccess,
+                  ChannelDbObject[] successfulChannels,
+                  WaveFormArm parent,
+                  SodExceptionListener sodExceptionListener){
+    this.eventAccess = eventAccess;
+    this.networkAccess = networkAccess;
+    this.eventStationSubsetter = eventStationSubsetter;
+    this.localSeismogramArm = localSeismogramArm;
+    this.successfulChannels = successfulChannels;
+    this.parent = parent;
+    addSodExceptionListener(sodExceptionListener);
     }
 
     public void run() {
-	try {
-	    
-	    processWaveFormArm(eventAccess);
-	} catch(Throwable ce) {
-	    logger.error("Waveform processing thread dies unexpectantly. ", 
-			 ce);
-//	    notifyListeners(this, ce);
-	}
-	
+    try {
+
+        processWaveFormArm(eventAccess);
+    } catch(Throwable ce) {
+        logger.error("Waveform processing thread dies unexpectantly. ",
+             ce);
+//      notifyListeners(this, ce);
+    }
+
     }
 
      /**
@@ -66,67 +65,67 @@ public class WaveFormArmThread extends SodExceptionSource implements Runnable{
      */
     public void processWaveFormArm(EventDbObject eventDbObject) throws Throwable{
 
-	EventAccessOperations eventAccess = eventDbObject.getEventAccess();
-	if (successfulChannels[0]  == null) logger.debug("Chan is NULL");
-	else logger.debug("channel is NOT NULL");
-	for(int counter = 0; counter < successfulChannels.length; counter++) {
-	    if(eventDbObject.getDbId() == 11 && successfulChannels[counter].getDbId() == 27) {
-		logger.debug("got the needed one IN PROCESS WAVEFORM ARM");
-		logger.debug("The eventid is "+eventDbObject.getDbId());
-		logger.debug("The channelid is "+successfulChannels[counter].getDbId());
-		//	System.exit(0);
-	}
-	parent.setFinalStatus(eventDbObject,
-			      successfulChannels[counter],
-			      Status.PROCESSING,
-			      "WaveformArmProcessingStarted");
-	    boolean bESS;
-		    synchronized(eventStationSubsetter) {
-		bESS = eventStationSubsetter.accept(eventAccess, 
-						 networkAccess.getNetworkAccess(), 
-						 successfulChannels[counter].getChannel().my_site.my_station, 
-						 null);
-		if(!bESS) {
-		    parent.setFinalStatus(eventDbObject,
-					  successfulChannels[counter],
-					  Status.COMPLETE_REJECT,
-					  "EventStationSubsetterFailed");
-		}
-	    }
-	    if( bESS ) {
-		parent.setFinalStatus(eventDbObject,
-				      successfulChannels[counter],
-				      Status.PROCESSING,
-				      "EventStationSubsetterSucceeded");
-	
-		localSeismogramArm.processLocalSeismogramArm(eventDbObject, 
-							     networkAccess, 
-							     successfulChannels[counter], 
-							     parent);
-/*	Start.getQueue().setFinalStatus((EventAccess)((CacheEvent)eventAccess).getEventAccess(), 
-				     Status.COMPLETE_SUCCESS);*/
-	}//end of if
-	}//end of for
-	//parent.signalWaveFormArm();
+    EventAccessOperations eventAccess = eventDbObject.getEventAccess();
+    if (successfulChannels[0]  == null) logger.debug("Chan is NULL");
+    else logger.debug("channel is NOT NULL");
+    for(int counter = 0; counter < successfulChannels.length; counter++) {
+        if(eventDbObject.getDbId() == 11 && successfulChannels[counter].getDbId() == 27) {
+        logger.debug("got the needed one IN PROCESS WAVEFORM ARM");
+        logger.debug("The eventid is "+eventDbObject.getDbId());
+        logger.debug("The channelid is "+successfulChannels[counter].getDbId());
+        //  System.exit(0);
+    }
+    parent.setFinalStatus(eventDbObject,
+                  successfulChannels[counter],
+                  Status.PROCESSING,
+                  "WaveformArmProcessingStarted");
+        boolean bESS;
+            synchronized(eventStationSubsetter) {
+        bESS = eventStationSubsetter.accept(eventAccess,
+                         networkAccess.getNetworkAccess(),
+                         successfulChannels[counter].getChannel().my_site.my_station,
+                         null);
+        if(!bESS) {
+            parent.setFinalStatus(eventDbObject,
+                      successfulChannels[counter],
+                      Status.COMPLETE_REJECT,
+                      "EventStationSubsetterFailed");
+        }
+        }
+        if( bESS ) {
+        parent.setFinalStatus(eventDbObject,
+                      successfulChannels[counter],
+                      Status.PROCESSING,
+                      "EventStationSubsetterSucceeded");
+
+        localSeismogramArm.processLocalSeismogramArm(eventDbObject,
+                                 networkAccess,
+                                 successfulChannels[counter],
+                                 parent);
+/*  Start.getQueue().setFinalStatus((EventAccess)((CacheEvent)eventAccess).getEventAccess(),
+                     Status.COMPLETE_SUCCESS);*/
+    }//end of if
+    }//end of for
+    //parent.signalWaveFormArm();
     }
 
      private EventDbObject eventAccess;
 
     private NetworkDbObject networkAccess;
-    
+
     private EventStationSubsetter eventStationSubsetter = null;//new NullEventStationSubsetter();
 
-   
+
     private LocalSeismogramArm localSeismogramArm = null;
 
     private NetworkArm networkArm;
 
     private ChannelDbObject[] successfulChannels;
-    
+
     private WaveFormArm parent;
 
-  
-    static Category logger = 
-	Category.getInstance(WaveFormArmThread.class.getName());
-    
+
+    static Category logger =
+    Category.getInstance(WaveFormArmThread.class.getName());
+
 }// WaveFormArmThread
