eee3a316e5feddbd8f43fe9db9f4401f58730a38
crotwell <none@none>
2003-10-07 10:09:23 -0400

    [svn r6017] use new maven repository
    --HG--
    branch : sod

--- scripts/sod.sh
+++ scripts/sod.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
+MAVEN=~/.maven
 
-JACORB=$MAVEN_HOME/repository/JacORB/jars/JacORB-1.4.1.jar
+JACORB=$MAVEN/repository/JacORB/jars/JacORB-1.4.1.jar
 # for orbacus
-OB=$MAVEN_HOME/repository/OB/jars/OB-4.1.0.jar
-OBNAMING=$MAVEN_HOME/repository/OBNaming/jars/OBNaming-4.1.0.jar
+OB=$MAVEN/repository/OB/jars/OB-4.1.0.jar
+OBNAMING=$MAVEN/repository/OBNaming/jars/OBNaming-4.1.0.jar
 
-SEEDCODEC=$MAVEN_HOME/repository/SeedCodec/jars/SeedCodec-1.0Beta.jar
-FISSURESUTIL=$MAVEN_HOME/repository/fissuresUtil/jars/fissuresUtil-1.0Beta.jar
-FISSURESIMPL=$MAVEN_HOME/repository/fissuresImpl/jars/fissuresImpl-1.1Beta.jar
-FISSURESIDL=$MAVEN_HOME/repository/fissuresIDL/jars/fissuresIDL-1.0.jar
-GEOTOOLS=$MAVEN_HOME/repository/Geotools1/jars/geotools1.2.jar
-LOG4J=$MAVEN_HOME/repository/log4j/jars/log4j-1.2.6.jar
-TAUP=$MAVEN_HOME/repository/TauP/jars/TauP-1.1.4.jar
-XALAN=$MAVEN_HOME/repository/xalan/jars/xalan-2.4.1.jar
-XERCES=$MAVEN_HOME/repository/xerces/jars/xerces-2.3.0.jar
-XMLAPI=$MAVEN_HOME/repository/xml-apis/jars/xml-apis-1.0.b2.jar
-JAICORE=$MAVEN_HOME/repository/jars/jai_core.jar
-JAICODEC=$MAVEN_HOME/repository/jars/jai_codec.jar
-HSQLDB=$MAVEN_HOME/repository/hsqldb/jars/hsqldb-1.7.1.jar
-SOD=$MAVEN_HOME/repository/sod/jars/sod-1.0Beta.jar
+SEEDCODEC=$MAVEN/repository/SeedCodec/jars/SeedCodec-1.0Beta.jar
+FISSURESUTIL=$MAVEN/repository/fissuresUtil/jars/fissuresUtil-1.0Beta.jar
+FISSURESIMPL=$MAVEN/repository/fissuresImpl/jars/fissuresImpl-1.1Beta.jar
+FISSURESIDL=$MAVEN/repository/fissuresIDL/jars/fissuresIDL-1.0.jar
+GEOTOOLS=$MAVEN/repository/Geotools1/jars/geotools1.2.jar
+LOG4J=$MAVEN/repository/log4j/jars/log4j-1.2.6.jar
+TAUP=$MAVEN/repository/TauP/jars/TauP-1.1.4.jar
+XALAN=$MAVEN/repository/xalan/jars/xalan-2.4.1.jar
+XERCES=$MAVEN/repository/xerces/jars/xerces-2.3.0.jar
+XMLAPI=$MAVEN/repository/xml-apis/jars/xml-apis-1.0.b2.jar
+JAICORE=$MAVEN/repository/jars/jai_core.jar
+JAICODEC=$MAVEN/repository/jars/jai_codec.jar
+HSQLDB=$MAVEN/repository/hsqldb/jars/hsqldb-1.7.1.jar
+SOD=$MAVEN/repository/sod/jars/sod-1.0Beta.jar
 
 
 java -cp ${SEEDCODEC}:${SOD}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${OB}:${OBNAMING}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${CLASSPATH} edu.sc.seis.sod.Start $*
