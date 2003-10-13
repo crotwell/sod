5ab34d39db4d819a9789b2944d9f6b80d8de59cc
groves <none@none>
2003-10-13 15:51:55 -0400

    [svn r6093] changed capitalization error
    --HG--
    branch : sod

--- scripts/cwg_run.sh
+++ scripts/cwg_run.sh
@@ -17,7 +17,7 @@ OB=$MAVEN/OB/jars/OB-4.1.0.jar
 OBNAMING=$MAVEN/OBNaming/jars/OBNaming-4.1.0.jar
 
 SEEDCODEC=$MAVEN/SeedCodec/jars/SeedCodec-1.0Beta.jar
-FISSURESUTIL=$MAVEN/fissuresUtil/jars/fissuresUtil-1.0.1Beta.jar
+FISSURESUTIL=$MAVEN/fissuresUtil/jars/fissuresUtil-1.0.1beta.jar
 FISSURESIMPL=$MAVEN/fissuresImpl/jars/fissuresImpl-1.1Beta.jar
 FISSURESIDL=$MAVEN/fissuresIDL/jars/fissuresIDL-1.0.jar
 LOG4J=$MAVEN/log4j/jars/log4j-1.2.6.jar
