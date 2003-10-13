23a5e9c112b3c9d6b05ff4d78851caba03c2435f
groves <none@none>
2003-10-13 01:45:04 -0400

    [svn r6077] removed unused statements
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/database/AbstractConfigDatabase.java
+++ src/edu/sc/seis/sod/database/AbstractConfigDatabase.java
@@ -22,193 +22,191 @@ import java.sql.*;
 
 public abstract class AbstractConfigDatabase implements ConfigDatabase{
     public AbstractConfigDatabase (Connection connection, String tableName){
-	this.connection = connection;
-	this.tableName = tableName;
-	init();
+        this.connection = connection;
+        this.tableName = tableName;
+        init();
     }
     
     public abstract void create();
-
+    
     private void init() {
-	try {
-	    create();
-	    Statement stmt = connection.createStatement();
-	    
-	    setTimeStmt = connection.prepareStatement(" INSERT into  "+tableName+
-						      " VALUES(?, ? , ? ) ");
-	    updateTimeStmt = connection.prepareStatement(" UPDATE "+tableName+" set time = ? "+
-							 " WHERE serverName = ? AND "+
-							 " serverDNS = ? ");
-	    
-	    getTimeStmt = connection.prepareStatement(" SELECT time from "+tableName+
-						      " WHERE serverName = ? AND "+
-						      " serverDNS = ? ");
-
-	    deleteStmt = " DELETE FROM ";
-	} catch(Exception e) {
-	  
-	}
-	
-
+        try {
+            create();
+            setTimeStmt = connection.prepareStatement(" INSERT into  "+tableName+
+                                                          " VALUES(?, ? , ? ) ");
+            updateTimeStmt = connection.prepareStatement(" UPDATE "+tableName+" set time = ? "+
+                                                             " WHERE serverName = ? AND "+
+                                                             " serverDNS = ? ");
+            
+            getTimeStmt = connection.prepareStatement(" SELECT time from "+tableName+
+                                                          " WHERE serverName = ? AND "+
+                                                          " serverDNS = ? ");
+            deleteStmt = " DELETE FROM ";
+        } catch(Exception e) {
+            
+        }
+        
+        
     }
-    public AbstractConfigDatabase(edu.iris.Fissures.Time time, 
-			 String serverDNS,
-			 String serverName) {
-	
-	setTime(serverName,
-		serverDNS,
-		time);
+    public AbstractConfigDatabase(edu.iris.Fissures.Time time,
+                                  String serverDNS,
+                                  String serverName) {
+        
+        setTime(serverName,
+                serverDNS,
+                time);
     }
-
+    
     
     public synchronized void setTime(String serverName,
-			String serverDNS,
-			edu.iris.Fissures.Time time) {
-	try {
-	    if(getTime(serverName, serverDNS) == null) {
-		MicroSecondDate ms = new MicroSecondDate(time);
-		setTimeStmt.setString(1, serverName);
-		setTimeStmt.setString(2, serverDNS);
-		setTimeStmt.setTimestamp(3, ms.getTimestamp());
-		setTimeStmt.executeUpdate();
-	    } else {
-		updateTime(serverName, serverDNS, time);
-	    }
-	} catch(SQLException sqle) {
-	    sqle.printStackTrace();
-	}
+                                     String serverDNS,
+                                     edu.iris.Fissures.Time time) {
+        try {
+            if(getTime(serverName, serverDNS) == null) {
+                MicroSecondDate ms = new MicroSecondDate(time);
+                setTimeStmt.setString(1, serverName);
+                setTimeStmt.setString(2, serverDNS);
+                setTimeStmt.setTimestamp(3, ms.getTimestamp());
+                setTimeStmt.executeUpdate();
+            } else {
+                updateTime(serverName, serverDNS, time);
+            }
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
     }
     
-
-   //  public void setServerDNS(String serverDNS) {
-// 	this.serverDNS = serverDNS;
-//     }
-
-//     public void setServerName(String serverName) {
-
-// 	this.serverName = serverName;
-//     }
-
-
+    
+    //  public void setServerDNS(String serverDNS) {
+    //  this.serverDNS = serverDNS;
+    //     }
+    
+    //     public void setServerName(String serverName) {
+    
+    //  this.serverName = serverName;
+    //     }
+    
+    
     public synchronized edu.iris.Fissures.Time getTime(String serverName,
-					  String serverDNS) {
-	try {
-	    getTimeStmt.setString(1, serverName);
-	    getTimeStmt.setString(2, serverDNS);
-	    ResultSet rs = getTimeStmt.executeQuery();
-	    if(rs.next()) {
-		MicroSecondDate ms = new MicroSecondDate(rs.getTimestamp("time"));
-		return ms.getFissuresTime();
-	    }
-	    return null;
-	} catch(SQLException sqle) {
-	    sqle.printStackTrace();
-	    return null;
-	}
-    }
-
-    public synchronized void incrementTime(String serverName,
-			      String serverDNS,
-			      int days) {
-	edu.iris.Fissures.Time time = getTime(serverName, serverDNS);
-	MicroSecondDate microSecondDate = new MicroSecondDate(time);
-	Calendar calendar = Calendar.getInstance();
-	calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
-	calendar.setTime(microSecondDate);
-	calendar.add(Calendar.DAY_OF_YEAR, days);
-	microSecondDate = new MicroSecondDate(calendar.getTime());
-	time = microSecondDate.getFissuresTime();
-	updateTime(serverName,
-		   serverDNS,
-		   time);
-    }
-
-    private void updateTime(String serverName,
-			    String serverDNS,
-			    edu.iris.Fissures.Time time) {
-	try {
-	    MicroSecondDate ms = new MicroSecondDate(time);
-	    
-	    updateTimeStmt.setTimestamp(1, ms.getTimestamp());
-	    updateTimeStmt.setString(2, serverName);
-	    updateTimeStmt.setString(3, serverDNS);
-	    updateTimeStmt.executeUpdate();
-	} catch(SQLException sqle) {
-	    sqle.printStackTrace();
-	}
+                                                       String serverDNS) {
+        try {
+            getTimeStmt.setString(1, serverName);
+            getTimeStmt.setString(2, serverDNS);
+            ResultSet rs = getTimeStmt.executeQuery();
+            if(rs.next()) {
+                MicroSecondDate ms = new MicroSecondDate(rs.getTimestamp("time"));
+                return ms.getFissuresTime();
+            }
+            return null;
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+            return null;
+        }
     }
     
-
+    public synchronized void incrementTime(String serverName,
+                                           String serverDNS,
+                                           int days) {
+        edu.iris.Fissures.Time time = getTime(serverName, serverDNS);
+        MicroSecondDate microSecondDate = new MicroSecondDate(time);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+        calendar.setTime(microSecondDate);
+        calendar.add(Calendar.DAY_OF_YEAR, days);
+        microSecondDate = new MicroSecondDate(calendar.getTime());
+        time = microSecondDate.getFissuresTime();
+        updateTime(serverName,
+                   serverDNS,
+                   time);
+    }
+    
+    private void updateTime(String serverName,
+                            String serverDNS,
+                            edu.iris.Fissures.Time time) {
+        try {
+            MicroSecondDate ms = new MicroSecondDate(time);
+            
+            updateTimeStmt.setTimestamp(1, ms.getTimestamp());
+            updateTimeStmt.setString(2, serverName);
+            updateTimeStmt.setString(3, serverDNS);
+            updateTimeStmt.executeUpdate();
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+    
+    
     private byte[] getBytes(Object obj) {
-	try {
-	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    ObjectOutputStream oos = new ObjectOutputStream(baos);
-	    oos.writeObject(obj);
-	    oos.close();
-	    byte[] bytes = baos.toByteArray();
-	    baos.close();
-	    return bytes;
-	} catch(Exception e) {
-	    e.printStackTrace();
-	    return new byte[0];
-	}
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(obj);
+            oos.close();
+            byte[] bytes = baos.toByteArray();
+            baos.close();
+            return bytes;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
     }
-
+    
     private Object getObject(byte[] bytes) {
-	try {
-	    if(bytes == null) return null;
-	    ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-	    ObjectInputStream ois = new ObjectInputStream(bais);
-	    Object obj = ois.readObject();
-	    bais.close();
-	    ois.close();
-	    return obj;
-	} catch(Exception e) {
-	    e.printStackTrace();
-	    return null;
-	}
+        try {
+            if(bytes == null) return null;
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            Object obj = ois.readObject();
+            bais.close();
+            ois.close();
+            return obj;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-//this close method must be elimintaed the 
-// database manager is responsible for closing 
-// the connection and not the individual databases
+    //this close method must be elimintaed the
+    // database manager is responsible for closing
+    // the connection and not the individual databases
     public void close() {
-	try {
-	    connection.close();
-	} catch(SQLException sqle) {
-	    sqle.printStackTrace();
-	}
-				    
+        try {
+            connection.close();
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        
     }
-
+    
     public void delete(String tableName) {
-	try {
-	    connection.createStatement().execute(deleteStmt+tableName);
-	} catch(SQLException sqle) {
-	    sqle.printStackTrace();
-	}
+        try {
+            connection.createStatement().execute(deleteStmt+tableName);
+        } catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
     }
-
+    
     public void clean() {
-	delete(this.tableName);
+        delete(this.tableName);
     }
     
     public String getTableName() {
-	return this.tableName;
+        return this.tableName;
     }
-
-
+    
+    
     protected  Connection connection;
-
+    
     protected String tableName;
-
+    
     private PreparedStatement setTimeStmt;
     
     private PreparedStatement updateTimeStmt;
-
+    
     private PreparedStatement getTimeStmt;
     
     private String deleteStmt;
     
-   
+    
 }// AbstractConfigDatabase
+
 
