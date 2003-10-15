4235723030129b9e17de457a5e8179960c345446
crotwell <none@none>
2003-10-15 16:59:54 -0400

    [svn r6108] start using xpath for replacement
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/CommandLineEditor.java
+++ src/edu/sc/seis/sod/editor/CommandLineEditor.java
@@ -24,17 +24,20 @@ import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import org.w3c.dom.DOMException;
 import org.apache.log4j.BasicConfigurator;
+import edu.sc.seis.fissuresUtil.xml.DataSetToXML;
+import org.apache.xpath.XPathAPI;
+import javax.xml.transform.TransformerException;
 
 
 
 public class CommandLineEditor {
 
-    public CommandLineEditor(String[] args) throws ParserConfigurationException, IOException, SAXException {
+    public CommandLineEditor(String[] args) throws ParserConfigurationException, IOException, SAXException, ParserConfigurationException, SAXException, DOMException, IOException, TransformerException {
         this.args = args;
         processArgs();
     }
 
-    void processArgs() throws DOMException, IOException, ParserConfigurationException, IOException, SAXException {
+    void processArgs() throws DOMException, IOException, ParserConfigurationException, IOException, SAXException, ParserConfigurationException, TransformerException {
         boolean help = false;
         for (int i = 0; i < args.length; i++) {
             if (i < args.length-1 && args[i].equals("-f")) {
@@ -48,8 +51,31 @@ public class CommandLineEditor {
         if (help) {
             printOptions(new DataOutputStream(System.out));
         } else {
-
+            doReplacements(args);
         }
+    }
+
+    Document doReplacements(String[] args) throws ParserConfigurationException, TransformerException {
+        Document doc = start.getDocument();
+        Document outDoc = DataSetToXML.getDocumentBuilder().newDocument();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-f") || args[i].equals("-help")) {
+                //skip help
+            } else {
+                String expr = args[i].substring(1, args[i].indexOf("="));
+                if ( ! expr.endsWith("/text()")) {
+                        expr += "/text()";
+                }
+                String value = args[i].substring(args[i].indexOf("="));
+                Node node = XPathAPI.selectSingleNode(doc, expr);
+                if (node instanceof Text) {
+                    ((Text)node).setData(value);
+                } else {
+                    throw new TransformerException("Trying to set text for "+expr+" but did not get a Text node.");
+                }
+            }
+        }
+        return outDoc;
     }
 
     void initConfigFile() throws FileNotFoundException, ParserConfigurationException, IOException, SAXException {
@@ -74,7 +100,7 @@ public class CommandLineEditor {
             } else if (n instanceof Text) {
                 Text textNode = (Text)n;
                 if ( ! textNode.getNodeValue().trim().equals("")) {
-                    out.writeBytes(getFullName((Element)textNode.getParentNode())+"  "+textNode.getNodeValue()+"\n");
+                    out.writeBytes(getFullName((Element)textNode.getParentNode())+"="+textNode.getNodeValue()+"\n");
                 } else {
                     // ignore whitespace
                 }
@@ -84,7 +110,7 @@ public class CommandLineEditor {
 
     protected String getFullName(Element e) {
         if (e.getParentNode() != null && e.getParentNode() instanceof Element) {
-            return getFullName((Element)e.getParentNode())+"."+e.getTagName();
+            return getFullName((Element)e.getParentNode())+"/"+e.getTagName();
         } else {
             return e.getTagName();
         }
@@ -93,7 +119,7 @@ public class CommandLineEditor {
     /**
      *
      */
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException, ParserConfigurationException, SAXException, IOException, DOMException {
         BasicConfigurator.configure();
         CommandLineEditor cle = new CommandLineEditor(args);
         System.out.println("Done editing.");
