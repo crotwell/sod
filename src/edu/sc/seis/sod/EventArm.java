4564d55843f0cbe192070a315f3006353858306c
groves <none@none>
2003-10-15 18:55:55 -0400

    [svn r6115] added status monitors, made multiple processors possible, fixed event checks going over the current time
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/EventArm.java
+++ src/edu/sc/seis/sod/EventArm.java
@@ -1,10 +1,9 @@
 package edu.sc.seis.sod;
 
 import edu.iris.Fissures.IfEvent.*;
-import edu.sc.seis.sod.subsetter.eventArm.*;
+import java.util.*;
 
 import edu.iris.Fissures.Quantity;
-import edu.iris.Fissures.Time;
 import edu.iris.Fissures.TimeRange;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.QuantityImpl;
@@ -14,9 +13,9 @@ import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import edu.sc.seis.sod.subsetter.eventArm.EventChannelFinder;
 import edu.sc.seis.sod.subsetter.eventArm.EventFinder;
-import java.util.Calendar;
-import java.util.Properties;
-import java.util.TimeZone;
+import edu.sc.seis.sod.subsetter.eventArm.EventTimeRange;
+import edu.sc.seis.sod.subsetter.eventArm.NullEventAttrSubsetter;
+import edu.sc.seis.sod.subsetter.eventArm.NullOriginSubsetter;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -48,10 +47,10 @@ public class EventArm extends SodExceptionSource implements Runnable{
         addSodExceptionListener(sodExceptionListener);
         processConfig(config);
     }
-
+    
     public void run() {
         try {
-            processEventArm();
+            getEvents();
         } catch(Exception e) {
             logger.error("Exception caught while processing the EventArm", e);
             //notify Exception listeners in case of an exception
@@ -61,20 +60,18 @@ public class EventArm extends SodExceptionSource implements Runnable{
         // if eventChannelFinder is false ... i.e., if no eventChannel is configured
         // then the eventArm can signal end of processing
         // by setting the sourceAlive attribute of the EventQueue to be false.
-
+        
         if(eventChannelFinder == null) Start.getEventQueue().setSourceAlive(false);
         logger.debug("After setting the source Alive to be false");
         logger.debug("IN EVENT ARM **** The number of events in the eventQueue are "
                          +Start.getEventQueue().getLength());
     }
-
+    
     /**
-     * This method processes the eventArm subsetters
+     * This method processes the eventArm configuration
      *
-     * @param config an <code>Element</code> value
-     * @exception ConfigurationException if an error occurs
      */
-    protected void processConfig(Element config)
+    private void processConfig(Element config)
         throws ConfigurationException {
         Start.getEventQueue().setSourceAlive(true);
         NodeList children = config.getChildNodes();
@@ -90,19 +87,21 @@ public class EventArm extends SodExceptionSource implements Runnable{
                 if(sodElement instanceof edu.sc.seis.sod.subsetter.eventArm.EventFinder) {
                     eventFinderSubsetter = (edu.sc.seis.sod.subsetter.eventArm.EventFinder)sodElement;
                 } else if(sodElement instanceof edu.sc.seis.sod.subsetter.eventArm.EventChannelFinder) {
-                        eventChannelFinder = (edu.sc.seis.sod.subsetter.eventArm.EventChannelFinder)sodElement;
+                    eventChannelFinder = (edu.sc.seis.sod.subsetter.eventArm.EventChannelFinder)sodElement;
                 } else if(sodElement instanceof EventAttrSubsetter) {
                     eventAttrSubsetter = (EventAttrSubsetter) sodElement;
                 } else if(sodElement instanceof OriginSubsetter) {
                     originSubsetter = (OriginSubsetter)sodElement;
                 } else if(sodElement instanceof EventArmProcess) {
-                    eventArmProcess = (EventArmProcess)sodElement;
+                    processors.add(sodElement);
+                }else if(sodElement instanceof EventStatus) {
+                    statusMonitors.add(sodElement);
                 }
             } // end of if (node instanceof Element)
         } // end of for (int i=0; i<children.getSize(); i++)
     }
-
-    public void processEventArm() throws Exception{
+    
+    private void getEvents() throws Exception{
         if(eventChannelFinder != null) {
             eventChannelFinder.setEventArm(this);
             Thread thread = new Thread(eventChannelFinder);
@@ -139,46 +138,47 @@ public class EventArm extends SodExceptionSource implements Runnable{
             searchTypes = eventFinderSubsetter.getMagnitudeRange().getSearchTypes();
         }
         logger.debug("getting events from "+eventFinderSubsetter.getEventTimeRange().getTimeRange().start_time.date_time+" to "+eventFinderSubsetter.getEventTimeRange().getTimeRange().end_time.date_time);
-
+        
         String source = eventFinderSubsetter.getSourceName();
         String dns =  eventFinderSubsetter.getDNSName();
         EventTimeRange reqTimeRange = eventFinderSubsetter.getEventTimeRange();
         boolean done = false;
         while(!done){
-            //Get the start time from the database
-            Time queryStartTime = Start.getEventQueue().getTime(source, dns);
-            if(queryStartTime == null) {//If the database doesn't have a start time,
-                queryStartTime = reqTimeRange.getStartTime();//Use the total request's time
-                Start.getEventQueue().setTime(source, dns, queryStartTime);
+            MicroSecondDate queryStart = reqTimeRange.getStartMSD();
+            if(Start.getEventQueue().getTime(source, dns) != null) {//If the database has a start time
+                //use the database's start time
+                queryStart = new MicroSecondDate(Start.getEventQueue().getTime(source, dns));
             }
-            Time queryEndTime = incrementTime(queryStartTime);
-            MicroSecondDate requestEndDate = reqTimeRange.getEndMSD();
-            if(requestEndDate.before(new MicroSecondDate(queryEndTime))){
-                queryEndTime = requestEndDate.getFissuresTime();
+            MicroSecondDate queryEnd = incrementTime(queryStart);
+            MicroSecondDate requestEnd = reqTimeRange.getEndMSD();
+            if(requestEnd.before(queryEnd)){
+                queryEnd = requestEnd;
             }
-            MicroSecondDate queryStartMSD = new MicroSecondDate(queryStartTime);
-            MicroSecondDate queryEndMSD = new MicroSecondDate(queryEndTime);
-            if(!queryEndMSD.before(queryStartMSD) && !pastNow(queryStartMSD)){
+            if(pastNow(queryEnd)){
+                queryEnd = ClockUtil.now();
+            }
+            if(!queryEnd.before(queryStart)){
                 EventAccess[] events =
                     finder.query_events(eventFinderSubsetter.getArea(),
                                         minDepth, maxDepth,
-                                        new TimeRange(queryStartTime, queryEndTime),
+                                        new TimeRange(queryStart.getFissuresTime(),
+                                                      queryEnd.getFissuresTime()),
                                         searchTypes, minMagnitude, maxMagnitude,
                                         eventFinderSubsetter.getCatalogs(),
                                         eventFinderSubsetter.getContributors(),
                                         10, eventSeqIterHolder);
                 logger.debug("Found "+events.length+" events between "+
-                                 queryStartMSD + " and "+ queryEndMSD);
-                Start.getEventQueue().incrementTime(source, dns, getIncrement());
-                handleEventAttrSubsetter(makeCacheEvents(events));
+                                 queryStart + " and "+ queryEnd);
+                Start.getEventQueue().setTime(source, dns, queryEnd.getFissuresTime());
+                startEventSubsetter(makeCacheEvents(events));
                 Start.getEventQueue().waitForProcessing();
-            }else if(!isDone(queryStartMSD, reqTimeRange.getEndMSD())){
+            }else if(!isDone(queryEnd, requestEnd)){
                 done = true;
             }
         }
         logger.debug("Finished processing the event arm.");
     }
-
+    
     private EventAccessOperations[] makeCacheEvents(EventAccessOperations[] uncached){
         EventAccessOperations[] cached = new EventAccessOperations[uncached.length];
         for(int counter = 0; counter < cached.length; counter++) {
@@ -186,56 +186,73 @@ public class EventArm extends SodExceptionSource implements Runnable{
         }
         return cached;
     }
-
-    private void handleEventAttrSubsetter(EventAccessOperations[] event)throws Exception{
+    
+    private void startEventSubsetter(EventAccessOperations[] event)throws Exception{
         for (int i = 0; i < event.length; i++) {
-            handleEventAttrSubsetter(event[i], event[i].get_attributes());
+            startEventSubsetter(event[i], event[i].get_attributes());
         }
     }
-
-    public void handleEventAttrSubsetter(EventAccessOperations eventAccess, EventAttr eventAttr) throws Exception {
-        if(eventAttrSubsetter == null || eventAttrSubsetter.accept(eventAttr, null)) {
-            handleOriginSubsetter(eventAccess, eventAccess.get_preferred_origin());
+    
+    public void startEventSubsetter(EventAccessOperations event, EventAttr attr) throws Exception {
+        begin(event);
+        if(eventAttrSubsetter == null || eventAttrSubsetter.accept(attr, null)) {
+            Origin origin = event.get_preferred_origin();
+            if(originSubsetter.accept(event, origin, null)) {
+                Start.getEventQueue().push(eventFinderSubsetter.getDNSName(),
+                                           eventFinderSubsetter.getSourceName(),
+                                               (EventAccess)((CacheEvent)event).getEventAccess(),
+                                           origin);
+                pass(event);
+                startProcessors(event);
+            }else{
+                fail(event, "The event origin subsetter did not accept this event");
+            }
+        }else{
+            fail(event, "The event attribute subsetter did not accept this event");
         }
     }
-
-    /**
-     * handles the OriginSubsetter
-     *
-     * @param eventAccess an <code>EventAccessOperations</code> value
-     * @param origin an <code>Origin</code> value
-     * @exception Exception if an error occurs
-     */
-    public void handleOriginSubsetter(EventAccessOperations eventAccess, Origin origin) throws Exception{
-        if(originSubsetter.accept(eventAccess, origin, null)) {
-            handleEventArmProcess(eventAccess, origin);
+    
+    private void begin(EventAccessOperations event) {
+        Iterator it = statusMonitors.iterator();
+        while(it.hasNext()){
+            ((EventStatus)it.next()).begin(event);
         }
     }
-
-    public void handleEventArmProcess(EventAccessOperations eventAccess, Origin origin) throws Exception{
-        Start.getEventQueue().push(eventFinderSubsetter.getDNSName(),
-                                   eventFinderSubsetter.getSourceName(),
-                                       (EventAccess)((CacheEvent)eventAccess).getEventAccess(),
-                                   origin);
-        eventArmProcess.process(eventAccess, null);
-
+    private void pass(EventAccessOperations event) {
+        Iterator it = statusMonitors.iterator();
+        while(it.hasNext()){
+            ((EventStatus)it.next()).pass(event);
+        }
     }
-
+    
+    private void fail(EventAccessOperations event, String reason) {
+        Iterator it = statusMonitors.iterator();
+        while(it.hasNext()){
+            ((EventStatus)it.next()).fail(event, reason);
+        }
+    }
+    
+    private void startProcessors(EventAccessOperations eventAccess) throws Exception {
+        Iterator it = processors.iterator();
+        while(it.hasNext()){
+            ((EventArmProcess)it.next()).process(eventAccess, null);
+        }
+    }
+    
     /**
      * this increments the passed in time by the amount of time each event
      * query is set to cover in the edu.sc.seis.sod.daystoincrement property
      *
      */
-    private Time incrementTime(Time startTime) {
-        MicroSecondDate startDate = new MicroSecondDate(startTime);
+    private MicroSecondDate incrementTime(MicroSecondDate startDate) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
         calendar.setTime(startDate);
         calendar.add(Calendar.DAY_OF_YEAR, getIncrement());
         MicroSecondDate incrementedDate = new MicroSecondDate(calendar.getTime());
-        return incrementedDate.getFissuresTime();
+        return incrementedDate;
     }
-
+    
     /**
      * checks to see if the eventArm is done processing for the time interval
      * specified in the configuration file.
@@ -247,7 +264,7 @@ public class EventArm extends SodExceptionSource implements Runnable{
         }
         return false;
     }
-
+    
     /**
      * Method pastNow checks if time is later than now
      *
@@ -255,9 +272,9 @@ public class EventArm extends SodExceptionSource implements Runnable{
      *
      */
     private boolean pastNow(MicroSecondDate time){
-        return firstAfterSecond(time, ClockUtil.now());
+        return ClockUtil.now().after(time);
     }
-
+    
     /**
      * Method past checks if firstTime is later than second time
      *
@@ -267,7 +284,7 @@ public class EventArm extends SodExceptionSource implements Runnable{
         if(second.before(first) || second.equals(first)) return true;
         return false;
     }
-
+    
     /***
      * passivates the eventArm (makes it to sleep) based on the refreshInterval
      * and quitTime specified in the property file.
@@ -286,12 +303,12 @@ public class EventArm extends SodExceptionSource implements Runnable{
         Start.getEventQueue().deleteTimeConfig();
         return false;
     }
-
+    
     /**
      * returns the increment value. This value is specified as a property in the
      * property file.
      */
-
+    
     private int getIncrement() {
         String value = props.getProperty("edu.sc.seis.sod.daystoincrement");
         if(value == null) return 1;
@@ -302,18 +319,21 @@ public class EventArm extends SodExceptionSource implements Runnable{
             return 1;
         }
     }
-
+    
     private EventFinder eventFinderSubsetter;
-
+    
     private EventChannelFinder eventChannelFinder = null;
-
+    
     private EventAttrSubsetter eventAttrSubsetter = new NullEventAttrSubsetter();
-
+    
     private OriginSubsetter originSubsetter = new NullOriginSubsetter();
-
-    private EventArmProcess eventArmProcess = new NullEventProcess();
-
+    
+    private List processors = new ArrayList();
+    
+    private List statusMonitors = new ArrayList();
+    
     private Properties props;
-
+    
     private static Logger logger = Logger.getLogger(EventArm.class);
 }// EventArm
+
