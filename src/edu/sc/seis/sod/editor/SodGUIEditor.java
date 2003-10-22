c1c6810537f7fe918e2ea4e4dfd0ec08375dbaac
crotwell <none@none>
2003-10-22 13:36:21 -0400

    [svn r6189] fixed extends, add something for properties for testing ideas
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/SodGUIEditor.java
+++ src/edu/sc/seis/sod/editor/SodGUIEditor.java
@@ -15,10 +15,13 @@ import org.apache.log4j.BasicConfigurator;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
+import javax.swing.JLabel;
+import org.w3c.dom.*;
+import org.apache.xpath.XPathAPI;
 
 
 
-public class SodGUIEditor extends SimpleGUI {
+public class SodGUIEditor extends SimpleGUIEditor {
 
     SodGUIEditor(String[] args) throws IOException, ParserConfigurationException, TransformerException, DOMException, SAXException {
         super(args);
@@ -26,7 +29,21 @@ public class SodGUIEditor extends SimpleGUI {
 
 
     void addElementToPanel(JPanel panel, Element element, GridBagConstraints gbc) {
-        super.addElementToPanel(panel, element, gbc);
+        try {
+            if (element.getTagName().equals("property")) {
+                Node node = XPathAPI.selectSingleNode(element, "name/text()");
+                Text text = (Text)node;
+                JLabel label = new JLabel("property "+text.getNodeValue()+" = ");
+                panel.add(label, gbc);
+                gbc.gridx++;node = XPathAPI.selectSingleNode(element, "value/text()");
+                text = (Text)node;
+                addTextNodeToPanel(panel, text, gbc);
+            } else {
+                super.addElementToPanel(panel, element, gbc);
+            }
+        } catch (Exception e) {
+            super.addElementToPanel(panel, element, gbc);
+        }
     }
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException, DOMException, SAXException {
