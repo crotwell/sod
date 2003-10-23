20e3d0c76e3e1fd021467b84d699789b27ec784b
groves <none@none>
2003-10-23 14:16:15 -0400

    [svn r6204] no longer extends LogLevel and instead provides a LogLevel accessor that is the same as this RunStatus so that log levels arent created unless theyre needed
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/RunStatus.java
+++ src/edu/sc/seis/sod/RunStatus.java
@@ -1,5 +1,5 @@
 /**
- * Status.java
+ * RunStatus.java
  *
  * @author Created by Omnicore CodeGuide
  */
@@ -8,11 +8,11 @@ package edu.sc.seis.sod;
 
 import org.apache.log4j.lf5.LogLevel;
 
-public class RunStatus extends LogLevel{
+public class RunStatus{
     private RunStatus(String name){
-        super(name, statusCount++);
+        this.name = name;
     }
-
+    
     public static final RunStatus NEW = new RunStatus("New");
     
     public static final RunStatus PASSED = new RunStatus("Passed");
@@ -21,8 +21,33 @@ public class RunStatus extends LogLevel{
     
     public static final RunStatus GENERIC_STATUS = new RunStatus("Info");
     
-    public static final RunStatus[] DEFAULT_STATUS = { NEW, PASSED, FAILED, GENERIC_STATUS};
+    public static LogLevel[] LOG_LEVELS;
+    
+    public static LogLevel[] getLogLevels(){
+        if(LOG_LEVELS == null){
+            LogLevel[] levels = { NEW.getLogLevel(), PASSED.getLogLevel(),
+                    FAILED.getLogLevel(), GENERIC_STATUS.getLogLevel()};
+            LOG_LEVELS = levels;
+        }
+        return LOG_LEVELS;
+    }
+    
+    public String toString(){ return name; }
+    
+    private String name;
+    
+    //Run status uses this getLogLevel method so that the LogLevel isn't created
+    //unless it's accessed.  Since a log level includes a color by default, it
+    //requires a GraphicsConfiguration to create, and this allows sod to run in
+    //a headless system.
+    public LogLevel getLogLevel(){
+        if(level == null){
+            level = new LogLevel(name, statusCount++);
+        }
+        return level;
+    }
+        
+    private LogLevel level;
     
     private static int statusCount = 0;
 }
-
