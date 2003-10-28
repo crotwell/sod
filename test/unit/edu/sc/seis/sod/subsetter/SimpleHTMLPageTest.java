6a12a277e1fe32b8062354dff2c4c327467f56e1
groves <none@none>
2003-10-27 20:38:21 -0500

    [svn r6238] initial
    --HG--
    branch : sod

---
+++ test/unit/edu/sc/seis/sod/subsetter/SimpleHTMLPageTest.java
@@ -0,0 +1,47 @@
+/**
+ * TestSimpleHTMLPage.java
+ *
+ * @author Created by Charles Groves
+ */
+
+package edu.sc.seis.sod.subsetter;
+
+import java.io.File;
+import junit.framework.TestCase;
+import java.io.IOException;
+
+public class TestSimpleHTMLPage extends TestCase{
+    public TestSimpleHTMLPage(String name){
+        super(name);
+    }
+    
+    public void setUp(){
+        baseDir = new SimpleHTMLPage("Test", new File("test.html"));
+        threeBelowDir = new SimpleHTMLPage("Test2", new File("./" +oneTwoThree + "test2.html"));
+        oneAboveDir = new SimpleHTMLPage("Test3", new File("../test3.html"));
+    }
+    
+    public void testRelativePathTo(){
+        assertEquals(oneTwoThree + "test2.html", baseDir.relativePathTo(threeBelowDir));
+        assertEquals(dotDotDot + "test.html", threeBelowDir.relativePathTo(baseDir));
+        assertEquals("../test3.html", baseDir.relativePathTo(oneAboveDir));
+        assertEquals(dotDotDot + "../test3.html", threeBelowDir.relativePathTo(oneAboveDir));
+    }
+    
+    public void testDotsToCommonBase() throws IOException{
+        assertEquals("", baseDir.dotsToCommonBase(baseDir));
+        assertEquals("", baseDir.dotsToCommonBase(threeBelowDir));
+        assertEquals(dotDotDot, threeBelowDir.dotsToCommonBase(baseDir));
+     }
+    
+    public void testGetCommonBase() throws IOException{
+        assertEquals(0, baseDir.getCommonBaseDistance(baseDir.getDirectory()));
+        assertEquals(0, baseDir.getCommonBaseDistance(threeBelowDir.getDirectory()));
+        assertEquals(3, threeBelowDir.getCommonBaseDistance(baseDir.getDirectory()));
+        assertEquals(0, oneAboveDir.getCommonBaseDistance(baseDir.getDirectory()));
+    }
+    
+    private String oneTwoThree = "one/two/three/";
+    private String dotDotDot = "../../../";
+    private SimpleHTMLPage baseDir, threeBelowDir, oneAboveDir;
+}
