a1e8b6177286389599f3d727e3b4df4d7a91805b
groves <none@none>
2003-11-03 16:46:51 -0500

    [svn r6340] refactored after being harnessed in a unit test
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/NameGenerator.java
+++ src/edu/sc/seis/sod/subsetter/NameGenerator.java
@@ -1,150 +1,123 @@
 package edu.sc.seis.sod.subsetter;
 
-import org.w3c.dom.*;
-import edu.iris.Fissures.IfEvent.*;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfEvent.Magnitude;
+import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
+import edu.iris.Fissures.IfEvent.Origin;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.sc.seis.fissuresUtil.display.ParseRegions;
-import edu.sc.seis.sod.*;
-import org.apache.log4j.*;
+import edu.sc.seis.sod.SodUtil;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
-/**
- * NameGenerator.java
- *
- *
- * Created: Fri Mar 28 16:04:01 2003
- *
- * @author <a href="mailto:crotwell@owl.seis.sc.edu">Philip Crotwell</a>
- * @version 1.0
- */
 public class NameGenerator {
+    public NameGenerator(){ this(null); }
+    
     public NameGenerator(Element config) {
         this.config = config;
         regions = new ParseRegions();
-    } // NameGenerator constructor
-
+    }
+    
     public String getName(EventAccessOperations event) {
+        if(config == null) return getDefaultName(event);
         NodeList children = config.getChildNodes();
         Node node;
-        String name = "";
-        String separator = "";
-        String separatorValue = "_";
-        for(int counter = 0; counter < children.getLength(); counter++ ) {
-            node = children.item(counter);
-            if(node instanceof Element ) {
-                if(((Element)node).getTagName().equals("separator")) {
-                    separatorValue = SodUtil.getText((Element)node);
-                    if (separatorValue == null) {
-                        separatorValue = "";
-                    } // end of if (sep == null)
-                }
-            }
-        }
-
+        StringBuffer name = new StringBuffer();
         for(int counter = 0; counter < children.getLength(); counter++ ) {
             node = children.item(counter);
             if (node instanceof Text) {
-                String text = ((Text)node).getData();
-                text = text.trim();
-                StringBuffer sb = new StringBuffer(text.length());
-                char[] chars = text.toCharArray();
+                char[] chars = ((Text)node).getData().toCharArray();
                 for (int i=0; i<chars.length; i++) {
-                    if (Character.isWhitespace(chars[i])) {
-                        // skip
-                    } else {
-                        sb.append(chars[i]);
-                    } // end of else
+                    if (!Character.isWhitespace(chars[i]))name.append(chars[i]);
                 } // end of for (int i=0; i<chars.length; i++)
-                text = sb.toString();
-                text = text.replace(' ','_');
-                //      logger.debug("Breq text node:"+text);
-                name += text;
             } else if(node instanceof Element ) {
-                name += separator;
-                separator = separatorValue;
-                if(((Element)node).getTagName().equals("feRegionName")) {
-                    String regionName =
-                        regions.getRegionName(event.get_attributes().region);
-                    regionName = regionName.replace(' ', '_');
-                    regionName = regionName.replace(',', '_');
-                    name += regionName;
-                } else if(((Element)node).getTagName().equals("feRegionNumber")) {
-                    int regionNum =
-                        event.get_attributes().region.number;
-                    name += regionNum;
-                } else if(((Element)node).getTagName().equals("depth")) {
-                    try {
-                        name +=
-                            event.get_preferred_origin().my_location.depth.value;
-                    } catch (NoPreferredOrigin e) {
-                    } // end of try-catch
-                } else if(((Element)node).getTagName().equals("latitude")) {
-                    try {
-                        name +=
-                            event.get_preferred_origin().my_location.latitude;
-                    } catch (NoPreferredOrigin e) {
-                    } // end of try-catch
-                } else if(((Element)node).getTagName().equals("longitude")) {
-                    try {
-                        name +=
-                            event.get_preferred_origin().my_location.longitude;
-                    } catch (NoPreferredOrigin e) {
-                    } // end of try-catch
-                } else if(((Element)node).getTagName().equals("magnitude")) {
-                    try {
-                        Magnitude[] mags =
-                            event.get_preferred_origin().magnitudes;
-                        if (mags.length > 0) {
-                            name +=
-                                mags[0].value;
-                        } // end of if (mags.length > 0)
-                    } catch (NoPreferredOrigin e) {
-                    } // end of try-catch
-                } else if(((Element)node).getTagName().equals("originTime")) {
-                    try {
-                        String formatStr = SodUtil.getText((Element)node);
-                        //logger.debug("Breqfast label originTime:"+formatStr);
-                        if (formatStr.length() == 0) {
-                            formatStr = "yyyyMMdd'T'HHmmss.SSS";
-                        } // end of if (formatStr.length == 0)
-            
-                        SimpleDateFormat labelFormat = new SimpleDateFormat(formatStr);
-                        labelFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-                        MicroSecondDate msd =
-                            new MicroSecondDate(event.get_preferred_origin().origin_time);
-                        name +=
-                            labelFormat.format(msd);
-                    } catch (NoPreferredOrigin e) {
-                    } // end of try-catch
-                } else if(((Element)node).getTagName().equals("separator")) {
-                    // ignore as this was processed previously
+                Element el = (Element)node;
+                if(el.getTagName().equals("feRegionName")) {
+                    name.append(getRegionName(event));
+                } else if(el.getTagName().equals("feRegionNumber")) {
+                    name.append(getRegionNum(event));
+                } else if(el.getTagName().equals("depth")) {
+                    name.append(getDepth(event));
+                } else if(el.getTagName().equals("latitude")) {
+                    name.append(getLatitude(event));
+                } else if(el.getTagName().equals("longitude")) {
+                    name.append(getLongitude(event));
+                } else if(el.getTagName().equals("magnitude")) {
+                    name.append(getMag(event));
+                } else if(el.getTagName().equals("originTime")) {
+                    name.append(getTime(event, SodUtil.getText(el)));
                 } else {
-                    logger.warn("label tag "+((Element)node).getTagName()+" is not understood.");
+                    logger.warn("label tag "+el.getTagName()+" is not understood.");
                 } // end of else
-                
             }
         }
-        name = name.replace(' ', '_');
-        name = name.replace(',', '_');
-        logger.debug("Breqfast label: "+name);
-        return name;
+        return name.toString();
+    }
+    
+    public String getFilizedName(EventAccessOperations event) {
+        return filize(getName(event));
     }
     
     public static String filize(String fileName){
-        fileName = fileName.replace(' ', '_');
-        fileName = fileName.replace(',', '_');
-        fileName = fileName.replace('/', '_');
-        fileName = fileName.replace(':', '_');
-        return fileName;
+        fileName = fileName.replaceAll("[/ :]", "_");
+        return fileName.replaceAll("[\t\n\f\r]", "");
     }
-
-    Element config;
-
-    ParseRegions regions;
-
-    static Category logger =
-        Category.getInstance(NameGenerator.class.getName());
     
-
+    public String getDefaultName(EventAccessOperations event){
+        String eventFileName =
+            regions.getRegionName(event.get_attributes().region);
+        eventFileName += " " + getOrigin(event).origin_time.date_time;
+        return eventFileName;
+    }
+    
+    private String getTime(EventAccessOperations event, String formatStr) {
+        if (formatStr.length() == 0) formatStr = "yyyyMMdd'T'HHmmss.SSS";
+        SimpleDateFormat labelFormat = new SimpleDateFormat(formatStr);
+        labelFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        MicroSecondDate msd = new MicroSecondDate(getOrigin(event).origin_time);
+        return labelFormat.format(msd);
+    }
+    
+    private int getLongitude(EventAccessOperations event) {
+        return (int)getOrigin(event).my_location.longitude;
+    }
+    
+    private int getLatitude(EventAccessOperations event) {
+        return (int)getOrigin(event).my_location.latitude;
+    }
+    
+    private int getDepth(EventAccessOperations event) {
+        return (int)getOrigin(event).my_location.depth.value;
+    }
+    
+    private int getRegionNum(EventAccessOperations event) {
+        return event.get_attributes().region.number;
+    }
+    
+    private String getMag(EventAccessOperations event){
+        Magnitude[] mags = getOrigin(event).magnitudes;
+        if (mags.length > 0)  return "" + (int)mags[0].value;
+        throw new IllegalArgumentException("No magnitudes on event");
+    }
+    
+    private Origin getOrigin(EventAccessOperations event){
+        Origin o = event.get_origins()[0];
+        try{ o = event.get_preferred_origin(); }catch(NoPreferredOrigin e){}
+        return o;
+    }
+    
+    private String getRegionName(EventAccessOperations event){
+        return regions.getRegionName(event.get_attributes().region);
+    }
+    
+    Element config;
+    
+    ParseRegions regions;
+    
+    private static Logger logger = Logger.getLogger(NameGenerator.class);
 } // NameGenerator
