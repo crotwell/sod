f73ae6a4edd516c67ba9fe3074abe3d9bc0e7522
groves <none@none>
2003-11-21 21:47:30 -0500

    [svn r6441] changed from parse xml every time, to parse once, build string every time
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/NameGenerator.java
+++ src/edu/sc/seis/sod/subsetter/NameGenerator.java
@@ -8,55 +8,144 @@ import edu.iris.Fissures.model.MicroSecondDate;
 import edu.sc.seis.fissuresUtil.display.ParseRegions;
 import edu.sc.seis.sod.SodUtil;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.TimeZone;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 
 public class NameGenerator {
     public NameGenerator(){ this(null); }
     
     public NameGenerator(Element config) {
-        this.config = config;
-        regions = new ParseRegions();
+        parse(config);
+    }
+    
+    private void parse(Element conf){
+        if(conf == null) createDefaultBuilder();
+        else{
+            NodeList children = conf.getChildNodes();
+            for(int counter = 0; counter < children.getLength(); counter++ ) {
+                Node n = children.item(counter);
+                if(n.getNodeType() == Node.TEXT_NODE){
+                    builder.add(new Text(n.getNodeValue()));
+                } else if(n instanceof Element ) {
+                    String tag = ((Element)n).getTagName();
+                    if(tag.equals("feRegionName")) {
+                        builder.add(new RegionName());
+                    } else if(tag.equals("feRegionNumber")) {
+                        builder.add(new RegionNum());
+                    } else if(tag.equals("depth")) {
+                        builder.add(new Depth());
+                    } else if(tag.equals("latitude")) {
+                        builder.add(new Latitude());
+                    } else if(tag.equals("longitude")) {
+                        builder.add(new Longitude());
+                    } else if(tag.equals("magnitude")) {
+                        builder.add(new Mag());
+                    } else if(tag.equals("originTime")) {
+                        builder.add(new Time(SodUtil.getText((Element)n)));
+                    } else {
+                        if(n.getChildNodes().getLength() == 0) {
+                            builder.add(new Text("<" + n.getNodeName() + getAttrString(n) + "/>"));
+                        }else{
+                            builder.add(new Text("<" + n.getNodeName()+ getAttrString(n) + ">"));
+                            parse((Element)n);
+                            builder.add(new Text("</" + n.getNodeName() + ">"));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private void createDefaultBuilder() {
+        builder.add(new RegionName());
+        builder.add(new Time());
+    }
+    
+    private String getAttrString(Node n){
+        String result = "";
+        NamedNodeMap attr = n.getAttributes();
+        for (int i = 0; i < attr.getLength(); i++) {
+            result += " " + attr.item(i).getNodeName();
+            result += "=\"" + attr.item(i).getNodeValue() + "\"";
+        }
+        return result;
     }
     
     public String getName(EventAccessOperations event) {
-        if(config == null) return getDefaultName(event);
-        NodeList children = config.getChildNodes();
-        Node node;
         StringBuffer name = new StringBuffer();
-        for(int counter = 0; counter < children.getLength(); counter++ ) {
-            node = children.item(counter);
-            if (node instanceof Text) {
-                char[] chars = ((Text)node).getData().toCharArray();
-                for (int i=0; i<chars.length; i++) {
-                    if (!Character.isWhitespace(chars[i]))name.append(chars[i]);
-                } // end of for (int i=0; i<chars.length; i++)
-            } else if(node instanceof Element ) {
-                Element el = (Element)node;
-                if(el.getTagName().equals("feRegionName")) {
-                    name.append(getRegionName(event));
-                } else if(el.getTagName().equals("feRegionNumber")) {
-                    name.append(getRegionNum(event));
-                } else if(el.getTagName().equals("depth")) {
-                    name.append(getDepth(event));
-                } else if(el.getTagName().equals("latitude")) {
-                    name.append(getLatitude(event));
-                } else if(el.getTagName().equals("longitude")) {
-                    name.append(getLongitude(event));
-                } else if(el.getTagName().equals("magnitude")) {
-                    name.append(getMag(event));
-                } else if(el.getTagName().equals("originTime")) {
-                    name.append(getTime(event, SodUtil.getText(el)));
-                } else {
-                    logger.warn("label tag "+el.getTagName()+" is not understood.");
-                } // end of else
-            }
+        Iterator it = builder.iterator();
+        while(it.hasNext()){
+            name.append(((Piece)it.next()).getPiece(event));
         }
         return name.toString();
+    }
+    
+    private interface Piece{
+        public String getPiece(EventAccessOperations event);
+    }
+    
+    private class Text implements Piece{
+        public Text(String piece){ this.piece = piece; }
+        
+        public String getPiece(EventAccessOperations event){ return piece; }
+        
+        private String piece;
+    }
+    
+    private class RegionName implements Piece{
+        public String getPiece(EventAccessOperations ev){
+            return getRegionName(ev);
+        }
+    }
+    
+    private class Depth implements Piece{
+        public String getPiece(EventAccessOperations ev){ return
+                Integer.toString(getDepth(ev));
+        }
+    }
+    
+    private class RegionNum implements Piece{
+        public String getPiece(EventAccessOperations ev){
+            return Integer.toString(getRegionNum(ev));
+        }
+    }
+    
+    private class Latitude implements Piece{
+        public String getPiece(EventAccessOperations ev){
+            return Integer.toString(getLatitude(ev));
+        }
+    }
+    
+    private class Longitude implements Piece{
+        public String getPiece(EventAccessOperations ev){
+            return Integer.toString(getLongitude(ev));
+        }
+    }
+    
+    private class Mag implements Piece{
+        public String getPiece(EventAccessOperations ev){ return getMag(ev); }
+    }
+    
+    private class Time implements Piece{
+        public Time(){this("yyyyMMdd'T'HH:mm:ss.SSS");}
+        
+        public Time(String format){
+            sdf = new SimpleDateFormat(format);
+            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        }
+        
+        public String getPiece(EventAccessOperations ev){
+            return sdf.format(new MicroSecondDate(getOrigin(ev).origin_time));
+        }
+        
+        private SimpleDateFormat sdf;
     }
     
     public String getFilizedName(EventAccessOperations event) {
@@ -66,21 +155,6 @@ public class NameGenerator {
     public static String filize(String fileName){
         fileName = fileName.replaceAll("[/ :]", "_");
         return fileName.replaceAll("[\t\n\f\r]", "");
-    }
-    
-    public String getDefaultName(EventAccessOperations event){
-        String eventFileName =
-            regions.getRegionName(event.get_attributes().region);
-        eventFileName += " " + getOrigin(event).origin_time.date_time;
-        return eventFileName;
-    }
-    
-    private String getTime(EventAccessOperations event, String formatStr) {
-        if (formatStr.length() == 0) formatStr = "yyyyMMdd'T'HHmmss.SSS";
-        SimpleDateFormat labelFormat = new SimpleDateFormat(formatStr);
-        labelFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-        MicroSecondDate msd = new MicroSecondDate(getOrigin(event).origin_time);
-        return labelFormat.format(msd);
     }
     
     private int getLongitude(EventAccessOperations event) {
@@ -111,13 +185,13 @@ public class NameGenerator {
         return o;
     }
     
-    private String getRegionName(EventAccessOperations event){
+    public static String getRegionName(EventAccessOperations event){
         return regions.getRegionName(event.get_attributes().region);
     }
     
-    Element config;
+    private List builder = new ArrayList();
     
-    ParseRegions regions;
+    static ParseRegions regions = new ParseRegions();
     
     private static Logger logger = Logger.getLogger(NameGenerator.class);
 } // NameGenerator
