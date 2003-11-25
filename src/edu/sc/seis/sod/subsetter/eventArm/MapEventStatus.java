2d93ebb63ae7df226fa01d650620bfc99ec546d5
oliverpa <none@none>
2003-11-25 14:25:19 -0500

    [svn r6475] fixed some formatting issues
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventArm/MapEventStatus.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/MapEventStatus.java
@@ -12,44 +12,41 @@ import org.w3c.dom.Element;
  *
  * @author Created by Philip Oliver-Paull
  */
-public class MapEventStatus implements SodElement, EventStatus
-{
-	OpenMap map = new OpenMap("edu/sc/seis/fissuresUtil/data/maps/dcwpo-browse");
-	EventLayer events;
-	String fileLoc;
-		
-	public MapEventStatus(Element element){
-		fileLoc = element.getAttribute("xlink:link");
-		events = new EventLayer(map.getMapBean());
-		map.setEventLayer(events);
-	}
-	
-	/**
-	 * Method change
-	 *
-	 * @param    event               an EventAccessOperations
-	 * @param    status              a  RunStatus
-	 *
-	 */
-	public void change(EventAccessOperations event, RunStatus status)
-	{
-		if (status == RunStatus.PASSED){
-			events.eventDataChanged(new EQDataEvent(this, new EventAccessOperations[]{event}));
-			map.writeMapToPNG(fileLoc);
-		}
-		
-	}
-	
-	/**
-	 * Method setArmStatus
-	 *
-	 * @param    status              a  String
-	 *
-	 */
-	public void setArmStatus(String status)
-	{
-		// noImpl
-	}
-	
+public class MapEventStatus implements SodElement, EventStatus {
+    private OpenMap map = new OpenMap("edu/sc/seis/fissuresUtil/data/maps/dcwpo-browse");
+    private EventLayer events;
+    private String fileLoc;
+    
+    public MapEventStatus(Element element){
+        fileLoc = element.getAttribute("xlink:link");
+        events = new EventLayer(map.getMapBean());
+        map.setEventLayer(events);
+    }
+    
+    /**
+     * Method change
+     *
+     * @param    event               an EventAccessOperations
+     * @param    status              a  RunStatus
+     *
+     */
+    public void change(EventAccessOperations event, RunStatus status) {
+        if (status == RunStatus.PASSED){
+            events.eventDataChanged(new EQDataEvent(this, new EventAccessOperations[]{event}));
+            map.writeMapToPNG(fileLoc);
+        }
+        
+    }
+    
+    /**
+     * Method setArmStatus
+     *
+     * @param    status              a  String
+     *
+     */
+    public void setArmStatus(String status) {
+        // noImpl
+    }
+    
 }
 
