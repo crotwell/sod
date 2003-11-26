c093ae8bfd0b42544202333d642f26094d09b4e9
crotwell <none@none>
2003-11-26 09:38:53 -0500

    [svn r6478] log orb class name
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/CommonAccess.java
+++ src/edu/sc/seis/sod/CommonAccess.java
@@ -18,9 +18,9 @@ import org.apache.log4j.lf5.util.LogMonitorAdapter;
 
 public class CommonAccess {
     private CommonAccess (){}
-    
+
     public static CommonAccess getCommonAccess() { return commonAccess; }
-    
+
     public FissuresNamingServiceImpl getFissuresNamingService() throws Exception{
         if (fissuresNamingService == null) {
             fissuresNamingService = new FissuresNamingServiceImpl(getORB());
@@ -29,46 +29,47 @@ public class CommonAccess {
                 fissuresNamingService.setNameServiceCorbaLoc((String)props.get(NAME_SERVICE_PROP));
             } // end of if ()
         } // end of if (fissuresNamingService == null)
-        
+
         return fissuresNamingService;
-        
+
     }
-    
+
     protected void initORB(String[] args, java.util.Properties props) {
         if (orb == null) {
             // Initialize the ORB.
             orb = (org.omg.CORBA_2_3.ORB)org.omg.CORBA.ORB.init(args, props);
+            logger.info("ORB class is "+orb.getClass().getName());
             // register valuetype factories
             AllVTFactory vt = new AllVTFactory();
             vt.register(orb);
         }
     }
-    
+
     public org.omg.CORBA_2_3.ORB getORB() throws ConfigurationException  {
         if (orb == null) {
             initORB(null, null);
         } // end of if (orb == null)
         return orb;
     }
-    
+
     public LogMonitorAdapter getLF5Adapter(){
         if(adapter == null){
             adapter = LogMonitorAdapter.newInstance(RunStatus.getLogLevels());
         }
         return adapter;
     }
-    
+
     private static CommonAccess commonAccess = new CommonAccess();
-    
+
     private LogMonitorAdapter adapter;
-    
+
     private org.omg.CORBA_2_3.ORB orb = null;
-    
+
     FissuresNamingServiceImpl fissuresNamingService;
-    
+
     static final String NAME_SERVICE_PROP =
         "edu.sc.seis.sod.nameServiceCorbaLoc";
-    
+
     static Logger logger = Logger.getLogger(CommonAccess.class);
 }// CommonAccess
 
