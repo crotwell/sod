e0f06031387d6f337a3879bd41ce60ba1309df20
groves <none@none>
2003-12-01 14:04:07 -0500

    [svn r6490] added map images
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventArm/EventStatusTemplate.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/EventStatusTemplate.java
@@ -4,6 +4,7 @@ import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.sod.EventStatus;
 import edu.sc.seis.sod.RunStatus;
 import edu.sc.seis.sod.subsetter.ExternalFileTemplate;
+import edu.sc.seis.sod.subsetter.GenericTemplate;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -19,34 +20,44 @@ public class EventStatusTemplate extends ExternalFileTemplate implements EventSt
     }
     
     public boolean isInterpreted(String tag){
-        if(tag.equals("events") || tag.equals("status")) return true;
+        if(tag.equals("events") || tag.equals("status") || tag.equals("mapImgLink"))
+            return true;
         return false;
     }
     
     public Object getInterpreter(String tag, Element el){
         if(tag.equals("events")){
             EventGroupTemplate egt = new EventGroupTemplate(el);
-            eventGroups.add(egt);
+            internalStatusWatchers.add(egt);
             return egt;
         }else if(tag.equals("status")) return new StatusFormatter();
+        else if(tag.equals("mapImgLink")){
+            MapEventStatus mapStatus = new MapImgSrc(el);
+            internalStatusWatchers.add(mapStatus);
+            return mapStatus;
+        }
         return null;
     }
     
+    private class MapImgSrc extends MapEventStatus implements GenericTemplate{
+        public MapImgSrc(Element el){ super(el); }
+        
+        public String getResult() { return fileLoc; }
+    }
+    
     public void change(EventAccessOperations event, RunStatus status) {
-        Iterator it = eventGroups.iterator();
-        while(it.hasNext()){
-            ((EventGroupTemplate)it.next()).change(event, status);
-        }
+        Iterator it = internalStatusWatchers.iterator();
+        while(it.hasNext()) ((EventStatus)it.next()).change(event, status);
         update();
     }
     
-    public void setUp(){ eventGroups = new ArrayList(); }
+    public void setUp(){ internalStatusWatchers = new ArrayList(); }
     
-    private class StatusFormatter{
-        public String toString(){ return status; }
+    private class StatusFormatter implements GenericTemplate{
+        public String getResult(){ return status; }
     }
     
-    private List eventGroups;
+    private List internalStatusWatchers;
     
     private String status = "";
 }
