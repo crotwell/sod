3fee5273a6e97701d0a518c390f0e085f658442b
crotwell <none@none>
2003-12-04 13:32:02 -0500

    [svn r6533] reindent
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventArm/EventFinder.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/EventFinder.java
@@ -10,65 +10,65 @@ import edu.iris.Fissures.event.*;
 import edu.iris.Fissures.*;
 import org.apache.log4j.*;
 
-/** 
+/**
  * This subsetter specifies the source of eventDC and the parameters required to query for events.
  *<pre>
  *  &lt;eventFinder&gt;
- *		&lt;name&gt;ANHINGAEVENTDC&lt;/name&gt;
- *		&lt;dns&gt;edu/sc/seis&lt;/dns&gt;
- *		&lt;globalArea/&gt;
- *		&lt;originDepthRange&gt;
- *			&lt;unitRange&gt;
- *				&lt;unit&gt;KILOMETER&lt;/unit&gt;
- *				&lt;min&gt;0&lt;/min&gt;
- *				&lt;max&gt;1000&lt;/max&gt;
- *			&lt;/unitRange&gt;
- *		&lt;/originDepthRange&gt;
- *		&lt;eventTimeRange&gt;
- *		&lt;timeRange&gt;
- *			&lt;startTime&gt;1999-01-01T00:00:00Z&lt;/startTime&gt;
- *			&lt;endTime&gt;2003-01-01T00:00:00Z&lt;/endTime&gt;
- *		&lt;/timeRange&gt;
- *		&lt;/eventTimeRange&gt;
- *		&lt;catalog&gt;&lt;value&gt;BIGQUAKE&lt;/value&gt;&lt;/catalog&gt;
- *		&lt;contributor&gt;&lt;value&gt;NEIC&lt;/value&gt;&lt;/contributor&gt;
+ *      &lt;name&gt;ANHINGAEVENTDC&lt;/name&gt;
+ *      &lt;dns&gt;edu/sc/seis&lt;/dns&gt;
+ *      &lt;globalArea/&gt;
+ *      &lt;originDepthRange&gt;
+ *          &lt;unitRange&gt;
+ *              &lt;unit&gt;KILOMETER&lt;/unit&gt;
+ *              &lt;min&gt;0&lt;/min&gt;
+ *              &lt;max&gt;1000&lt;/max&gt;
+ *          &lt;/unitRange&gt;
+ *      &lt;/originDepthRange&gt;
+ *      &lt;eventTimeRange&gt;
+ *      &lt;timeRange&gt;
+ *          &lt;startTime&gt;1999-01-01T00:00:00Z&lt;/startTime&gt;
+ *          &lt;endTime&gt;2003-01-01T00:00:00Z&lt;/endTime&gt;
+ *      &lt;/timeRange&gt;
+ *      &lt;/eventTimeRange&gt;
+ *      &lt;catalog&gt;&lt;value&gt;BIGQUAKE&lt;/value&gt;&lt;/catalog&gt;
+ *      &lt;contributor&gt;&lt;value&gt;NEIC&lt;/value&gt;&lt;/contributor&gt;
  * &lt;/eventFinder&gt;
- * 
+ *
  *                      (or)
  *
  *  &lt;eventFinder&gt;
- *		&lt;name&gt;ANHINGAEVENTDC&lt;/name&gt;
- *		&lt;dns&gt;edu/sc/seis&lt;/dns&gt;
- *		&lt;globalArea/&gt;
- *		&lt;eventTimeRange&gt;
- *		&lt;timeRange&gt;
- *			&lt;startTime&gt;1999-01-01T00:00:00Z&lt;/startTime&gt;
- *			&lt;endTime&gt;2003-01-01T00:00:00Z&lt;/endTime&gt;
- *		&lt;/timeRange&gt;
- *		&lt;/eventTimeRange&gt;
- *		&lt;catalog&gt;&lt;value&gt;BIGQUAKE&lt;/value&gt;&lt;/catalog&gt;
- *		&lt;contributor&gt;&lt;value&gt;NEIC&lt;/value&gt;&lt;/contributor&gt;
+ *      &lt;name&gt;ANHINGAEVENTDC&lt;/name&gt;
+ *      &lt;dns&gt;edu/sc/seis&lt;/dns&gt;
+ *      &lt;globalArea/&gt;
+ *      &lt;eventTimeRange&gt;
+ *      &lt;timeRange&gt;
+ *          &lt;startTime&gt;1999-01-01T00:00:00Z&lt;/startTime&gt;
+ *          &lt;endTime&gt;2003-01-01T00:00:00Z&lt;/endTime&gt;
+ *      &lt;/timeRange&gt;
+ *      &lt;/eventTimeRange&gt;
+ *      &lt;catalog&gt;&lt;value&gt;BIGQUAKE&lt;/value&gt;&lt;/catalog&gt;
+ *      &lt;contributor&gt;&lt;value&gt;NEIC&lt;/value&gt;&lt;/contributor&gt;
  *  &lt;/eventFinder&gt;
  *
  *                                  (or)
  *
  *  &lt;eventFinder&gt;
- *		&lt;name&gt;ANHINGAEVENTDC&lt;/name&gt;
- *		&lt;dns&gt;edu/sc/seis&lt;/dns&gt;
- *		&lt;globalArea/&gt;
- *		&lt;originDepthRange&gt;
- *			&lt;unitRange&gt;
- *				&lt;unit&gt;KILOMETER&lt;/unit&gt;
- *				&lt;min&gt;0&lt;/min&gt;
- *				&lt;max&gt;1000&lt;/max&gt;
- *			&lt;/unitRange&gt;
- *		&lt;/originDepthRange&gt;
- *		&lt;eventTimeRange&gt;
- *		&lt;timeRange&gt;
- *			&lt;startTime&gt;1999-01-01T00:00:00Z&lt;/startTime&gt;
- *			&lt;endTime&gt;2003-01-01T00:00:00Z&lt;/endTime&gt;
- *		&lt;/timeRange&gt;
- *		&lt;/eventTimeRange&gt;
+ *      &lt;name&gt;ANHINGAEVENTDC&lt;/name&gt;
+ *      &lt;dns&gt;edu/sc/seis&lt;/dns&gt;
+ *      &lt;globalArea/&gt;
+ *      &lt;originDepthRange&gt;
+ *          &lt;unitRange&gt;
+ *              &lt;unit&gt;KILOMETER&lt;/unit&gt;
+ *              &lt;min&gt;0&lt;/min&gt;
+ *              &lt;max&gt;1000&lt;/max&gt;
+ *          &lt;/unitRange&gt;
+ *      &lt;/originDepthRange&gt;
+ *      &lt;eventTimeRange&gt;
+ *      &lt;timeRange&gt;
+ *          &lt;startTime&gt;1999-01-01T00:00:00Z&lt;/startTime&gt;
+ *          &lt;endTime&gt;2003-01-01T00:00:00Z&lt;/endTime&gt;
+ *      &lt;/timeRange&gt;
+ *      &lt;/eventTimeRange&gt;
  *  &lt;/eventFinder&gt;
  *</pre>
  */
@@ -80,52 +80,52 @@ public class EventFinder extends AbstractSource implements SodElement {
      * @param config an <code>Element</code> value
      */
     public EventFinder (Element config) throws Exception{
-	super(config);
-	this.config = config;
-	CommonAccess commonAccess = CommonAccess.getCommonAccess();
+        super(config);
+        this.config = config;
+        CommonAccess commonAccess = CommonAccess.getCommonAccess();
         processConfig();
-	fissuresNamingService = commonAccess.getFissuresNamingService();
-       
+        fissuresNamingService = commonAccess.getFissuresNamingService();
+
     }
-    
+
     /**
      * Describe <code>processConfig</code> method here.
      *
      * @exception ConfigurationException if an error occurs
      */
     protected void processConfig() throws ConfigurationException{
-	
-	NodeList childNodes = config.getChildNodes();
-	Node node;
-	catalogs = new ArrayList();
-	contributors = new ArrayList();
-	for(int counter = 0; counter < childNodes.getLength(); counter++) {
 
-	    node = childNodes.item(counter);
-	    if(node instanceof Element) {
+        NodeList childNodes = config.getChildNodes();
+        Node node;
+        catalogs = new ArrayList();
+        contributors = new ArrayList();
+        for(int counter = 0; counter < childNodes.getLength(); counter++) {
 
-		String tagName = ((Element)node).getTagName();
-		if(!tagName.equals("name") && !tagName.equals("dns")) {
+            node = childNodes.item(counter);
+            if(node instanceof Element) {
+
+                String tagName = ((Element)node).getTagName();
+                if(!tagName.equals("name") && !tagName.equals("dns")) {
 
 
-		    Object object = SodUtil.load((Element)node, "edu.sc.seis.sod.subsetter.eventArm");
-		    if(tagName.equals("originDepthRange")) depthRange = ((OriginDepthRange)object);
-		    else if(tagName.equals("eventTimeRange")) eventTimeRange = ((EventTimeRange)object);
-		    else if(tagName.equals("magnitudeRange")) magnitudeRange = (MagnitudeRange)object;
-		    else if(object instanceof edu.iris.Fissures.Area) area = (edu.iris.Fissures.Area)object;
-		    else if(tagName.equals("catalog")) {
-			catalog = (Catalog)object;
-			catalogs.add(catalog.getCatalog());
-		    }
-		    else if(tagName.equals("contributor")) {
-			contributor = (Contributor)object;
-			contributors.add(contributor.getContributor());
-		    }
-		} 
-	    }
+                    Object object = SodUtil.load((Element)node, "edu.sc.seis.sod.subsetter.eventArm");
+                    if(tagName.equals("originDepthRange")) depthRange = ((OriginDepthRange)object);
+                    else if(tagName.equals("eventTimeRange")) eventTimeRange = ((EventTimeRange)object);
+                    else if(tagName.equals("magnitudeRange")) magnitudeRange = (MagnitudeRange)object;
+                    else if(object instanceof edu.iris.Fissures.Area) area = (edu.iris.Fissures.Area)object;
+                    else if(tagName.equals("catalog")) {
+                        catalog = (Catalog)object;
+                        catalogs.add(catalog.getCatalog());
+                    }
+                    else if(tagName.equals("contributor")) {
+                        contributor = (Contributor)object;
+                        contributors.add(contributor.getContributor());
+                    }
+                }
+            }
 
-	}
-	
+        }
+
     }
 
     /**
@@ -135,7 +135,7 @@ public class EventFinder extends AbstractSource implements SodElement {
      */
     public EventDC getEventDC() throws Exception{
 
-	return fissuresNamingService.getEventDC(getDNSName(), getSourceName());
+        return fissuresNamingService.getEventDC(getDNSName(), getSourceName());
     }
 
     /**
@@ -144,7 +144,7 @@ public class EventFinder extends AbstractSource implements SodElement {
      * @return a <code>DepthRange</code> value
      */
     public OriginDepthRange getDepthRange() {
-	return depthRange;
+        return depthRange;
 
     }
 
@@ -155,18 +155,18 @@ public class EventFinder extends AbstractSource implements SodElement {
      */
     public MagnitudeRange getMagnitudeRange() {
 
-	return magnitudeRange;
+        return magnitudeRange;
 
     }
-    
+
     /**
      * Describe <code>getArea</code> method here.
      *
      * @return an <code>edu.iris.Fissures.Area</code> value
      */
     public edu.iris.Fissures.Area getArea() {
-	return area;
-	
+        return area;
+
     }
 
     /**
@@ -176,7 +176,7 @@ public class EventFinder extends AbstractSource implements SodElement {
      */
     public EventTimeRange getEventTimeRange() {
 
-	return eventTimeRange;
+        return eventTimeRange;
 
     }
 
@@ -187,9 +187,9 @@ public class EventFinder extends AbstractSource implements SodElement {
      */
     public String[] getCatalogs() {
 
-	String[] rtnValues = new String[catalogs.size()];
-	rtnValues = (String[]) catalogs.toArray(rtnValues);
-	return rtnValues;
+        String[] rtnValues = new String[catalogs.size()];
+        rtnValues = (String[]) catalogs.toArray(rtnValues);
+        return rtnValues;
     }
 
     /**
@@ -199,15 +199,15 @@ public class EventFinder extends AbstractSource implements SodElement {
      */
     public String[] getContributors() {
 
-	String[] rtnValues = new String[contributors.size()];
-	rtnValues = (String[])contributors.toArray(rtnValues);
-	return rtnValues;
+        String[] rtnValues = new String[contributors.size()];
+        rtnValues = (String[])contributors.toArray(rtnValues);
+        return rtnValues;
     }
 
 
     private FissuresNamingServiceImpl fissuresNamingService = null;
-			 
-			   
+
+
     private Element config = null;
 
     private Catalog catalog;
@@ -223,10 +223,10 @@ public class EventFinder extends AbstractSource implements SodElement {
     private MagnitudeRange magnitudeRange;
 
     private EventTimeRange eventTimeRange;
-    
+
     private edu.iris.Fissures.Area area;
 
-    static Category logger = 
+    static Category logger =
         Category.getInstance(EventFinder.class.getName());
 
 }// EventFinder
