10cccb5fec906fd451fb1729e92c7d644d84f62b
crotwell <none@none>
2003-12-04 14:35:05 -0500

    [svn r6543] add get combo box with selected, spacing changes on indent
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/EditorUtil.java
+++ src/edu/sc/seis/sod/editor/EditorUtil.java
@@ -42,13 +42,14 @@ public class EditorUtil {
     public static Box indent(JComponent top, JComponent bottom) {
         Box box = Box.createVerticalBox();
         Box topRow = Box.createHorizontalBox();
+        box.add(Box.createRigidArea(new Dimension(10, 10)));
         box.add(topRow);
         Box botRow = Box.createHorizontalBox();
         box.add(botRow);
 
         topRow.add(top);
         topRow.add(Box.createGlue());
-        botRow.add(Box.createRigidArea(new Dimension(10, 10)));
+        botRow.add(Box.createRigidArea(new Dimension(10, 1)));
         botRow.add(bottom);
         botRow.add(Box.createGlue());
         return box;
@@ -58,9 +59,25 @@ public class EditorUtil {
     public static JComboBox getComboBox(Element element, Object[] vals) throws TransformerException {
         Node node = XPathAPI.selectSingleNode(element, "text()");
         Text text = (Text)node;
+        return getComboBox(element, vals, text.getNodeValue());
+    }
+
+    public static JComboBox getComboBox(Element element, Object[] vals, Object selected) throws TransformerException {
+        Node node = XPathAPI.selectSingleNode(element, "text()");
+        Text text = (Text)node;
         JComboBox combo = new JComboBox(vals);
-        combo.addItem(text.getNodeValue());
-        combo.setSelectedItem(text.getNodeValue());
+        boolean found = false;
+
+        for (int i = 0; i < vals.length; i++) {
+            if (vals[i].equals(selected)) {
+                found = true;
+                break;
+            }
+        }
+        if ( ! found) {
+            combo.addItem(selected);
+        }
+        combo.setSelectedItem(selected);
         combo.addItemListener(new TextItemListener(text));
         return combo;
     }
