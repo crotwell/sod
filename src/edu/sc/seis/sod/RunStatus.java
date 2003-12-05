819ea8bada435389cf8d9233cd9ba1697ca372a8
groves <none@none>
2003-12-05 15:25:52 -0500

    [svn r6579] changed translate to just return a runstatus, and added several more translations
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/RunStatus.java
+++ src/edu/sc/seis/sod/RunStatus.java
@@ -10,25 +10,28 @@ import edu.sc.seis.sod.database.Status;
 import org.apache.log4j.lf5.LogLevel;
 
 public class RunStatus{
-    private RunStatus(String name){
-        this.name = name;
-    }
+    private RunStatus(String name){ this.name = name; }
     
     public static final RunStatus NEW = new RunStatus("New");
-    
     public static final RunStatus PASSED = new RunStatus("Passed");
-    
     public static final RunStatus FAILED = new RunStatus("Failed");
-    
+    public static final RunStatus SYSTEM_FAILURE = new RunStatus("System Failure");
     public static final RunStatus GENERIC_STATUS = new RunStatus("Info");
+    public static final RunStatus PROCESSING = new RunStatus("Processing");
     
     public static LogLevel[] LOG_LEVELS;
     
-    public static LogLevel translate(Status status) {
-        if(status == Status.NEW) return NEW.getLogLevel();
-        else if(status == Status.COMPLETE_SUCCESS) return PASSED.getLogLevel();
-        else if(status == Status.COMPLETE_REJECT) return FAILED.getLogLevel();
-        else return GENERIC_STATUS.getLogLevel();
+    public static RunStatus translate(Status status) {
+        if(status == Status.NEW || status == Status.RE_OPEN) return NEW;
+        else if(status == Status.COMPLETE_SUCCESS ||
+                status == Status.RE_OPEN_SUCCESS) return PASSED;
+        else if(status == Status.COMPLETE_REJECT ||
+                status == Status.RE_OPEN_REJECT) return FAILED;
+        else if(status == Status.PROCESSING ||
+                status == Status.RE_OPEN_PROCESSING ||
+                status == Status.AWAITING_FINAL_STATUS) return PROCESSING;
+        else if(status == Status.SOD_FAILURE) return SYSTEM_FAILURE;
+        else return GENERIC_STATUS;
     }
     
     public static LogLevel[] getLogLevels(){
@@ -49,9 +52,7 @@ public class RunStatus{
     //requires a GraphicsConfiguration to create, and this allows sod to run in
     //a headless system.
     public LogLevel getLogLevel(){
-        if(level == null){
-            level = new LogLevel(name, statusCount++);
-        }
+        if(level == null) level = new LogLevel(name, statusCount++);
         return level;
     }
     
