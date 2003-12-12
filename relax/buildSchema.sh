fdfcc56d6fcf8e4af0eccbc967eab66b2c58fc20
crotwell <none@none>
2003-12-12 15:55:35 -0500

    [svn r6653] use default maven location
    --HG--
    branch : sod

--- relax/buildSchema.sh
+++ relax/buildSchema.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-REPOSITORY=${HOME}/repository
+REPOSITORY=${HOME}/.maven/repository
 
 echo writing schema
 java -cp `./classpath_creator.sh -seperator : -repository ${REPOSITORY}` com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng 
