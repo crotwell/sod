1b29eaa4f033ac19fde5642ca8287926f9e353a3
oliverpa <none@none>
2004-01-15 14:01:08 -0500

    [svn r6773] Lots of stuff.  Everything involving network status goes through here now.
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/networkArm/NetworkInfoTemplateGenerator.java
+++ src/edu/sc/seis/sod/subsetter/networkArm/NetworkInfoTemplateGenerator.java
@@ -1,7 +1,7 @@
 /**
  * NetworkTemplateGenerator.java
  *
- * @author Created by Omnicore CodeGuide
+ * @author Created by Philip Oliver-Paull
  */
 
 package edu.sc.seis.sod.subsetter.networkArm;
@@ -14,11 +14,12 @@ import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.NetworkStatus;
 import edu.sc.seis.sod.RunStatus;
 import edu.sc.seis.sod.subsetter.NetworkFormatter;
+import edu.sc.seis.sod.subsetter.SiteFormatter;
+import edu.sc.seis.sod.subsetter.StationFormatter;
 import edu.sc.seis.sod.subsetter.TemplateFileLoader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -26,92 +27,216 @@ import org.w3c.dom.NodeList;
 
 public class NetworkInfoTemplateGenerator implements NetworkStatus {
     
-    private NetworkFormatter formatter;
-    private Map templatesByNet = new HashMap();
-    private Map templatesByStation = new HashMap();
-    private Element config;
+    private String fileDir, netsOutputFileName, stasOutputFileName, sitesOutputFileName, chansOutputFileName;
+    private NetworkFormatter netFormatter;
+    private StationFormatter staFormatter;
+    private SiteFormatter siteFormatter;
+    private NetworkStatusTemplate netTemplate;
+    private HashMap stationTemplates = new HashMap(); //station templates by network
+    private HashMap siteTemplates = new HashMap(); //site templates by station
+    private HashMap channelTemplates = new HashMap(); //channel templates by site
     private Logger logger = Logger.getLogger(NetworkInfoTemplateGenerator.class);
+    private Element netConfig, staConfig, siteConfig, chanConfig;
     
     public NetworkInfoTemplateGenerator(Element el){
-        config = el;
         NodeList nl = el.getChildNodes();
         for (int i = 0; i < nl.getLength(); i++) {
             Node n = nl.item(i);
-            if (n.getNodeName().equals("fileLoc")){
-                formatter = new NetworkFormatter((Element)n);
+            if (n.getNodeName().equals("fileDir")){
+                fileDir = n.getFirstChild().getNodeValue();
             }
-            else if (n.getNodeName().equals("externalConfig")){
-                try {
-                    config = TemplateFileLoader.getTemplate((Element)n);
-                } catch (IOException e) {
-                    CommonAccess.handleException(e, "trouble getting config template");
-                }
+            else if (n.getNodeName().equals("netsOutputFileName")){
+                netsOutputFileName = n.getFirstChild().getNodeValue();
+            }
+            else if (n.getNodeName().equals("netDir")){
+                netFormatter = new NetworkFormatter((Element)n);
+            }
+            else if (n.getNodeName().equals("stationsOutputFileName")){
+                stasOutputFileName = n.getFirstChild().getNodeValue();
+            }
+            else if (n.getNodeName().equals("stationDir")){
+                staFormatter = new StationFormatter((Element)n);
+            }
+            else if (n.getNodeName().equals("sitesOutputFileName")){
+                sitesOutputFileName = n.getFirstChild().getNodeValue();
+            }
+            else if (n.getNodeName().equals("siteDir")){
+                siteFormatter = new SiteFormatter((Element)n);
+            }
+            else if (n.getNodeName().equals("channelsOutputFileName")){
+                chansOutputFileName = n.getFirstChild().getNodeValue();
+            }
+            else if (n.getNodeName().equals("netConfig")){
+                netConfig = getConfig((Element)n);
+            }
+            else if (n.getNodeName().equals("stationConfig")){
+                staConfig = getConfig((Element)n);
+            }
+            else if (n.getNodeName().equals("siteConfig")){
+                siteConfig = getConfig((Element)n);
+            }
+            else if (n.getNodeName().equals("channelConfig")){
+                chanConfig = getConfig((Element)n);
             }
         }
-        if (formatter == null){
-            throw new IllegalArgumentException("The configuration element must contain a fileLoc and an external config");
+        if (fileDir == null || netFormatter == null || staFormatter == null || siteFormatter == null
+            || netConfig == null || staConfig == null || siteConfig == null || chanConfig == null
+            || netsOutputFileName == null || stasOutputFileName == null || sitesOutputFileName == null
+            || chansOutputFileName == null){
+            throw new IllegalArgumentException(
+                "The configuration element must contain a fileDir, netsOutputFileName, netDir, stationsOutputFileName, stationDir, sitesOutputFileName,"
+                    + " siteDir, channelsOutputFileName, netConfig, stationConfig, siteConfig, and chanConfig.");
         }
+        
+        try {
+            netTemplate = new NetworkStatusTemplate(netConfig, fileDir + '/' + netsOutputFileName);
+        } catch (IOException e) {
+            CommonAccess.handleException(e, "trouble creating networkStatusTemplate");
+        }
+    }
+    
+    private Element getConfig(Element el){
+        Element tmpEl = null;
+        try {
+            tmpEl = TemplateFileLoader.getTemplate(el);
+        } catch (IOException e) {
+            CommonAccess.handleException(e, "trouble getting config template");
+        }
+        return tmpEl;
     }
     
     public void change(NetworkAccess net, RunStatus status){
-        getTemplate(net);
+        netTemplate.change(net, status);
+        getStationsInNetworkTemplate(net);
     }
     
-    public void change(Station station, RunStatus status) {
+    public void change(Station station, RunStatus status){
         logger.debug("change(station, status): " + station.get_code() + ", " + status.toString());
-        if (!templatesByStation.containsKey(station)){
-            logger.debug("templatesByStation does NOT contain Key " + station.get_code());
-            NetworkAccess staNet = null;
-            logger.debug("size of TemplatesByNet: " + templatesByNet.keySet().size());
-            Iterator it = templatesByNet.keySet().iterator();
-            while(it.hasNext() && staNet == null){
-                NetworkAccess cur = (NetworkAccess)it.next();
-                //tests by network's name right now
-                //this may need to be changed...
-                if (cur.get_attributes().name.equals(station.my_network.name)){
-                    staNet = cur;
-                    logger.debug("found station "
-                                     + station.get_code()
-                                     + "'s network ("
-                                     + staNet.get_attributes().get_code()
-                                     +") in templatesByNet");
-                }
-            }
-            if (staNet != null){
-                logger.debug("staNet != null");
-                NetworkInfoTemplate nit = getTemplate(staNet);
-                nit.change(station, status);
-                templatesByStation.put(station, nit);
-            }
-        }
-        else{
-            logger.debug("templatesByStation.containsKey(" + station.get_code() + ")");
-            NetworkInfoTemplate nit = (NetworkInfoTemplate)templatesByStation.get(station);
-            nit.change(station, status);
-        }
+        StationsInNetworkTemplate snt = getStationsInNetworkTemplate(station);
+        snt.change(station, status);
+        getSitesInStationTemplate(station);
     }
     
-    public NetworkInfoTemplate getTemplate(NetworkAccess net){
-        if (!templatesByNet.containsKey(net)){
-            templatesByNet.put(net, new NetworkInfoTemplate(config, formatter.getResult(net), net));
+    public void change(Site site, RunStatus status) {
+        logger.debug("change(site, status): " + SiteFormatter.formatSiteCode(site.get_code()) + ", " + status.toString());
+        SitesInStationTemplate sst = getSitesInStationTemplate(site);
+        sst.change(site, status);
+        getChannelsInSiteTemplate(site);
+    }
+    
+    public void change(Channel channel, RunStatus status) {
+        logger.debug("change(channel, status): " + channel.get_code() + ", " + status.toString());
+        ChannelsInSiteTemplate cst = getChannelsInSiteTemplate(channel);
+        cst.change(channel, status);
+    }
+    
+    public NetworkStatusTemplate getNetworkStatusTemplate(){
+        return netTemplate;
+    }
+    
+    public StationsInNetworkTemplate getStationsInNetworkTemplate(NetworkAccess net){
+        if (!contains(net)){
+            stationTemplates.put(net.get_attributes().name + net.get_attributes().get_id().begin_time.date_time,
+                                 new StationsInNetworkTemplate(staConfig,
+                                                               fileDir
+                                                                   + '/'
+                                                                   + netFormatter.getResult(net)
+                                                                   + '/'
+                                                                   + stasOutputFileName,
+                                                               net));
         }
-        return (NetworkInfoTemplate)templatesByNet.get(net);
+        return (StationsInNetworkTemplate)stationTemplates.get(net.get_attributes().name
+                                                                   + net.get_attributes().get_id().begin_time.date_time);
+    }
+    
+    public StationsInNetworkTemplate getStationsInNetworkTemplate(Station station){
+        return getStationsInNetworkTemplate(getNetworkFromStation(station));
+    }
+    
+    public SitesInStationTemplate getSitesInStationTemplate(Station station){
+        if (!contains(station)){
+            siteTemplates.put(station.name
+                                  + station.get_id().begin_time.date_time,
+                              new SitesInStationTemplate(siteConfig,
+                                                         fileDir
+                                                             + '/'
+                                                             + netFormatter.getResult(getNetworkFromStation(station))
+                                                             + '/'
+                                                             + staFormatter.getResult(station)
+                                                             + '/'
+                                                             + sitesOutputFileName,
+                                                         station));
+        }
+        return (SitesInStationTemplate)siteTemplates.get(station.name
+                                                             + station.get_id().begin_time.date_time);
+    }
+    
+    public SitesInStationTemplate getSitesInStationTemplate(Site site){
+        return getSitesInStationTemplate(site.my_station);
+    }
+    
+    public ChannelsInSiteTemplate getChannelsInSiteTemplate(Site site){
+        if (!contains(site)){
+            channelTemplates.put(site.my_station.my_network.get_code()
+                                     + site.my_station.get_code() + SiteFormatter.formatSiteCode(site.get_code())
+                                     + site.get_id().begin_time.date_time,
+                                 new ChannelsInSiteTemplate(chanConfig,
+                                                            fileDir
+                                                                + '/'
+                                                                + netFormatter.getResult(getNetworkFromStation(site.my_station))
+                                                                + '/'
+                                                                + staFormatter.getResult(site.my_station)
+                                                                + '/'
+                                                                + siteFormatter.getResult(site)
+                                                                + '/'
+                                                                + chansOutputFileName,
+                                                            site));
+        }
+        return (ChannelsInSiteTemplate)channelTemplates.get(site.my_station.my_network.get_code()
+                                                                + site.my_station.get_code()
+                                                                + SiteFormatter.formatSiteCode(site.get_code())
+                                                                + site.get_id().begin_time.date_time);
+    }
+    
+    public ChannelsInSiteTemplate getChannelsInSiteTemplate(Channel chan){
+        return getChannelsInSiteTemplate(chan.my_site);
+    }
+    
+    public NetworkAccess getNetworkFromStation(Station station){
+        NetworkAccess staNet = null;
+        Iterator it = stationTemplates.keySet().iterator();
+        while (it.hasNext() && staNet == null){
+            String cur = (String)it.next();
+            if (cur.equals(station.my_network.name + station.my_network.get_id().begin_time.date_time)){
+                staNet = ((StationsInNetworkTemplate)stationTemplates.get(cur)).getNetwork();
+                logger.debug("found station "
+                                 + station.get_code()
+                                 + "'s network ("
+                                 + staNet.get_attributes().get_code()
+                                 +") in stationTemplates");
+            }
+        }
+        return staNet;
     }
     
     public boolean contains(NetworkAccess net){
-        return templatesByNet.containsKey(net);
+        return stationTemplates.containsKey(net.get_attributes().name
+                                                + net.get_attributes().get_id().begin_time.date_time);
     }
     
-    public void setArmStatus(String status) {}
+    public boolean contains(Station sta){
+        return siteTemplates.containsKey(sta.name + sta.get_id().begin_time.date_time);
+    }
     
-    public void change(Site site, RunStatus status) {}
+    public boolean contains(Site site){
+        return channelTemplates.containsKey(site.my_station.my_network.get_code()
+                                                + site.my_station.get_code()
+                                                + SiteFormatter.formatSiteCode(site.get_code())
+                                                + site.get_id().begin_time.date_time);
+    }
     
-    public void change(Channel channel, RunStatus status) {}
-    
-    
-    
-    
-    
+    public void setArmStatus(String status) { netTemplate.setArmStatus(status); }
     
 }
+
 
