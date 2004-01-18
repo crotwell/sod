dd6a8d10a71a716b65a8d1bc6179be7686e64aa8
crotwell <none@none>
2004-01-18 14:26:48 -0500

    [svn r6825] update jar versions
    --HG--
    branch : sod

--- scripts/sod.sh
+++ scripts/sod.sh
@@ -7,8 +7,8 @@ OB=$MAVEN/repository/OB/jars/OB-4.1.0.jar
 OBNAMING=$MAVEN/repository/OBNaming/jars/OBNaming-4.1.0.jar
 
 SEEDCODEC=$MAVEN/repository/SeedCodec/jars/SeedCodec-1.0Beta.jar
-FISSURESUTIL=$MAVEN/repository/fissuresUtil/jars/fissuresUtil-1.0.3beta.jar
-FISSURESIMPL=$MAVEN/repository/fissuresImpl/jars/fissuresImpl-1.1.2beta.jar
+FISSURESUTIL=$MAVEN/repository/fissuresUtil/jars/fissuresUtil-1.0.4beta.jar
+FISSURESIMPL=$MAVEN/repository/fissuresImpl/jars/fissuresImpl-1.1.2.jar
 FISSURESIDL=$MAVEN/repository/fissuresIDL/jars/fissuresIDL-1.0.jar
 GEOTOOLS=$MAVEN/repository/Geotools1/jars/geotools1.2.jar
 LOG4J=$MAVEN/repository/log4j/jars/log4j-1.2.8.jar
@@ -20,8 +20,9 @@ JAICORE=$MAVEN/repository/jars/jai_core.jar
 JAICODEC=$MAVEN/repository/jars/jai_codec.jar
 HSQLDB=$MAVEN/repository/hsqldb/jars/hsqldb-1.7.1.jar
 OPENMAP=$MAVEN/repository/openmap/jars/openmap-4.5.4.jar
+JING=$MAVEN/repository/jing/jars/jing-20030619.jar
 SOD=$MAVEN/repository/sod/jars/sod-1.0Beta.jar
 
 
-java -Xmx128m -cp ${SEEDCODEC}:${SOD}:${OPENMAP}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${CLASSPATH} edu.sc.seis.sod.Start $*
+java   -Xmx428m -cp ${SEEDCODEC}:${SOD}:${OPENMAP}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${JING}:${CLASSPATH} edu.sc.seis.sod.Start $*
 
