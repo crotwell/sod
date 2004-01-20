084aed6f53386b50ce7f5f535f556eda11bd69fa
groves <none@none>
2004-01-20 13:01:48 -0500

    [svn r6832] synchronize around access to the channel map
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/ChannelGroupTemplate.java
+++ src/edu/sc/seis/sod/subsetter/ChannelGroupTemplate.java
@@ -7,31 +7,39 @@ import java.util.Map;
 import org.w3c.dom.Element;
 
 public class ChannelGroupTemplate extends Template implements GenericTemplate{
-    Map channelMap = new HashMap();
+    private Map channelMap = new HashMap();
     
     public ChannelGroupTemplate(Element el){ parse(el);  }
     
     public String getResult() {
         StringBuffer buf = new StringBuffer();
-        Iterator it = channelMap.keySet().iterator();
-        while(it.hasNext()){
-            Channel curChan = (Channel)it.next();
-            Iterator templateIt = templates.iterator();
-            while(templateIt.hasNext()){
-                buf.append(((ChannelTemplate)templateIt.next()).getResult(curChan));
+        synchronized(channelMap){
+            Iterator it = channelMap.keySet().iterator();
+            while(it.hasNext()){
+                Channel curChan = (Channel)it.next();
+                Iterator templateIt = templates.iterator();
+                while(templateIt.hasNext()){
+                    buf.append(((ChannelTemplate)templateIt.next()).getResult(curChan));
+                }
             }
         }
         return buf.toString();
     }
     
-     protected Object textTemplate(final String text) {
+    protected Object textTemplate(final String text) {
         return new ChannelTemplate(){
             public String getResult(Channel chan) { return text; }
         };
     }
     
     public void change(Channel chan, RunStatus status){
-        channelMap.put(chan, status);
+        synchronized(channelMap) {
+            channelMap.put(chan, status);
+        }
+    }
+    
+    public String getStatus(Channel chan){
+        return (String)channelMap.get(chan);
     }
     
     public Object getTemplate(String tag, Element element){
