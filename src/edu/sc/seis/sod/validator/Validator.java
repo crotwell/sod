0130bf1a2f4557b45845d7ee076a904b3a136cfd
crotwell <none@none>
2004-01-31 11:14:55 -0500

    [svn r6927] Charlie's validator error stuff
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/Validator.java
+++ src/edu/sc/seis/sod/validator/Validator.java
@@ -16,7 +16,7 @@ public class Validator{
         throws IOException, SAXException{
         return validate(in, true);
     }
-    
+
     public static boolean validate(InputSource in, boolean verbose)
         throws IOException, SAXException{
         synchronized(validationLock){
@@ -32,18 +32,20 @@ public class Validator{
             }
         }
     }
-    
+
     private static String xmlFileMalformed =
         "The xml file being validated has an error in it";
-    
+
     private static String xmlFileReadingError =
         "An I/O Error occured reading in the xml file to be validated";
-    
+
     private static void createDriver(boolean verbose)
         throws IOException, SAXException{
-        PropertyMap pm = PropertyMap.EMPTY;
-        if(!verbose)
+        PropertyMap pm = PropertyMap.EMPTY;//new SinglePropertyMap(ValidateProperty.ERROR_HANDLER,
+                                            //   sodEH);
+        if(!verbose){
             pm = new SinglePropertyMap(ValidateProperty.ERROR_HANDLER, quietEH);
+        }
         Validator.verbose = verbose;
         driver = new ValidationDriver(pm);
         try{
@@ -54,29 +56,47 @@ public class Validator{
             throw new SAXException("The loaded schema is not well formed", e);
         }
     }
-    
+
     private static InputSource getSchemaSource() {
         ClassLoader loader = (Validator.class).getClassLoader();
         return new InputSource(loader.getResourceAsStream(schemaLoc));
     }
-    
+
     public static void setSchemaLoc(String loc) { schemaLoc = loc; }
-    
+
     private static ErrorHandler quietEH = new ErrorHandler(){
         public void warning(SAXParseException exception){}
-        
+
         public void error(SAXParseException exception){}
-        
+
         public void fatalError(SAXParseException exception){}
     };
-    
+
+    private static ErrorHandler sodEH = new ErrorHandler(){
+        public void error(SAXParseException exception){
+            handle(exception);
+        }
+
+        public void fatalError(SAXParseException exception) {
+            handle(exception);
+        }
+
+        public void warning(SAXParseException exception) {
+            handle(exception);
+        }
+
+        private void handle(SAXParseException e){
+            System.out.println("The config file being used by Sod has an error at line " + e.getLineNumber() + " at column " + e.getColumnNumber() + " at the identifier " + e.getPublicId());
+        }
+    };
+
     private static boolean verbose;
-    
+
     private static Object validationLock = new Object();
-    
+
     private static String schemaLoc = "edu/sc/seis/sod/data/sod.rng";
-    
+
     private static ValidationDriver driver;
-    
+
     private static Logger logger = Logger.getLogger(Validator.class);
 }
