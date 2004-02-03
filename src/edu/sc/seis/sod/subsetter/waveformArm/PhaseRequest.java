c86d1ec787b9f3a32ca586f2dc2ba3c9f3548e74
groves <none@none>
2004-02-03 12:53:16 -0500

    [svn r6948] use .equals instead of == in checking tauModelName
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/waveformArm/PhaseRequest.java
+++ src/edu/sc/seis/sod/subsetter/waveformArm/PhaseRequest.java
@@ -1,19 +1,27 @@
 package edu.sc.seis.sod.subsetter.waveFormArm;
 
 import edu.sc.seis.sod.*;
-import edu.sc.seis.TauP.*;
 
-import edu.iris.Fissures.IfEvent.*;
-import edu.iris.Fissures.event.*;
-import edu.iris.Fissures.IfNetwork.*;
-import edu.iris.Fissures.network.*;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfEvent.Origin;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.Location;
-import edu.iris.Fissures.model.*;
-import edu.iris.Fissures.IfSeismogramDC.*;
-
-import java.util.*;
-
-import org.w3c.dom.*;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.QuantityImpl;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.TauP.Arrival;
+import edu.sc.seis.TauP.SphericalCoords;
+import edu.sc.seis.TauP.TauModelException;
+import edu.sc.seis.TauP.TauP_Time;
+import edu.sc.seis.sod.subsetter.waveFormArm.PhaseRequest;
+import java.util.Properties;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * sample xml file
@@ -42,7 +50,7 @@ public class PhaseRequest implements RequestGenerator{
      * @param config an <code>Element</code> value
      */
     public PhaseRequest (Element config) throws ConfigurationException{
-
+        
         NodeList childNodes = config.getChildNodes();
         Node node;
         for(int counter = 0; counter < childNodes.getLength(); counter++) {
@@ -67,7 +75,7 @@ public class PhaseRequest implements RequestGenerator{
             }
         }
     }
-
+    
     /**
      * Describe <code>generateRequest</code> method here.
      *
@@ -85,7 +93,7 @@ public class PhaseRequest implements RequestGenerator{
         double arrivalStartTime = -100.0;
         double arrivalEndTime = -100.0;
         origin = event.get_preferred_origin();
-
+        
         if ( prevRequestFilter != null &&
             origin.my_location.equals(prevOriginLoc) &&
             channel.my_site.my_location.equals(prevSiteLoc) ) {
@@ -102,14 +110,14 @@ public class PhaseRequest implements RequestGenerator{
             prevSiteLoc = channel.my_site.my_location;
             prevRequestFilter = null;
         } // end of else
-
-
+        
+        
         Properties props = Start.getProperties();
         String tauPModel = new String();
         tauPModel = props.getProperty("edu.sc.seis.sod.TaupModel");
         if(tauPModel == null) tauPModel = "prem";
-
-
+        
+        
         String phaseNames= "";
         if ( ! beginPhase.equals(ORIGIN)) {
             phaseNames += " "+beginPhase;
@@ -117,13 +125,13 @@ public class PhaseRequest implements RequestGenerator{
         if ( ! endPhase.equals(ORIGIN)) {
             phaseNames += " "+endPhase;
         } // end of if (beginPhase.equals("origin"))
-
-
+        
+        
         Arrival[] arrivals = calculateArrivals(tauPModel,
                                                phaseNames,
                                                origin.my_location,
                                                channel.my_site.my_location);
-
+        
         for(int counter = 0; counter < arrivals.length; counter++) {
             String arrivalName = arrivals[counter].getName();
             if(beginPhase.startsWith("tt")) {
@@ -141,7 +149,7 @@ public class PhaseRequest implements RequestGenerator{
                 break;
             }
         }
-
+        
         for(int counter = 0; counter < arrivals.length; counter++) {
             String arrivalName = arrivals[counter].getName();
             if(endPhase.startsWith("tt")) {
@@ -159,24 +167,24 @@ public class PhaseRequest implements RequestGenerator{
                 break;
             }
         }
-
+        
         if (beginPhase.equals(ORIGIN)) {
             arrivalStartTime = 0;
         }
         if (endPhase.equals(ORIGIN)) {
             arrivalEndTime = 0;
         }
-
+        
         if(arrivalStartTime == -100.0 || arrivalEndTime == -100.0) {
             // no arrivals found, return zero length request filters
             prevRequestFilter = new RequestFilter[0];
             return prevRequestFilter;
         }
-
+        
         // round to milliseconds
         arrivalStartTime = Math.rint(1000*arrivalStartTime)/1000;
         arrivalEndTime = Math.rint(1000*arrivalEndTime)/1000;
-
+        
         edu.iris.Fissures.Time originTime = origin.origin_time;
         MicroSecondDate originDate = new MicroSecondDate(originTime);
         TimeInterval bInterval = beginOffset.getTimeInterval();
@@ -194,26 +202,25 @@ public class PhaseRequest implements RequestGenerator{
                               bDate.getFissuresTime(),
                               eDate.getFissuresTime()
                              );
-
+        
         prevRequestFilter = filters;
         return filters;
-
+        
     }
-
+    
     protected static TauP_Time tauPTime = new TauP_Time();
-
-    protected synchronized static Arrival[] calculateArrivals(
-        String tauPModelName,
-        String phases,
-        Location originLoc,
-        Location channelLoc)
+    
+    protected synchronized static Arrival[] calculateArrivals(String tauPModelName,
+                                                              String phases,
+                                                              Location originLoc,
+                                                              Location channelLoc)
         throws java.io.IOException, TauModelException {
-        if (tauPTime.getTauModelName() != tauPModelName) {
+        if (!tauPTime.getTauModelName().equals(tauPModelName)) {
             tauPTime.loadTauModel(tauPModelName);
         }
         tauPTime.clearPhaseNames();
         tauPTime.parsePhaseList(phases);
-
+        
         double originDepth =
             ((QuantityImpl)originLoc.depth).convertTo(UnitImpl.KILOMETER).value;
         tauPTime.setSourceDepth(originDepth);
@@ -221,22 +228,22 @@ public class PhaseRequest implements RequestGenerator{
                                                     originLoc.longitude,
                                                     channelLoc.latitude,
                                                     channelLoc.longitude));
-
+        
         return tauPTime.getArrivals();
     }
-
+    
     private BeginOffset beginOffset;
-
+    
     private String beginPhase;
-
+    
     private EndOffset endOffset;
-
+    
     private String endPhase;
-
+    
     private RequestFilter[] prevRequestFilter = null;
     private Location prevOriginLoc = null;
     protected Location prevSiteLoc = null;
-
+    private static Logger logger = Logger.getLogger(PhaseRequest.class);
     private static final String ORIGIN = "origin";
-
+    
 }// PhaseRequest
