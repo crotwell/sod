7d9d5f59a999d6432262db9ddf88745ab2b11622
groves <none@none>
2004-02-13 11:39:30 -0500

    [svn r7092] removed queues, add new db stuff, use new unit and value interval properties
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/Start.java
+++ src/edu/sc/seis/sod/Start.java
@@ -1,10 +1,8 @@
 package edu.sc.seis.sod;
 
-import edu.sc.seis.fissuresUtil.exceptionHandler.WrappedException;
-import edu.sc.seis.sod.database.HSqlDbQueue;
-import edu.sc.seis.sod.database.Queue;
-import edu.sc.seis.sod.database.WaveformDbQueue;
-import edu.sc.seis.sod.database.WaveformQueue;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.sod.database.event.JDBCEventStatus;
 import edu.sc.seis.sod.validator.Validator;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -23,8 +21,9 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import java.sql.SQLException;
 
-public class Start implements SodExceptionListener {
+public class Start{
     /**
      * Creates a new <code>Start</code> instance set to use the XML config file
      * the input stream points to as its configuration
@@ -57,20 +56,20 @@ public class Start implements SodExceptionListener {
         }
         initDocument(args);
     }
-
+    
     public Start(Document document) throws Exception {
         this(document, new String[0]);
     }
-
+    
     public Start(Document document, String[] args) throws Exception {
         this.document = document;
         initDocument(args);
     }
-
+    
     protected void initDocument(String[] args) throws Exception {
         // get some defaults
         loadProps((Start.class).getClassLoader().getResourceAsStream(DEFAULT_PROPS));
-
+        
         for (int i=0; i<args.length-1; i++) {
             if (args[i].equals("-props")) {
                 // override with values in local directory,
@@ -79,10 +78,10 @@ public class Start implements SodExceptionListener {
                 System.out.println("loaded file props from "+args[i+1]+"  log4j.rootCategory="+props.getProperty("log4j.rootCategory"));
             }
         }
-
+        
         PropertyConfigurator.configure(props);
         logger.info("logging configured");
-
+        
         //now override the properties with the properties specified
         // in the configuration file.
         Element docElement = getDocument().getDocumentElement();
@@ -93,20 +92,14 @@ public class Start implements SodExceptionListener {
         } else {
             logger.debug("No properties specified in the configuration file");
         }
-
+        
         //here the orb must be initialized ..
         //configure commonAccess
         CommonAccess.getCommonAccess().initORB(args, props);
-
-        checkRestartOptions();
-
-        //configure the eventQueue and waveformQueue.
-        eventQueue = new HSqlDbQueue(props);
-        eventQueue.clean();
-        waveformQueue = new WaveformDbQueue(props);
-        waveformQueue.clean();
+        
+        executeRestartOptions();
     }
-
+    
     private InputSource createInputSource(String loc) throws IOException{
         InputStream in = null;
         if(loc.startsWith("http:") || loc.startsWith("ftp:")){
@@ -119,19 +112,19 @@ public class Start implements SodExceptionListener {
         }
         return new InputSource(new BufferedInputStream(in));
     }
-
+    
     public static WaveFormArm getWaveformArm() {
         return waveform;
     }
-
+    
     public static EventArm getEventArm() {
         return event;
     }
-
+    
     public static NetworkArm getNetworkArm() {
         return network;
     }
-
+    
     public static Document createDoc(InputSource source)
         throws SAXException, IOException, ParserConfigurationException{
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -141,7 +134,7 @@ public class Start implements SodExceptionListener {
         Document doc =  builder.parse(source);
         return doc;
     }
-
+    
     public void start() throws Exception {
         Element docElement = document.getDocumentElement();
         logger.info("start "+docElement.getTagName());
@@ -155,7 +148,7 @@ public class Start implements SodExceptionListener {
                     logger.info(el.getTagName());
                 } else if (el.getTagName().equals("eventArm")) {
                     logger.info(el.getTagName());
-                    event = new EventArm(el, this, props);
+                    event = new EventArm(el);
                     eventArmThread = new Thread(event, "eventArm thread");
                     eventArmThread.start();
                 } else if (el.getTagName().equals("networkArm")) {
@@ -166,51 +159,44 @@ public class Start implements SodExceptionListener {
                     int poolSize =
                         Integer.parseInt(props.getProperty("edu.sc.seis.sod.waveformarm.threads",
                                                            "5"));
-                    waveform = new WaveFormArm(el, network, this, poolSize);
+                    waveform = new WaveFormArm(el, network, poolSize);
                     waveFormArmThread = new Thread(waveform, "waveFormArm Thread");
                     waveFormArmThread.start();
-
+                    
                 }  else {
                     logger.debug("process "+el.getTagName());
                 }
             }
         }
     }
-
-    public static Properties getProperties() {
-        return props;
-    }
-
+    
+    public static Properties getProperties() {return props; }
+    
     public Document getDocument(){ return document; }
-
-    public static WaveformQueue getWaveformQueue(){ return waveformQueue; }
-
-    public static Queue getEventQueue(){ return eventQueue; }
-
+    
     public static void main (String[] args) {
         try {
-
+            
             String confFilename = null;
-
+            
             for (int i=0; i<args.length-1; i++) {
                 if(args[i].equals("-conf") || args[i].equals("-f")) {
                     confFilename = args[i+1];
                 }
             }
-
+            
             if (confFilename == null) {
                 System.err.println("No configuration file given, quiting....");
                 return;
             }
             Start start = new Start(confFilename, args);
-
+            
             logger.info("Start start()");
             start.start();
-
+            
             eventArmThread.join();
             waveFormArmThread.join();
-
-            eventQueue.closeDatabase();
+            
             logger.debug("Did not track the Thread bug Yet. so using System.exit()");
             System.exit(1);
         } catch(Exception e) {
@@ -218,7 +204,7 @@ public class Start implements SodExceptionListener {
         }
         logger.info("Done.");
     } // end of main ()
-
+    
     private static void loadProps(InputStream propStream){
         try {
             props.load(propStream);
@@ -229,20 +215,35 @@ public class Start implements SodExceptionListener {
             System.exit(0);
         }
     }
-
-    public void sodExceptionHandler(SodException sodException) {
-        CommonAccess.handleException(sodException.getThrowable(),
-                                     "Caught Exception in start because of the Listener");
+    
+    public static void add(Properties newProps){
+        props.putAll(newProps);
     }
-
-    private static  void checkRestartOptions() {
+    
+    private static  void executeRestartOptions() {
+        //TODO - use this in new db
         Start.REMOVE_DATABASE = isRemoveDatabase();
-        Start.QUIT_TIME = getQuitTime();
-        Start.RE_OPEN_EVENTS = isReOpenEvents();
-        Start.GET_NEW_EVENTS = isGetNewEvents();
-        Start.REFRESH_INTERVAL = getRefreshInterval();
+        if(isReopenEvents()){
+            try {
+                JDBCEventStatus eventStatus = new JDBCEventStatus();
+                eventStatus.restartCompletedEvents();
+            } catch (SQLException e) {
+                CommonAccess.handleException("Trouble restarting completed events", e);
+            }
+        }
     }
-
+    
+    public static TimeInterval getIntervalProp(TimeInterval defaultInterval,
+                                               String propName) throws NoSuchFieldException {
+        String unitName = props.getProperty(propName + ".unit", "DAY");
+        if(unitName != null){
+            UnitImpl unit = UnitImpl.getUnitFromString(unitName);
+            int val = getIntProp(propName + ".value", -1);
+            if(val != -1) return new TimeInterval(val, unit);
+        }
+        return defaultInterval;
+    }
+    
     private static boolean isRemoveDatabase() {
         String str = props.getProperty("edu.sc.seis.sod.database.remove");
         if(str != null) {
@@ -250,77 +251,46 @@ public class Start implements SodExceptionListener {
         }
         return false;
     }
-
-    private static int getRefreshInterval() {
-        String str = props.getProperty("edu.sc.seis.sod.database.eventRefreshInterval");
+    
+    public static int getIntProp(String propName, int defaultValue){
+        String str = props.getProperty(propName);
         if(str != null) {
             try {
                 return Integer.parseInt(str);
             } catch(NumberFormatException nfe) {}
         }
-        return REFRESH_INTERVAL;
+        return defaultValue;
     }
-
-    private static int getQuitTime() {
-        String str = props.getProperty("edu.sc.seis.sod.database.quitTime");
-        if(str != null) {
-            try {
-                return Integer.parseInt(str);
-            } catch(NumberFormatException nfe) {}
-        }
-        return QUIT_TIME;
-    }
-
-    private static boolean isReOpenEvents() {
-        String str = props.getProperty("edu.sc.seis.sod.database.reopenEvents");
+    
+    private static boolean isReopenEvents() {
+        String str = props.getProperty("sod.start.ReopenEvents");
         if(str != null && str.equalsIgnoreCase("true")) {
             return true;
         }
         return false;
     }
-
-    private static boolean isGetNewEvents() {
-        String str = props.getProperty("edu.sc.seis.sod.database.getNewEvents");
-        if(str != null) {
-            if(str.equalsIgnoreCase("true")) { return true;}
-        }
-        return false;
-    }
-
+    
     public static final String
         DEFAULT_PARSER_NAME = "org.apache.xerces.parsers.SAXParser";
-
+    
     public static boolean REMOVE_DATABASE = false;
-
-    //later quitetime must be changed relatively
-    public static int QUIT_TIME = 30; //quit time in terms of number of days;
-
-    public static boolean RE_OPEN_EVENTS = false;
-
-    public static boolean GET_NEW_EVENTS = false;
-
-    public static int REFRESH_INTERVAL = 600;
-
-    private static Properties props = System.getProperties();;
-
+    
+    private static Properties props = System.getProperties();
+    
     private Document document;
-
+    
     private static Logger logger = Logger.getLogger(Start.class);
-
-    private static WaveformQueue waveformQueue;
-
-    private static Queue eventQueue;
-
+    
     private static Thread waveFormArmThread;
-
+    
     private static Thread eventArmThread;
-
+    
     private static WaveFormArm waveform;
-
+    
     private static EventArm event;
-
+    
     private static NetworkArm network;
-
+    
     private static String DEFAULT_PROPS = "edu/sc/seis/sod/sod.prop";
 }// Start
 
