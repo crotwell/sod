38c5b1d2ef445863bc9a3f66c97bb4902f591f54
groves <none@none>
2004-02-13 11:47:36 -0500

    [svn r7099] cleaned up
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/database/DatabaseManager.java
+++ src/edu/sc/seis/sod/database/DatabaseManager.java
@@ -14,38 +14,36 @@ import java.util.*;
  */
 
 public class DatabaseManager {
-    private DatabaseManager() {
-
-    }
+    private DatabaseManager() {}
     
     public static AbstractDatabaseManager getDatabaseManager(Properties props, String type) {
-	String className = null;
-	try {
-	if(databaseManager != null) return databaseManager;
-	//use reflection to get the appropriate Database Manager.
-	className = getDatabaseType(props);
-	Class[] constructorArgTypes = new Class[1];
-	constructorArgTypes[0] = Properties.class;
-	Class externalClass = Class.forName(className);
-	Constructor constructor = 
-		externalClass.getConstructor(constructorArgTypes);
-	Object[] constructorArgs = new Object[1];
-	constructorArgs[0] = props;
-	Object obj = 
-		constructor.newInstance(constructorArgs);
-	databaseManager = (AbstractDatabaseManager)obj;
-	return databaseManager;
-	} catch(Exception e) {
-		e.printStackTrace();
-		return null;
-	}
+        String className = null;
+        try {
+            if(databaseManager != null) return databaseManager;
+            //use reflection to get the appropriate Database Manager.
+            className = getDatabaseType(props);
+            Class[] constructorArgTypes = new Class[1];
+            constructorArgTypes[0] = Properties.class;
+            Class externalClass = Class.forName(className);
+            Constructor constructor =
+                externalClass.getConstructor(constructorArgTypes);
+            Object[] constructorArgs = new Object[1];
+            constructorArgs[0] = props;
+            Object obj =
+                constructor.newInstance(constructorArgs);
+            databaseManager = (AbstractDatabaseManager)obj;
+            return databaseManager;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-
+    
     private static String getDatabaseType(Properties props) {
-	if(props == null) return "edu.sc.seis.sod.database.HSqlDbManager";
-	String rtnValue = props.getProperty("edu.sc.seis.sod.databasetype");
-	if(rtnValue != null) return rtnValue;
-	return "edu.sc.seis.sod.database.HSqlDbManager";
+        if(props == null) return "edu.sc.seis.sod.database.HSqlDbManager";
+        String rtnValue = props.getProperty("edu.sc.seis.sod.databasetype");
+        if(rtnValue != null) return rtnValue;
+        return "edu.sc.seis.sod.database.HSqlDbManager";
     }
     
     private static AbstractDatabaseManager databaseManager;
