08cd1fbeb5b96a77a5b034e11ec92bf08baeb81c
groves <none@none>
2004-02-17 17:35:09 -0500

    [svn r7187] moved from the main sod package
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/waveformArm/EventChannelSubsetter.java
@@ -0,0 +1,34 @@
+package edu.sc.seis.sod.subsetter.waveFormArm;
+import edu.sc.seis.sod.*;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+
+/**
+ * EventChannelSubsetter.java
+ *
+ *
+ * Created: Thu Dec 13 17:19:47 2001
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public interface EventChannelSubsetter  extends Subsetter {
+
+    /**
+     * Describe <code>accept</code> method here.
+     *
+     * @param event an <code>EventAccessOperations</code> value
+     * @param network a <code>NetworkAccess</code> value
+     * @param channel a <code>Channel</code> value
+     * @param cookies a <code>CookieJar</code> value
+     * @return a <code>boolean</code> value
+     * @exception Exception if an error occurs
+     */
+    public boolean accept(EventAccessOperations event,
+              NetworkAccess network,
+              Channel channel,
+              CookieJar cookies) throws Exception;
+    
+}// EventChannelSubsetter
