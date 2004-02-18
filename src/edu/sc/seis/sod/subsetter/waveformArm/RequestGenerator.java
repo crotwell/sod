0ef8ef415decc8e0c8a4c0a3bc1958ada42769eb
groves <none@none>
2004-02-18 15:03:15 -0500

    [svn r7207] moved from base sod package
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/waveformArm/RequestGenerator.java
@@ -0,0 +1,36 @@
+package edu.sc.seis.sod.subsetter.waveFormArm;
+import edu.sc.seis.sod.*;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.sc.seis.sod.subsetter.Subsetter;
+
+/**
+ * RequestGenerator.java
+ *
+ *
+ * Created: Thu Dec 13 17:25:25 2001
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public interface  RequestGenerator extends Subsetter{
+
+    /**
+     * Describe <code>generateRequest</code> method here.
+     *
+     * @param event an <code>EventAccessOperations</code> value
+     * @param network a <code>NetworkAccess</code> value
+     * @param channel a <code>Channel</code> value
+     * @param cookies a <code>CookieJar</code> value
+     * @return a <code>RequestFilter[]</code> value
+     * @exception Exception if an error occurs
+     */
+    public RequestFilter[] generateRequest(EventAccessOperations event,
+                       NetworkAccess network,
+                       Channel channel,
+                       CookieJar cookies) throws Exception;
+    
+}// RequestGenerator
