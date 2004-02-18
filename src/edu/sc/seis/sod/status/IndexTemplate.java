a8189a15fbb406abd68d880edfc12afd256cdd57
groves <none@none>
2004-02-18 13:59:16 -0500

    [svn r7198] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/status/IndexTemplate.java
@@ -0,0 +1,39 @@
+package edu.sc.seis.sod.status;
+import edu.sc.seis.sod.CommonAccess;
+import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.Start;
+import edu.sc.seis.sod.status.eventArm.MapEventStatus;
+import java.io.IOException;
+import org.w3c.dom.Element;
+
+
+
+public class IndexTemplate extends FileWritingTemplate{
+    public IndexTemplate(String dirName) throws IOException{
+        super(dirName + "/index.html");
+        try {
+            Element template = TemplateFileLoader.getTemplate(getClass().getClassLoader(),
+                                                              indexLoc);
+            parse(template);
+            write();
+            SodUtil.copyFile(cssLoc, dirName+"/main.css");
+            SodUtil.copyFile(Start.getConfigFileName(), dirName + "/" + Start.getConfigFileName());
+        } catch (Exception e) {
+            CommonAccess.handleException("unexpected problem creating index.html page", e);
+        }
+    }
+    
+    protected Object getTemplate(String tagName, Element el){
+        if(tagName.equals("eventMap")){
+            MapEventStatus mes = new MapEventStatus(el, true);
+            return new RelativeLocationTemplate(getOutputLocation(),
+                                                mes.getLocation());
+        }
+        return super.getTemplate(tagName, el);
+    }
+    
+    private static String indexLoc = "jar:edu/sc/seis/sod/data/templates/index.xml";
+    
+    private static String cssLoc = "jar:edu/sc/seis/sod/data/templates/main.css";
+}
+
