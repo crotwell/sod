0379288bb1bf3b2309cb86fa4a27d81ca9297e84
oliverpa <none@none>
2004-02-20 17:31:05 -0500

    [svn r7247] put writing to png in a runnable that gets invoked by SwingUtilities.invokeAndWait()
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveformArm/SeismogramImageProcess.java
+++ src/edu/sc/seis/sod/process/waveformArm/SeismogramImageProcess.java
@@ -15,100 +15,113 @@ import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.display.BasicSeismogramDisplay;
 import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.xml.MemoryDataSetSeismogram;
+import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.CookieJar;
-import edu.sc.seis.sod.process.waveFormArm.LocalSeismogramProcess;
 import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.process.waveFormArm.LocalSeismogramProcess;
 import edu.sc.seis.sod.status.ChannelFormatter;
 import edu.sc.seis.sod.status.EventFormatter;
 import edu.sc.seis.sod.status.StationFormatter;
 import edu.sc.seis.sod.status.TemplateFileLoader;
+import java.io.IOException;
 import java.util.Timer;
+import javax.swing.SwingUtilities;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class SeismogramImageProcess implements LocalSeismogramProcess {
-
+	
     private Logger logger = Logger.getLogger(SeismogramImageProcess.class);
     private String fileDir;
     private EventFormatter eventFormatter;
     private StationFormatter stationFormatter;
     private ChannelFormatter chanFormatter;
     private Timer t = new Timer();
-
+	
     public SeismogramImageProcess(String fileDir, EventFormatter eventDirFormatter,
-                                  StationFormatter stationDirFormatter,
-                                  ChannelFormatter imageNameFormatter){
-        this.fileDir = fileDir;
-        eventFormatter = eventDirFormatter;
-        stationFormatter = stationDirFormatter;
-        chanFormatter = imageNameFormatter;
+								  StationFormatter stationDirFormatter,
+								  ChannelFormatter imageNameFormatter){
+		this.fileDir = fileDir;
+		eventFormatter = eventDirFormatter;
+		stationFormatter = stationDirFormatter;
+		chanFormatter = imageNameFormatter;
     }
-
+	
     public SeismogramImageProcess(Element el) throws Exception{
-        NodeList nl = el.getChildNodes();
-        for (int i = 0; i < nl.getLength(); i++) {
-            Node n = nl.item(i);
-            if (n.getNodeName().equals("fileDir")){
-                fileDir = n.getFirstChild().getNodeValue();
-            }
-            else if (n.getNodeName().equals("seismogramConfig")){
-                Element seismogramImageConfig = TemplateFileLoader.getTemplate((Element)n);
-
-                Node tmpEl = SodUtil.getElement(seismogramImageConfig, "outputLocation");
-                Node tmpEl2 = SodUtil.getElement((Element)tmpEl, "eventDir");
-                eventFormatter = new EventFormatter((Element)tmpEl2);
-                tmpEl2 = SodUtil.getElement((Element)tmpEl, "stationDir");
-                stationFormatter = new StationFormatter((Element)tmpEl2);
-                seismogramImageConfig.removeChild(tmpEl);
-
-                tmpEl = SodUtil.getElement(seismogramImageConfig, "picName");
-                chanFormatter = new ChannelFormatter((Element)tmpEl);
-                seismogramImageConfig.removeChild(tmpEl);
-            }
-        }
-        if (fileDir == null || eventFormatter == null || stationFormatter == null || chanFormatter == null){
-            throw new IllegalArgumentException("The configuration element must contain a fileDir and a waveformSeismogramConfig");
-        }
+		NodeList nl = el.getChildNodes();
+		for (int i = 0; i < nl.getLength(); i++) {
+			Node n = nl.item(i);
+			if (n.getNodeName().equals("fileDir")){
+				fileDir = n.getFirstChild().getNodeValue();
+			}
+			else if (n.getNodeName().equals("seismogramConfig")){
+				Element seismogramImageConfig = TemplateFileLoader.getTemplate((Element)n);
+				
+				Node tmpEl = SodUtil.getElement(seismogramImageConfig, "outputLocation");
+				Node tmpEl2 = SodUtil.getElement((Element)tmpEl, "eventDir");
+				eventFormatter = new EventFormatter((Element)tmpEl2);
+				tmpEl2 = SodUtil.getElement((Element)tmpEl, "stationDir");
+				stationFormatter = new StationFormatter((Element)tmpEl2);
+				seismogramImageConfig.removeChild(tmpEl);
+				
+				tmpEl = SodUtil.getElement(seismogramImageConfig, "picName");
+				chanFormatter = new ChannelFormatter((Element)tmpEl);
+				seismogramImageConfig.removeChild(tmpEl);
+			}
+		}
+		if (fileDir == null || eventFormatter == null || stationFormatter == null || chanFormatter == null){
+			throw new IllegalArgumentException("The configuration element must contain a fileDir and a waveformSeismogramConfig");
+		}
     }
-
+	
     /**
-     * Processes localSeismograms, possibly modifying them.
-     *
-     * @param event an <code>EventAccessOperations</code> value
-     * @param network a <code>NetworkAccess</code> value
-     * @param channel a <code>Channel</code> value
-     * @param original a <code>RequestFilter[]</code> value
-     * @param available a <code>RequestFilter[]</code> value
-     * @param seismograms a <code>LocalSeismogram[]</code> value
-     * @param cookies a <code>CookieJar</code> value
-     * @exception Exception if an error occurs
-     */
+	 * Processes localSeismograms, possibly modifying them.
+	 *
+	 * @param event an <code>EventAccessOperations</code> value
+	 * @param network a <code>NetworkAccess</code> value
+	 * @param channel a <code>Channel</code> value
+	 * @param original a <code>RequestFilter[]</code> value
+	 * @param available a <code>RequestFilter[]</code> value
+	 * @param seismograms a <code>LocalSeismogram[]</code> value
+	 * @param cookies a <code>CookieJar</code> value
+	 * @exception Exception if an error occurs
+	 */
     public LocalSeismogram[] process(EventAccessOperations event, NetworkAccess network, Channel channel,
-                                     RequestFilter[] original, RequestFilter[] available, LocalSeismogram[] seismograms,
-                                     CookieJar cookies) throws Exception {
-        logger.debug("process() called");
-
-        BasicSeismogramDisplay bsd = new BasicSeismogramDisplay(null);
-        bsd.PRINTING = true;
-
-        MemoryDataSetSeismogram memDSS = new MemoryDataSetSeismogram(original[0], "");
-        memDSS.setBeginTime(DisplayUtils.firstBeginDate(original).getFissuresTime());
-        memDSS.setEndTime(DisplayUtils.lastEndDate(original).getFissuresTime());
-        for (int i = 0; i < seismograms.length; i++) {
-            memDSS.add((LocalSeismogramImpl)seismograms[i]);
-        }
-        bsd.add(new MemoryDataSetSeismogram[]{memDSS});
-
-        String picFileName = fileDir + '/'
-            + eventFormatter.getResult(event) + '/'
-            + stationFormatter.getResult(channel.my_site.my_station) + '/'
-            + chanFormatter.getResult(channel);
-        logger.debug("writing " + picFileName);
-        bsd.outputToPNG(picFileName);
-
-        return seismograms;
+									 RequestFilter[] original, RequestFilter[] available, LocalSeismogram[] seismograms,
+									 CookieJar cookies) throws Exception {
+		logger.debug("process() called");
+		
+		final BasicSeismogramDisplay bsd = new BasicSeismogramDisplay(null);
+		bsd.PRINTING = true;
+		
+		MemoryDataSetSeismogram memDSS = new MemoryDataSetSeismogram(original[0], "");
+		memDSS.setBeginTime(DisplayUtils.firstBeginDate(original).getFissuresTime());
+		memDSS.setEndTime(DisplayUtils.lastEndDate(original).getFissuresTime());
+		for (int i = 0; i < seismograms.length; i++) {
+			memDSS.add((LocalSeismogramImpl)seismograms[i]);
+		}
+		bsd.add(new MemoryDataSetSeismogram[]{memDSS});
+		
+		final String picFileName = fileDir + '/'
+			+ eventFormatter.getResult(event) + '/'
+			+ stationFormatter.getResult(channel.my_site.my_station) + '/'
+			+ chanFormatter.getResult(channel);
+		
+		SwingUtilities.invokeAndWait(new Runnable(){
+					public void run(){
+						logger.debug("writing " + picFileName);
+						try {
+							bsd.outputToPNG(picFileName);
+						} catch (IOException e) {
+							CommonAccess.handleException("unable to save map to "+ picFileName, e);
+						}
+					}
+				});
+		
+		
+		return seismograms;
     }
 }
 
--- src/edu/sc/seis/sod/status/waveformArm/MapWaveFormStatus.java
+++ src/edu/sc/seis/sod/status/waveformArm/MapWaveFormStatus.java
@@ -19,134 +19,138 @@ import edu.sc.seis.fissuresUtil.map.layers.EventLayer;
 import edu.sc.seis.fissuresUtil.map.layers.StationLayer;
 import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.EventChannelPair;
-import edu.sc.seis.sod.status.waveFormArm.WaveFormStatus;
-import edu.sc.seis.sod.database.Status;
 import edu.sc.seis.sod.database.waveform.EventChannelCondition;
 import edu.sc.seis.sod.status.MapPool;
+import edu.sc.seis.sod.status.waveFormArm.WaveFormStatus;
 import java.io.IOException;
+import javax.swing.SwingUtilities;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
 
 public class MapWaveFormStatus implements WaveFormStatus {
-
+	
     private String fileLoc;
     private List events = new ArrayList();
     private Map channelMap = new HashMap();
     private MapPool pool;
-
+	
     public MapWaveFormStatus(Element element) {
-        this(element.getAttribute("xlink:href"));
+		this(element.getAttribute("xlink:href"));
     }
-
+	
     public MapWaveFormStatus(String fileLoc){
-        this(fileLoc, new MapPool(1));
+		this(fileLoc, new MapPool(1));
     }
-
+	
     public MapWaveFormStatus(String fileLoc, MapPool pool){
-        this.fileLoc = fileLoc;
-        this.pool = pool;
-        write();
+		this.fileLoc = fileLoc;
+		this.pool = pool;
+		write();
     }
     private class MapWriter extends TimerTask{
-        public void run() {
-            try{
-                synchronized(channelMap){
-                    OpenMap map = pool.getMap();
-                    StationLayer sl = map.getStationLayer();
-                    sl.honorRepaint(false);
-                    Iterator it = channelMap.keySet().iterator();
-                    while(it.hasNext()){
-                        Channel cur = (Channel)it.next();
-                        sl.stationDataChanged(new StationDataEvent(this, new Station[]{cur.my_site.my_station}));
-                        EventChannelCondition status = (EventChannelCondition)channelMap.get(cur);
-                        if (status == EventChannelCondition.FAILURE ||
-                            status == EventChannelCondition.CORBA_FAILURE||
-                            status == EventChannelCondition.SUBSETTER_FAILED ||
-                           status == EventChannelCondition.NO_AVAILABLE_DATA){
-                            sl.stationAvailabiltyChanged(new AvailableStationDataEvent(this,
-                                                                                       cur.my_site.my_station,
-                                                                                       AvailableStationDataEvent.DOWN));
-                        }
-                        else if (status == EventChannelCondition.SUCCESS ||
-                                status == EventChannelCondition.SUBSETTER_PASSED){
-                            sl.stationAvailabiltyChanged(new AvailableStationDataEvent(this,
-                                                                                       cur.my_site.my_station,
-                                                                                       AvailableStationDataEvent.UP));
-                        }
-                        else{
-                            sl.stationAvailabiltyChanged(new AvailableStationDataEvent(this,
-                                                                                       cur.my_site.my_station,
-                                                                                       AvailableStationDataEvent.UNKNOWN));
-                        }
-                    }
-                    sl.honorRepaint(true);
-                    EventLayer el = map.getEventLayer();
-                    synchronized(events){
-                        it = events.iterator();
-                        while(it.hasNext()){
-                            EventAccessOperations ev = (EventAccessOperations)it.next();
-                            el.eventDataChanged(new EQDataEvent(this, new EventAccessOperations[]{ev}));
-                        }
-                    }
-
-                    try{
-                        map.writeMapToPNG(fileLoc);
-                    } catch (IOException e) {
-                        CommonAccess.handleException("unable to save map to "+fileLoc, e);
-                    }
-                    pool.returnMap(map);
-                    scheduled = new Boolean(false);
-                }
-            }catch(Throwable t){
-                CommonAccess.handleException(t, "Waveform map updater had a problem");
-            }
-        }
+		public void run() {
+			try{
+				synchronized(channelMap){
+					final OpenMap map = pool.getMap();
+					StationLayer sl = map.getStationLayer();
+					sl.honorRepaint(false);
+					Iterator it = channelMap.keySet().iterator();
+					while(it.hasNext()){
+						Channel cur = (Channel)it.next();
+						sl.stationDataChanged(new StationDataEvent(this, new Station[]{cur.my_site.my_station}));
+						EventChannelCondition status = (EventChannelCondition)channelMap.get(cur);
+						if (status == EventChannelCondition.FAILURE ||
+							status == EventChannelCondition.CORBA_FAILURE||
+							status == EventChannelCondition.SUBSETTER_FAILED ||
+							status == EventChannelCondition.NO_AVAILABLE_DATA){
+							sl.stationAvailabiltyChanged(new AvailableStationDataEvent(this,
+																					   cur.my_site.my_station,
+																					   AvailableStationDataEvent.DOWN));
+						}
+						else if (status == EventChannelCondition.SUCCESS ||
+								 status == EventChannelCondition.SUBSETTER_PASSED){
+							sl.stationAvailabiltyChanged(new AvailableStationDataEvent(this,
+																					   cur.my_site.my_station,
+																					   AvailableStationDataEvent.UP));
+						}
+						else{
+							sl.stationAvailabiltyChanged(new AvailableStationDataEvent(this,
+																					   cur.my_site.my_station,
+																					   AvailableStationDataEvent.UNKNOWN));
+						}
+					}
+					sl.honorRepaint(true);
+					EventLayer el = map.getEventLayer();
+					synchronized(events){
+						it = events.iterator();
+						while(it.hasNext()){
+							EventAccessOperations ev = (EventAccessOperations)it.next();
+							el.eventDataChanged(new EQDataEvent(this, new EventAccessOperations[]{ev}));
+						}
+					}
+					SwingUtilities.invokeAndWait(new Runnable(){
+								public void run(){
+									try{
+										map.writeMapToPNG(fileLoc);
+									} catch (IOException e) {
+										CommonAccess.handleException("unable to save map to "+fileLoc, e);
+									}
+								}
+							});
+					
+					pool.returnMap(map);
+					scheduled = new Boolean(false);
+				}
+			}catch(Throwable t){
+				CommonAccess.handleException(t, "Waveform map updater had a problem");
+			}
+		}
     }
-
+	
     private static Timer t = new Timer();
-
+	
     private Boolean scheduled = new Boolean(false);
-
+	
     public void write(){
-        synchronized(scheduled){
-            if(scheduled.equals(Boolean.FALSE)){
-                t.schedule(new MapWriter(), 120 * 1000);
-                scheduled = new Boolean(true);
-            }
-        }
+		synchronized(scheduled){
+			if(scheduled.equals(Boolean.FALSE)){
+				t.schedule(new MapWriter(), 120 * 1000);
+				scheduled = new Boolean(true);
+			}
+		}
     }
-
+	
     public void update(EventChannelPair ecp) {
-        if(add(ecp.getEvent())){
-            add(ecp.getChannel(), ecp.getStatus());
-            write();
-        }else if(add(ecp.getChannel(), ecp.getStatus()))
-            write();
+		if(add(ecp.getEvent())){
+			add(ecp.getChannel(), ecp.getStatus());
+			write();
+		}else if(add(ecp.getChannel(), ecp.getStatus()))
+			write();
     }
-
+	
     public boolean add(Channel chan, EventChannelCondition status){
-        synchronized(channelMap){
-            if(channelMap.put(chan, status) != status) return true;
-        }
-        return false;
+		synchronized(channelMap){
+			if(channelMap.put(chan, status) != status) return true;
+		}
+		return false;
     }
-
+	
     public boolean add(EventAccessOperations ev){
-        if (events.contains(ev)) return false;
-        synchronized(events){
-            events.add(ev);
-        }
-        return true;
+		if (events.contains(ev)) return false;
+		synchronized(events){
+			events.add(ev);
+		}
+		return true;
     }
-
+	
     public boolean contains(Channel chan){ return channelMap.containsKey(chan);}
-
+	
     public EventChannelCondition getStatus(Channel chan){ return (EventChannelCondition)channelMap.get(chan);}
-
+	
     public boolean contains(EventAccessOperations ev) {
-        return events.contains(ev);
+		return events.contains(ev);
     }
-
+	
     private static Logger logger = Logger.getLogger(MapWaveFormStatus.class);
 }
 
