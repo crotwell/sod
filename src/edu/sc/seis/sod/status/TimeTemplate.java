2b56d36dbbd93590ae5e59e400a89d91923e7b5c
groves <none@none>
2004-02-23 11:16:14 -0500

    [svn r7249] set timezone to be GMT and use time zone in default format
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/TimeTemplate.java
+++ src/edu/sc/seis/sod/status/TimeTemplate.java
@@ -9,46 +9,45 @@ package edu.sc.seis.sod.status;
 
 import edu.iris.Fissures.Time;
 import edu.iris.Fissures.model.MicroSecondDate;
-import edu.iris.Fissures.model.TimeInterval;
-import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import edu.sc.seis.sod.SodUtil;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 import org.w3c.dom.Element;
 
 public class TimeTemplate implements GenericTemplate {
     public TimeTemplate(Element config, boolean representTimeInFuture){
         sdf = createSDF(config);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         this.representTimeInFuture = representTimeInFuture;
     }
-    
+
     public TimeTemplate(Element config, Time time){
         this(config, true);
         setTime(time);
     }
-    
+
     public static SimpleDateFormat createSDF(Element el){
         String nestedText = SodUtil.getNestedText(el);
         if (nestedText != null) return new SimpleDateFormat(nestedText);
-        return new SimpleDateFormat();
+        return new SimpleDateFormat("MM/dd/yyyy HH:mm:ss z");
     }
-    
+
     public void setTime(Time t){ time = new MicroSecondDate(t); }
-    
+
     public String getResult(Time t){
         setTime(t);
         return getResult();
     }
-    
+
     public String getResult() {
         if(!representTimeInFuture && time.after(ClockUtil.future())) return "-";
         return sdf.format(time);
     }
-    
+
     private SimpleDateFormat sdf;
-    
+
     private MicroSecondDate time;
-    
+
     private boolean representTimeInFuture = true;
 }
-
