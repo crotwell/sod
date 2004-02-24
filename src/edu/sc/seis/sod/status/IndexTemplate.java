532fb020b73b6ee16fccdfc90be6b8581d7b8236
crotwell <none@none>
2004-02-24 15:30:43 -0500

    [svn r7263] add html exception reporter via index page
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/IndexTemplate.java
+++ src/edu/sc/seis/sod/status/IndexTemplate.java
@@ -5,6 +5,9 @@ import edu.sc.seis.sod.Start;
 import edu.sc.seis.sod.status.eventArm.MapEventStatus;
 import java.io.IOException;
 import org.w3c.dom.Element;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.fissuresUtil.exceptionHandler.HTMLReporter;
+import java.io.File;
 
 
 
@@ -12,6 +15,7 @@ public class IndexTemplate extends FileWritingTemplate{
     public IndexTemplate(String dirName) throws IOException{
         super(dirName + "/index.html");
         try {
+            initExceptionHandler();
             Element template = TemplateFileLoader.getTemplate(getClass().getClassLoader(),
                                                               indexLoc);
             parse(template);
@@ -22,7 +26,7 @@ public class IndexTemplate extends FileWritingTemplate{
             CommonAccess.handleException("unexpected problem creating index.html page", e);
         }
     }
-    
+
     protected Object getTemplate(String tagName, Element el){
         if(tagName.equals("eventMap")){
             MapEventStatus mes = new MapEventStatus(el, true);
@@ -31,9 +35,15 @@ public class IndexTemplate extends FileWritingTemplate{
         }
         return super.getTemplate(tagName, el);
     }
-    
+
+    protected void initExceptionHandler() throws IOException {
+        File errorDir = new File(getOutputDirectory(), "Errors");
+        errorDir.mkdirs();
+        GlobalExceptionHandler.add(new HTMLReporter(errorDir));
+    }
+
     private static String indexLoc = "jar:edu/sc/seis/sod/data/templates/index.xml";
-    
+
     private static String cssLoc = "jar:edu/sc/seis/sod/data/templates/main.css";
 }
 
