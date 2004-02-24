834d65da01d0a63c4e52c27cf59c12db9577230c
crotwell <none@none>
2004-02-24 13:14:35 -0500

    [svn r7258] add a try so that an individual network can have exceptions without killing all the networks. The exception is sent to the GlobalExceptionHandler
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/NetworkArm.java
+++ src/edu/sc/seis/sod/NetworkArm.java
@@ -26,6 +26,7 @@ import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 /**
  * Handles the subsetting of the Channels.
@@ -41,7 +42,7 @@ public class NetworkArm {
     public NetworkArm (Element config) throws ConfigurationException {
         processConfig(config);
     }
-    
+
     private void processConfig(Element config) throws ConfigurationException {
         networkDatabase = DatabaseManager.getDatabaseManager(Start.getProperties(), "postgres").getNetworkDatabase();
         NodeList children = config.getChildNodes();
@@ -54,10 +55,10 @@ public class NetworkArm {
             } // end of if (node instanceof Element)
         } // end of for (int i=0; i<children.getSize(); i++)
     }
-    
-    
+
+
     private static final String armName =  "networkArm";
-    
+
     void loadConfigElement(Object sodElement) throws ConfigurationException {
         if(sodElement instanceof edu.sc.seis.sod.subsetter.networkArm.NetworkFinder) {
             finder =
@@ -74,28 +75,28 @@ public class NetworkArm {
             } else {
                 throw new ConfigurationException("More than one StationSubsetter is in the configuration file: "+sodElement);
             } // end of else
-            
+
         } else if(sodElement instanceof SiteSubsetter) {
             if ( siteSubsetter instanceof NullSiteSubsetter ) {
                 siteSubsetter = (SiteSubsetter)sodElement;
             } else {
                 throw new ConfigurationException("More than one SiteSubsetter is in the configuration file: "+sodElement);
             } // end of else
-            
+
         } else if(sodElement instanceof ChannelSubsetter) {
             if ( channelSubsetter instanceof NullChannelSubsetter ) {
                 channelSubsetter = (ChannelSubsetter)sodElement;
             } else {
                 throw new ConfigurationException("More than one ChannelSubsetter is in the configuration file: "+sodElement);
             } // end of else
-            
+
         } else if(sodElement instanceof NetworkArmProcess) {
             networkArmProcesses.add(sodElement);
         }else if(sodElement instanceof NetworkStatus) {
             statusMonitors.add(sodElement);
         }
     }
-    
+
     public void processNetworkArm(NetworkAccess networkAccess,
                                   Channel channel,
                                   CookieJar cookieJar) throws Exception{
@@ -104,11 +105,11 @@ public class NetworkArm {
             ((NetworkArmProcess)it.next()).process(networkAccess, channel, cookieJar);
         }
     }
-    
+
     public synchronized  Channel getChannel(int dbid) {
         return networkDatabase.getChannel(dbid);
     }
-    
+
     public synchronized NetworkAccess getNetworkAccess(int dbid) {
         try {
             NetworkDbObject[] netDbs = getSuccessfulNetworks();
@@ -118,22 +119,22 @@ public class NetworkArm {
         } catch (Exception e) {
             logger.debug("getting networks with the cache failed, go back to the old standby", e);
         }
-        
+
         return networkDatabase.getNetworkAccess(dbid);
     }
-    
+
     public synchronized int getSiteDbId(int channelid) {
         return networkDatabase.getSiteDbId(channelid);
     }
-    
+
     public synchronized int getStationDbId(int siteid) {
         return networkDatabase.getStationDbId(siteid);
     }
-    
+
     public synchronized int getNetworkDbId(int stationid) {
         return networkDatabase.getNetworkDbId(stationid);
     }
-    
+
     /**
      * checks if the refresh interval specified in the configuration of the
      * NetworkFinder has passed since the last time networks were checked
@@ -170,7 +171,7 @@ public class NetworkArm {
         }
         return false;
     }
-    
+
     /**
      * returns an array of SuccessfulNetworks.
      * if the refreshInterval is valid it gets the networks from the database(may be embedded or external).
@@ -191,23 +192,27 @@ public class NetworkArm {
         NetworkAccess[] allNets = netDC.a_finder().retrieve_all();
         logger.debug("found " + allNets.length + " network access objects from the network DC finder");
         for(int i = 0; i < allNets.length; i++) {
-            if (allNets[i] != null) {
-                CookieJar cookieJar = new CookieJar();
-                cookieJarCache.put(allNets[i], cookieJar);
-                allNets[i] = new CacheNetworkAccess(new RetryNetworkAccess(allNets[i], 2));
-                if(attrSubsetter.accept(allNets[i].get_attributes(),cookieJar)){
-                    int dbid = networkDatabase.putNetwork(finder.getSourceName(),
-                                                          finder.getDNSName(),
-                                                          allNets[i]);
-                    networkDBs.add(new NetworkDbObject(dbid, allNets[i]));
-                }else change(allNets[i], RunStatus.FAILED);
-            } // end of if (allNets[counter] != null)
+            try {
+                if (allNets[i] != null) {
+                    CookieJar cookieJar = new CookieJar();
+                    cookieJarCache.put(allNets[i], cookieJar);
+                    allNets[i] = new CacheNetworkAccess(new RetryNetworkAccess(allNets[i], 2));
+                    if(attrSubsetter.accept(allNets[i].get_attributes(),cookieJar)){
+                        int dbid = networkDatabase.putNetwork(finder.getSourceName(),
+                                                              finder.getDNSName(),
+                                                              allNets[i]);
+                        networkDBs.add(new NetworkDbObject(dbid, allNets[i]));
+                    }else change(allNets[i], RunStatus.FAILED);
+                } // end of if (allNets[counter] != null)
+            } catch(Throwable th) {
+                GlobalExceptionHandler.handle("Got an exception while trying getSuccessfulNetworks for the "+i+"th networkAccess", th);
+            }
         }
         //Set the time of the last check to now
         networkDatabase.setTime(finder.getSourceName(),
                                 finder.getDNSName(),
                                 ClockUtil.now().getFissuresTime());
-        
+
         netDbs = new NetworkDbObject[networkDBs.size()];
         netDbs = (NetworkDbObject[]) networkDBs.toArray(netDbs);
         logger.debug("got " + netDbs.length + " networkDBobjects");
@@ -218,7 +223,7 @@ public class NetworkArm {
         }
         return netDbs;
     }
-    
+
     /**
      * Obtains the Stations corresponding to the given networkDbObject, processes them
      * using stationIdSubsetter and stationSubsetters, returns the successful stations as
@@ -248,7 +253,7 @@ public class NetworkArm {
                     change(stations[subCounter], RunStatus.FAILED);
                 }
             }
-            
+
         } catch(Exception e) {
             CommonAccess.handleException(e, "Problem in method getSuccessfulStations");
         }
@@ -261,7 +266,7 @@ public class NetworkArm {
         }
         return rtnValues;
     }
-    
+
     /**
      * Obtains the Channels corresponding to the stationDbObject, retrievesthe station from each channel
      * and processes the channels using SiteIdSubsetter and SiteSubsetter  and returns an array of
@@ -311,9 +316,9 @@ public class NetworkArm {
             change(rtnValues[i].getSite(), RunStatus.PASSED);
         }
         return rtnValues;
-        
+
     }
-    
+
     private boolean containsSite(SiteDbObject siteDbObject, ArrayList arrayList) {
         for(int counter = 0; counter < arrayList.size(); counter++) {
             SiteDbObject tempObject = (SiteDbObject) arrayList.get(counter);
@@ -321,9 +326,9 @@ public class NetworkArm {
         }
         return false;
     }
-    
-    
-    
+
+
+
     /**
      * Obtains the Channels corresponding to the siteDbObject, processes them using the
      * channelIdSubsetter and ChannelSubsetter and returns an array of succesful ChannelDbObjects.
@@ -340,11 +345,11 @@ public class NetworkArm {
         List successes = new ArrayList();
         NetworkAccess networkAccess = networkDbObject.getNetworkAccess();
         CookieJar cookieJar = (CookieJar)cookieJarCache.get(networkAccess);
-        
+
         Site site = siteDbObject.getSite();
         try {
             Channel[] channels = networkAccess.retrieve_for_station(site.my_station.get_id());
-            
+
             for(int subCounter = 0; subCounter < channels.length; subCounter++) {
                 change(channels[subCounter], RunStatus.NEW);
                 if(!isSameSite(site, channels[subCounter].my_site)){
@@ -372,7 +377,7 @@ public class NetworkArm {
         }
         return values;
     }
-    
+
     private void statusChanged(String newStatus) {
         Iterator it = statusMonitors.iterator();
         while(it.hasNext()){
@@ -384,7 +389,7 @@ public class NetworkArm {
             }
         }
     }
-    
+
     private void change(Channel chan, RunStatus newStatus) {
         Iterator it = statusMonitors.iterator();
         while(it.hasNext()){
@@ -396,7 +401,7 @@ public class NetworkArm {
             }
         }
     }
-    
+
     private void change(Station sta, RunStatus newStatus) {
         Iterator it = statusMonitors.iterator();
         while(it.hasNext()){
@@ -408,7 +413,7 @@ public class NetworkArm {
             }
         }
     }
-    
+
     private void change(NetworkAccess na, RunStatus newStatus) {
         Iterator it = statusMonitors.iterator();
         while(it.hasNext()){
@@ -420,7 +425,7 @@ public class NetworkArm {
             }
         }
     }
-    
+
     private void change(Site site, RunStatus newStatus) {
         Iterator it = statusMonitors.iterator();
         while(it.hasNext()){
@@ -432,7 +437,7 @@ public class NetworkArm {
             }
         }
     }
-    
+
     private boolean isSameSite(Site givenSite, Site tempSite) {
         SiteId givenSiteId = givenSite.get_id();
         SiteId tempSiteId = tempSite.get_id();
@@ -443,34 +448,34 @@ public class NetworkArm {
         }
         return false;
     }
-    
-    
+
+
     private edu.sc.seis.sod.subsetter.networkArm.NetworkFinder finder = null;
-    
+
     private NetworkSubsetter attrSubsetter = new NullNetworkSubsetter();
-    
+
     private StationSubsetter stationSubsetter = new NullStationSubsetter();
-    
+
     private SiteSubsetter siteSubsetter = new NullSiteSubsetter();
-    
+
     private ChannelSubsetter channelSubsetter = new NullChannelSubsetter();
-    
+
     private List networkArmProcesses = new ArrayList();
-    
+
     //Set to true the first time getSuccessfulNetworks is called
     private boolean networksBeenChecked = false;
-    
+
     private NetworkDbObject[] netDbs;
-    
+
     private NetworkDatabase networkDatabase;
-    
+
     private HashMap cookieJarCache = new HashMap();
-    
+
     private List statusMonitors = new ArrayList();
-    
+
     private String status;
-    
+
     private static Logger logger = Logger.getLogger(NetworkArm.class);
-    
+
     static Logger failure = Logger.getLogger(NetworkArm.class.getName()+".failure");
 }// NetworkArm
