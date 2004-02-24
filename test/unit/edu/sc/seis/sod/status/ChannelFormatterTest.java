25558105cc9000f815d137f1889c4ce6d03e93b7
groves <none@none>
2004-02-24 10:41:44 -0500

    [svn r7255] set time zone
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/status/ChannelFormatterTest.java
+++ test/unit/edu/sc/seis/sod/status/ChannelFormatterTest.java
@@ -7,39 +7,41 @@ import edu.iris.Fissures.model.MicroSecondDate;
 import edu.sc.seis.mockFissures.IfNetwork.MockChannel;
 import edu.sc.seis.sod.XMLConfigUtil;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 import junit.framework.TestCase;
 
 public class ChannelFormatterTest extends TestCase{
     public ChannelFormatterTest(String name){ super(name); }
-    
+
     public void setUp(){
         chan = MockChannel.createChannel();
     }
-    
+
     public void testStationCode(){
         assertEquals(chan.get_id().station_code, create("<stationCode/>").getResult(chan));
     }
-    
+
     public void testNetworkCode(){
         assertEquals(chan.get_id().network_id.network_code,
                      create("<networkCode/>").getResult(chan));
     }
-    
+
     public void testSiteCode(){
         assertEquals(chan.get_id().site_code, create("<siteCode/>").getResult(chan));
     }
-    
+
     public void testBeginTime(){
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         ChannelFormatter cf = create("<beginTime>HH:mm</beginTime>");
         assertEquals(sdf.format(new MicroSecondDate(chan.get_id().begin_time)),
                      cf.getResult(chan));
     }
-    
+
     public void testName(){
         assertEquals("Test Channel", create("<name/>").getResult(chan));
     }
-    
+
     private ChannelFormatter create(String config){
         try {
             return new ChannelFormatter(XMLConfigUtil.parse(open + config + close));
@@ -47,10 +49,10 @@ public class ChannelFormatterTest extends TestCase{
             throw new RuntimeException(e);
         }
     }
-    
+
     private String open = "<channelFormat>";
-    
+
     private String close = "</channelFormat>";
-    
+
     private Channel chan;
 }
