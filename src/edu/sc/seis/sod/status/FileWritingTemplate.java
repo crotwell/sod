f1051379044a1b4e8c7c5a8a360dbeeebdb730e4
oliverpa <none@none>
2004-03-04 12:07:52 -0500

    [svn r7405] added menu tag
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/FileWritingTemplate.java
+++ src/edu/sc/seis/sod/status/FileWritingTemplate.java
@@ -3,73 +3,88 @@ package edu.sc.seis.sod.status;
 import edu.iris.Fissures.model.MicroSecondDate;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
-import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import edu.sc.seis.sod.CommonAccess;
+import edu.sc.seis.sod.SodUtil;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.Timer;
-import java.util.TimerTask;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
 
 public class FileWritingTemplate extends Template implements GenericTemplate {
     protected FileWritingTemplate(String loc) throws IOException  {
-        this.outputLocation = testOutputLoc(loc);
+		this.outputLocation = testOutputLoc(loc);
     }
-
+	
     private static String testOutputLoc(String loc) throws IOException  {
-        File outFile = new File(loc);
-        outFile.getCanonicalFile().getParentFile().mkdirs();
-        return loc;
+		File outFile = new File(loc);
+		outFile.getCanonicalFile().getParentFile().mkdirs();
+		return loc;
     }
-
+	
     public void write(){ w.actIfPeriodElapsed(); }
-
+	
     private static final TimeInterval TWO_MINUTES = new TimeInterval(2, UnitImpl.MINUTE);
-
+	
     private MicroSecondDate lastWriteTime;
-
+	
     public String getResult() {
-        StringBuffer buf = new StringBuffer();
-        Iterator e = templates.iterator();
-        while(e.hasNext()) buf.append(((GenericTemplate)e.next()).getResult());
-        return buf.toString();
+		StringBuffer buf = new StringBuffer();
+		Iterator e = templates.iterator();
+		while(e.hasNext()) {
+			Object cur = e.next();
+			buf.append(((GenericTemplate)cur).getResult());
+		}
+		return buf.toString();
     }
-
+	
     protected Object textTemplate(final String text) {
-        return new GenericTemplate() {
-            public String getResult() { return text; }
-        };
+		return new GenericTemplate() {
+			public String getResult() { return text; }
+		};
     }
-
+	
     public String getOutputLocation(){ return outputLocation; }
-
+	
     public String getFilename()  {
-        return new File(outputLocation).getName();
+		return new File(outputLocation).getName();
     }
-
+	
     protected File getOutputDirectory() {
-        return new File(outputLocation).getParentFile();
+		return new File(outputLocation).getParentFile();
     }
-
+	
+	protected Object getTemplate(String tag, Element el){
+		if (tag.equals("menu")){
+			try {
+				return new MenuTemplate(TemplateFileLoader.getTemplate(el), getOutputLocation());
+			} catch (Exception e) {
+				CommonAccess.handleException("Problem getting template for Menu", e);
+			}
+		}
+		
+		return super.getTemplate(tag, el);
+	}
+	
     private class Writer extends PeriodicAction{
-        public void act(){
-            logger.debug("writing " + outputLocation);
-            File loc = new File(outputLocation);
-            try {
-                File temp = File.createTempFile(loc.getName(), null);
-                BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
-                writer.write(getResult());
-                writer.close();
-                loc.delete();
-                temp.renameTo(loc);
-            } catch (IOException e) {}
-        }
+		public void act(){
+			logger.debug("writing " + outputLocation);
+			File loc = new File(outputLocation);
+			try {
+				File temp = File.createTempFile(loc.getName(), null);
+				BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
+				writer.write(getResult());
+				writer.close();
+				loc.delete();
+				temp.renameTo(loc);
+			} catch (IOException e) {}
+		}
     }
-
+	
     private Writer w = new Writer();
     private String outputLocation;
     private static Logger logger = Logger.getLogger(FileWritingTemplate.class);
 }
+
