d660f1d2a01c4ac41e290b4c01313704fe97cb1c
groves <none@none>
2004-03-04 17:10:27 -0500

    [svn r7423] use map pool
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/eventArm/MapEventStatus.java
+++ src/edu/sc/seis/sod/status/eventArm/MapEventStatus.java
@@ -1,28 +1,23 @@
 package edu.sc.seis.sod.status.eventArm;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.fissuresUtil.display.EQDataEvent;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.fissuresUtil.map.OpenMap;
 import edu.sc.seis.fissuresUtil.map.layers.EventLayer;
-import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.SodElement;
 import edu.sc.seis.sod.Start;
+import edu.sc.seis.sod.database.event.ConditionEvent;
 import edu.sc.seis.sod.database.event.EventCondition;
+import edu.sc.seis.sod.database.event.JDBCEventStatus;
+import edu.sc.seis.sod.status.MapPool;
 import edu.sc.seis.sod.status.PeriodicAction;
 import edu.sc.seis.sod.status.eventArm.EventStatus;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.IOException;
+import java.sql.SQLException;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
-import java.io.IOException;
 
 public class MapEventStatus extends PeriodicAction implements SodElement, EventStatus{
-    private static List maps = new ArrayList();
-    private boolean isDuplicate = false;
-    OpenMap map = new OpenMap("edu/sc/seis/fissuresUtil/data/maps/dcwpo-browse");
-    EventLayer events;
     protected String fileLoc;
 
     public MapEventStatus(Element element){
@@ -36,44 +31,46 @@ public class MapEventStatus extends PeriodicAction implements SodElement, EventS
      * adds itself to the EventArm's status listeners
      */
     public MapEventStatus(Element element, boolean addToEventArm){
+        try {
+            events = new JDBCEventStatus();
+        } catch (SQLException e) {
+            GlobalExceptionHandler.handle("Trouble creating event status db for use in map", e);
+        }
         fileLoc = element.getAttribute("xlink:href");
-        synchronized(maps){
-            Iterator it = maps.iterator();
-            while(it.hasNext()){
-                if (((MapEventStatus)it.next()).fileLoc.equals(fileLoc)){
-                    isDuplicate = true;
-                    break;
-                }
-            }
-        }
-        if (!isDuplicate){
-            events = new EventLayer(map.getMapBean());
-            map.setEventLayer(events);
-        }
         if(addToEventArm)Start.getEventArm().add(this);
         actIfPeriodElapsed();
     }
 
     public void change(EventAccessOperations event, EventCondition status){
-        if (status == EventCondition.PROCESSOR_PASSED && !isDuplicate){
-            events.eventDataChanged(new EQDataEvent(this, new EventAccessOperations[]{event}));
-            actIfPeriodElapsed();
-        }
+        actIfPeriodElapsed();
     }
 
     public String getLocation(){ return fileLoc; }
 
-    public boolean isDuplicateForLocation(){ return isDuplicate; }
-
     public void act() {
+        OpenMap map = pool.getMap();
         try {
-            map.writeMapToPNG(fileLoc);
+            try {
+                ConditionEvent[] evs = events.getAll();
+                EventLayer el = map.getEventLayer();
+                for (int i = 0; i < evs.length; i++) {
+                    el.eventDataChanged(new EQDataEvent(this, new EventAccessOperations[]{evs[i]}));
+                }
+                map.writeMapToPNG(fileLoc);
+            } catch (SQLException e) {
+                GlobalExceptionHandler.handle(e);
+            }
         } catch (IOException e) {
             throw new RuntimeException("Trouble writing map", e);
+        }finally{
+            pool.returnMap(map);
         }
     }
 
     public void setArmStatus(String status){}// noImpl
+
+    private static MapPool pool = new MapPool(1);
+    private JDBCEventStatus events;
     private static Logger logger = Logger.getLogger(MapEventStatus.class);
 }
 
