2ff269038280fed830d727aa5ee0eaf32c748953
crotwell <none@none>
2004-03-05 12:39:29 -0500

    [svn r7435] create logger
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/waveformArm/WaveformEventTemplateGenerator.java
+++ src/edu/sc/seis/sod/status/waveformArm/WaveformEventTemplateGenerator.java
@@ -17,6 +17,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.apache.log4j.Logger;
 
 public class WaveformEventTemplateGenerator implements EventStatus, WaveFormStatus{
     public WaveformEventTemplateGenerator(Element el) throws IOException, SAXException, ParserConfigurationException {
@@ -29,11 +30,11 @@ public class WaveformEventTemplateGenerator implements EventStatus, WaveFormStat
             }
             else if(n.getNodeName().equals("waveformConfig")){
                 config = TemplateFileLoader.getTemplate((Element)n);
-                
+
                 Node tmp = SodUtil.getElement(config, "outputLocation");
                 formatter = new EventFormatter((Element)tmp);
                 config.removeChild(tmp);
-                
+
                 tmp = SodUtil.getElement(config, "filename");
                 filename = tmp.getFirstChild().getNodeValue();
                 config.removeChild(tmp);
@@ -43,32 +44,34 @@ public class WaveformEventTemplateGenerator implements EventStatus, WaveFormStat
             throw new IllegalArgumentException("The configuration element must contain a fileDir and a waveformConfig");
         }
     }
-    
+
     public void change(EventAccessOperations event, EventCondition status) throws IOException {
         getTemplate(event);
     }
-    
+
     public WaveformEventTemplate getTemplate(EventAccessOperations ev) throws IOException {
         if(!eventTemplates.containsKey(ev)){
             eventTemplates.put(ev, new WaveformEventTemplate(config, fileDir + '/' + formatter.getResult(ev) + '/' + filename, ev));
         }
         return (WaveformEventTemplate)eventTemplates.get(ev);
     }
-    
+
     public boolean contains(EventAccessOperations ev){
         return eventTemplates.containsKey(ev);
     }
-    
+
     public void setArmStatus(String status) {}
-    
+
     public void update(EventChannelPair ecp) {}
-    
-    
+
+
     private Element config;
-    
+
     private EventFormatter formatter;
-    
+
     private String fileDir, filename;
-    
+
     private Map eventTemplates = new HashMap();
+
+    private static Logger logger = Logger.getLogger(WaveformEventTemplateGenerator.class);
 }
