b63f638b4be25170b6a310d3bc198c5c760a59a6
groves <none@none>
2004-03-09 12:29:58 -0500

    [svn r7526] use new Status stuff
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/database/waveform/JDBCEventChannelStatusTest.java
+++ test/unit/edu/sc/seis/sod/database/waveform/JDBCEventChannelStatusTest.java
@@ -6,23 +6,22 @@
 
 package edu.sc.seis.sod.database.waveform;
 
-import edu.sc.seis.mockFissures.IfEvent.MockEventAccessOperations;
-import edu.sc.seis.mockFissures.IfNetwork.MockChannel;
-import junit.framework.TestCase;
+import edu.sc.seis.sod.Status;
 import java.sql.SQLException;
+import junit.framework.TestCase;
 
 public class JDBCEventChannelStatusTest extends TestCase{
     public JDBCEventChannelStatusTest(String name){ super(name); }
-    
+
     public void setUp() throws SQLException{
         evChanStatus = new JDBCEventChannelStatus();
     }
-    
+
     public void testPut() throws SQLException{
-        int id = evChanStatus.put(0,0, EventChannelCondition.NEW);
-        assertEquals(id,  evChanStatus.put(0,0,
-                                           EventChannelCondition.SUBSETTER_FAILED));
+        int id = evChanStatus.put(0,0, Status.get(Status.SPECIAL, Status.NEW));
+        assertEquals(id,  evChanStatus.put(0,0, Status.get(Status.EVENT_CHANNEL_SUBSETTER,
+                                                           Status.REJECT)));
     }
-    
+
     private JDBCEventChannelStatus evChanStatus;
 }
