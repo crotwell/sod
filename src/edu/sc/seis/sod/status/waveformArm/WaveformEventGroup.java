d2c6030b1387fb344767e02f4a5b7c00512b0330
groves <none@none>
2004-03-09 12:07:52 -0500

    [svn r7494] removed channel counter in favour of new db stuff
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/waveformArm/WaveformEventGroup.java
+++ src/edu/sc/seis/sod/status/waveformArm/WaveformEventGroup.java
@@ -2,11 +2,9 @@ package edu.sc.seis.sod.status.waveFormArm;
 
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.sod.EventChannelPair;
-import edu.sc.seis.sod.status.waveFormArm.WaveFormStatus;
-import edu.sc.seis.sod.database.Status;
-import edu.sc.seis.sod.database.waveform.EventChannelCondition;
 import edu.sc.seis.sod.status.eventArm.EventGroupTemplate;
 import edu.sc.seis.sod.status.eventArm.EventTemplate;
+import edu.sc.seis.sod.status.waveFormArm.WaveFormStatus;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -21,7 +19,6 @@ public class WaveformEventGroup extends EventGroupTemplate implements WaveFormSt
     public WaveformEventGroup(Element el) { parse(el); }
 
     public Object getTemplate(String tag, Element el) {
-        if(tag.equals("channelCount")) return new EventChannelStatus(el);
         try {
             // this is a weird template due to dependence on external an xml template
             // maybe this should be changed later...
@@ -36,54 +33,6 @@ public class WaveformEventGroup extends EventGroupTemplate implements WaveFormSt
     public void update(EventChannelPair ecp) throws Exception {
         Iterator it = ecpListeners.iterator();
         while(it.hasNext())((WaveFormStatus)it.next()).update(ecp);
-    }
-
-    public void useDefaultConfig(){
-        templates.add(new EventChannelStatus(null));
-        templates.add("\n");
-    }
-
-    private class EventChannelStatus implements EventTemplate, WaveFormStatus{
-        public EventChannelStatus(Element el){
-            ecpListeners.add(this);
-            if(el != null && el.getChildNodes().getLength() != 0){
-                NodeList nl = el.getChildNodes();
-                for (int i = 0; i < nl.getLength(); i++) {
-                    String curNode = nl.item(i).getNodeName();
-                    if(curNode.equals("SUCCESS")){
-                        monitoredStatus.add(EventChannelCondition.SUCCESS);
-                    }else if(curNode.equals("FAILURE")){
-                        monitoredStatus.add(EventChannelCondition.FAILURE);
-                    }else if(curNode.equals("NEW")){
-                        monitoredStatus.add(EventChannelCondition.NEW);
-                    }else if(curNode.equals("PROCESSING")){
-                        monitoredStatus.add(EventChannelCondition.SUBSETTER_PASSED);
-                    }
-                }
-            }else monitoredStatus.add(EventChannelCondition.SUCCESS);
-        }
-
-        public void update(EventChannelPair ecp) {
-            if(monitoredStatus.contains(ecp.getStatus())){
-                Integer cur = new Integer(0);
-                if(evChans.containsKey(ecp.getEvent())){
-                    cur = (Integer)evChans.get(ecp.getEvent());
-                }
-                cur = new Integer(cur.intValue() + 1);
-                evChans.put(ecp.getEvent(), cur);
-            }
-        }
-
-        public String getResult(EventAccessOperations ev) {
-            if(evChans.containsKey(ev))
-                return ((Integer)evChans.get(ev)).toString();
-            else
-                return "0";
-        }
-
-        private List monitoredStatus =  new ArrayList();
-
-        private Map evChans = new HashMap();
     }
 
     private List ecpListeners = new ArrayList();
