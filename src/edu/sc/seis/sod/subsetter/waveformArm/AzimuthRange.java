7202117a6b6a3b2699a9ca384b23b97a460c52be
groves <none@none>
2004-03-09 12:04:17 -0500

    [svn r7488] restructured accept method
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/waveformArm/AzimuthRange.java
+++ src/edu/sc/seis/sod/subsetter/waveformArm/AzimuthRange.java
@@ -1,18 +1,16 @@
 package edu.sc.seis.sod.subsetter.waveFormArm;
 
-import edu.sc.seis.sod.*;
-import edu.sc.seis.sod.subsetter.*;
-
-import edu.sc.seis.TauP.*;
-
-import edu.iris.Fissures.IfEvent.*;
-import edu.iris.Fissures.event.*;
-import edu.iris.Fissures.IfNetwork.*;
-import edu.iris.Fissures.network.*;
-
-import edu.iris.Fissures.*;
-
-import org.w3c.dom.*;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfEvent.Origin;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfNetwork.Station;
+import edu.iris.Fissures.Location;
+import edu.sc.seis.TauP.SphericalCoords;
+import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.subsetter.RangeSubsetter;
+import org.w3c.dom.Element;
 
 
 /**
@@ -25,43 +23,25 @@ import org.w3c.dom.*;
  *</pre>
  */
 
-
-
-
-public class AzimuthRange extends RangeSubsetter implements EventStationSubsetter {
-    /**
-     * Creates a new <code>AzimuthRange</code> instance.
-     *
-     * @param config an <code>Element</code> value
-     */
+public class AzimuthRange extends RangeSubsetter implements EventChannelSubsetter {
     public AzimuthRange (Element config) throws ConfigurationException {
         super(config);
     }
 
-    /**
-     * Describe <code>accept</code> method here.
-     *
-     * @param eventAccess an <code>EventAccessOperations</code> value
-     * @param network a <code>NetworkAccess</code> value
-     * @param station a <code>Station</code> value
-     * @param cookies a <code>CookieJar</code> value
-     * @return a <code>boolean</code> value
-     */
-    public boolean accept(EventAccessOperations eventAccess,  NetworkAccess network,Station station, CookieJar cookies) throws Exception {
+    public boolean accept(EventAccessOperations eventAccess,  NetworkAccess network,Channel chan, CookieJar cookies) throws Exception {
         float minValue = getMinValue();
         float maxValue = getMaxValue();
         if(minValue > 180) minValue = minValue - 360;
         if(maxValue > 180) maxValue = maxValue - 360;
         Origin origin = eventAccess.get_preferred_origin();
-        double azimuth = SphericalCoords.azimuth(origin.my_location.latitude,
-                                                 origin.my_location.longitude,
-                                                 station.my_location.latitude,
-                                                 station.my_location.longitude);
+        Location originLoc = origin.my_location;
+        Location loc = chan.my_site.my_location;
+        double azimuth = SphericalCoords.azimuth(originLoc.latitude,
+                                                 originLoc.longitude,
+                                                 loc.latitude,
+                                                 loc.longitude);
 
-        if(azimuth >= minValue && azimuth <= maxValue) {
-            return true;
-        } else return false;
+        if(azimuth >= minValue && azimuth <= maxValue) return true;
+        else return false;
     }
-
-
 }// AzimuthRange
