93c854c9853c4097c202baec318aad708802da59
crotwell <none@none>
2004-03-12 15:01:44 -0500

    [svn r7610] remove println
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/StatusTest.java
+++ test/unit/edu/sc/seis/sod/StatusTest.java
@@ -13,11 +13,6 @@ public class StatusTest extends TestCase {
     public void testGetFromInt() throws NoSuchFieldException{
         for (int i = 0; i < Status.ALL.length; i++) {
             for (int j = 0; j < Status.ALL[i].length; j++) {
-                System.out.println("Check status for "+i+", "+j+ " "+
-                                       Status.ALL[i][j].getAsShort() +" "
-                                       + Status.ALL[i][j].getStage().getVal()+
-                                       (Status.ALL[i][j].getStage().getVal()<<8)+
-                                       ((Status.ALL[i][j].getStage().getVal()<<8)>>>8));
                 assertEquals("Check status for "+i+", "+j,
                              Status.ALL[i][j],
                              Status.getFromShort(Status.ALL[i][j].getAsShort()));
