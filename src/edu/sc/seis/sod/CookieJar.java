8d4e6f23c537ad9fdad493dd06161b31011c896f
crotwell <none@none>
2004-03-31 17:50:17 -0500

    [svn r7957] add velocity and formatters to commoncontext
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/CookieJar.java
+++ src/edu/sc/seis/sod/CookieJar.java
@@ -2,10 +2,21 @@ package edu.sc.seis.sod;
 
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
+import edu.sc.seis.fissuresUtil.display.ParseRegions;
+import edu.sc.seis.sod.status.FissuresFormatter;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.tools.generic.DateTool;
+import org.apache.velocity.tools.generic.IteratorTool;
+import org.apache.velocity.tools.generic.MathTool;
+import org.apache.velocity.tools.generic.NumberTool;
+import org.apache.velocity.tools.generic.RenderTool;
 
 /**
- * CookieJar.java
+ * CookieJar exists as a way for various subsetters and processors in
+ * the waveform arm to pass information down the chain. It is implemented
+ * as a Velocity Context which allows the cusomization of output status
+ * pages through velocity template files. The Event and Channel are placed in
+ * the context with names "sod_event" and "sod_channel".
  *
  *
  * Created: Thu Dec 13 18:18:48 2001
@@ -17,9 +28,11 @@ import org.apache.velocity.VelocityContext;
 public class CookieJar {
 
     public CookieJar (EventAccessOperations event, Channel channel){
-        context = new VelocityContext();
+        context = new VelocityContext(commonContext);
         context.put("sod_event", event);
+        context.put("sod_eventAttr", event);
         context.put("sod_channel", channel);
+
     }
 
     /**
@@ -40,5 +53,18 @@ public class CookieJar {
     }
 
     VelocityContext context;
+
+    static VelocityContext commonContext = new VelocityContext();
+
+    static {
+        commonContext.put("FERegion", ParseRegions.getInstance());
+        commonContext.put("fissures", new FissuresFormatter());
+        commonContext.put("velocity_date", new DateTool());
+        commonContext.put("velocity_math", new MathTool());
+        commonContext.put("velocity_number", new NumberTool());
+        commonContext.put("velocity_iterator", new IteratorTool());
+        commonContext.put("velocity_render", new RenderTool());
+
+    }
 
 }// CookieJar
