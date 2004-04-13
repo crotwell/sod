fd3a985090248694368639b47bc6d8c76a6e5030
groves <none@none>
2004-04-13 17:30:01 -0400

    [svn r8136] use event formatter
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/eventArm/LastEventTemplate.java
+++ src/edu/sc/seis/sod/status/eventArm/LastEventTemplate.java
@@ -2,11 +2,20 @@ package edu.sc.seis.sod.status.eventArm;
 
 import edu.sc.seis.sod.Start;
 import edu.sc.seis.sod.status.AllTypeTemplate;
+import edu.sc.seis.sod.status.EventFormatter;
+import org.w3c.dom.Element;
+import edu.sc.seis.sod.ConfigurationException;
 
 public class LastEventTemplate extends AllTypeTemplate{
+    public LastEventTemplate(Element el) throws ConfigurationException{
+        ef = new EventFormatter(el);
+    }
+
     public String getResult() {
         if(Start.getEventArm() != null){
-            return Start.getEventArm().getLastEvent();
+            return ef.getResult(Start.getEventArm().getLastEvent());
         }else{ return "None"; }
     }
+
+    private EventFormatter ef;
 }
