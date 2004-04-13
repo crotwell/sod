614b5862bb6b3fae21d81408764c13da08106c40
groves <none@none>
2004-04-13 16:58:01 -0400

    [svn r8131] support more powerful sql
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventArm/EventSorter.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/EventSorter.java
@@ -7,9 +7,16 @@
 package edu.sc.seis.sod.subsetter.eventArm;
 
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.Stage;
+import edu.sc.seis.sod.Standing;
+import edu.sc.seis.sod.Status;
 import edu.sc.seis.sod.database.event.JDBCEventStatus;
 import edu.sc.seis.sod.database.event.StatefulEvent;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import org.w3c.dom.Element;
 
 public class EventSorter{
@@ -24,21 +31,57 @@ public class EventSorter{
         setSorting(config);
     }
 
+    private String makeExtraClause(List statii){
+        String extraClause = " and eventcondition IN (";
+        Iterator it = statii.iterator();
+        boolean first = true;
+        while(it.hasNext()){
+            if(first){ first = false; }
+            else{ extraClause += ", "; }
+            extraClause += " " + ((Status)it.next()).getAsShort();
+        }
+        extraClause += ") and eventid = origin_event_id ";
+        return extraClause;
+    }
+
     public void setSorting(Element config){
         if(config != null  && config.getChildNodes().getLength() != 0){
             Element sortType = (Element)config.getFirstChild();
+            String extraClause = "";
+            if(config.getElementsByTagName("status").getLength() > 0){
+                List statii = new ArrayList();
+                Element status = (Element)config.getElementsByTagName("status").item(0);
+                if(SodUtil.getNestedText(status).equals("SUCCESS")){
+                    statii.add( Status.get(Stage.EVENT_CHANNEL_POPULATION,
+                                           Standing.SUCCESS));
+                    statii.add(Status.get(Stage.EVENT_CHANNEL_POPULATION,
+                                          Standing.IN_PROG));
+
+                }else if(SodUtil.getNestedText(status).equals("FAILED")){
+                    statii.add(Status.get(Stage.EVENT_ORIGIN_SUBSETTER,
+                                          Standing.REJECT));
+                }else if(SodUtil.getNestedText(status).equals("IN PROGRESS")){
+                    statii.add(Status.get(Stage.EVENT_ORIGIN_SUBSETTER,
+                                          Standing.IN_PROG));
+                }
+                extraClause = makeExtraClause(statii);
+            }
             if(sortType.getNodeName().equals("time")){
-                query = "SELECT DISTINCT origin_event_id, time_stamp FROM origin, time WHERE origin_time_id = time_id ORDER BY time_stamp";
+                query = "SELECT DISTINCT origin_event_id, time_stamp, eventcondition FROM origin, time, eventstatus " +
+                    "WHERE origin_time_id = time_id" + extraClause +
+                    " ORDER BY time_stamp";
             }else if(sortType.getNodeName().equals("magnitude")){
-                query = "SELECT DISTINCT origin_event_id, magnitudevalue FROM origin, magnitude " +
+                query = "SELECT DISTINCT origin_event_id, magnitudevalue, eventcondition FROM origin, magnitude, eventstatus " +
                     "WHERE origin_id IN (SELECT origin_id FROM eventaccess) and " +
                     "magnitudevalue IN (SELECT MAX(magnitudevalue) FROM magnitude WHERE origin_id = originid) " +
-                    "ORDER BY  magnitudevalue";
+                    extraClause +
+                    " ORDER BY  magnitudevalue";
             }else if(sortType.getNodeName().equals("depth")){
-                query = "SELECT DISTINCT origin_event_id, quantity_value  FROM origin, quantity " +
+                query = "SELECT DISTINCT origin_event_id, quantity_value, eventcondition  FROM origin, quantity, eventstatus " +
                     "WHERE origin_id IN (SELECT origin_id FROM eventaccess) and " +
                     "quantity_value IN (SELECT quantity_value FROM quantity WHERE quantity_id IN (SELECT loc_depth_id FROM location WHERE origin_location_id = loc_id)) " +
-                    "ORDER BY  quantity_value";
+                    extraClause +
+                    " ORDER BY  quantity_value";
             }
             if(query != null){
                 String ordering = sortType.getAttribute("order");
