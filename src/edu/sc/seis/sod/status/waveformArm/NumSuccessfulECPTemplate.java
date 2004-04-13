8f89bf379f527a1c1158c2e1967643dfcc5db9cf
groves <none@none>
2004-04-13 16:53:20 -0400

    [svn r8125] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/status/waveformArm/NumSuccessfulECPTemplate.java
@@ -0,0 +1,29 @@
+package edu.sc.seis.sod.status.waveformArm;
+
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.sod.Stage;
+import edu.sc.seis.sod.Standing;
+import edu.sc.seis.sod.Status;
+import edu.sc.seis.sod.database.waveform.JDBCEventChannelStatus;
+import edu.sc.seis.sod.status.AllTypeTemplate;
+import java.sql.SQLException;
+import org.w3c.dom.Element;
+
+public class NumSuccessfulECPTemplate extends AllTypeTemplate{
+    public NumSuccessfulECPTemplate() throws SQLException{
+        ecs = new JDBCEventChannelStatus();
+    }
+
+    public String getResult(){
+        try {
+            return "" + ecs.getNumOfStatus(Status.get(Stage.PROCESSOR,
+                                                      Standing.SUCCESS));
+        } catch (SQLException e) {
+            GlobalExceptionHandler.handle(e);
+            return "";
+        }
+    }
+
+    private JDBCEventChannelStatus ecs;
+}
+
