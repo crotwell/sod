59588aa28f62f87b686ddf769c3693aa4341673d
groves <none@none>
2004-04-13 16:49:29 -0400

    [svn r8118] changed registerWithEventArm to performRegistration
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/IndexTemplate.java
+++ src/edu/sc/seis/sod/status/IndexTemplate.java
@@ -1,19 +1,19 @@
 package edu.sc.seis.sod.status;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.fissuresUtil.exceptionHandler.HTMLReporter;
-import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.Start;
 import edu.sc.seis.sod.status.eventArm.MapEventStatus;
+import edu.sc.seis.sod.status.waveformArm.WaveformArmMonitor;
 import java.io.File;
 import java.io.IOException;
 import org.w3c.dom.Element;
+import edu.sc.seis.sod.EventChannelPair;
 
 
 
-public class IndexTemplate extends FileWritingTemplate{
-
+public class IndexTemplate extends FileWritingTemplate implements WaveformArmMonitor{
     public IndexTemplate() throws IOException {
         this(FileWritingTemplate.getBaseDirectoryName());
     }
@@ -33,10 +33,14 @@ public class IndexTemplate extends FileWritingTemplate{
         }
     }
 
+    public void update(EventChannelPair ecp) { write(); }
+
+
     /** Exists so IndexTemplate can be created before arms, in order for exceptions
      * in initialization to be in the status pages.*/
-    public void registerMapWithEventArm() {
+    public void performRegistration() {
         Start.getEventArm().add(mapEventStatus);
+        Start.getWaveformArm().addStatusMonitor(this);
     }
 
     protected Object getTemplate(String tagName, Element el) throws ConfigurationException  {
