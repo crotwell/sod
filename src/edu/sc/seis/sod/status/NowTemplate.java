d1c007250c4613c26038b6d8ef947cef4e3e718a
groves <none@none>
2004-04-13 17:40:14 -0400

    [svn r8139] changed dateformatting
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/NowTemplate.java
+++ src/edu/sc/seis/sod/status/NowTemplate.java
@@ -4,11 +4,14 @@ package edu.sc.seis.sod.status;
 import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class NowTemplate extends AllTypeTemplate{
+    public NowTemplate(){ df.setTimeZone(TimeZone.getTimeZone("GMT")); }
+
     public String getResult() {
         return df.format(ClockUtil.now());
     }
-    
-    private DateFormat df = new SimpleDateFormat("h:mm:ss.S a M/d/yyyy");
+
+    private DateFormat df = new SimpleDateFormat("H:mm:ss.S M/d/yyyy z");
 }
--- src/edu/sc/seis/sod/status/StartTimeTemplate.java
+++ src/edu/sc/seis/sod/status/StartTimeTemplate.java
@@ -8,8 +8,15 @@ package edu.sc.seis.sod.status;
 
 
 import edu.sc.seis.sod.Start;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class StartTimeTemplate extends AllTypeTemplate{
-    public String getResult() { return Start.getStartTime().toString(); }
+    public StartTimeTemplate(){ df.setTimeZone(TimeZone.getTimeZone("GMT")); }
+
+    public String getResult() { return df.format(Start.getStartTime()); }
+
+    private DateFormat df = new SimpleDateFormat("H:mm M/d/yyyy z");
 }
 
