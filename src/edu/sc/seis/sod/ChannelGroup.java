25449adf6d9cd1d7105ce9d829a68e40641a080a
crotwell <none@none>
2004-04-16 12:44:20 -0400

    [svn r8179] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/ChannelGroup.java
@@ -0,0 +1,61 @@
+/**
+ * ChannelGroup.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.sod;
+
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.network.ChannelIdUtil;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ChannelGroup {
+
+    public ChannelGroup(Channel[] channels) {
+        this.channels = channels;
+    }
+
+    public Channel[] getChannels() {
+        return channels;
+    }
+
+
+    public static ChannelGroup[] group(Channel[] channels) {
+        // this is a placeholder implementation until we implement the
+        // full grouping xml config file idea
+
+        // first sort by band and gain, ie all but last char of channel code
+        HashMap bandGain = new HashMap();
+        for (int i = 0; i < channels.length; i++) {
+            MicroSecondDate msd = new MicroSecondDate(channels[i].get_id().begin_time);
+            // key is channel begin time + netCode.staCode.siteCode.bandgain
+            String key = ChannelIdUtil.toStringNoDates(channels[i].get_id());
+            key = key.substring(0, key.length()-1);
+            key = msd+key;
+            LinkedList chans = (LinkedList)bandGain.get(key);
+            if (chans == null) {
+                chans = new LinkedList();
+                bandGain.put(key, chans);
+            }
+            chans.add(channels[i]);
+        }
+        LinkedList out = new LinkedList();
+        Iterator it = bandGain.keySet().iterator();
+        while (it.hasNext()) {
+            LinkedList chans = (LinkedList)bandGain.get(it.next());
+            if (chans.size() == 3) {
+                out.add(new ChannelGroup((Channel[])chans.toArray(new Channel[3])));
+            }
+        }
+        return (ChannelGroup[])out.toArray(new ChannelGroup[0]);
+    }
+
+    private Channel[] channels;
+
+
+}
+
