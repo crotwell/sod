769695ccf1f33f64fa9880b878d5fe4282cb5675
crotwell <none@none>
2004-04-16 12:28:47 -0400

    [svn r8178] redo from Charlies python script generator
    --HG--
    branch : sod

--- scripts/sod.sh
+++ scripts/sod.sh
@@ -1,42 +1,52 @@
 #!/bin/sh
-MAVEN=~/.maven/repository
 
-# timeout in milliseconds, use large enough number to avoid thrashing the server
-JACORB_TIMEOUT=900000
+JAVA=java
+SOD_HOME=~/.maven
+PROP=cwg.prop
 
-JACORB_LIB=$MAVEN/jars
-JACORB=$JACORB_LIB/JacORB-2.1.jar
-JACORB_ANTLR=$JACORB_LIB/antlr-2.7.2.jar
-JACORB_AVALON=$JACORB_LIB/avalon-framework-4.1.5.jar
-JACORB_CONCURRENT=$JACORB_LIB/concurrent-1.3.2.jar
-JACORB_LOGKIT=$JACORB_LIB/logkit-1.2.jar
+REPOSITORY=${SOD_HOME}/repository
 
+JING=${REPOSITORY}/jing/jars/jing-20030619.jar
+OPENMAP=${REPOSITORY}/openmap/jars/openmap-4.6.jar
+JCALENDAR=${REPOSITORY}/jcalendar/jars/jcalendar-0.6.1beta.jar
+JACORB=${REPOSITORY}/JacORB/jars/JacORB-2.1.jar
+IDL=${REPOSITORY}/JacORB/jars/idl-2.1.jar
+ANTLR=${REPOSITORY}/JacORB/jars/antlr-2.7.2.jar
+AVALON_FRAMEWORK=${REPOSITORY}/JacORB/jars/avalon-framework-4.1.5.jar
+CONCURRENT=${REPOSITORY}/JacORB/jars/concurrent-1.3.2.jar
+LOGKIT=${REPOSITORY}/JacORB/jars/logkit-1.2.jar
+FISSURESUTIL=${REPOSITORY}/fissuresUtil/jars/fissuresUtil-1.0.7beta.jar
+FISSURESIMPL=${REPOSITORY}/fissuresImpl/jars/fissuresImpl-1.1.5beta.jar
+FISSURESIDL=${REPOSITORY}/fissuresIDL/jars/fissuresIDL-1.0.jar
+SEEDCODEC=${REPOSITORY}/SeedCodec/jars/SeedCodec-1.0Beta2.jar
+TAUP=${REPOSITORY}/TauP/jars/TauP-1.1.4.jar
+MOCKFISSURES=${REPOSITORY}/mockFissures/jars/mockFissures-0.2.jar
+JUNIT_ADDONS=${REPOSITORY}/junit-addons/jars/junit-addons-1.3.jar
+JUNIT=${REPOSITORY}/junit/jars/junit-3.8.1.jar
+MCKOI=${REPOSITORY}/mckoi/jars/mckoi-1.0.2.jar
+HSQLDB=${REPOSITORY}/hsqldb/jars/hsqldb-1.7.2-rc5.jar
+XERCES=${REPOSITORY}/xerces/jars/xerces-2.3.0.jar
+XML_APIS=${REPOSITORY}/xml-apis/jars/xml-apis-2.0.2.jar
+XALAN=${REPOSITORY}/xalan/jars/xalan-2.3.1.jar
+EASYMOCK=${REPOSITORY}/easyMock/jars/easyMock-1.0.jar
+LOG4J=${REPOSITORY}/log4j/jars/log4j-1.2.8.jar
+RNGCONV=${REPOSITORY}/rngconv/jars/rngconv-20030225.jar
+MSV=${REPOSITORY}/msv/jars/msv-20030807.jar
+ISORELAX=${REPOSITORY}/msv/jars/isorelax-20030807.jar
+RELAXNGDATATYPE=${REPOSITORY}/msv/jars/relaxngDatatype-20030807.jar
+XSDLIB=${REPOSITORY}/msv/jars/xsdlib-20030807.jar
+VELOCITY=${REPOSITORY}/velocity/jars/velocity-1.4-rc1.jar
+COMMONS_COLLECTIONS=${REPOSITORY}/commons-collections/jars/commons-collections-3.0.jar
+VELOCITY_TOOLS_GENERIC=${REPOSITORY}/velocity-tools/jars/velocity-tools-generic-1.1-rc1.jar
+SOD=${REPOSITORY}/sod/jars/sod-1.0Beta.jar
 
-SEEDCODEC=$MAVEN/jars/SeedCodec-1.0Beta2.jar
-FISSURESUTIL=$MAVEN/jars/fissuresUtil-1.0.7beta.jar
-FISSURESIMPL=$MAVEN/jars/fissuresImpl-1.1.5beta.jar
-FISSURESIDL=$MAVEN/jars/fissuresIDL-1.0.jar
-LOG4J=$MAVEN/jars/log4j-1.2.8.jar
-TAUP=$MAVEN/jars/TauP-1.1.4.jar
-XALAN=$MAVEN/jars/xalan-2.5.1.jar
-XERCES=$MAVEN/jars/xerces-2.4.0.jar
-XMLAPI=$MAVEN/apis/jars/xml-apis-1.0.b2.jar
-JAICORE=$MAVEN/jai_core.jar
-JAICODEC=$MAVEN/jai_codec.jar
-HSQLDB=$MAVEN/jars/hsqldb-20040212.jar
-OPENMAP=$MAVEN/jars/openmap-4.6.jar
-JING=$MAVEN/jars/jing-20030619.jar
-VELOCITY=$MAVEN/jars/velocity-1.4-rc1.jar
-VELOCITY_TOOLS=$MAVEN/tools/jars/velocity-tools-generic-1.1-rc1.jar
-COMMONS_COLL=$MAVEN/collections/jars/commons-collections-3.0.jar
-SOD=$MAVEN/jars/sod-1.0Beta.jar
+CLASSPATH=${JING}:${OPENMAP}:${JCALENDAR}:${JACORB}:${IDL}:${ANTLR}:${AVALON_FRAMEWORK}:${CONCURRENT}:${LOGKIT}:${FISSURESUTIL}:${FISSURESIMPL}:${FISSURESIDL}:${SEEDCODEC}:${TAUP}:${MOCKFISSURES}:${JUNIT_ADDONS}:${JUNIT}:${MCKOI}:${HSQLDB}:${XERCES}:${XML_APIS}:${XALAN}:${EASYMOCK}:${LOG4J}:${RNGCONV}:${MSV}:${ISORELAX}:${RELAXNGDATATYPE}:${XSDLIB}:${VELOCITY}:${COMMONS_COLLECTIONS}:${VELOCITY_TOOLS_GENERIC}:${SOD}:.
 
-
-java -Djava.endorsed.dirs=${JACORB_LIB}  \
-    -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB \
-    -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton \
-    -Djacorb.connection.client.pending_reply_timeout=${JACORB_TIMEOUT} \
-    -Xmx512m \
-    -cp ${JACORB}:${JACORB_ANTLR}:${JACORB_AVALON}:${JACORB_CONCURRENT}:${JACORB_LOGKIT}:${VELOCITY}:${COMMONS_COLL}:${VELOCITY_TOOLS}:${JING}:${OPENMAP}:${SEEDCODEC}:${SOD}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${CLASSPATH} \
-    edu.sc.seis.sod.Start $*
-
+${JAVA} \
+-Xmx512m \
+-Djava.endorsed.dirs=${REPOSITORY}/JacOrb/jars \
+-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB \
+-Djacorb.connection.client.pending_reply_timeout=120000 \
+-Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton \
+-cp ${CLASSPATH} edu.sc.seis.sod.Start \
+$*
