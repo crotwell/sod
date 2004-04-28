e3efd71a540fe6f07e26e924edd683d0a155645c
crotwell <none@none>
2004-04-28 13:29:16 -0400

    [svn r8317] jar versions
    --HG--
    branch : sod

--- scripts/editor.sh
+++ scripts/editor.sh
@@ -1,34 +1,66 @@
-#!/bin/sh
-MAVEN=~/.maven
+#!/bin/bash
 
-JACORB=$MAVEN/repository/JacORB/jars/JacORB-1.4.1.jar
-# for orbacus
-OB=$MAVEN/repository/OB/jars/OB-4.1.0.jar
-OBNAMING=$MAVEN/repository/OBNaming/jars/OBNaming-4.1.0.jar
+JAVA=java
+SOD_HOME=~/.maven
+PROP=cwg.prop
 
-SEEDCODEC=$MAVEN/repository/SeedCodec/jars/SeedCodec-1.0Beta.jar
-FISSURESUTIL=$MAVEN/repository/fissuresUtil/jars/fissuresUtil-1.0.2beta.jar
-FISSURESIMPL=$MAVEN/repository/fissuresImpl/jars/fissuresImpl-1.1.jar
-FISSURESIDL=$MAVEN/repository/fissuresIDL/jars/fissuresIDL-1.0.jar
-GEOTOOLS=$MAVEN/repository/Geotools1/jars/geotools1.2.jar
-LOG4J=$MAVEN/repository/log4j/jars/log4j-1.2.6.jar
-TAUP=$MAVEN/repository/TauP/jars/TauP-1.1.4.jar
-XALAN=$MAVEN/repository/xalan/jars/xalan-2.5.1.jar
-XERCES=$MAVEN/repository/xerces/jars/xerces-2.4.0.jar
-XMLAPI=$MAVEN/repository/xml-apis/jars/xml-apis-1.0.b2.jar
-JAICORE=$MAVEN/repository/jars/jai_core.jar
-JAICODEC=$MAVEN/repository/jars/jai_codec.jar
-HSQLDB=$MAVEN/repository/hsqldb/jars/hsqldb-1.7.1.jar
-JCAL=$MAVEN/repository/jcalendar/jars/jcalendar-0.6.1beta.jar
-SOD=$MAVEN/repository/sod/jars/sod-1.0Beta.jar
+REPOSITORY=${SOD_HOME}/repository
 
+JING=${REPOSITORY}/jing/jars/jing-20030619.jar
+OPENMAP=${REPOSITORY}/openmap/jars/openmap-4.6.jar
+JCALENDAR=${REPOSITORY}/jcalendar/jars/jcalendar-0.6.1beta.jar
+JACORB=${REPOSITORY}/JacORB/jars/JacORB-2.1.jar
+IDL=${REPOSITORY}/JacORB/jars/idl-2.1.jar
+ANTLR=${REPOSITORY}/JacORB/jars/antlr-2.7.2.jar
+AVALON_FRAMEWORK=${REPOSITORY}/JacORB/jars/avalon-framework-4.1.5.jar
+CONCURRENT=${REPOSITORY}/JacORB/jars/concurrent-1.3.2.jar
+LOGKIT=${REPOSITORY}/JacORB/jars/logkit-1.2.jar
+FISSURESUTIL=${REPOSITORY}/fissuresUtil/jars/fissuresUtil-1.0.7beta.jar
+FISSURESIMPL=${REPOSITORY}/fissuresImpl/jars/fissuresImpl-1.1.5beta.jar
+FISSURESIDL=${REPOSITORY}/fissuresIDL/jars/fissuresIDL-1.0.jar
+SEEDCODEC=${REPOSITORY}/SeedCodec/jars/SeedCodec-1.0Beta2.jar
+TAUP=${REPOSITORY}/TauP/jars/TauP-1.1.4.jar
+MOCKFISSURES=${REPOSITORY}/mockFissures/jars/mockFissures-0.2.jar
+JUNIT_ADDONS=${REPOSITORY}/junit-addons/jars/junit-addons-1.3.jar
+JUNIT=${REPOSITORY}/junit/jars/junit-3.8.1.jar
+MCKOI=${REPOSITORY}/mckoi/jars/mckoi-1.0.2.jar
+HSQLDB=${REPOSITORY}/hsqldb/jars/hsqldb-1.7.2-rc5.jar
+XERCES=${REPOSITORY}/xerces/jars/xerces-2.3.0.jar
+XML_APIS=${REPOSITORY}/xml-apis/jars/xml-apis-2.0.2.jar
+XALAN=${REPOSITORY}/xalan/jars/xalan-2.3.1.jar
+EASYMOCK=${REPOSITORY}/easyMock/jars/easyMock-1.0.jar
+LOG4J=${REPOSITORY}/log4j/jars/log4j-1.2.8.jar
+RNGCONV=${REPOSITORY}/rngconv/jars/rngconv-20030225.jar
+MSV=${REPOSITORY}/msv/jars/msv-20030807.jar
+ISORELAX=${REPOSITORY}/msv/jars/isorelax-20030807.jar
+RELAXNGDATATYPE=${REPOSITORY}/msv/jars/relaxngDatatype-20030807.jar
+XSDLIB=${REPOSITORY}/msv/jars/xsdlib-20030807.jar
+VELOCITY=${REPOSITORY}/velocity/jars/velocity-1.4-rc1.jar
+COMMONS_COLLECTIONS=${REPOSITORY}/commons-collections/jars/commons-collections-3.0.jar
+VELOCITY_TOOLS_GENERIC=${REPOSITORY}/velocity-tools/jars/velocity-tools-generic-1.1-rc1.jar
+SOD=${REPOSITORY}/sod/jars/sod-1.0Beta.jar
 
+CLASSPATH=${JING}:${OPENMAP}:${JCALENDAR}:${JACORB}:${IDL}:${ANTLR}:${AVALON_FRAMEWORK}:${CONCURRENT}:${LOGKIT}:${FISSURESUTIL}:${FISSURESIMPL}:${FISSURESIDL}:${SEEDCODEC}:${TAUP}:${MOCKFISSURES}:${JUNIT_ADDONS}:${JUNIT}:${MCKOI}:${HSQLDB}:${XERCES}:${XML_APIS}:${XALAN}:${EASYMOCK}:${LOG4J}:${RNGCONV}:${MSV}:${ISORELAX}:${RELAXNGDATATYPE}:${XSDLIB}:${VELOCITY}:${COMMONS_COLLECTIONS}:${VELOCITY_TOOLS_GENERIC}:${SOD}:.
+
+# editor class to run:
+#
 # command line text only
-#java -cp ${SEEDCODEC}:${SOD}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${CLASSPATH} edu.sc.seis.sod.editor.CommandLineEditor $*
+# MAINCLASS=edu.sc.seis.sod.editor.CommandLineEditor
 
 # simple gui editor
-#java -cp ${SEEDCODEC}:${SOD}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${CLASSPATH} edu.sc.seis.sod.editor.SimpleGUIEditor $*
+# MAINCLASS=edu.sc.seis.sod.editor.SimpleGUIEditor
 
 # sod gui editor
-java -cp ${SEEDCODEC}:${SOD}:${FISSURESIDL}:${FISSURESIMPL}:${FISSURESUTIL}:${XERCES}:${XMLAPI}:${XALAN}:${TAUP}:${LOG4J}:${HSQLDB}:${JCAL}:${CLASSPATH} edu.sc.seis.sod.editor.SodGUIEditor $*
+MAINCLASS=edu.sc.seis.sod.editor.SodGUIEditor
 
+echo "Running ${MAINCLASS}"
+
+${JAVA} \
+-Xmx128m \
+-Djava.endorsed.dirs=${REPOSITORY}/JacOrb/jars \
+-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB \
+-Djacorb.connection.client.pending_reply_timeout=120000 \
+-Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton \
+-cp ${CLASSPATH} ${MAINCLASS} $*
+
+echo DONE!
