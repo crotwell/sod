cdbefde899fbaf2daa402364be2f97be8bdd9472
groves <none@none>
2004-04-29 13:26:49 -0400

    [svn r8365] use UnitRangeEditor with specific capabilities
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/OriginPointDistanceEditor.java
+++ src/edu/sc/seis/sod/editor/OriginPointDistanceEditor.java
@@ -6,26 +6,18 @@
 
 package edu.sc.seis.sod.editor;
 
-import edu.sc.seis.fissuresUtil.xml.XMLUtil;
+import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.sod.SodUtil;
 import javax.swing.Box;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.border.TitledBorder;
 import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Element;
 import org.w3c.dom.Text;
 
 public class OriginPointDistanceEditor  implements EditorPlugin {
-    public OriginPointDistanceEditor(SodGUIEditor editor) {
-        this.editor = editor;
-    }
-
     public JComponent getGUI(Element element) throws Exception {
-
-
         Box latLonBox = Box.createHorizontalBox();
         latLonBox.setBorder(new TitledBorder("Center point"));
         latLonBox.add(Box.createHorizontalGlue());
@@ -42,8 +34,7 @@ public class OriginPointDistanceEditor  implements EditorPlugin {
         latLonBox.add(EditorUtil.createNumberSpinner(longitude, -90, 90, 1));
         latLonBox.add(Box.createHorizontalGlue());
 
-        EditorPlugin unitRangeEditor = editor.getCustomEditor("unitRange");
-        JComponent unitEditor = unitRangeEditor.getGUI(element);
+        JComponent unitEditor = degreeEditor.getGUI(element);
         unitEditor.setBorder(new TitledBorder("Distance from center point"));
 
         Box vBox = Box.createVerticalBox();
@@ -53,6 +44,7 @@ public class OriginPointDistanceEditor  implements EditorPlugin {
         return vBox;
     }
 
-    SodGUIEditor editor;
+    private UnitRangeEditor degreeEditor = new UnitRangeEditor(new UnitImpl[]{ UnitImpl.DEGREE });
+
 }
 
