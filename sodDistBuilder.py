b1b87a26f06371ec224936d6b4ddf5564a718c35
groves <none@none>
2004-04-30 16:11:23 -0400

    [svn r8431] added relax and tutorial stuff
    --HG--
    branch : sod

--- sodDistBuilder.py
+++ sodDistBuilder.py
@@ -7,10 +7,13 @@ def buildDist(proj, name=None):
     if not os.path.exists('scripts/logs'): os.mkdir('scripts/logs')
     extras = [('scripts/yjpagent.dll', 'yjpagent.dll'),
               ('scripts/revtest.xml', 'revtest.xml'),
+              ('scripts/tutorial.xml', 'tutorial.xml'),
+              ('scripts/weed.xml', 'weed.xml'),
               ('scripts/cwg.prop', 'cwg.prop'),
-              ('scripts/logs', 'logs', False)]
-    scripts = sodScriptBuilder.buildSodScripts(proj)
-    if name is None: name = proj.name + '-' + time.strftime('%m%d%y')
+              ('scripts/logs', 'logs', False),
+              ('relax', 'relax')]
+    scripts = sodScriptBuilder.buildAll(proj)
+    if name is None: name = proj.name + '-' + time.strftime('%y%m%d')
     distBuilder.buildDist(proj, True, extras, scripts, name)
 
 if __name__ == "__main__":
