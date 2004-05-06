9632c401e2e2903dc22d71cc7089b7a20206b202
groves <none@none>
2004-05-06 12:55:14 -0400

    [svn r8552] added element node serialization
    --HG--
    branch : sod

--- relax/buildSchema.sh
+++ relax/buildSchema.sh
@@ -2,6 +2,9 @@
 REPOSITORY=${HOME}/.maven/repository
 
 echo writing schema
-java -cp `./classpath_creator.sh -seperator : -repository ${REPOSITORY}` com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng 
+java -cp `./classpath_creator.sh -seperator \: ` com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng 
+
+echo serializing element nodes
+java -cp `./classpath_creator.sh -seperator \: ` edu.sc.seis.sod.editor.SchemaGrammar 
 
 echo Done.
