7bcfe944968d5c0bc4132523260e92b23785bb8a
groves <none@none>
2004-05-07 11:17:23 -0400

    [svn r8573] build project before building tar
    --HG--
    branch : sod

--- sodDistBuilder.py
+++ sodDistBuilder.py
@@ -1,9 +1,11 @@
+import sodBuilder
 import sys, os, time
 sys.path.append("../devTools/maven")
 sys.path.append("./scripts")
 import distBuilder, sodScriptBuilder, ProjectParser
 
 def buildDist(proj, name=None):
+    sodBuilder.build(proj)
     if not os.path.exists('scripts/logs'): os.mkdir('scripts/logs')
     extras = [('scripts/yjpagent.dll', 'yjpagent.dll'),
               ('scripts/revtest.xml', 'revtest.xml'),
