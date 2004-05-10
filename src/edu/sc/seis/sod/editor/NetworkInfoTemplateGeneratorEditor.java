f90840ccee79bef24525fdc8e4a8b60f9b1a8852
groves <none@none>
2004-05-10 10:44:10 -0400

    [svn r8592] changed centering
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/NetworkInfoTemplateGeneratorEditor.java
+++ src/edu/sc/seis/sod/editor/NetworkInfoTemplateGeneratorEditor.java
@@ -5,20 +5,20 @@
  */
 
 package edu.sc.seis.sod.editor;
-import java.awt.BorderLayout;
+import javax.swing.Box;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
 import org.w3c.dom.Element;
 
 
 
 public class NetworkInfoTemplateGeneratorEditor implements EditorPlugin {
-
     public JComponent getGUI(Element element) throws Exception {
-        return new JLabel(SimpleGUIEditor.getDisplayName(element.getTagName()));
+        Box b = Box.createHorizontalBox();
+        b.add(Box.createHorizontalStrut(10));
+        b.add(new JLabel(SimpleGUIEditor.getDisplayName(element.getTagName())));
+        b.add(Box.createHorizontalGlue());
+        return b;
     }
-
 }
 
