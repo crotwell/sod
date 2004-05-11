efbe29e696b0bb94edd06397d9b3a7f41caab3ed
crotwell <none@none>
2004-05-11 15:54:19 -0400

    [svn r8674] change to have directory and filePattern separate in ResponseWriter
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/networkArm/ResponseWriter.java
+++ src/edu/sc/seis/sod/process/networkArm/ResponseWriter.java
@@ -5,6 +5,7 @@ import edu.iris.Fissures.IfNetwork.*;
 import edu.iris.Fissures.network.ResponsePrint;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.process.networkArm.NetworkArmProcess;
 import edu.sc.seis.sod.status.ChannelFormatter;
 import java.io.File;
@@ -15,39 +16,42 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class ResponseWriter implements NetworkArmProcess {
-	
-	public ResponseWriter (Element config) throws ConfigurationException  {
-		
-		Node node = config.getElementsByTagName("responseFile").item(0);
-		cf = new ChannelFormatter((Element)node);
-		
-	}
-	
-    public ResponseWriter (NetworkDC netdc){ this(netdc.a_finder()); }
-	
-    public ResponseWriter(NetworkFinder netFinder){
-		finder = netFinder;
+
+    public ResponseWriter (Element config) throws ConfigurationException  {
+        String dirName = SodUtil.getNestedText(SodUtil.getElement(config, "directory"));
+        directory = new File(dirName);
+        directory.mkdirs();
+        cf = new ChannelFormatter(SodUtil.getElement(config, "filePattern"));
     }
-	public void process(NetworkAccess network, Channel chan) throws Exception {
-		PrintWriter printWriter = null;
-		try {
-			ChannelId channel_id = chan.get_id();
-			Instrumentation inst = network.retrieve_instrumentation(channel_id,channel_id.begin_time);
-			String response = ResponsePrint.printResponse(channel_id,inst);
-			String location = cf.getResult(chan);
-			File f = new File(location);
-			f.getParentFile().mkdirs();
-			FileOutputStream fileStream = new FileOutputStream(f);
-			printWriter = new PrintWriter(fileStream);
-			printWriter.print(response);
-			printWriter.close();
-		}catch(ChannelNotFound ex) {
-			GlobalExceptionHandler.handle("Channel not found",ex);
-		}catch(FileNotFoundException fe) {
-			GlobalExceptionHandler.handle("Error while response file",fe);
-		}
-	}
-	NetworkFinder finder;
-	ChannelFormatter cf;
-	
+
+    public ResponseWriter (NetworkDC netdc){ this(netdc.a_finder()); }
+
+    public ResponseWriter(NetworkFinder netFinder){
+        finder = netFinder;
+    }
+    public void process(NetworkAccess network, Channel chan) throws Exception {
+        PrintWriter printWriter = null;
+        try {
+            ChannelId channel_id = chan.get_id();
+            Instrumentation inst = network.retrieve_instrumentation(channel_id,channel_id.begin_time);
+            String response = ResponsePrint.printResponse(channel_id,inst);
+            String location = cf.getResult(chan);
+            File f = new File(directory, location);
+            f.getParentFile().mkdirs();
+            FileOutputStream fileStream = new FileOutputStream(f);
+            printWriter = new PrintWriter(fileStream);
+            printWriter.print(response);
+            printWriter.close();
+        }catch(ChannelNotFound ex) {
+            GlobalExceptionHandler.handle("Channel not found",ex);
+        }catch(FileNotFoundException fe) {
+            GlobalExceptionHandler.handle("Error while response file",fe);
+        }
+    }
+
+    protected NetworkFinder finder;
+
+    protected ChannelFormatter cf;
+
+    protected File directory;
 }
