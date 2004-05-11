c787a6b0dc7ac9ff436425f9c1940639339e77cb
groves <none@none>
2004-05-11 10:49:00 -0400

    [svn r8640] added internal and external versions
    --HG--
    branch : sod

--- sodDistBuilder.py
+++ sodDistBuilder.py
@@ -4,19 +4,39 @@ sys.path.append("../devTools/maven")
 sys.path.append("./scripts")
 import distBuilder, sodScriptBuilder, ProjectParser
 
-def buildDist(proj, name=None):
+def buildInternal(proj, name=None):
+    scripts=sodScriptBuilder.buildAll(proj)
+    name =  'internal' + buildName(proj)
+    extras = [('scripts/revtest.xml', 'bin/revtest.xml'),
+              ('scripts/tutorial.xml', 'bin/tutorial.xml'),
+              ('scripts/weed.xml', 'bin/weed.xml'),
+              ('scripts/yjpagent.dll', 'bin/yjpagent.dll'),
+              ('scripts/cwg.prop', 'bin/cwg.prop'),
+              ('scripts/logs', 'bin/logs', False)]
+    buildDist(proj, scripts, name, extras)
+
+def buildExternal(proj, name=None):
+    scripts=sodScriptBuilder.buildSodScripts(proj)
+    scripts.extend(sodScriptBuilder.buildEditorScripts(proj))
+    extras = [('scripts/tutorial.xml', 'docs/tutorial.xml'),
+              ('scripts/weed.xml', 'docs/weed.xml'),
+              ('site/generatedSite', 'docs')]
+    buildDist(proj, scripts, name, extras)
+
+def buildDist(proj, scripts, name=None, extras=[]):
     sodBuilder.build(proj)
     if not os.path.exists('scripts/logs'): os.mkdir('scripts/logs')
-    extras = [('scripts/yjpagent.dll', 'yjpagent.dll'),
-              ('scripts/revtest.xml', 'revtest.xml'),
-              ('scripts/tutorial.xml', 'tutorial.xml'),
-              ('scripts/weed.xml', 'weed.xml'),
-              ('scripts/cwg.prop', 'cwg.prop'),
-              ('scripts/logs', 'logs', False),
-              ('relax', 'relax')]
-    scripts = sodScriptBuilder.buildAll(proj)
-    if name is None: name = proj.name + '-' + time.strftime('%y%m%d')
-    distBuilder.buildDist(proj, True, extras, scripts, name)
+    scriptsWithTarloc = []
+    for script in scripts:
+        scriptsWithTarloc.append((script, 'bin/'+script))
+    if name is None: name = buildName(proj)
+    distBuilder.buildDist(proj, True, extras, scriptsWithTarloc, name)
+
+def buildName(proj):
+    return proj.name + '-' + time.strftime('%y%m%d')
 
 if __name__ == "__main__":
-    buildDist(ProjectParser.ProjectParser('./project.xml'))
+    sodScriptBuilder.sodScriptParameters.homeloc='../'
+    proj = ProjectParser.ProjectParser('./project.xml')
+    if len(sys.argv) > 1:  buildExternal(proj)
+    else: buildInternal(proj)
