0c46cde13fab16ef1cf1b0cfced9980d56938939
groves <none@none>
2004-05-11 11:53:10 -0400

    [svn r8646] not recursive to avoid CVS dir
    --HG--
    branch : sod

--- site/buildSite.sh
+++ site/buildSite.sh
@@ -1,5 +1,5 @@
 echo 'Generating html from xml'
 java org.apache.xalan.xslt.Process -in allPages.xml -xsl pageGenerator.xsl
 echo 'Copying over included stuff'
-cp -r include/* generatedSite
+cp include/* generatedSite
 echo 'Site now complete in generatedSite'
