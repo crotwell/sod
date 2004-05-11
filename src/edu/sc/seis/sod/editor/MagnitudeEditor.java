04b1d6a9e5e911e1861c79a5f1e9c50be04f98cd
oliverpa <none@none>
2004-05-11 14:18:14 -0400

    [svn r8660] now uses conditionals for optional components
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/MagnitudeEditor.java
+++ src/edu/sc/seis/sod/editor/MagnitudeEditor.java
@@ -23,23 +23,36 @@ public class MagnitudeEditor implements EditorPlugin{
     public JComponent getGUI(Element element) throws Exception {
         Box b = Box.createHorizontalBox();
         b.setBorder(new TitledBorder(SimpleGUIEditor.getDisplayName(element.getTagName())));
-        b.add(new JLabel(SimpleGUIEditor.getDisplayName("magType")));
+        b.add(Box.createHorizontalGlue());
+
         Node node = XPathAPI.selectSingleNode(element, "magType/text()");
-        final Text text = (Text)node;
-        final JComboBox magBox = new JComboBox(new String[]{"M", "%", "MB"});
-        magBox.setSelectedItem(text.getNodeValue());
-        b.add(magBox);
-        b.add(Box.createHorizontalGlue());
-        magBox.addActionListener(new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        text.setNodeValue(magBox.getSelectedItem().toString());
-                    }
-                });
-        b.add(new JLabel(SimpleGUIEditor.getDisplayName("min")));
-        b.add(EditorUtil.createNumberSpinner((Text)XPathAPI.selectSingleNode(element, "min/text()"), 0, 10, .1));
-        b.add(Box.createHorizontalGlue());
-        b.add(new JLabel(SimpleGUIEditor.getDisplayName("max")));
-        b.add(EditorUtil.createNumberSpinner((Text)XPathAPI.selectSingleNode(element, "max/text()"), 0, 10, .1));
+        if (node != null){
+            b.add(new JLabel(SimpleGUIEditor.getDisplayName("magType")));
+            final Text text = (Text)node;
+            final JComboBox magBox = new JComboBox(new String[]{"M", "%", "MB"});
+            magBox.setSelectedItem(text.getNodeValue());
+            b.add(magBox);
+            b.add(Box.createHorizontalGlue());
+            magBox.addActionListener(new ActionListener(){
+                        public void actionPerformed(ActionEvent e) {
+                            text.setNodeValue(magBox.getSelectedItem().toString());
+                        }
+                    });
+        }
+
+        node = XPathAPI.selectSingleNode(element, "min/text()");
+        if (node != null){
+            b.add(new JLabel(SimpleGUIEditor.getDisplayName("min")));
+            b.add(EditorUtil.createNumberSpinner((Text)node, 0, 10, .1));
+            b.add(Box.createHorizontalGlue());
+        }
+
+        node = XPathAPI.selectSingleNode(element, "max/text()");
+        if (node != null){
+            b.add(new JLabel(SimpleGUIEditor.getDisplayName("max")));
+            b.add(EditorUtil.createNumberSpinner((Text)XPathAPI.selectSingleNode(element, "max/text()"), 0, 10, .1));
+        }
+
         b.add(Box.createHorizontalGlue());
         return b;
     }
