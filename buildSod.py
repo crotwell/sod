9058b84ad15f5e22b423a9dc8943948b7dbbd0a1
groves <none@none>
2004-05-13 11:04:28 -0400

    [svn r8719] moved from sodBuilder.py
    --HG--
    branch : sod

---
+++ buildSod.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python -O
+import sys, os
+sys.path.append("../devTools/maven")
+import ProjectParser, mavenExecutor
+
+def build(sodProj):
+    curdir = os.path.abspath('.')
+    os.chdir(sodProj.path)
+    allProj = [ProjectParser.ProjectParser('../fissures/project.xml'),
+               ProjectParser.ProjectParser('../fissuresUtil/project.xml'),
+               sodProj]
+    for proj in allProj: mavenExecutor.mavenExecutor(proj).jarinst()
+    os.chdir(curdir)
+
+if __name__ == "__main__":
+    build(ProjectParser.ProjectParser())
