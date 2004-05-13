47149f1a8a6db4f95d1af0649119767fa7461514
groves <none@none>
2004-05-13 11:05:56 -0400

    [svn r8723] moved from sodScriptBuilder.py
    --HG--
    branch : sod

---
+++ scripts/buildSodScripts.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python -O
+import sys
+sys.path.append('../../devTools/maven')
+import scriptBuilder, ProjectParser, depCopy
+class sodScriptParameters(scriptBuilder.jacorbParameters):
+    homeloc = '.'
+    def __init__(self, mods):
+        scriptBuilder.jacorbParameters.__init__(self)
+        for mod in mods: self.update(mod)
+        self.name = 'sod'
+        homevar = self.add('SOD_HOME', self.homeloc, 'initial', 1)
+        libvar = self.getVar('LIB', 'initial')
+        libvar.setValue(homevar.interp+'/lib')
+        self.mainclass = 'edu.sc.seis.sod.Start'
+        self.xOptions['mx']='mx256m'
+
+class queryTimer(sodScriptParameters):
+    def __init__(self, mods):
+        sodScriptParameters.__init__(self, [])
+        for mod in mods: self.update(mod)
+        self.mainclass='edu.sc.seis.sod.QueryTimer'
+        self.name='queryTimer'
+
+class editor(sodScriptParameters):
+    def __init__(self, mods):
+        sodScriptParameters.__init__(self, [])
+        for mod in mods: self.update(mod)
+        self.mainclass='edu.sc.seis.sod.editor.SodGUIEditor'
+        self.name='editor'
+
+def buildAll(proj):
+    scripts = buildSodScripts(proj)
+    scripts.extend(buildQueryTimerScripts(proj))
+    scripts.extend(buildEditorScripts(proj))
+    scripts.extend(buildProfileScripts(proj))
+    return scripts
+
+def buildSodScripts(proj):
+    scriptBuilder.setVarSh()
+    scripts = [scriptBuilder.build(sodScriptParameters([]), proj)]
+    scriptBuilder.setVarWindows()
+    scripts.append(scriptBuilder.build(sodScriptParameters([scriptBuilder.windowsParameters()]), proj))
+    return scripts
+
+def buildProfileScripts(proj):
+    scriptBuilder.setVarWindows()
+    profileParams = sodScriptParameters([scriptBuilder.profileParameters(), scriptBuilder.windowsParameters()])
+    profileParams.name='profile'
+    return [scriptBuilder.build(profileParams, proj)]
+
+def buildQueryTimerScripts(proj):
+    scriptBuilder.setVarSh()
+    scripts = [scriptBuilder.build(queryTimer([]), proj)]
+    scriptBuilder.setVarWindows()
+    scripts.append(scriptBuilder.build(queryTimer([scriptBuilder.windowsParameters()]), proj))
+    return scripts
+
+def buildEditorScripts(proj):
+    scriptBuilder.setVarSh()
+    scripts = [scriptBuilder.build(editor([]), proj)]
+    scriptBuilder.setVarWindows()
+    scripts.append(scriptBuilder.build(editor([scriptBuilder.windowsParameters()]), proj))
+    return scripts
+
+if __name__ == "__main__":
+    proj = ProjectParser.ProjectParser('../project.xml')
+    depCopy.copy(proj)
+    buildAll(proj)
