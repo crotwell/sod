717ca099614d34c522d1b567cba9c478433e531d
groves <none@none>
2004-05-13 11:05:06 -0400

    [svn r8721] moved from sodDistBuilder.py
    --HG--
    branch : sod

---
+++ prepareSodDist.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python -O
+
+import buildSod
+import sys, os, time, zipfile, tarfile
+sys.path.append("../devTools/maven")
+sys.path.append("./scripts")
+import distBuilder, buildSodScripts, ProjectParser
+
+def buildInternal(proj, name=None):
+    scripts=buildSodScripts.buildAll(proj)
+    name =  'internal' + buildName(proj)
+    extras = [('scripts/revtest.xml', 'bin/revtest.xml'),
+              ('scripts/tutorial.xml', 'bin/tutorial.xml'),
+              ('scripts/weed.xml', 'bin/weed.xml'),
+              ('scripts/yjpagent.dll', 'bin/yjpagent.dll'),
+              ('scripts/cwg.prop', 'bin/cwg.prop'),
+              ('scripts/logs', 'bin/logs', False)]
+    buildDist(proj, scripts, name, extras)
+
+def buildExternal(proj, name=None):
+    scripts=buildSodScripts.buildSodScripts(proj)
+    scripts.extend(buildSodScripts.buildEditorScripts(proj))
+    extras = [('scripts/tutorial.xml', 'docs/tutorial.xml'),
+              ('scripts/weed.xml', 'docs/weed.xml'),
+              ('site/generatedSite', 'docs')]
+    name = buildName(proj)
+    zip = zipfile.ZipFile(name + ".zip", 'w')
+    tar = tarfile.open(name + '.tar', 'w')
+    buildDist(proj, scripts, name, extras, [tar, zip])
+
+def buildDist(proj, scripts, name=None, extras=[], archives=[]):
+    buildSod.build(proj)
+    if not os.path.exists('scripts/logs'): os.mkdir('scripts/logs')
+    scriptsWithTarloc = [(script, 'bin/'+script) for script in scripts]
+    if name is None: name = buildName(proj)
+    distBuilder.buildDist(proj, archives, True, extras, scriptsWithTarloc, name)
+
+def buildName(proj):
+    return proj.name + '-' + time.strftime('%y%m%d')
+
+if __name__ == "__main__":
+    buildSodScripts.sodScriptParameters.homeloc='../'
+    proj = ProjectParser.ProjectParser('./project.xml')
+    if len(sys.argv) > 1:  buildExternal(proj)
+    else: buildInternal(proj)
