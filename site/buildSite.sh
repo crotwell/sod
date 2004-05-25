bff38eca4f8c88e657b91ccc8fde5c96efe48012
groves <none@none>
2004-05-25 10:58:51 -0400

    [svn r8902] use new setup
    --HG--
    branch : sod

--- site/buildSite.sh
+++ site/buildSite.sh
@@ -1,5 +1,5 @@
 echo 'Generating html from xml'
-java org.apache.xalan.xslt.Process -in allPages.xml -xsl pageGenerator.xsl
+java org.apache.xalan.xslt.Process -in allPages.xml -xsl allPageGenerator.xsl
 echo 'Copying over included stuff'
 cp include/* generatedSite
 echo 'Site now complete in generatedSite'
