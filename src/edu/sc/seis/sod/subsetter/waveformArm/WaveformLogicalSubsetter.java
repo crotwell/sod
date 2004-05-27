3cf878ab92b3c98f2691dcdd5522eab72c0bd871
groves <none@none>
2004-05-27 14:21:49 -0400

    [svn r8939] fixed imports
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/waveformArm/WaveformLogicalSubsetter.java
+++ src/edu/sc/seis/sod/subsetter/waveformArm/WaveformLogicalSubsetter.java
@@ -1,14 +1,12 @@
 package edu.sc.seis.sod.subsetter.waveformArm;
-import edu.sc.seis.sod.*;
-import edu.sc.seis.sod.subsetter.*;
-
+import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.subsetter.LogicalSubsetter;
 import org.w3c.dom.Element;
 
 public class WaveformLogicalSubsetter extends LogicalSubsetter{
     public WaveformLogicalSubsetter (Element config) throws ConfigurationException{
         super(config);
     }
-    
+
     public String getArmName() { return "waveformArm"; }
-    
 }// WaveFormLogicalSubsetter
