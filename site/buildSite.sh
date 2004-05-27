55798aa88a65b0722e56b04943d0c6416da1d77b
groves <none@none>
2004-05-27 14:13:54 -0400

    [svn r8936] clear out previous build and CVS dir
    --HG--
    branch : sod

--- site/buildSite.sh
+++ site/buildSite.sh
@@ -1,5 +1,8 @@
+echo 'clearing out previous build'
+rm -r generatedSite
 echo 'Generating html from xml'
 java org.apache.xalan.xslt.Process -in allPages.xml -xsl allPageGenerator.xsl
 echo 'Copying over included stuff'
-cp include/* generatedSite
+cp -r include/* generatedSite
+rm -r generatedSite/CVS
 echo 'Site now complete in generatedSite'
