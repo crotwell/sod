47e98cb163f4f8c662bc93655dc4e7890b379dac
crotwell <none@none>
2004-05-27 22:48:36 -0400

    [svn r8973] regenerated by python
    --HG--
    branch : sod

--- scripts/sod.sh
+++ scripts/sod.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 JAVA=java
+if [ -z ${SOD_HOME} ] ; then
 SOD_HOME=.
+fi
 LIB=${SOD_HOME}/lib
 
 ISTI_UTIL=${LIB}/isti/jars/isti.util-1.0.1USC.jar
@@ -10,8 +12,8 @@ JDOM=${LIB}/jdom/jars/jdom-b9.jar
 JING=${LIB}/jing/jars/jing-20030619.jar
 OPENMAP=${LIB}/openmap/jars/openmap-4.6.jar
 JCALENDAR=${LIB}/jcalendar/jars/jcalendar-0.6.1beta.jar
-JACORB=${LIB}/JacORB/jars/JacORB-2.2.jar
-IDL=${LIB}/JacORB/jars/idl-2.2.jar
+JACORB=${LIB}/JacORB/jars/JacORB-2.1.jar
+IDL=${LIB}/JacORB/jars/idl-2.1.jar
 ANTLR=${LIB}/JacORB/jars/antlr-2.7.2.jar
 AVALON_FRAMEWORK=${LIB}/JacORB/jars/avalon-framework-4.1.5.jar
 CONCURRENT=${LIB}/JacORB/jars/concurrent-1.3.2.jar
@@ -36,12 +38,16 @@ MSV=${LIB}/msv/jars/msv-20030807.jar
 ISORELAX=${LIB}/msv/jars/isorelax-20030807.jar
 RELAXNGDATATYPE=${LIB}/msv/jars/relaxngDatatype-20030807.jar
 XSDLIB=${LIB}/msv/jars/xsdlib-20030807.jar
-VELOCITY=${LIB}/velocity/jars/velocity-1.4-rc1.jar
+VELOCITY=${LIB}/velocity/jars/velocity-1.4.jar
 COMMONS_COLLECTIONS=${LIB}/commons-collections/jars/commons-collections-3.0.jar
 VELOCITY_TOOLS_GENERIC=${LIB}/velocity-tools/jars/velocity-tools-generic-1.1-rc1.jar
+JAX_QNAME=${LIB}/stax/jars/jax-qname-1.0.jar
+JSR173_API=${LIB}/stax/jars/jsr173_api-1.0.jar
+JSR173_RI=${LIB}/stax/jars/jsr173_ri-1.0.jar
+NAMESPACE=${LIB}/stax/jars/namespace-1.0.jar
 SOD=${LIB}/sod/jars/sod-1.0Beta.jar
 
-CLASSPATH=${ISTI_UTIL}:${ISTI_UTIL_TOPLEVEL}:${JDOM}:${JING}:${OPENMAP}:${JCALENDAR}:${JACORB}:${IDL}:${ANTLR}:${AVALON_FRAMEWORK}:${CONCURRENT}:${LOGKIT}:${FISSURESUTIL}:${FISSURESIMPL}:${FISSURESIDL}:${SEEDCODEC}:${TAUP}:${MOCKFISSURES}:${JUNIT_ADDONS}:${JUNIT}:${MCKOI}:${HSQLDB}:${XERCES}:${XML_APIS}:${XALAN}:${EASYMOCK}:${LOG4J}:${RNGCONV}:${MSV}:${ISORELAX}:${RELAXNGDATATYPE}:${XSDLIB}:${VELOCITY}:${COMMONS_COLLECTIONS}:${VELOCITY_TOOLS_GENERIC}:${SOD}
+CLASSPATH=${ISTI_UTIL}:${ISTI_UTIL_TOPLEVEL}:${JDOM}:${JING}:${OPENMAP}:${JCALENDAR}:${JACORB}:${IDL}:${ANTLR}:${AVALON_FRAMEWORK}:${CONCURRENT}:${LOGKIT}:${FISSURESUTIL}:${FISSURESIMPL}:${FISSURESIDL}:${SEEDCODEC}:${TAUP}:${MOCKFISSURES}:${JUNIT_ADDONS}:${JUNIT}:${MCKOI}:${HSQLDB}:${XERCES}:${XML_APIS}:${XALAN}:${EASYMOCK}:${LOG4J}:${RNGCONV}:${MSV}:${ISORELAX}:${RELAXNGDATATYPE}:${XSDLIB}:${VELOCITY}:${COMMONS_COLLECTIONS}:${VELOCITY_TOOLS_GENERIC}:${JAX_QNAME}:${JSR173_API}:${JSR173_RI}:${NAMESPACE}:${SOD}
 
 ${JAVA} -Xmx256m \
 -Djacorb.connection.client.pending_reply_timeout=120000 \
