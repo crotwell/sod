cbdf78ae2f33903a53d3484d1350d7a7c597f006
oliverpa <none@none>
2004-05-28 11:02:39 -0400

    [svn r8977] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/database/JDBCConfig.java
@@ -0,0 +1,63 @@
+/**
+ * JDBCConfig.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.sod.database;
+
+import edu.sc.seis.fissuresUtil.database.ConnMgr;
+import edu.sc.seis.fissuresUtil.database.DBUtil;
+import edu.sc.seis.fissuresUtil.database.NotFound;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCConfig extends SodJDBC {
+
+    public JDBCConfig(String config) throws SQLException, IOException{
+        conn = ConnMgr.createConnection();
+        if (!DBUtil.tableExists("config", conn)){
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(ConnMgr.getSQL("config.create"));
+            stmt.executeUpdate("INSERT INTO config (configString) values ('"
+                                   + config
+                                   + "')");
+        }
+        getConfig = prepare("SELECT configString FROM config");
+    }
+
+    public boolean isSameConfig(String config) throws NotFound, SQLException{
+        ResultSet rs = getConfig.executeQuery();
+        if (rs.next()){
+            String val = rs.getString("configString");
+            return val.equals(config);
+        }
+        throw new NotFound("There is no config stored in the database");
+    }
+
+    public static String getConfigString(File configFile) throws IOException{
+        BufferedReader r1 = new BufferedReader(new FileReader(configFile));
+        StringBuffer buf = new StringBuffer();
+        String line;
+        while((line = r1.readLine()) != null){
+            buf.append(line);
+        }
+        r1.close();
+        return buf.toString();
+    }
+
+    private PreparedStatement prepare(String query) throws SQLException{
+        return conn.prepareStatement(query);
+    }
+
+    private Connection conn;
+    private PreparedStatement getConfig;
+}
+
