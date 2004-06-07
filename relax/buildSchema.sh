c2b9ccbdde3920cbb1e81955bd3393ea38336759
danala <none@none>
2004-06-07 14:56:54 -0400

    [svn r9099] modified to write grouper Schema
    --HG--
    branch : sod

--- relax/buildSchema.sh
+++ relax/buildSchema.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-REPOSITORY=${HOME}/.maven/repository
+REPOSITORY=${HOME}/repository
 
 echo writing schema
 java -cp `./classpath_creator.sh -seperator \: ` com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng 
+
+java -cp `./classpath_creator.sh -seperator \: ` com.sun.msv.writer.relaxng.Driver network/grouper.rng > ../src/edu/sc/seis/sod/data/grouper.rng 
 
 echo serializing element nodes
 java -cp `./classpath_creator.sh -seperator \: ` edu.sc.seis.sod.editor.SchemaGrammar 
