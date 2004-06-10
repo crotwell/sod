04d539de11e3914f5b9a3708f618975046918b1f
oliverpa <none@none>
2004-06-10 13:33:57 -0400

    [svn r9136] took out old chooser widget and replaced with a blank panel for now
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/EventStatusTemplateEditor.java
+++ src/edu/sc/seis/sod/editor/EventStatusTemplateEditor.java
@@ -25,37 +25,44 @@ import org.w3c.dom.Node;
 public class EventStatusTemplateEditor implements EditorPlugin{
 
     public EventStatusTemplateEditor(){
-        for (int i = 0; i < visibleValues.length; i++) {
-            valueMap.put(visibleValues[i], xlinkValues[i]);
-            reverseValueMap.put(xlinkValues[i], visibleValues[i]);
-        }
+//      for (int i = 0; i < visibleValues.length; i++) {
+//          valueMap.put(visibleValues[i], xlinkValues[i]);
+//          reverseValueMap.put(xlinkValues[i], visibleValues[i]);
+//      }
     }
 
     public JComponent getGUI(Element element) throws Exception {
-        Box panel = Box.createHorizontalBox();
-        panel.setBorder(new TitledBorder(SimpleGUIEditor.getDisplayName(element.getTagName())));
-        final JComboBox box = new JComboBox(visibleValues);
-        Node n = XPathAPI.selectSingleNode(element, "eventConfig/@xlink:href");
-        final Attr xlink = (Attr)n;
-        box.addActionListener(new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        Object item = box.getSelectedItem();
-                        xlink.setValue((String)valueMap.get(item));
-                    }
+        Box b = Box.createHorizontalBox();
+        b.add(Box.createHorizontalStrut(10));
+        b.add(new JLabel(SimpleGUIEditor.getDisplayName(element.getTagName())));
+        b.add(Box.createHorizontalGlue());
+        return b;
 
-                });
-        box.setSelectedItem(reverseValueMap.get(xlink.getValue()));
-        panel.add(new JLabel("Sort by:"));
-        panel.add(box);
-        panel.add(Box.createHorizontalGlue());
 
-        return panel;
+        //        Box panel = Box.createHorizontalBox();
+        //        panel.setBorder(new TitledBorder(SimpleGUIEditor.getDisplayName(element.getTagName())));
+        //        final JComboBox box = new JComboBox(visibleValues);
+        //        Node n = XPathAPI.selectSingleNode(element, "eventConfig/@xlink:href");
+        //        final Attr xlink = (Attr)n;
+        //        box.addActionListener(new ActionListener(){
+        //                    public void actionPerformed(ActionEvent e) {
+        //                        Object item = box.getSelectedItem();
+        //                        xlink.setValue((String)valueMap.get(item));
+        //                    }
+        //
+        //                });
+        //        box.setSelectedItem(reverseValueMap.get(xlink.getValue()));
+        //        panel.add(new JLabel("Sort by:"));
+        //        panel.add(box);
+        //        panel.add(Box.createHorizontalGlue());
+        //
+        //        return panel;
     }
 
-    private Map valueMap = new HashMap();
-    private Map reverseValueMap = new HashMap();
-    public static String prefix = "jar:edu/sc/seis/sod/data/templates/eventArm/";
-    protected String[] visibleValues = {"Depth", "Time", "Magnitude"};
-    protected String[] xlinkValues = {prefix + "depthSorted.xml", prefix + "timeSorted.xml", prefix + "magSorted.xml"};
+//    private Map valueMap = new HashMap();
+//    private Map reverseValueMap = new HashMap();
+//    public static String prefix = "jar:edu/sc/seis/sod/data/templates/eventArm/";
+//    protected String[] visibleValues = {"Depth", "Time", "Magnitude"};
+//    protected String[] xlinkValues = {prefix + "depthSorted.xml", prefix + "timeSorted.xml", prefix + "magSorted.xml"};
 }
 
