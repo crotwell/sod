2e6f010a2f4b60b648e6b9e0f96bc4cf8c66eaec
danala <none@none>
2004-06-10 10:54:53 -0400

    [svn r9135] Changed in accordance to the changes in Validator class which changed from static to instantiated
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/validator/ValidatorTest.java
+++ test/unit/edu/sc/seis/sod/validator/ValidatorTest.java
@@ -4,24 +4,25 @@ import junit.framework.TestCase;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import java.io.IOException;
+import edu.sc.seis.sod.validator.Validator;
 
 public class ValidatorTest extends TestCase{
     public ValidatorTest(String name){
         super(name);
-        Validator.setSchemaLoc("edu/sc/seis/sod/data/validator/simpleSchema.rng");
+		validator = new Validator("edu/sc/seis/sod/data/validator/simpleSchema.rng");
     }
     
     public void testInvalid() throws SAXException, IOException{
-        assertFalse(Validator.validate(getIS(invalid), false));
+        assertFalse(validator.validate(getIS(invalid), false));
     }
     
     public void testValid() throws SAXException, IOException{
-        assertTrue(Validator.validate(getIS(valid), false));
+        assertTrue(validator.validate(getIS(valid), false));
     }
     
     public void testMalformed() throws IOException{
         try {
-            Validator.validate(getIS(malformed), false);
+            validator.validate(getIS(malformed), false);
             assertTrue("Validate should throw a SAXException on malformed data", false);
         } catch (SAXException e) {
             assertTrue(true);
@@ -39,4 +40,6 @@ public class ValidatorTest extends TestCase{
     private String valid = "edu/sc/seis/sod/data/validator/valid.xml";
     
     private String invalid = "edu/sc/seis/sod/data/validator/invalid.xml";
+	
+	private Validator validator;
 }
