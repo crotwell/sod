b417a745515b7876923d654e61b621b5d03cb151
groves <none@none>
2004-06-16 15:50:57 -0400

    [svn r9190] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/validator/VelocityModelHelper.java
@@ -0,0 +1,39 @@
+/**
+ * VelocityModelHelper.java
+ *
+ * @author Charles Groves
+ */
+
+package edu.sc.seis.sod.validator;
+
+import edu.sc.seis.sod.validator.model.Choice;
+import edu.sc.seis.sod.validator.model.Form;
+import edu.sc.seis.sod.validator.model.Group;
+import edu.sc.seis.sod.validator.model.Interleave;
+import edu.sc.seis.sod.validator.model.MultigenitorForm;
+
+public class VelocityModelHelper {
+    public String getClass(Form f){
+        if(f == null){return "CRAP='7'"; }
+        String classAttr = "class=\"";
+        if(f.getMin() == 0){
+            if(f.getMax() == 1){ classAttr += "zeroToOne"; }
+            else{ classAttr += "zeroToMany"; }
+        }else{
+            if(f.getMax() == 1){ classAttr += "one"; }
+            else{ classAttr += "oneToMany"; }
+        }
+        if(f instanceof MultigenitorForm){
+            classAttr = appendMultigenitorClass((MultigenitorForm)f, classAttr);
+        }
+        return classAttr+"\"";
+    }
+
+    private String appendMultigenitorClass(MultigenitorForm mf, String classAttr) {
+        if(mf instanceof Choice){ classAttr += "Choice"; }
+        else if(mf instanceof Interleave){ classAttr += "Interleave"; }
+        return classAttr;
+    }
+
+    public boolean isMultigen(Form f){ return f instanceof MultigenitorForm; }
+}
