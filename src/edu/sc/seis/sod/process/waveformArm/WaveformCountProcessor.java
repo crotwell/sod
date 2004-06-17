6f0fc65ae19710d8b541d99a2bacd6c6567523e6
danala <none@none>
2004-06-17 19:50:08 -0400

    [svn r9218] Counts the number of waveforms emailed by Sod nightly build script
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/process/waveformArm/WaveformCountProcessor.java
@@ -0,0 +1,36 @@
+
+package edu.sc.seis.sod.process.waveformArm;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.Stage;
+import edu.sc.seis.sod.Standing;
+import edu.sc.seis.sod.Status;
+import edu.sc.seis.sod.database.waveform.JDBCEventChannelStatus;
+import edu.sc.seis.sod.status.StringTreeLeaf;
+import java.io.FileWriter;
+import java.sql.SQLException;
+import org.w3c.dom.Element;
+
+
+public class WaveformCountProcessor implements LocalSeismogramProcess {
+	public WaveformCountProcessor(Element config) throws SQLException {
+		jdbcEventChannelStatus= new JDBCEventChannelStatus();
+	}
+	
+	public LocalSeismogramResult process(EventAccessOperations event, Channel channel,
+										 RequestFilter[] original, RequestFilter[] available,
+										 LocalSeismogramImpl[] seismograms,
+										 CookieJar cookieJar) throws Exception {
+		 
+		int num_waveforms = jdbcEventChannelStatus.getNumOfStatus(Status.get(Stage.PROCESSOR,
+																			 Standing.SUCCESS));
+		FileWriter fileWriter = new FileWriter("waveforms.txt");
+		fileWriter.write("Number of Waveforms processed = " + num_waveforms);
+		fileWriter.close();
+		return new LocalSeismogramResult(true, seismograms, new StringTreeLeaf(this, true));
+	}
+	JDBCEventChannelStatus jdbcEventChannelStatus;
+}
