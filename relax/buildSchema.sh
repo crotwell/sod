d7205e274390ac53aca3217e7f048bfaf00d4db6
groves <none@none>
2004-06-18 15:26:44 -0400

    [svn r9229] added dire warning about commiting jazz made by this
    --HG--
    branch : sod

--- relax/buildSchema.sh
+++ relax/buildSchema.sh
@@ -9,4 +9,4 @@ java -cp `./classpath_creator.sh -seperator \: ` com.sun.msv.writer.relaxng.Driv
 echo serializing element nodes
 java -cp `./classpath_creator.sh -seperator \: ` edu.sc.seis.sod.editor.SchemaGrammar 
 
-echo Done.
+echo Done.  If you're not going to monkey with the schema any more you'll want to commit elementNode.ser, sod.rng, and grouper.rng in src/edu/sc/seis/sod/data.  If not, I'll come complain to you.  If I'm reading this message at a later date I'll want to commit elementNode.ser, sod.rng and grouper.rng in src/edu/sc/seis/sod/data so that someone else doesn't come to complain to me.  
