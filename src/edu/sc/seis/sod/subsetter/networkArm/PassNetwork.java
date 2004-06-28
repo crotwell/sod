cea67b1da26b6575ff77ac4cdfbd7ad26b8becb4
danala <none@none>
2004-06-28 12:04:26 -0400

    [svn r9371] Originally NullNetworkSubsetter
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/networkArm/PassNetwork.java
@@ -0,0 +1,21 @@
+package edu.sc.seis.sod.subsetter.networkArm;
+
+import edu.iris.Fissures.IfNetwork.NetworkAttr;
+
+/**
+ * PassNetwork.java
+ *
+ *
+ * Created: Thu Dec 13 17:09:18 2001
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class  PassNetwork implements NetworkSubsetter{
+
+    public boolean accept(NetworkAttr networkAttr) {
+        return true;
+    }
+
+}// PassNetwork
