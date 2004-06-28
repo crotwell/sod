c53bbd20f8fcc5679f9069e241e40cdf2bcd72e5
danala <none@none>
2004-06-28 12:00:08 -0400

    [svn r9367] Originally NullOriginSubsetter
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/eventArm/PassOrigin.java
@@ -0,0 +1,26 @@
+package edu.sc.seis.sod.subsetter.eventArm;
+
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfEvent.EventAttr;
+import edu.iris.Fissures.IfEvent.Origin;
+import org.w3c.dom.Element;
+
+/**
+ * PassOrigin.java
+ *
+ *
+ * Created: Thu Mar 14 14:02:33 2002
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class PassOrigin implements OriginSubsetter {
+    public PassOrigin() {}
+
+    public PassOrigin(Element config) { }
+
+    public boolean accept(EventAccessOperations event, EventAttr eventAttr, Origin e) throws Exception{
+        return true;
+    }
+}// PassOrigin
