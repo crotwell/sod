bdcadf7104e49323257e32374cbfb8f8c9245a2a
danala <none@none>
2004-06-28 12:03:52 -0400

    [svn r9370] Originally NullSiteSubsetter
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/networkArm/PassSite.java
@@ -0,0 +1,19 @@
+package edu.sc.seis.sod.subsetter.networkArm;
+
+import edu.iris.Fissures.IfNetwork.Site;
+
+/**
+ * PassSiteSubsetter.java
+ *
+ *
+ * Created: Thu Dec 13 17:09:18 2001
+ *
+ * @author <a href="mailto:">Philip Crotwell</a>
+ * @version
+ */
+
+public class  PassSite implements SiteSubsetter{
+
+    public boolean accept(Site site) { return true;}
+
+}// NullSiteSubsetter
