a1e2b118d8037615b5bdab7aac97a23279a9683c
oliverpa <none@none>
2004-07-01 15:05:59 -0400

    [svn r9419] added example stuff
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/model/Annotation.java
+++ src/edu/sc/seis/sod/validator/model/Annotation.java
@@ -11,10 +11,45 @@ public class Annotation{
 
     public void setSummary(String summary){ this.summary = summary; }
 
+    public void setExample(String example){
+        this.example = example;
+    }
+
     public String getDescription(){ return desc; }
 
     public String getSummary(){ return summary; }
 
+    public String getExample(){
+        return getExample(DEFAULT_HTMLIZE);
+    }
+
+    public String getExample(boolean htmlize){
+        if (htmlize){
+            return getHTMLizedString(example);
+        }
+        else {
+            return example;
+        }
+    }
+
+    public static String getHTMLizedString(String bracketedXML){
+        StringBuffer buf = new StringBuffer();
+        char[] chars = bracketedXML.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '<'){
+                buf.append("&lt;");
+            } else if (chars[i] == '>'){
+                buf.append("&gt;");
+            } else {
+                buf.append(chars[i]);
+            }
+        }
+        return buf.toString();
+    }
+
     private String summary, desc;
+    private String example; // = generateExample(the tag);
+
+    public static boolean DEFAULT_HTMLIZE = true;
 }
 
