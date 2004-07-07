aea8cb2d8b3d00eed3add712b9425fc32bf55473
groves <none@none>
2004-07-07 16:58:08 -0400

    [svn r9500] added the ability to write to file and now uses the event formatter instead of a class specific formatting scheme
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/eventArm/PrintLineEventProcess.java
+++ src/edu/sc/seis/sod/process/eventArm/PrintLineEventProcess.java
@@ -1,39 +1,48 @@
 package edu.sc.seis.sod.process.eventArm;
 
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
-import edu.iris.Fissures.IfEvent.Magnitude;
-import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
-import edu.sc.seis.fissuresUtil.display.EventInfoDisplay;
-import edu.sc.seis.fissuresUtil.display.ParseRegions;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.process.eventArm.EventArmProcess;
+import edu.sc.seis.sod.status.EventFormatter;
+import edu.sc.seis.sod.status.TemplateFileLoader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import javax.xml.transform.TransformerException;
+import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Element;
 
 public class PrintLineEventProcess implements EventArmProcess {
-    public PrintLineEventProcess (Element config){
-        filename = SodUtil.getNestedText(config);
+    public PrintLineEventProcess (Element config) throws TransformerException, ConfigurationException{
+        Element fileEl = (Element)XPathAPI.selectSingleNode(config, "filename");
+        if(fileEl != null){
+            filename = SodUtil.getNestedText(fileEl);
+        }
+        //Use the config from the current config file if possible, otherwise
+        //load the default
+        Element formatEl = (Element)XPathAPI.selectSingleNode(config, "eventFormat");
+        if(formatEl == null){
+            try {
+                formatEl = TemplateFileLoader.getTemplate(getClass().getClassLoader(),
+                                                          "jar:edu/sc/seis/sod/data/defaultEventPrintFormat.xml");
+
+            } catch (Exception e) {
+                GlobalExceptionHandler.handle("Trouble loading the default event print format, using EventFormatter's default",
+                                              e);
+                ef = EventFormatter.getDefaultFormatter();
+            }
+        }
+        ef = new EventFormatter(formatEl, false);
     }
 
     public void process(EventAccessOperations event) throws IOException {
-        String eventStr = regions.getRegionName(event.get_attributes().region);
-        try {
-            String magString = "";
-            Magnitude mag = EventInfoDisplay.getBestForDisplay(event.get_preferred_origin().magnitudes);
-            if (mag != null) {
-                magString = mag.type+" "+mag.value;
-            }
-            eventStr =magString+" "+eventStr;
-            eventStr+=" "+event.get_preferred_origin().origin_time.date_time;
-        } catch (NoPreferredOrigin e) {
-            eventStr+=" No Pref Origin!";
-        } // end of try-catch
+        String eventStr = ef.getResult(event);
         if (filename != null) {
-            FileWriter fwriter = new FileWriter("_my_event_temp_", true);
+            FileWriter fwriter = new FileWriter(filename, true);
             BufferedWriter bwriter = new BufferedWriter(fwriter);
-            bwriter.write(eventStr, 0, eventStr.length());
+            bwriter.write(eventStr);
             bwriter.newLine();
             bwriter.close();
         } else {
@@ -41,7 +50,6 @@ public class PrintLineEventProcess implements EventArmProcess {
         } // end of else
     }
 
-    protected String filename = null;
-    protected static ParseRegions regions = ParseRegions.getInstance();
-
+    private EventFormatter ef;
+    private String filename = null;
 }// PrintLineEventProcess
