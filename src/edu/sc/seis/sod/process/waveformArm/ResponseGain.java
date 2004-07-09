4c55e0fe01a2c43a40f4657c4e8349a9fd688ad4
groves <none@none>
2004-07-09 12:14:01 -0400

    [svn r9541] get INstrumentation here to pass to ResponseGain.
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveformArm/ResponseGain.java
+++ src/edu/sc/seis/sod/process/waveformArm/ResponseGain.java
@@ -2,6 +2,8 @@ package edu.sc.seis.sod.process.waveformArm;
 
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.Instrumentation;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.sod.CookieJar;
@@ -17,15 +19,10 @@ import org.w3c.dom.Element;
  * Created: Wed Nov  6 17:58:10 2002
  *
  * @author <a href="mailto:www@seis.sc.edu">Philip Crotwell</a>
- * @version $Id: ResponseGain.java 9089 2004-06-07 15:44:11Z crotwell $
+ * @version $Id: ResponseGain.java 9541 2004-07-09 16:14:01Z groves $
  */
 
 public class ResponseGain implements LocalSeismogramProcess {
-
-    public ResponseGain (Element config) {
-        responseGain = new edu.sc.seis.fissuresUtil.bag.ResponseGain(Start.getNetworkArm().getFinder());
-    }
-
     public LocalSeismogramResult process(EventAccessOperations event,
                                          Channel channel,
                                          RequestFilter[] original,
@@ -34,12 +31,15 @@ public class ResponseGain implements LocalSeismogramProcess {
                                          CookieJar cookieJar)
         throws Exception {
         LocalSeismogramImpl[] out = new LocalSeismogramImpl[seismograms.length];
-        for (int i=0; i<seismograms.length; i++) {
-            out[i] = responseGain.apply(seismograms[i]);
-        } // end of for (int i=0; i<seismograms.length; i++)
-        return new LocalSeismogramResult(true, out, new StringTreeLeaf(this, true));
+        NetworkAccess na  = Start.getNetworkArm().getNetwork(channel.get_id().network_id);
+        if(seismograms.length > 0){
+            Instrumentation inst = na.retrieve_instrumentation(channel.get_id(), seismograms[0].begin_time);
+            for (int i=0; i<seismograms.length; i++) {
+                out[i] = edu.sc.seis.fissuresUtil.bag.ResponseGain.apply(seismograms[i], inst);
+            } // end of for (int i=0; i<seismograms.length; i++)
+            return new LocalSeismogramResult(true, out, new StringTreeLeaf(this, true));
+        }else{
+            return new LocalSeismogramResult(true, seismograms);
+        }
     }
-
-    edu.sc.seis.fissuresUtil.bag.ResponseGain responseGain;
-
 }// ResponseGainProcessor
