3908e1c97be6579e88ef7d97c47d98825aea8fa0
oliverpa <none@none>
2004-07-12 16:17:09 -0400

    [svn r9561] examplebuilder moved from Model. initial for ExampleValidator
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/validator/example/ExampleValidator.java
@@ -0,0 +1,16 @@
+/**
+ * ExampleValidator.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.sod.validator.example;
+
+public class ExampleValidator {
+
+    public static void validate(){
+
+    }
+
+}
+
