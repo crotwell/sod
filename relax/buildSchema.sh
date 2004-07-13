a74f940fbb54fd293b463117502f6648602dcbb5
groves <none@none>
2004-07-13 12:16:06 -0400

    [svn r9567] use python scripts
    --HG--
    branch : sod

--- relax/buildSchema.sh
+++ relax/buildSchema.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-REPOSITORY=${HOME}/.maven/repository
-
-echo CLASSPATH is...
-echo `./classpath_creator.sh -seperator \: `
-
-echo writing schema
-java -cp `./classpath_creator.sh -seperator \: ` com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng 
-
-java -cp `./classpath_creator.sh -seperator \: ` com.sun.msv.writer.relaxng.Driver network/grouper.rng > ../src/edu/sc/seis/sod/data/grouper.rng 
-
-echo serializing element nodes
-java -cp `./classpath_creator.sh -seperator \: ` edu.sc.seis.sod.editor.SchemaGrammar 
-
+python schemaBuilderScriptBuilder.py
+sh bigSchemaBuilder.sh
 echo Done.  If you're not going to monkey with the schema any more you'll want to commit elementNode.ser, sod.rng, and grouper.rng in src/edu/sc/seis/sod/data.  If not, I'll come complain to you.  If I'm reading this message at a later date I'll want to commit elementNode.ser, sod.rng and grouper.rng in src/edu/sc/seis/sod/data so that someone else doesn't come to complain to me.  
