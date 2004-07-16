599ce9009f246879cba530a7f849c559a54dfecd
oliverpa <none@none>
2004-07-16 15:20:59 -0400

    [svn r9628] extend Subsetter
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/waveformArm/EventChannelGroupSubsetter.java
+++ src/edu/sc/seis/sod/subsetter/waveformArm/EventChannelGroupSubsetter.java
@@ -9,8 +9,9 @@ package edu.sc.seis.sod.subsetter.waveformArm;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.sod.ChannelGroup;
 import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.subsetter.Subsetter;
 
-public interface EventChannelGroupSubsetter {
+public interface EventChannelGroupSubsetter extends Subsetter {
 
     public boolean accept(EventAccessOperations event,
                           ChannelGroup channel, CookieJar cookieJar) throws Exception;
