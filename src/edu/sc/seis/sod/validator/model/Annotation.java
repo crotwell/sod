dd5e5c06ba8004e096bba185d7513a6a730a6d96
groves <none@none>
2004-07-16 03:22:33 -0400

    [svn r9598] removed printlns
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/model/Annotation.java
+++ src/edu/sc/seis/sod/validator/model/Annotation.java
@@ -1,75 +1,98 @@
 /**
  * Annotation.java
- *
+ * 
  * @author Charles Groves
  */
 
 package edu.sc.seis.sod.validator.model;
 import edu.sc.seis.sod.validator.example.*;
 
-import edu.sc.seis.sod.validator.ModelWalker;
+public class Annotation {
+    public void setDescription(String description) {
+        desc = description;
+    }
 
-public class Annotation{
-    public void setDescription(String description){ desc = description; }
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 
-    public void setSummary(String summary){ this.summary = summary; }
-
-    public void setExample(String example){
+    public void setExample(String example) {
         this.example = example;
     }
 
-    public String getDescription(){ return desc; }
+    public String getDescription() {
+        return desc;
+    }
 
-    public String getSummary(){ return summary; }
+    public String getSummary() {
+        return summary;
+    }
 
-    public String getExample(){
+    public String getExample() {
         //System.out.println("getting example");
         return getExample(DEFAULT_HTMLIZE);
     }
 
-    public void setFormProvider(FormProvider fp){
+    public void setFormProvider(FormProvider fp) {
         formProvider = fp;
     }
 
-    public FormProvider getFormProvider(){
+    public FormProvider getFormProvider() {
         return formProvider;
     }
 
-    public String getExample(boolean htmlize){
-        //System.out.println("getExample(" + htmlize + ")");
-        if (example.equals("") || example == null){
+    public String getExample(boolean htmlize) {
+        if (example.equals("") || example == null) {
             ExampleBuilder eb = new ExampleBuilder(false);
-            //System.out.println("created new ExampleBuilder");
             eb.write(formProvider.getForm());
-            //System.out.println("wrote example");
             example = eb.toString();
-            //System.out.println("got example");
-            //System.out.println(example);
         }
-        if (htmlize){
-            //System.out.println("htmlize");
-            String string = getHTMLizedString(example);
-            //System.out.println("htmlized existed example");
-            System.out.println(string);
-            return string;
+        if (htmlize) {
+            return getHTMLizedString(example);
         }
-        //System.out.println("blah");
         return example;
     }
 
-    public boolean hasExample(){
+    public boolean hasExample() {
         return !(example.equals("") || example == null);
     }
 
-    public void setInclude(boolean include){
+    public void setInclude(boolean include) {
         this.include = include;
     }
 
-    public boolean getInclude(){
+    public boolean getInclude() {
         return include;
     }
 
-    public static String getHTMLizedString(String bracketedXML){
+    public String getContained() {
+        StringBuffer buf = new StringBuffer();
+        getContained(getFormProvider().getForm(), buf);
+        return buf.toString();
+    }
+
+    private void getContained(Form containedRoot, StringBuffer buf) {
+        buf.append("<div>\n");
+        Form curForm = getFormProvider().getForm();
+        String countString = "";
+        if (curForm.getMin() == 0) {
+            if (curForm.getMax() > 1) {
+                countString = "";
+            } else {
+
+            }
+        } else {
+
+        }
+        if (curForm instanceof Choice) {
+            buf.append("<p>A choice between</p>");
+        } else {
+
+        }
+        buf.append("</div>\n");
+    }
+
+    public static String getHTMLizedString(String bracketedXML) {
         StringBuffer buf = new StringBuffer(bracketedXML);
         String newString = replaceAllInstances(buf.toString(), "<", "&lt;");
         buf = new StringBuffer(newString);
@@ -77,10 +100,11 @@ public class Annotation{
         return newString;
     }
 
-    public static String replaceAllInstances(String string, String orig, String replacement){
+    public static String replaceAllInstances(String string, String orig,
+            String replacement) {
         StringBuffer buf = new StringBuffer(string);
         int repIndex = buf.indexOf(orig);
-        while (repIndex != -1){
+        while (repIndex != -1) {
             buf.replace(repIndex, repIndex + 1, replacement);
             repIndex = buf.indexOf(orig);
         }
@@ -88,8 +112,11 @@ public class Annotation{
     }
 
     private String summary, desc;
+
     private String example = "";
+
     private FormProvider formProvider;
+
     private boolean include = false;
 
     public static boolean DEFAULT_HTMLIZE = true;
