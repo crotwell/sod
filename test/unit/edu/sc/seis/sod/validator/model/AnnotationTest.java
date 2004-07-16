29e4ea5b5b675f46da775b7d7f61d78af9fc5eab
groves <none@none>
2004-07-16 03:31:27 -0400

    [svn r9606] new annotation location tests
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/validator/model/AnnotationTest.java
+++ test/unit/edu/sc/seis/sod/validator/model/AnnotationTest.java
@@ -5,17 +5,40 @@
  */
 
 package edu.sc.seis.sod.validator.model;
-import edu.sc.seis.sod.validator.model.*;
-
 import java.io.IOException;
 import javax.xml.stream.XMLStreamException;
 import junit.framework.TestCase;
 
 public class AnnotationTest extends TestCase{
-    public void testMostBasic() throws IOException, XMLStreamException{
+    public void testAnnotationAssignment() throws IOException, XMLStreamException{
         StAXModelBuilder modBuild = new StAXModelBuilder("jar:edu/sc/seis/sod/data/validator/annotation.rng");
-        modBuild.getRoot().accept(new FormPrinter(8));
+        NamedElement base = (NamedElement)modBuild.getRoot();
+        assertEquals(base.getName(), base.getAnnotation().getSummary());
+        Group baseChild = (Group)base.getChild();
+        Form[] baseChildren = baseChild.getChildren();
+        for (int i = 0; i < baseChildren.length; i++) {
+            handle(baseChildren[i]);
+        }
     }
 
+    private static void handle(Form f){
+        if(f instanceof NamedElement){
+            NamedElement cur = (NamedElement)f;
+            assertEquals(cur.getName(), cur.getAnnotation().getSummary());
+        }else if(f instanceof Choice){
+            assertEquals("choice", f.getAnnotation().getSummary());
+        }else if(f instanceof Data){
+            assertTrue(f.getAnnotation().getInclude());
+        }
+    }
+
+    public void testAnnotationOnCardinality() throws IOException, XMLStreamException{
+        try{
+            new StAXModelBuilder("jar:edu/sc/seis/sod/data/validator/wrongAnnotation.rng");
+            assertTrue("Should've thrown a runtime exception when it encountered an Annotation in a cardinality operator", false);
+        }catch(RuntimeException e){
+            assertTrue(true);
+        }
+    }
 }
 
