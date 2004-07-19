c852b947c9506b96560089b5696ed5b4be77fe78
groves <none@none>
2004-07-19 10:25:45 -0400

    [svn r9653] windows version of the relax builder.  still borked
    --HG--
    branch : sod

---
+++ relax/buildSchema.bat
@@ -0,0 +1,3 @@
+python schemaBuilderScriptBuilder.py
+bigSchemaBuilder.bat
+echo Done.  If you're not going to monkey with the schema any more you'll want to commit elementNode.ser, sod.rng, and grouper.rng in src/edu/sc/seis/sod/data.  If not, I'll come complain to you.  If I'm reading this message at a later date I'll want to commit elementNode.ser, sod.rng and grouper.rng in src/edu/sc/seis/sod/data so that someone else doesn't come to complain to me.  
