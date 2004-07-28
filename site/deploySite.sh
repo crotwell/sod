7751f0cf0e404802c218c00e6ca4e7d6104a709c
groves <none@none>
2004-07-28 15:48:23 -0400

    [svn r9856] copy site over to pooh in tar
    --HG--
    branch : sod

--- site/deploySite.sh
+++ site/deploySite.sh
@@ -1,9 +1,11 @@
 buildSite.sh
 cd schemaDocs
 buildSchemaDocs.py
-cd ..
-tar cjvf site.tar generatedSite/*
+cd ../generatedSite
+tar cjvf site.tar.bz2 *
 echo 'copying site to pooh'
-scp -r site.tar sac@pooh:/seis/raid1/Apache/htdocs/SOD
+scp -r site.tar.bz2 sac@pooh:/seis/raid1/Apache/htdocs/sod
+ssh sac@pooh 'cd /seis/raid1/Apache/htdocs/sod ; bunzip2 site.tar.bz2 ; tar vxf site.tar; rm site.tar'
+rm site.tar.bz2
 echo done
 
