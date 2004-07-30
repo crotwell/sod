dc3ae16c8e1097afb24416444c6a1f60b81b3f19
groves <none@none>
2004-07-30 17:58:51 -0400

    [svn r9929] moved parse regions initialization to initializer
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveformArm/PrintlineSeismogramProcess.java
+++ src/edu/sc/seis/sod/process/waveformArm/PrintlineSeismogramProcess.java
@@ -1,5 +1,10 @@
 package edu.sc.seis.sod.process.waveformArm;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
 import edu.iris.Fissures.IfNetwork.Channel;
@@ -7,70 +12,48 @@ import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.network.ChannelIdUtil;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.display.ParseRegions;
+import edu.sc.seis.fissuresUtil.exceptionHandler.ExceptionReporterUtils;
 import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.status.StringTreeLeaf;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import org.apache.log4j.Logger;
-import org.w3c.dom.Element;
-
-/**
- * sample xml file
- *<pre>
- * &lt;sacFileProcessor&gt;
- *        &lt;dataDirectory&gt;SceppEvents&lt;/dataDirectory&gt;
- * &lt;/sacFileProcessor&gt;
- *</pre>
- */
 
 public class PrintlineSeismogramProcess implements LocalSeismogramProcess {
 
-    public PrintlineSeismogramProcess (Element config){
+    public PrintlineSeismogramProcess(Element config) {
         filename = SodUtil.getNestedText(config);
-        regions = ParseRegions.getInstance();
     }
 
     public LocalSeismogramResult process(EventAccessOperations event,
                                          Channel channel,
                                          RequestFilter[] original,
                                          RequestFilter[] available,
-                                         LocalSeismogramImpl[] seismograms, CookieJar cookieJar)
-        throws IOException, NoPreferredOrigin {
-        if (filename != null && filename.length() != 0) {
+                                         LocalSeismogramImpl[] seismograms,
+                                         CookieJar cookieJar)
+            throws IOException, NoPreferredOrigin {
+        String regionName = regions.getRegionName(event.get_attributes().region);
+        String dateTime = event.get_preferred_origin().origin_time.date_time;
+        String debugStr = "Got " + seismograms.length + " seismograms for "
+                + ChannelIdUtil.toString(channel.get_id()) + " for event in "
+                + regionName + " at " + dateTime;
+        if(filename != null && filename.length() != 0) {
             FileWriter fwriter = new FileWriter(filename, true);
             BufferedWriter bwriter = new BufferedWriter(fwriter);
-            String debugStr = "Got "+seismograms.length+" seismograms for "+
-                ChannelIdUtil.toString(channel.get_id())+
-                " for event in "+
-                regions.getRegionName(event.get_attributes().region)+
-                " at "+event.get_preferred_origin().origin_time.date_time;
             bwriter.write(debugStr, 0, debugStr.length());
             bwriter.newLine();
             bwriter.close();
         } else {
-            System.out.println(new java.util.Date()+" "+
-                                   edu.sc.seis.fissuresUtil.exceptionHandler.ExceptionReporterUtils.getMemoryUsage()+" "+
-                                   " Got "+seismograms.length+" seismograms for "+
-                                   ChannelIdUtil.toStringNoDates(channel.get_id())+
-                                   " for event in "+
-                                   regions.getRegionName(event.get_attributes().region)+
-                                   " at "+event.get_preferred_origin().origin_time.date_time);
-            logger.debug("Got "+seismograms.length+" seismograms for "+
-                             ChannelIdUtil.toStringNoDates(channel.get_id())+
-                             " for event in "+
-                             regions.getRegionName(event.get_attributes().region)+
-                             " at "+event.get_preferred_origin().origin_time.date_time);
+            System.out.println(new java.util.Date() + " "
+                    + ExceptionReporterUtils.getMemoryUsage() + debugStr);
+            logger.debug(debugStr);
         } // end of else
-        return new LocalSeismogramResult(true, seismograms, new StringTreeLeaf(this, true));
+        return new LocalSeismogramResult(true,
+                                         seismograms,
+                                         new StringTreeLeaf(this, true));
     }
 
-    ParseRegions regions;
+    private ParseRegions regions = ParseRegions.getInstance();
 
-    String filename = null;
+    private String filename;
 
-    static private Logger logger =
-        Logger.getLogger(PrintlineSeismogramProcess.class);
-
+    private static Logger logger = Logger.getLogger(PrintlineSeismogramProcess.class);
 }// PrintlineWaveformProcessor
