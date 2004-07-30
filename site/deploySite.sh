8205659460fa0aa1026de4bd69ad1828f59ac9e3
groves <none@none>
2004-07-30 18:45:53 -0400

    [svn r9947] moved running of schemaDoc to buildSite
    --HG--
    branch : sod

--- site/deploySite.sh
+++ site/deploySite.sh
@@ -1,11 +1,8 @@
 buildSite.sh
-cd schemaDocs
-buildSchemaDocs.py
-cd ../generatedSite
+cd generatedSite
 tar cjvf site.tar.bz2 *
 echo 'copying site to pooh'
 scp -r site.tar.bz2 sac@pooh:/seis/raid1/Apache/htdocs/sod
 ssh sac@pooh 'cd /seis/raid1/Apache/htdocs/sod ; bunzip2 site.tar.bz2 ; tar vxf site.tar; rm site.tar'
 rm site.tar.bz2
 echo done
-
