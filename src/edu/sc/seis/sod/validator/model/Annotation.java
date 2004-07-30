8f5b70343196558157b82f0a2cc26db818d7a501
groves <none@none>
2004-07-30 18:00:05 -0400

    [svn r9931] added changed hasExample to hasExampleFromAnnotation, return a canned description if a multigenitor from a definition with no description in the relax
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/model/Annotation.java
+++ src/edu/sc/seis/sod/validator/model/Annotation.java
@@ -3,13 +3,14 @@
  * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod.validator.model;
+
 import edu.sc.seis.sod.validator.tour.MinimalVisitGuide;
 import edu.sc.seis.sod.validator.tour.TourGuide;
 import edu.sc.seis.sod.validator.tour.XMLWritingTourist;
 
 public class Annotation {
+
     public void setDescription(String description) {
         desc = description;
     }
@@ -20,9 +21,19 @@ public class Annotation {
 
     public void setExample(String example) {
         this.example = example;
+        hasExampleFromAnnotation = true;
     }
 
     public String getDescription() {
+        Form f = getFormProvider().getForm();
+        if(desc == null && f.isFromDef() && f instanceof MultigenitorForm) {
+            Definition def = f.getDef();
+            return "This contains all of the "
+                    + def.getName()
+                    + " tactics.  It is not a real "
+                    + def.getName()
+                    + " tactic.  Instead of using this directly in the strategy file, use one of the tactics listed in the 'This consists of' section of this page.";
+        }
         return desc;
     }
 
@@ -44,24 +55,18 @@ public class Annotation {
     }
 
     public String getExample(boolean htmlize) {
-        if (example.equals("") || example == null) {
-            //ExampleBuilder eb = new ExampleBuilder(false);
-            //eb.write(formProvider.getForm());
-            //example = eb.toString();
+        if(example.equals("") || example == null) {
             XMLWritingTourist tourist = new XMLWritingTourist();
             TourGuide guide = new MinimalVisitGuide(formProvider.getForm());
             guide.lead(tourist);
             example = tourist.getResult();
-            
         }
-        if (htmlize) {
-            return getHTMLizedString(example);
-        }
+        if(htmlize) { return getHTMLizedString(example); }
         return example;
     }
 
-    public boolean hasExample() {
-        return !(example.equals("") || example == null);
+    public boolean hasExampleFromAnnotation() {
+        return hasExampleFromAnnotation;
     }
 
     public void setInclude(boolean include) {
@@ -72,33 +77,6 @@ public class Annotation {
         return include;
     }
 
-    public String getContained() {
-        StringBuffer buf = new StringBuffer();
-        getContained(getFormProvider().getForm(), buf);
-        return buf.toString();
-    }
-
-    private void getContained(Form containedRoot, StringBuffer buf) {
-        buf.append("<div>\n");
-        Form curForm = getFormProvider().getForm();
-        String countString = "";
-        if (curForm.getMin() == 0) {
-            if (curForm.getMax() > 1) {
-                countString = "";
-            } else {
-
-            }
-        } else {
-
-        }
-        if (curForm instanceof Choice) {
-            buf.append("<p>A choice between</p>");
-        } else {
-
-        }
-        buf.append("</div>\n");
-    }
-
     public static String getHTMLizedString(String bracketedXML) {
         StringBuffer buf = new StringBuffer(bracketedXML);
         String newString = replaceAllInstances(buf.toString(), "<", "&lt;");
@@ -107,16 +85,19 @@ public class Annotation {
         return newString;
     }
 
-    public static String replaceAllInstances(String string, String orig,
-            String replacement) {
+    public static String replaceAllInstances(String string,
+                                             String orig,
+                                             String replacement) {
         StringBuffer buf = new StringBuffer(string);
         int repIndex = buf.indexOf(orig);
-        while (repIndex != -1) {
+        while(repIndex != -1) {
             buf.replace(repIndex, repIndex + 1, replacement);
             repIndex = buf.indexOf(orig);
         }
         return buf.toString();
     }
+
+    private boolean hasExampleFromAnnotation = false;
 
     private String summary, desc;
 
@@ -128,4 +109,3 @@ public class Annotation {
 
     public static boolean DEFAULT_HTMLIZE = true;
 }
-
