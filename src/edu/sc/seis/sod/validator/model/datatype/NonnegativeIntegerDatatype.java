dffc8c4c6c28958e7ae4fd9d918f5807d7b56238
groves <none@none>
2004-07-30 17:59:13 -0400

    [svn r9930] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/validator/model/datatype/NonnegativeIntegerDatatype.java
@@ -0,0 +1,64 @@
+package edu.sc.seis.sod.validator.model.datatype;
+
+import org.relaxng.datatype.DatatypeException;
+import org.relaxng.datatype.DatatypeStreamingValidator;
+import org.relaxng.datatype.ValidationContext;
+import edu.sc.seis.sod.validator.model.ModelDatatype;
+
+/**
+ * @author groves Created on Jul 30, 2004
+ */
+public class NonnegativeIntegerDatatype implements ModelDatatype {
+
+    public String getDescription() {
+        return "A non negative integer";
+    }
+
+    public String getExampleValue() {
+        return "12";
+    }
+
+    public String toString() {
+        return "Non negative integer";
+    }
+
+    public boolean isValid(String arg0, ValidationContext arg1) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void checkValid(String arg0, ValidationContext arg1)
+            throws DatatypeException {
+    // TODO Auto-generated method stub
+    }
+
+    public DatatypeStreamingValidator createStreamingValidator(ValidationContext arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object createValue(String arg0, ValidationContext arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean sameValue(Object arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public int valueHashCode(Object arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int getIdType() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public boolean isContextDependent() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
