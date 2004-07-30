fa171f1639c184316f0e2e90d13f51f0ed5d6463
groves <none@none>
2004-07-30 18:44:53 -0400

    [svn r9945] only run if the jars, templates or this file have changed
    --HG--
    branch : sod

--- site/schemaDocs/buildSchemaDocs.py
+++ site/schemaDocs/buildSchemaDocs.py
@@ -14,15 +14,29 @@ def main(argv):
     startdir = os.path.abspath('.')
     proj = ProjectParser.ProjectParser('../../project.xml')
     build.buildJars(proj)
-    build.buildSchemaDocScripts(proj)
-    depCopy.copy(proj)
     os.chdir(startdir)
-    print 'starting schemaDocumenter' 
-    if os.environ.has_key('OS') and os.environ['OS'] == 'Windows_NT':
-        os.spawnlp(os.P_WAIT, 'schemaDocumenter.bat', 'cmd', 'schemaDocumenter.bat')
+    files = filter(lambda x:x.endswith('xsl') or x.endswith('xml'), os.listdir('..'))
+    files = ['../' + file for file in files]
+    files.append(proj.path + 'target/' + proj.builtJar)
+    files.append('buildSchemaDocs.py')
+    files.append('elementPage.vm')
+    fileModTimes = [os.stat(file).st_mtime for file in files]
+    generateTime = os.stat('../generatedSite/tagDocs/sod/start.html').st_mtime
+    doTheWork = False
+    for modTime in fileModTimes:
+        if modTime > generateTime:
+            doTheWork = True
+    if doTheWork:
+        build.buildSchemaDocScripts(proj)
+        depCopy.copy(proj)
+        print 'starting schemaDocumenter' 
+        if os.environ.has_key('OS') and os.environ['OS'] == 'Windows_NT':
+            os.spawnlp(os.P_WAIT, 'schemaDocumenter.bat', 'cmd', 'schemaDocumenter.bat')
+        else:
+            os.spawnlp(os.P_WAIT, 'schemaDocumenter.sh', 'sh', 'schemaDocumenter.sh')
+        print 'done'
     else:
-        os.spawnlp(os.P_WAIT, 'schemaDocumenter.sh', 'sh', 'schemaDocumenter.sh')
-    print '\ndone'
+        print "not running schemaDocumenter.  The jars haven't changed and neither have the templates"
 
 if __name__ == "__main__":
     main(sys.argv[1:])
