18b2cfcb4af05b604ba15d684eecf016b55c0ad4
groves <none@none>
2004-07-31 17:56:19 -0400

    [svn r9953] print out a . for every finished doc
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/documenter/SchemaDocumenter.java
+++ src/edu/sc/seis/sod/validator/documenter/SchemaDocumenter.java
@@ -1,11 +1,9 @@
 /**
  * SchemaDocumenter.java
- *
+ * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod.validator.documenter;
-
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,15 +11,12 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-
 import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.validator.ModelWalker;
 import edu.sc.seis.sod.validator.model.Definition;
@@ -31,10 +26,10 @@ import edu.sc.seis.sod.validator.tour.DepthAwareGuide;
 import edu.sc.seis.sod.validator.tour.HTMLOutlineTourist;
 
 public class SchemaDocumenter {
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         StAXModelBuilder handler = new StAXModelBuilder("../../relax/sod.rng");
         relaxBase = "../../relax/";
-
         //Setup velocity
         VelocityEngine ve = new VelocityEngine();
         ve.init();
@@ -44,32 +39,31 @@ public class SchemaDocumenter {
         c.put("util", new SodUtil());
         c.put("helper", new VelocityModelHelper());
         c.put("doc", new SchemaDocumenter());
-        
         //Setup xsl transforms
         TransformerFactory tFactory = TransformerFactory.newInstance();
         Transformer transformer = tFactory.newTransformer(new StreamSource("../pageGenerator.xsl"));
-        
         //Run velocity then xsl on all definitions
         Collection defs = StAXModelBuilder.getAllDefinitions();
         Iterator it = defs.iterator();
-        while(it.hasNext()){
+        while(it.hasNext()) {
             Definition cur = (Definition)it.next();
             //if(makePath(cur).startsWith("base/include/timeRange/t")){
-                render(c, ve, cur, transformer);
+            render(c, ve, cur, transformer);
             //}
+            System.out.print(".");
         }
     }
 
     static Set renderLocs = new HashSet();
 
-    public static void render(VelocityContext c, VelocityEngine ve,
-                              Definition def, Transformer t) throws Exception{
+    public static void render(VelocityContext c,
+                              VelocityEngine ve,
+                              Definition def,
+                              Transformer t) throws Exception {
         String path = makePath(def);
         File xmlFile = new File("xml/" + path + ".xml");
         xmlFile.getParentFile().mkdirs();
-        System.out.println("writin' " + xmlFile);
         FileWriter fw = new FileWriter(xmlFile);
-
         c.put("def", def);
         HTMLOutlineTourist tourist = new HTMLOutlineTourist(makePath(def));
         DepthAwareGuide guide = new DepthAwareGuide(def.getForm());
@@ -77,36 +71,43 @@ public class SchemaDocumenter {
         c.put("contained", tourist.getResult());
         ve.mergeTemplate("elementPage.vm", new VelocityContext(c), fw);
         fw.close();
-
-        String outputLoc = "../generatedSite/tagDocs/"+ path + ".html";
+        String outputLoc = "../generatedSite/tagDocs/" + path + ".html";
         File htmlFile = new File(outputLoc);
         htmlFile.getParentFile().mkdirs();
         String relPath = SodUtil.getRelativePath(outputLoc,
                                                  "../generatedSite/",
                                                  "/");
-        relPath = relPath.substring(0, relPath.length() - "../generatedSite".length());
+        relPath = relPath.substring(0, relPath.length()
+                - "../generatedSite".length());
         t.setParameter("base", relPath);
         t.setParameter("menu", "Reference");
         t.setParameter("page", "tagDocs/" + path + ".html");
-        t.transform(new StreamSource("xml/"+path+".xml"), new StreamResult(htmlFile));
+        t.transform(new StreamSource("xml/" + path + ".xml"),
+                    new StreamResult(htmlFile));
     }
 
-    public static Definition getNearestDef(Form f){
-        if(f.isFromDef()){ return f.getDef(); }
+    public static Definition getNearestDef(Form f) {
+        if(f.isFromDef()) { return f.getDef(); }
         return getNearestDef(f.getParent());
     }
 
-    public static String makePath(Form f){ return makePath(getNearestDef(f)); }
+    public static String makePath(Form f) {
+        return makePath(getNearestDef(f));
+    }
 
-    public static String makePath(Definition def){
+    public static String makePath(Definition def) {
         String rngLoc = def.getGrammar().getLoc();
         String path = rngLoc.substring(relaxBase.length(), rngLoc.length() - 4);
         path += "/" + def.getName();
-        if(def.getName().equals("")){ path += "start"; }
+        if(def.getName().equals("")) {
+            path += "start";
+        }
         return path;
     }
-    
+
     static String relaxBase = "";
+
     static Set writtenFiles = new HashSet();
-    static  File base = new File("html/");
+
+    static File base = new File("html/");
 }
