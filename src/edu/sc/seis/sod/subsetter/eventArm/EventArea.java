27c5dfe955b3f3e5f63b1255656b0927133a7380
groves <none@none>
2004-08-01 15:55:45 -0400

    [svn r9967] cleaned up
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventArm/EventArea.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/EventArea.java
@@ -1,5 +1,9 @@
 package edu.sc.seis.sod.subsetter.eventArm;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import edu.iris.Fissures.BoxArea;
 import edu.iris.Fissures.GlobalArea;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfEvent.EventAttr;
@@ -7,50 +11,24 @@ import edu.iris.Fissures.IfEvent.Origin;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodElement;
 import edu.sc.seis.sod.SodUtil;
-import edu.sc.seis.sod.subsetter.eventArm.OriginSubsetter;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
- * EventArea.java
- *
- *
  * Created: Thu Mar 14 14:02:33 2002
- *
- * @author <a href="mailto:">Philip Crotwell</a>
- * @version
- *
- * This class is used to represent the subsetter EventArea. Event Area implements EventSubsetter
- * and can be any one of GlobalArea or BoxArea or PointDistanceArea or FlinneEngdahlArea.
- *
- *<pre>
- * sample xml representation of EventArea are
- *
- *
- *              &lt;stationArea&gt;
- *                           &lt;boxArea&gt;
- *                                    &lt;latitudeRange&gt;
- *                                                   &lt;min&gt;30&lt;/min&gt;
- *                                                   &lt;max&gt;33&lt;/max&gt;
- *                                    &lt;/latitudeRange&gt;
- *                                    &lt;longitudeRange&gt;
- *                                                   &lt;min&gt;-100&lt;/min&gt;
- *                                                   &lt;max&gt;100&lt;/max&gt;
- *                                    &lt;/longitudeRange&gt;
- *                           &lt;/boxArea&gt;
- *              &lt;/stationArea&gt;
- *</pre>
+ * 
+ * @author <a href="mailto:">Philip Crotwell </a> This class is used to
+ *         represent the subsetter EventArea. Event Area implements
+ *         EventSubsetter and can be any one of GlobalArea or BoxArea or
+ *         PointDistanceArea or FlinneEngdahlArea.
  */
+public class EventArea implements OriginSubsetter, SodElement {
 
-
-public class EventArea implements OriginSubsetter,SodElement {
-    public EventArea (Element config) throws ConfigurationException {
+    public EventArea(Element config) throws ConfigurationException {
         NodeList children = config.getChildNodes();
-        for(int i = 0; i < children.getLength() ; i++) {
+        for(int i = 0; i < children.getLength(); i++) {
             Node node = children.item(i);
             if(node instanceof Element) {
-                area = (edu.iris.Fissures.Area)SodUtil.load((Element)node, "eventArm");
+                area = (edu.iris.Fissures.Area)SodUtil.load((Element)node,
+                                                            "eventArm");
                 break;
             }
         }
@@ -60,22 +38,18 @@ public class EventArea implements OriginSubsetter,SodElement {
      * returns true if the given origin is within the area specified in the
      * configuration file else returns false.
      */
-    public boolean accept(EventAccessOperations event, EventAttr eventAttr, Origin e) throws Exception {
-        if(area instanceof edu.iris.Fissures.BoxArea) {
-            edu.iris.Fissures.BoxArea boxArea = (edu.iris.Fissures.BoxArea)area;
-
+    public boolean accept(EventAccessOperations event,
+                          EventAttr eventAttr,
+                          Origin e) throws Exception {
+        if(area instanceof BoxArea) {
+            BoxArea boxArea = (BoxArea)area;
             if(e.my_location.latitude >= boxArea.min_latitude
-               && e.my_location.latitude <=boxArea.max_latitude
-               && e.my_location.longitude >= boxArea.min_longitude
-               && e.my_location.longitude <= boxArea.max_longitude) {
-                return true;
-            } else return false;
-
-        } else if (area instanceof GlobalArea) {
-            return true;
-        }
-        throw new Exception("Unknown Area, class="+area.getClass());
-
+                    && e.my_location.latitude <= boxArea.max_latitude
+                    && e.my_location.longitude >= boxArea.min_longitude
+                    && e.my_location.longitude <= boxArea.max_longitude) { return true; }
+            return false;
+        } else if(area instanceof GlobalArea) { return true; }
+        throw new Exception("Unknown Area, class=" + area.getClass());
     }
 
     private edu.iris.Fissures.Area area = null;
--- src/edu/sc/seis/sod/subsetter/eventArm/OriginPointDistance.java
+++ src/edu/sc/seis/sod/subsetter/eventArm/OriginPointDistance.java
@@ -1,50 +1,34 @@
-/**
- * OriginPointDistance.java
- *
- * @author Created by Omnicore CodeGuide
- */
-
 package edu.sc.seis.sod.subsetter.eventArm;
 
+import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfEvent.EventAttr;
 import edu.iris.Fissures.IfEvent.Origin;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.bag.DistAz;
-import org.apache.log4j.Logger;
-import org.w3c.dom.Element;
 
-public class OriginPointDistance extends AbstractOriginPoint implements OriginSubsetter{
+public class OriginPointDistance extends AbstractOriginPoint implements
+        OriginSubsetter {
 
-
-    /**
-     * Creates a new <code>OriginPointDistance</code> instance.
-     *
-     * @param config an <code>Element</code> value
-     */
-    public OriginPointDistance (Element config) throws Exception{
+    public OriginPointDistance(Element config) throws Exception {
         super(config);
+        min = getMin().convertTo(UnitImpl.DEGREE).get_value();
+        max = getMax().convertTo(UnitImpl.DEGREE).get_value();
     }
 
     /**
      * Accepts an origin only if it lies within the geven distance range of the
      * given lat and lon.
-     *
      */
-    public boolean accept(EventAccessOperations event, EventAttr eventAttr, Origin origin) {
+    public boolean accept(EventAccessOperations event,
+                          EventAttr eventAttr,
+                          Origin origin) {
         double oLat = origin.my_location.latitude;
         double oLon = origin.my_location.longitude;
         DistAz distaz = new DistAz(latitude, longitude, oLat, oLon);
-        if (getMin().convertTo(UnitImpl.DEGREE).get_value() <= distaz.getDelta() &&
-            getMax().convertTo(UnitImpl.DEGREE).get_value() >= distaz.getDelta()) {
-            return true;
-        } else {
-            logger.debug("reject distance "+origin+" distaz="+distaz.getDelta());
-            return false;
-        }
+        double delta = distaz.getDelta();
+        return min <= delta && max >= delta;
     }
 
-    private static final Logger logger = Logger.getLogger(OriginPointDistance.class);
-
+    private double min, max;
 }
-
