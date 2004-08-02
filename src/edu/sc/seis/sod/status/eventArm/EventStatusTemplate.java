3212665580d82bb2953ac49ad67f23ba56572609
groves <none@none>
2004-08-02 12:41:48 -0400

    [svn r9983] no longer use filedir element
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/eventArm/EventStatusTemplate.java
+++ src/edu/sc/seis/sod/status/eventArm/EventStatusTemplate.java
@@ -1,6 +1,5 @@
 package edu.sc.seis.sod.status.eventArm;
 
-
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.EventChannelPair;
@@ -21,71 +20,59 @@ import org.w3c.dom.DOMException;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
-public class EventStatusTemplate extends FileWritingTemplate implements EventArmMonitor, WaveformArmMonitor{
+public class EventStatusTemplate extends FileWritingTemplate implements
+        EventArmMonitor, WaveformArmMonitor {
 
-    public EventStatusTemplate(Element el)throws IOException, SAXException, ParserConfigurationException, ConfigurationException   {
-        super(extractConstructorBaseDirArg(el), extractConstructorFilenameArg(el));
-        Element config = TemplateFileLoader.getTemplate(SodUtil.getElement(el, "eventConfig"));
+    public EventStatusTemplate(Element el) throws IOException, SAXException,
+            ParserConfigurationException, ConfigurationException {
+        super(FileWritingTemplate.getBaseDirectoryName(),
+              extractConstructorFilenameArg(el));
+        Element config = TemplateFileLoader.getTemplate(el);
         config.removeChild(SodUtil.getElement(config, "filename"));
         parse(config);
     }
 
-    private static String extractConstructorFilenameArg(Element el) throws IOException, SAXException, ParserConfigurationException, DOMException{
-        String fileDir = extractConstructorBaseDirArg(el);
-        Element eventConfigEl = SodUtil.getElement(el, "eventConfig");
-        Element templateConfig = TemplateFileLoader.getTemplate(eventConfigEl);
+    private static String extractConstructorFilenameArg(Element el)
+            throws IOException, SAXException, ParserConfigurationException,
+            DOMException {
+        Element templateConfig = TemplateFileLoader.getTemplate(el);
         Element fileNameElement = SodUtil.getElement(templateConfig, "filename");
         String filename = fileNameElement.getFirstChild().getNodeValue();
         return filename;
     }
 
-    private static String extractConstructorBaseDirArg(Element el){
-        String fileDir = null;
-        try{
-            fileDir = SodUtil.getElement(el, "fileDir").getFirstChild().getNodeValue();
-        }
-        catch(NullPointerException e){
-            Logger.getLogger(EventStatusTemplate.class).debug("fileDir element is null! using default");
-        }
-        if (fileDir == null){
-            fileDir = FileWritingTemplate.getBaseDirectoryName();
-        }
-        return fileDir;
-    }
-
-    public void setArmStatus(String status) throws IOException {
+    public void setArmStatus(String status) {
         this.armStatus = status;
         write();
     }
 
-    public Object getTemplate(String tag, Element el) throws ConfigurationException  {
+    public Object getTemplate(String tag, Element el)
+            throws ConfigurationException {
         if(tag.equals("events")) {
-            return  new EventGroupTemplate(el);
+            return new EventGroupTemplate(el);
         } else if(tag.equals("armStatus")) {
             return new ArmStatusFormatter();
-        } else if (tag.equals("mapEventStatus")){
-            return new MapImgSrc(el);
-        }
+        } else if(tag.equals("mapEventStatus")) { return new MapImgSrc(el); }
         return super.getTemplate(tag, el);
     }
 
+    private class MapImgSrc implements GenericTemplate {
 
-
-    private class MapImgSrc implements GenericTemplate{
-        public MapImgSrc(Element el){
+        public MapImgSrc(Element el) {
             String mapFileLoc = MapEventStatus.getLocation(el);
-            synchronized(elementsToMaps){
-                if(elementsToMaps.containsKey(mapFileLoc)){
+            synchronized(elementsToMaps) {
+                if(elementsToMaps.containsKey(mapFileLoc)) {
                     map = (MapEventStatus)elementsToMaps.get(mapFileLoc);
-                }else{
+                } else {
                     map = new MapEventStatus(el);
                     elementsToMaps.put(mapFileLoc, map);
                 }
             }
         }
 
-        public String getResult(){
-            String myloc = getOutputDirectory().toString() + '/' + getFilename();
+        public String getResult() {
+            String myloc = getOutputDirectory().toString() + '/'
+                    + getFilename();
             return SodUtil.getRelativePath(myloc, map.getLocation(), "/");
         }
 
@@ -99,18 +86,23 @@ public class EventStatusTemplate extends FileWritingTemplate implements EventArm
     }
 
     public void change(EventAccessOperations event, Status status) {
-        if(!addedToWaveformArm && Start.getWaveformArm() != null){
+        if(!addedToWaveformArm && Start.getWaveformArm() != null) {
             Start.getWaveformArm().addStatusMonitor(this);
             addedToWaveformArm = true;
         }
         write();
     }
 
-    private class ArmStatusFormatter implements GenericTemplate{
-        public String getResult(){ return armStatus; }
+    private class ArmStatusFormatter implements GenericTemplate {
+
+        public String getResult() {
+            return armStatus;
+        }
     }
 
     private boolean addedToWaveformArm = false;
+
     private Logger logger = Logger.getLogger(EventStatusTemplate.class);
+
     private String armStatus = "";
 }
--- src/edu/sc/seis/sod/status/waveformArm/WaveformEventTemplateGenerator.java
+++ src/edu/sc/seis/sod/status/waveformArm/WaveformEventTemplateGenerator.java
@@ -1,8 +1,11 @@
 package edu.sc.seis.sod.status.waveformArm;
 
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
-import edu.iris.Fissures.IfEvent.NoPreferredOrigin;
-import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.EventChannelPair;
 import edu.sc.seis.sod.SodUtil;
@@ -12,49 +15,32 @@ import edu.sc.seis.sod.status.EventFormatter;
 import edu.sc.seis.sod.status.FileWritingTemplate;
 import edu.sc.seis.sod.status.TemplateFileLoader;
 import edu.sc.seis.sod.status.eventArm.EventArmMonitor;
-import edu.sc.seis.sod.status.waveformArm.WaveformArmMonitor;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import javax.xml.parsers.ParserConfigurationException;
-import org.apache.log4j.Logger;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-public class WaveformEventTemplateGenerator implements EventArmMonitor, WaveformArmMonitor {
-    public WaveformEventTemplateGenerator(Element el) throws IOException, SAXException, ParserConfigurationException, ConfigurationException {
-        NodeList nl = el.getChildNodes();
-        for (int i = 0; i < nl.getLength(); i++) {
-            Node n = nl.item(i);
-            if (n.getNodeName().equals("fileDir")){
-                fileDir = n.getFirstChild().getNodeValue();
-            }
-            else if(n.getNodeName().equals("waveformConfig")){
-                config = TemplateFileLoader.getTemplate((Element)n);
+public class WaveformEventTemplateGenerator implements EventArmMonitor,
+        WaveformArmMonitor {
 
-                Node tmp = SodUtil.getElement(config, "outputLocation");
-                formatter = new EventFormatter((Element)tmp);
-                config.removeChild(tmp);
-
-                tmp = SodUtil.getElement(config, "filename");
-                filename = tmp.getFirstChild().getNodeValue();
-                config.removeChild(tmp);
-            }
+    public WaveformEventTemplateGenerator(Element el) throws IOException,
+            SAXException, ParserConfigurationException, ConfigurationException {
+        config = TemplateFileLoader.getTemplate(el);
+        Element tmp = SodUtil.getElement(config, "outputLocation");
+        formatter = new EventFormatter(tmp);
+        config.removeChild(tmp);
+        tmp = SodUtil.getElement(config, "filename");
+        filename = tmp.getFirstChild().getNodeValue();
+        config.removeChild(tmp);
+        fileDir = FileWritingTemplate.getBaseDirectoryName();
+        if(formatter == null || config == null) { throw new IllegalArgumentException("The configuration element must contain a fileDir and a waveformConfig"); }
+        template = new WaveformEventTemplate(config,
+                                             fileDir,
+                                             formatter,
+                                             filename);
+        if(Start.getEventArm() != null) {
+            Start.getEventArm().add(this);
         }
-        if (fileDir == null){
-            fileDir = FileWritingTemplate.getBaseDirectoryName();
-        }
-        if(formatter == null  || config == null) {
-            throw new IllegalArgumentException("The configuration element must contain a fileDir and a waveformConfig");
-        }
-        template = new WaveformEventTemplate(config, fileDir, formatter, filename);
-        if(Start.getEventArm() != null) Start.getEventArm().add(this);
     }
 
     public void change(EventAccessOperations event, Status status) {
-        if(!added){
+        if(!added) {
             if(Start.getWaveformArm() != null) {
                 Start.getWaveformArm().addStatusMonitor(template);
                 added = true;
@@ -70,9 +56,14 @@ public class WaveformEventTemplateGenerator implements EventArmMonitor, Waveform
     }
 
     private boolean added = false;
+
     private WaveformEventTemplate template;
+
     private Element config;
+
     private EventFormatter formatter;
+
     private String fileDir, filename;
+
     private static Logger logger = Logger.getLogger(WaveformEventTemplateGenerator.class);
 }
