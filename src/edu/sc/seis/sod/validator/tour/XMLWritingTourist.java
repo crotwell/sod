e2ceb03f9359785023c4922dc60b535ace0a3bdf
groves <none@none>
2004-08-03 23:41:44 -0400

    [svn r10027] use user defined example if possible
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/tour/XMLWritingTourist.java
+++ src/edu/sc/seis/sod/validator/tour/XMLWritingTourist.java
@@ -8,13 +8,14 @@ import edu.sc.seis.sod.validator.model.*;
 /**
  * @author Charlie Groves
  */
-public class XMLWritingTourist implements Tourist{
-    public XMLWritingTourist(){
+public class XMLWritingTourist implements Tourist {
+
+    public XMLWritingTourist() {
         this(false);
     }
 
-    public XMLWritingTourist(boolean escapeBrackets){
-        if(escapeBrackets){
+    public XMLWritingTourist(boolean escapeBrackets) {
+        if(escapeBrackets) {
             open = "&lt;";
             close = "&gt;";
         }
@@ -22,92 +23,133 @@ public class XMLWritingTourist implements Tourist{
 
     private Form preAttrForm;
 
-    public void visit(Attribute attr){
-        result.replace(result.length() - close.length(), result.length(), "");
-        result.append(" " + attr.getName() + "=\"");
+    public void visit(Attribute attr) {
+        replaceClose("");
+        write(" " + attr.getName() + "=\"");
         preAttrForm = lastForm;
     }
 
-    public void leave(Attribute attr){
-        result.append("\"" + close);
+    public void leave(Attribute attr) {
+        write("\"" + close);
         lastForm = preAttrForm;
     }
 
-    public void visit(Choice choice){}
-
-    public void leave(Choice choice){}
-
-    public void visit(Data d){
+    public void visit(Data d) {
         lastForm = d;
-        if(d.getAnnotation().hasExampleFromAnnotation()){
-            result.append(d.getAnnotation().getExample());
-        } else{
-            result.append(d.getDatatype().getExampleValue());
+        if(d.getAnnotation().hasExampleFromAnnotation()) {
+            write(d.getAnnotation().getExample());
+        } else {
+            write(d.getDatatype().getExampleValue());
         }
     }
 
-    public void visit(Empty e){}
-
-    public void visit(Group g){}
-
-    public void leave(Group g){}
-
-    public void visit(Interleave i){}
-
-    public void leave(Interleave i){}
-
-    public void visit(NamedElement ne){
-        if(!ne.equals(lastForm) && lastForm != null && !leftLast){
-            result.append("\n");
+    public void visit(NamedElement ne) {
+        if(!ne.equals(lastForm) && lastForm != null && !leftLast) {
+            write("\n");
         }
-        write(open + ne.getName() + close);
-        lastForm = ne;
-        leftLast = false;
+        if(ne.getAnnotation().hasExampleFromAnnotation()) {
+            String example = ne.getAnnotation().getExample(true);
+            indentAndWrite(example.replaceAll("\n", "\n" + getCurIndent())
+                    + "\n");
+            waitToLeave = ne;
+        } else {
+            indentAndWrite(open + ne.getName() + close);
+            lastForm = ne;
+            leftLast = false;
+        }
         depth++;
     }
 
-    public void leave(NamedElement ne){
+    public void leave(NamedElement ne) {
         depth--;
-        if(ne.equals(lastForm)){
-            result.replace(result.length() - close.length(), result.length(),
-                    " /" + close + "\n");
-        } else{
-            write(open + "/" + ne.getName() + close + "\n");
+        if(ne.equals(lastForm)) {
+            replaceClose(" /" + close + "\n");
+        } else {
+            indentAndWrite(open + "/" + ne.getName() + close + "\n");
         }
         leftLast = true;
+        if(ne.equals(waitToLeave)) {
+            waitToLeave = null;
+        }
     }
 
-    private void write(String text){
-        if(result.length() > 0 && result.charAt(result.length() - 1) == '\n'){
-            for(int i = 0; i < depth; i++){
-                result.append("  ");
+    private void indentAndWrite(String text) {
+        write(getCurIndent() + text);
+    }
+
+    private String getCurIndent() {
+        return getIndent(depth);
+    }
+
+    private String getIndent(int depth) {
+        StringBuffer indent = new StringBuffer();
+        if(result.length() > 0 && result.charAt(result.length() - 1) == '\n') {
+            for(int i = 0; i < depth; i++) {
+                indent.append("  ");
             }
         }
-        result.append(text);
+        return indent.toString();
     }
 
-    public void visit(Text t){
+    private void write(String text) {
+        if(waitToLeave == null) {
+            result.append(text);
+        }
+    }
+
+    private void replaceClose(String with) {
+        if(waitToLeave == null) {
+            result.replace(result.length() - close.length(),
+                           result.length(),
+                           with);
+        }
+    }
+
+    public void visit(Text t) {
         lastForm = t;
-        result.append(DEFAULT_TEXT_VALUE);
+        write(DEFAULT_TEXT_VALUE);
     }
 
-    public void visit(Value v){
+    public void visit(Value v) {
         lastForm = v;
-        result.append(v.getValue());
+        write(v.getValue());
     }
 
-    public void visit(NotAllowed na){}
-
-    public String getResult(){
+    public String getResult() {
         return result.toString();
     }
 
+    public void visit(Choice choice) {}
+
+    public void leave(Choice choice) {}
+
+    public void visit(Empty e) {}
+
+    public void visit(Group g) {}
+
+    public void leave(Group g) {}
+
+    public void visit(Interleave i) {}
+
+    public void leave(Interleave i) {}
+
+    public void visit(NotAllowed na) {}
+
     private StringBuffer result = new StringBuffer();
+
     private boolean leftLast;
+
     int depth = 0;
+
     private Form lastForm;
+
+    private Form waitToLeave;
+
     public static final int DEFAULT_INT_VALUE = 12;
+
     public static final String DEFAULT_TEXT_VALUE = "text";
+
     private String open = "<";
+
     private String close = ">";
 }
