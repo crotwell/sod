044968ecfa76e85557adebd2fb7ba23334506b4f
oliverpa <none@none>
2004-08-10 15:53:40 -0400

    [svn r10131] use template element instead of href attribute
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/TemplateFileLoader.java
+++ src/edu/sc/seis/sod/status/TemplateFileLoader.java
@@ -1,24 +1,24 @@
 package edu.sc.seis.sod.status;
 
-import edu.sc.seis.sod.Start;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.Start;
 
 public class TemplateFileLoader{
     public static Element getTemplate(Element el) throws MalformedURLException,
         IOException, SAXException, ParserConfigurationException {
-        Attr attr =  (Attr)el.getAttributes().getNamedItem("xlink:href");
-        if(attr == null || attr.getValue().equals("")) {
-            throw new IllegalArgumentException("Expected the passed in element " + el.getNodeName() + " to have a xlink:href attribute, but none found");
+        Element templateEl = SodUtil.getElement(el, "template");
+        if(templateEl == null || SodUtil.getNestedText(templateEl).equals("")) {
+            throw new IllegalArgumentException("Expected the passed in element " + el.getNodeName() + " to have a template element, but none found");
         }
-        return getTemplate(el.getClass().getClassLoader(), attr.getValue());
+        return getTemplate(el.getClass().getClassLoader(), SodUtil.getNestedText(templateEl));
     }
     
     public static Element getTemplate(ClassLoader cl, String loc) throws MalformedURLException, SAXException, ParserConfigurationException, IOException{
