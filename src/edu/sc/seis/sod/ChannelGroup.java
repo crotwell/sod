d4453d60dc4339eafe2ef28a5a023ff4ca5e167c
crotwell <none@none>
2004-08-12 23:23:19 -0400

    [svn r10170] check for channel id equals as well
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/ChannelGroup.java
+++ src/edu/sc/seis/sod/ChannelGroup.java
@@ -7,22 +7,32 @@
 
 package edu.sc.seis.sod;
 import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.network.ChannelIdUtil;
+import org.apache.log4j.Logger;
 
 public class ChannelGroup {
-	public ChannelGroup(Channel[] channels) {
-		this.channels = channels;
+    public ChannelGroup(Channel[] channels) {
+        this.channels = channels;
     }
-	
+
     public Channel[] getChannels() {
-		return channels;
+        return channels;
     }
-	public boolean contains(Channel c) {
-		for(int i=0;i<channels.length;i++) {
-			if(channels[i].equals(c)) return true;
-		}
-		return false;
-	}
-	private Channel[] channels;
+    public boolean contains(Channel c) {
+        for(int i=0;i<channels.length;i++) {
+            if(channels[i].equals(c)) return true;
+        }
+        // didn't find by object equals, check for ids
+        for(int i=0;i<channels.length;i++) {
+            logger.warn("Found two channels with same id that are not equals()"+ChannelIdUtil.toString(c.get_id()));
+            if(ChannelIdUtil.areEqual(channels[i].get_id(), c.get_id())) return true;
+        }
+        return false;
+    }
+    private Channel[] channels;
+
+    private static final Logger logger = Logger.getLogger(ChannelGroup.class);
+
 }
 
 
