f374a051c89ac3b725b95b3c62dfaf328d4b6374
crotwell <none@none>
2004-09-02 15:32:12 -0400

    [svn r10316] make a thread and dont use Timers, also fix synchronization issue with adding
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/OutputScheduler.java
+++ src/edu/sc/seis/sod/status/OutputScheduler.java
@@ -8,6 +8,7 @@ package edu.sc.seis.sod.status;
 
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.sod.Start;
 import java.util.Collections;
@@ -17,35 +18,53 @@ import java.util.Timer;
 import java.util.TimerTask;
 import org.apache.log4j.Logger;
 
-public class OutputScheduler extends TimerTask{
-    public OutputScheduler(){
-        t.schedule(this, 10000);
+public class OutputScheduler extends Thread{
+    protected OutputScheduler(){
     }
 
-    public void schedule(Runnable a){ runnables.add(a); }
+    public void schedule(Runnable a){
+        synchronized(runnables){
+            runnables.add(a);
+        }
+    }
 
     public void scheduleForExit(Runnable a) {
-        onExitRunnables.add(a);
+        synchronized(onExitRunnables){
+            onExitRunnables.add(a);
+        }
     }
 
     public void run() {
-        runAll(runnables);
-        if(Start.getWaveformArm() != null && Start.getWaveformArm().isFinished()){
+        // initial sleep before first set of status pages
+        try {
+            Thread.sleep(INITIAL_SLEEP);
+        } catch (InterruptedException e) {}
+
+        while(true) {
+            System.out.println("****************************************************");
+            System.out.println("     Status pages update at: "+ClockUtil.now());
+            System.out.println("****************************************************");
             runAll(runnables);
-            runAll(onExitRunnables);
-            logger.debug("Output Scheduler done.");
+            if(Start.getWaveformArm() != null && Start.getWaveformArm().isFinished()){
+                runAll(runnables);
+                runAll(onExitRunnables);
+                logger.debug("Output Scheduler done.");
 
 
-            String lcOSName = System.getProperty("os.name").toLowerCase();
-            boolean MAC_OS_X = lcOSName.startsWith("mac os x");
-            if (MAC_OS_X) {
-                // hopefully everything is done!
-                System.out.println("Using System.exit(0) only on the mac due to AWT thread not exiting.");
-                logger.info("Using System.exit(0) only on the mac due to AWT thread not exiting.");
-                System.exit(0);
+                String lcOSName = System.getProperty("os.name").toLowerCase();
+                boolean MAC_OS_X = lcOSName.startsWith("mac os x");
+                if (MAC_OS_X) {
+                    // hopefully everything is done!
+                    System.out.println("Using System.exit(0) only on the mac due to AWT thread not exiting.");
+                    logger.info("Using System.exit(0) only on the mac due to AWT thread not exiting.");
+                    System.exit(0);
+                }
+                return;
             }
-        }else{
-            t.schedule(this, ACTION_INTERVAL_MILLIS);
+
+            try {
+                sleep(ACTION_INTERVAL_MILLIS);
+            } catch (InterruptedException e) {}
         }
     }
 
@@ -67,12 +86,14 @@ public class OutputScheduler extends TimerTask{
     public synchronized static OutputScheduler getDefault() {
         if (DEFAULT == null) {
             DEFAULT = new OutputScheduler();
+            DEFAULT.start();
         }
         return DEFAULT;
     }
 
     private static final TimeInterval ACTION_INTERVAL = new TimeInterval(2, UnitImpl.MINUTE);
     private static final long ACTION_INTERVAL_MILLIS = (long)ACTION_INTERVAL.convertTo(UnitImpl.MILLISECOND).get_value();
+    private static final long INITIAL_SLEEP = 10000l;
     private static OutputScheduler DEFAULT = null;
 
     private Set runnables = Collections.synchronizedSet(new HashSet());
