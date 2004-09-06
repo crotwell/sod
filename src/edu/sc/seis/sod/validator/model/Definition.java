4ddd97b1e99759a1c13578f01e57f6d883d5ed64
groves <none@none>
2004-09-06 12:38:51 -0400

    [svn r10378] fixed disappearing self referential ref problem
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/model/Definition.java
+++ src/edu/sc/seis/sod/validator/model/Definition.java
@@ -1,17 +1,17 @@
 /**
  * Definition.java
- *
+ * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod.validator.model;
 
-public class Definition{
-    public Definition(String name, Grammar containingGrammar){
+public class Definition {
+
+    public Definition(String name, Grammar containingGrammar) {
         this(name, containingGrammar, UNDEFINED);
     }
 
-    public Definition(String name, Grammar containingGrammar, int combine){
+    public Definition(String name, Grammar containingGrammar, int combine) {
         this.name = name;
         this.combine = combine;
         this.grammar = containingGrammar;
@@ -22,20 +22,32 @@ public class Definition{
         return null;
     }
 
-    public void set(FormProvider f){
+    public void set(FormProvider f) {
         form = f;
-        if(form instanceof Form){
+        if(form instanceof Form) {
             form = ((Form)form).deref(null, this);
         }
+        f.getClass();
     }
 
-    public Form getForm() { return form.getForm(); }
+    public Form getForm() {
+        //always return a copy so the original doesn't get screwed up. Some
+        // self referential structures will stop being self referential if
+        // they're copied from the original
+        return form.copyWithNewParent(null).getForm();
+    }
 
-    public String getName(){ return name; }
+    public String getName() {
+        return name;
+    }
 
-    public int getCombine(){ return combine; }
+    public int getCombine() {
+        return combine;
+    }
 
-    public Grammar getGrammar(){ return grammar; }
+    public Grammar getGrammar() {
+        return grammar;
+    }
 
     private int combine;
 
@@ -43,7 +55,8 @@ public class Definition{
     public static final int UNDEFINED = 0, CHOICE = 1, INTERLEAVE = 2;
 
     private FormProvider form;
+
     private String name;
+
     private Grammar grammar;
 }
-
