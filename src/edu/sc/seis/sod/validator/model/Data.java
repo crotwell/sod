2c2c46bdcae150a7adfe0574243a05aae6feba20
groves <none@none>
2004-09-09 14:51:30 -0400

    [svn r10424] fixed formatting, removed hashCode
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/model/Data.java
+++ src/edu/sc/seis/sod/validator/model/Data.java
@@ -1,37 +1,40 @@
 /**
  * Data.java
- *
+ * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod.validator.model;
 
 import edu.sc.seis.sod.validator.tour.Tourist;
 
-public class Data extends AbstractForm{
-    public Data(int min, int max, ModelDatatype datatype){
+public class Data extends AbstractForm {
+
+    public Data(int min, int max, ModelDatatype datatype) {
         this(min, max, datatype, null);
     }
 
-    public Data(int min, int max, ModelDatatype datatype, Form parent){
+    public Data(int min, int max, ModelDatatype datatype, Form parent) {
         super(min, max, parent);
         this.datatype = datatype;
     }
 
-    public FormProvider  copyWithNewParent(Form newParent) {
+    public FormProvider copyWithNewParent(Form newParent) {
         Data d = new Data(getMin(), getMax(), getDatatype(), newParent);
         super.copyGutsOver(d);
         return d;
     }
 
-    public int hashCode(){ return super.hashCode() * 37 + datatype.hashCode(); }
+    public ModelDatatype getDatatype() {
+        return datatype;
+    }
 
-    public ModelDatatype getDatatype(){ return datatype; }
+    public String toString() {
+        return "Data of type " + getDatatype();
+    }
 
-    public String toString(){ return "Data of type " + getDatatype(); }
-
-    public void accept(Tourist v) { v.visit(this);}
+    public void accept(Tourist v) {
+        v.visit(this);
+    }
 
     private ModelDatatype datatype;
 }
-
