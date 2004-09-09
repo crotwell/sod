d0e32fb8fdf98339eb5e8709a5b97b20d1264dca
groves <none@none>
2004-09-09 14:44:17 -0400

    [svn r10417] use new Object name parsing in StringTreeBranch
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/eventStation/EventStationAND.java
+++ src/edu/sc/seis/sod/subsetter/eventStation/EventStationAND.java
@@ -12,15 +12,15 @@ import java.util.Iterator;
 import org.w3c.dom.Element;
 
 public final class EventStationAND extends EventStationLogicalSubsetter
-    implements EventStationSubsetter {
+        implements EventStationSubsetter {
 
     public EventStationAND(Element config) throws ConfigurationException {
         super(config);
     }
 
     public StringTree accept(EventAccessOperations o,
-                          Station station,
-                          CookieJar cookieJar) throws Exception {
+                             Station station,
+                             CookieJar cookieJar) throws Exception {
         Iterator it = filterList.iterator();
         ArrayList reasons = new ArrayList();
         StringTree result = new StringTreeLeaf(this, true);
@@ -29,11 +29,11 @@ public final class EventStationAND extends EventStationLogicalSubsetter
             result = filter.accept(o, station, cookieJar);
             reasons.add(result);
         }
-        if (reasons.size() < filterList.size()) {
+        if(reasons.size() < filterList.size()) {
             reasons.add(new StringTreeLeaf("ShortCurcit", result.isSuccess()));
         }
-        return new StringTreeBranch("EventStationAND",
+        return new StringTreeBranch(this,
                                     result.isSuccess(),
-                                        (StringTree[])reasons.toArray(new StringTree[0]));
+                                    (StringTree[])reasons.toArray(new StringTree[0]));
     }
 }// EventStationAND
