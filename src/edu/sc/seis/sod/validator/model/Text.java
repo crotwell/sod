6c91c04d3e5bc8a8d44edc3ec8d7bb72846c6f9f
groves <none@none>
2004-09-09 14:52:41 -0400

    [svn r10429] formatting
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/validator/model/Text.java
+++ src/edu/sc/seis/sod/validator/model/Text.java
@@ -1,26 +1,33 @@
 /**
  * Text.java
- *
+ * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod.validator.model;
 
 import edu.sc.seis.sod.validator.tour.Tourist;
 
+public class Text extends Empty {
 
-public class Text extends Empty{
-    public Text(){ this(null); }
+    public Text() {
+        this(null);
+    }
 
-    public Text(Form parent){ super(parent); }
+    public Text(Form parent) {
+        super(parent);
+    }
 
-    public FormProvider  copyWithNewParent(Form newParent){
+    public FormProvider copyWithNewParent(Form newParent) {
         Text t = new Text(newParent);
         super.copyGutsOver(t);
         return t;
     }
 
-    public String toString(){ return "Any Text"; }
+    public String toString() {
+        return "Any Text";
+    }
 
-    public void accept(Tourist v) { v.visit(this);}
+    public void accept(Tourist v) {
+        v.visit(this);
+    }
 }
