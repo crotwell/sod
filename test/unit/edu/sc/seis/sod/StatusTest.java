459a83b6a011b3826e50f8cce4725fa0c67f1db3
groves <none@none>
2004-09-09 14:57:29 -0400

    [svn r10434] fixed unused exception
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/StatusTest.java
+++ test/unit/edu/sc/seis/sod/StatusTest.java
@@ -10,7 +10,7 @@ import junit.framework.TestCase;
 
 public class StatusTest extends TestCase {
 
-    public void testGetFromInt() throws NoSuchFieldException{
+    public void testGetFromInt(){
         for (int i = 0; i < Status.ALL.length; i++) {
             for (int j = 0; j < Status.ALL[i].length; j++) {
                 assertEquals("Check status for "+i+", "+j,
