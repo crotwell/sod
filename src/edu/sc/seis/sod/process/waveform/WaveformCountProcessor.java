6484c982b3571e3c4c0fad5202e075c1fd7f9146
danala <none@none>
2004-09-09 16:37:34 -0400

    [svn r10452] now implements WaveformMonitor interface and it doesnt anymore count the waveforms from database
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/WaveformCountProcessor.java
+++ src/edu/sc/seis/sod/process/waveform/WaveformCountProcessor.java
@@ -12,24 +12,35 @@ import edu.sc.seis.sod.status.StringTreeLeaf;
 import java.io.FileWriter;
 import java.sql.SQLException;
 import org.w3c.dom.Element;
+import edu.sc.seis.sod.status.waveformArm.WaveformMonitor;
+import edu.sc.seis.sod.EventChannelPair;
+import java.io.IOException;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 
-public class WaveformCountProcessor implements WaveformProcess {
+public class WaveformCountProcessor implements WaveformMonitor {
+	
     public WaveformCountProcessor(Element config) throws SQLException {
-        jdbcEventChannelStatus= new JDBCEventChannelStatus();
     }
-
-    public WaveformResult process(EventAccessOperations event, Channel channel,
-                                         RequestFilter[] original, RequestFilter[] available,
-                                         LocalSeismogramImpl[] seismograms,
-                                         CookieJar cookieJar) throws Exception {
-
-        int num_waveforms = jdbcEventChannelStatus.getNumOfStatus(Status.get(Stage.PROCESSOR,
-                                                                             Standing.SUCCESS));
-        FileWriter fileWriter = new FileWriter("waveforms.txt");
-        fileWriter.write("Number of Waveforms processed = " + num_waveforms);
-        fileWriter.close();
-        return new WaveformResult(seismograms, new StringTreeLeaf(this, true));
-    }
-    JDBCEventChannelStatus jdbcEventChannelStatus;
+	public void update(EventChannelPair ecp) {
+		Status processorSuccess = Status.get(Stage.PROCESSOR,Standing.SUCCESS);
+		if(ecp.getStatus() == processorSuccess) {
+			waveformCounter++;
+			FileWriter fileWriter = null;
+			try {
+				fileWriter = new FileWriter("waveforms.txt");
+				fileWriter.write("Number of Waveforms processed = " + waveformCounter);
+			}catch(IOException ie) {
+				GlobalExceptionHandler.handle("problem writing the waveformCount into file ",ie);
+			}finally {
+				try {
+					fileWriter.close();
+				}catch(IOException ie) {
+					GlobalExceptionHandler.handle("problem closing the output file" +
+													  "in WaveformCountProcessor ",ie);
+				}
+			}
+		}
+	}
+	int waveformCounter = 0;
 }
