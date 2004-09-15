b7f373634c9c5862b02e73137c6ad043426cf9f1
groves <none@none>
2004-09-15 10:07:22 -0400

    [svn r10529] write to temp file then move instead of writing directly to output
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/WaveformCountProcessor.java
+++ src/edu/sc/seis/sod/process/waveform/WaveformCountProcessor.java
@@ -16,8 +16,10 @@ import edu.sc.seis.sod.status.waveformArm.WaveformMonitor;
 
 public class WaveformCountProcessor implements WaveformMonitor {
 
+    private static final String WAVEFORM_FILE = "waveforms.txt";
+
     public WaveformCountProcessor(Element config) throws IOException {
-        waveformCounter = getNumWaveforms("waveforms.txt");
+        waveformCounter = getNumWaveforms(WAVEFORM_FILE);
     }
 
     public void update(EventChannelPair ecp) {
@@ -26,8 +28,12 @@ public class WaveformCountProcessor implements WaveformMonitor {
             waveformCounter++;
             FileWriter fileWriter = null;
             try {
-                fileWriter = new FileWriter("waveforms.txt");
+                File temp = File.createTempFile("", WAVEFORM_FILE);
+                fileWriter = new FileWriter(temp);
                 fileWriter.write(description + waveformCounter);
+                File actualOutputLocation = new File(WAVEFORM_FILE);
+                actualOutputLocation.delete();
+                temp.renameTo(actualOutputLocation);
             } catch(IOException ie) {
                 GlobalExceptionHandler.handle("problem writing the waveformCount into file ",
                                               ie);
