92d040888cac58bf5f8486c74985d8b75726cae8
groves <none@none>
2004-09-16 15:06:56 -0400

    [svn r10571] use alphabetic sort for nearly everything
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/data/templates/sorttable.js
+++ src/edu/sc/seis/sod/data/templates/sorttable.js
@@ -1,6 +1,7 @@
 // from http://www.kryogenix.org/code/browser/sorttable/
 
 var SORT_COLUMN_INDEX;
+var VALUE_EXTRACTOR;
 var img_dir = "";
 
 function set_img_dir(new_img_dir){
@@ -80,7 +81,7 @@ function ts_resortTable(lnk) {
     var td = lnk.parentNode;
     var table = getParent(td,'TABLE');
     var tr = getParent(td, 'TR');
-
+    if (table.rows.length <= 1) return;
     /* Safari 1.2 seems to be broken and doesn't understand cellIndex
     so we're traversing the cells to find the right one.  If you run across this
     in the future you should check http://www.quirksmode.org/dom/w3c_html.html
@@ -89,39 +90,27 @@ function ts_resortTable(lnk) {
     var kids = tr.cells;
     for (i=0; i<kids.length; i++){
         if(kids[i] == td){ column = i; }
-    }
-
-    // Work out a type for the column
-    if (table.rows.length <= 1) return;
+    }   
+    SORT_COLUMN_INDEX = column;    
+    
     var itm = ts_getInnerText(table.rows[1].cells[column]);
-    sortfn = ts_sort_caseinsensitive;
-    if (itm.match(/^[£$]/)) sortfn = ts_sort_currency;
-    if (itm.match(/^[\d\.]+/)) sortfn = ts_sort_numeric;
-    if (itm.match(/^\d\d[\/-]\d\d[\/-]\d\d\d\d/)) sortfn = ts_sort_date;
-    if (itm.match(/^\d\d[\/-]\d\d[\/-]\d\d/)) sortfn = ts_sort_date;
-    SORT_COLUMN_INDEX = column;
-    var firstRow = new Array();
+    VALUE_EXTRACTOR = defaultExtract;
+    if(itm.match(/^\d\d\d\d/)) VALUE_EXTRACTOR = defaultExtract;
+    else if (itm.match(/^[\d\.]+/)) VALUE_EXTRACTOR = numericExtract;
+    
     var newRows = new Array();
-    for (i=0;i<table.rows[0].length;i++) { firstRow[i] = table.rows[0][i]; }
     for (j=1;j<table.rows.length;j++) { newRows[j-1] = table.rows[j]; }
-
-    newRows.sort(sortfn);
 
     if (span.getAttribute("sortdir") == 'down') {
         ARROW = '<img src="'+img_dir+'down.gif"/>';
-        newRows.reverse();
+        newRows.sort(ts_sort_reverse);
         span.setAttribute('sortdir','up');
     } else {
         ARROW = '<img src="'+img_dir+'up.gif"/>';
+        newRows.sort(ts_sort_default);
         span.setAttribute('sortdir','down');
     }
-
-    // We appendChild rows that already exist to the tbody, so it moves them rather than creating new ones
-    // don't do sortbottom rows
-    for (i=0;i<newRows.length;i++) { if (!newRows[i].className || (newRows[i].className && (newRows[i].className.indexOf('sortbottom') == -1))) table.tBodies[0].appendChild(newRows[i]);}
-    // do sortbottom rows only
-    for (i=0;i<newRows.length;i++) { if (newRows[i].className && (newRows[i].className.indexOf('sortbottom') != -1)) table.tBodies[0].appendChild(newRows[i]);}
-
+ for (i=0;i<newRows.length;i++) {table.tBodies[0].appendChild(newRows[i]);}
     // Delete any other arrows there may be showing
     var allspans = document.getElementsByTagName("span");
     for (var ci=0;ci<allspans.length;ci++) {
@@ -131,7 +120,6 @@ function ts_resortTable(lnk) {
             }
         }
     }
-
     span.innerHTML = ARROW;
 }
 
@@ -142,83 +130,26 @@ function getParent(el, pTagName) {
     else
         return getParent(el.parentNode, pTagName);
 }
-function ts_sort_date(a,b) {
-    // y2k notes: two digit years less than 50 are treated as 20XX, greater than 50 are treated as 19XX
-    aa = ts_getInnerText(a.cells[SORT_COLUMN_INDEX]);
-    bb = ts_getInnerText(b.cells[SORT_COLUMN_INDEX]);
-
-    if(aa.length < 10){
-        yr = aa.substr(6,2);
-        if (parseInt(yr) < 50) { yr = '20'+yr; } else { yr = '19'+yr; }
-        dt1 = yr+aa.substr(3,2)+aa.substr(0,2);
-    }else{
-        dt1 = aa.substr(6,4)+aa.substr(3,2)+aa.substr(0,2);
-    }
-    if (aa.length >= 19){
-        dt1 += aa.substr(11,2) + aa.substr(14,2) + aa.substr(17,2);
-    }
-
-    if(bb.length < 10){
-        yr = bb.substr(6,2);
-        if (parseInt(yr) < 50) { yr = '20'+yr; } else { yr = '19'+yr; }
-        dt2 = yr+bb.substr(3,2)+bb.substr(0,2);
-    }else{
-        dt2 = bb.substr(6,4)+bb.substr(3,2)+bb.substr(0,2);
-    }
-    if(aa.length >= 19){
-        dt2 += bb.substr(11,2) + bb.substr(14,2) + bb.substr(17,2);
-    }
-
-    if (dt1==dt2) return 0;
-    if (dt1<dt2) return -1;
-    return 1;
-}
-
-function ts_sort_currency(a,b) {
-    aa = ts_getInnerText(a.cells[SORT_COLUMN_INDEX]).replace(/[^0-9.]/g,'');
-    bb = ts_getInnerText(b.cells[SORT_COLUMN_INDEX]).replace(/[^0-9.]/g,'');
-    return parseFloat(aa) - parseFloat(bb);
-}
-
-function ts_sort_numeric(a,b) {
-    aa = parseFloat(ts_getInnerText(a.cells[SORT_COLUMN_INDEX]));
-    if (isNaN(aa)) aa = 0;
-    bb = parseFloat(ts_getInnerText(b.cells[SORT_COLUMN_INDEX]));
-    if (isNaN(bb)) bb = 0;
-    return aa-bb;
-}
-
-function ts_sort_caseinsensitive(a,b) {
-    aa = ts_getInnerText(a.cells[SORT_COLUMN_INDEX]).toLowerCase();
-    bb = ts_getInnerText(b.cells[SORT_COLUMN_INDEX]).toLowerCase();
-    if (aa==bb) return 0;
-    if (aa<bb) return -1;
-    return 1;
-}
 
 function ts_sort_default(a,b) {
-    aa = ts_getInnerText(a.cells[SORT_COLUMN_INDEX]);
-    bb = ts_getInnerText(b.cells[SORT_COLUMN_INDEX]);
+    aa = VALUE_EXTRACTOR(ts_getInnerText(a.cells[SORT_COLUMN_INDEX]));
+    bb = VALUE_EXTRACTOR(ts_getInnerText(b.cells[SORT_COLUMN_INDEX]));
     if (aa==bb) return 0;
     if (aa<bb) return -1;
     return 1;
 }
 
-
-function addEvent(elm, evType, fn, useCapture)
-// addEvent and removeEvent
-// cross-browser event handling for IE5+,  NS6 and Mozilla
-// By Scott Andrew
-{
-  if (elm.addEventListener){
-    elm.addEventListener(evType, fn, useCapture);
-    return true;
-  } else if (elm.attachEvent){
-    var r = elm.attachEvent("on"+evType, fn);
-    return r;
-  } else {
-    alert("Handler could not be removed");
-  }
+function ts_sort_reverse(a, b){
+    var defResult = ts_sort_default(a, b);
+    if(defResult == 0){ return 0; }
+    else if(defResult > 0){ return -1; }
+    else{ return 1; }
 }
 
+function defaultExtract(thing){
+    return thing;
+}
 
+function numericExtract(thing){
+    return parseFloat(thing);
+}
