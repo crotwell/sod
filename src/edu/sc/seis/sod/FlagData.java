94d08dc137057e1297f3a6fcf24b89a5579a79c2
groves <none@none>
2004-09-20 17:45:00 -0400

    [svn r10651] added toString
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/FlagData.java
+++ src/edu/sc/seis/sod/FlagData.java
@@ -1,17 +1,35 @@
 package edu.sc.seis.sod;
+
 import java.io.Serializable;
 
-public class FlagData implements Serializable{
-	public FlagData(int flagLoc,int y_top,int y_bottom) {
-		this.flagLoc = flagLoc;
-		this.y_top = y_top;
-		this.y_bottom = y_bottom;
-	}
-	public int getFlagLoc() { return this.flagLoc;}
-	public int getFlagBottom() { return this.y_bottom;}
-	public int getFlagTop() { return this.y_top;}
-	
-	private int y_top = -1;
-	private int y_bottom = -1;
-	private int flagLoc = -1;
+public class FlagData implements Serializable {
+
+    public FlagData(int flagLoc, int y_top, int y_bottom) {
+        this.flagLoc = flagLoc;
+        this.y_top = y_top;
+        this.y_bottom = y_bottom;
+    }
+
+    public int getFlagLoc() {
+        return this.flagLoc;
+    }
+
+    public int getFlagBottom() {
+        return this.y_bottom;
+    }
+
+    public int getFlagTop() {
+        return this.y_top;
+    }
+
+    public String toString() {
+        return "Flag X: " + getFlagLoc() + " Bottom: " + getFlagBottom()
+                + " Top: " + getFlagTop();
+    }
+
+    private int y_top = -1;
+
+    private int y_bottom = -1;
+
+    private int flagLoc = -1;
 }
