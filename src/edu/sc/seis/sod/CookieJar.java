f0548377fa5cc0a98ed35020666903ccbb14e00c
groves <none@none>
2004-09-20 16:52:35 -0400

    [svn r10642] formatting
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/CookieJar.java
+++ src/edu/sc/seis/sod/CookieJar.java
@@ -28,37 +28,36 @@ import edu.sc.seis.sod.database.waveform.JDBCVelocityContext;
 import edu.sc.seis.sod.status.FissuresFormatter;
 
 /**
- * CookieJar exists as a way for various subsetters and processors in
- * the waveform arm to pass information down the chain. It is implemented
- * as a Velocity Context which allows the cusomization of output status
- * pages through velocity template files. The Event and Channel are placed in
- * the context with names "sod_event" and "sod_channel".
- *
- *
- * Created: Thu Dec 13 18:18:48 2001
- *
- * @author <a href="mailto:">Philip Crotwell</a>
+ * CookieJar exists as a way for various subsetters and processors in the
+ * waveform arm to pass information down the chain. It is implemented as a
+ * Velocity Context which allows the cusomization of output status pages through
+ * velocity template files. The Event and Channel are placed in the context with
+ * names "sod_event" and "sod_channel". Created: Thu Dec 13 18:18:48 2001
+ * 
+ * @author <a href="mailto:">Philip Crotwell </a>
  * @version
  */
-
 public class CookieJar {
 
-    public CookieJar (EventChannelPair ecp) throws SQLException {
+    public CookieJar(EventChannelPair ecp) throws SQLException {
         this.ecp = ecp;
-        if (jdbcCookie == null) {
+        if(jdbcCookie == null) {
             jdbcCookie = new JDBCEventChannelCookieJar();
         }
         memoryContext = getChannelContext(ecp.getEvent(), ecp.getChannel());
-        memoryContext.put("sod_cookieJar", this); // needed for eval and recurse velocity tool
+        memoryContext.put("sod_cookieJar", this); // needed for eval and recurse
+        // velocity tool
         updateStatus();
         context = new JDBCVelocityContext(ecp, jdbcCookie, memoryContext);
-        ((Context)memoryContext.get("sod_site_context")).put(ChannelIdUtil.toString(ecp.getChannel().get_id()), context);
+        ((Context)memoryContext.get("sod_site_context")).put(ChannelIdUtil.toString(ecp.getChannel()
+                                                                     .get_id()),
+                                                             context);
     }
 
     /**
      * Returns the Velocity Context that stores the put data
-     *
-     * @return    a  VelocityContext
+     * 
+     * @return a VelocityContext
      */
     public Context getContext() {
         return context;
@@ -78,20 +77,21 @@ public class CookieJar {
 
     Context context;
 
-    /** this holds items that are not perisent and can be recreated from the
-     * EventChannelPair. */
+    /**
+     * this holds items that are not perisent and can be recreated from the
+     * EventChannelPair.
+     */
     VelocityContext memoryContext;
 
     EventChannelPair ecp;
 
     static JDBCEventChannelCookieJar jdbcCookie = null;
 
-
     public static VelocityContext getChannelContext(EventAccessOperations event,
                                                     Channel channel) {
         VelocityContext siteContext = getSiteContext(event, channel.my_site);
         String chanIdStr = ChannelIdUtil.toString(channel.get_id());
-        if ( ! siteContext.containsKey(chanIdStr)) {
+        if(!siteContext.containsKey(chanIdStr)) {
             VelocityContext chanContext = new VelocityContext(siteContext);
             chanContext.put("channel_code", channel.get_code());
             chanContext.put("channel_id", channel.get_id());
@@ -101,20 +101,18 @@ public class CookieJar {
             // channel into the site context for use by other channels in the
             // site
             chanContext.put("sod_site_context", siteContext);
-            siteContext.put(chanIdStr+"_memory", chanContext);
+            siteContext.put(chanIdStr + "_memory", chanContext);
             //siteContext.put(chanIdStr, chanContext);
             ((Collection)siteContext.get("allChanIds")).add(chanIdStr);
         }
-        return (VelocityContext)siteContext.get(chanIdStr+"_memory");
-
+        return (VelocityContext)siteContext.get(chanIdStr + "_memory");
     }
 
     public static VelocityContext getSiteContext(EventAccessOperations event,
                                                  Site site) {
-        VelocityContext staContext =
-            getStationContext(event, site.my_station);
+        VelocityContext staContext = getStationContext(event, site.my_station);
         String siteIdStr = SiteIdUtil.toString(site.get_id());
-        if ( ! staContext.containsKey(siteIdStr)) {
+        if(!staContext.containsKey(siteIdStr)) {
             VelocityContext siteContext = new VelocityContext(staContext);
             siteContext.put("site_code", site.get_code());
             siteContext.put("site_id", site.get_id());
@@ -127,9 +125,10 @@ public class CookieJar {
 
     public static VelocityContext getStationContext(EventAccessOperations event,
                                                     Station sta) {
-        VelocityContext nContext = getNetworkContext(event, sta.my_network.get_id());
+        VelocityContext nContext = getNetworkContext(event,
+                                                     sta.my_network.get_id());
         String staIdStr = StationIdUtil.toString(sta.get_id());
-        if ( ! nContext.containsKey(staIdStr)) {
+        if(!nContext.containsKey(staIdStr)) {
             VelocityContext staContext = new VelocityContext(nContext);
             staContext.put("station_code", sta.get_code());
             staContext.put("station_id", sta.get_id());
@@ -138,14 +137,13 @@ public class CookieJar {
             ((Collection)nContext.get("allStationIds")).add(staIdStr);
         }
         return (VelocityContext)nContext.get(staIdStr);
-
     }
 
     public static VelocityContext getNetworkContext(EventAccessOperations event,
                                                     NetworkId netId) {
         VelocityContext eContext = getEventContext(event);
         String netIdStr = NetworkIdUtil.toString(netId);
-        if ( ! eContext.containsKey(netIdStr)) {
+        if(!eContext.containsKey(netIdStr)) {
             VelocityContext netContext = new VelocityContext(eContext);
             netContext.put("network_code", netId.network_code);
             netContext.put("network_id", netId);
@@ -153,13 +151,12 @@ public class CookieJar {
             eContext.put(netIdStr, netContext);
         }
         return (VelocityContext)eContext.get(netIdStr);
-
     }
 
     public synchronized static VelocityContext getEventContext(EventAccessOperations event) {
-        if ( ! eventContexts.containsKey(event)) {
+        if(!eventContexts.containsKey(event)) {
             // check size and kill oldest if too big
-            if (eventOrder.size() >= MAX_EVENTS) {
+            if(eventOrder.size() >= MAX_EVENTS) {
                 EventAccessOperations oldEvent = (EventAccessOperations)eventOrder.getLast();
                 cleanEvent(oldEvent);
             }
@@ -171,11 +168,13 @@ public class CookieJar {
         return (VelocityContext)eventContexts.get(event);
     }
 
-    /** removes storage for VelocityContexts associated with the given
-     * event. Should be called once there is no longer any chance of
-     * further processing for an event. */
+    /**
+     * removes storage for VelocityContexts associated with the given event.
+     * Should be called once there is no longer any chance of further processing
+     * for an event.
+     */
     public static void cleanEvent(EventAccessOperations event) {
-        logger.debug("Cleaning event "+event);
+        logger.debug("Cleaning event " + event);
         eventOrder.remove(event);
         eventContexts.remove(event);
     }
@@ -191,7 +190,6 @@ public class CookieJar {
     private static HashMap eventContexts = new HashMap();
 
     static VelocityContext commonContext = new VelocityContext();
-
     static {
         commonContext.put("FERegion", ParseRegions.getInstance());
         commonContext.put("fissures", new FissuresFormatter());
@@ -204,8 +202,4 @@ public class CookieJar {
     }
 
     private static final Logger logger = Logger.getLogger(CookieJar.class);
-
-
 }// CookieJar
-
-
