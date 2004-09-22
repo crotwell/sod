b79e629fc331134de218d30a3440b3a11ef04875
groves <none@none>
2004-09-22 17:00:05 -0400

    [svn r10680] format output to only two decimal places
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/waveformArm/WPHTemplate.java
+++ src/edu/sc/seis/sod/status/waveformArm/WPHTemplate.java
@@ -1,11 +1,14 @@
 /**
  * WPHTemplate.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.sod.status.waveformArm;
 
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
@@ -15,28 +18,35 @@ import edu.sc.seis.sod.Start;
 import edu.sc.seis.sod.Status;
 import edu.sc.seis.sod.database.waveform.JDBCEventChannelStatus;
 import edu.sc.seis.sod.status.AllTypeTemplate;
-import java.sql.SQLException;
 
-public class WPHTemplate extends AllTypeTemplate{
-    public WPHTemplate() throws SQLException{
+public class WPHTemplate extends AllTypeTemplate {
+
+    public WPHTemplate() throws SQLException {
         ecs = new JDBCEventChannelStatus();
     }
+
     public String getResult() {
         try {
-            return "" + ecs.getNumOfStatus(Status.get(Stage.PROCESSOR,
-                                                      Standing.SUCCESS))/getElapsedTime();
-        } catch (SQLException e) {
+            int numSuccessful = ecs.getNumOfStatus(SUCCESS);
+            double elapsedTime = getElapsedTime();
+            return df.format(numSuccessful / elapsedTime);
+        } catch(SQLException e) {
             GlobalExceptionHandler.handle(e);
             return "";
         }
     }
 
     private double getElapsedTime() {
-        return ClockUtil.now().subtract(Start.getStartTime()).convertTo(UnitImpl.HOUR).getValue();
+        MicroSecondDate now = ClockUtil.now();
+        MicroSecondDate startTime = Start.getStartTime();
+        TimeInterval elapsedTime = now.subtract(startTime);
+        return elapsedTime.convertTo(UnitImpl.HOUR).getValue();
     }
+
+    private DecimalFormat df = new DecimalFormat("0.00");
 
     private JDBCEventChannelStatus ecs;
 
-
+    private static final Status SUCCESS = Status.get(Stage.PROCESSOR,
+                                                     Standing.SUCCESS);
 }
-
