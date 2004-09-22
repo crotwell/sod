dc4bb8c6658521fa40f85aff2f492970b2ad5666
groves <none@none>
2004-09-22 16:59:49 -0400

    [svn r10679] switched to MB
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/waveformArm/SacDataWrittenTemplate.java
+++ src/edu/sc/seis/sod/status/waveformArm/SacDataWrittenTemplate.java
@@ -1,11 +1,16 @@
 package edu.sc.seis.sod.status.waveformArm;
 
+import java.text.DecimalFormat;
 import edu.sc.seis.sod.process.waveform.SaveSeismogramToFile;
 import edu.sc.seis.sod.status.AllTypeTemplate;
 
 public class SacDataWrittenTemplate extends AllTypeTemplate{
     public String getResult() {
-        return "" + SaveSeismogramToFile.getBytesWritten();
+        return df.format(SaveSeismogramToFile.getBytesWritten()/BYTES_IN_MB);
     }
+    
+    private static final double BYTES_IN_MB = 1024*1024;
+    
+    DecimalFormat df = new DecimalFormat("0.00");
 }
 
