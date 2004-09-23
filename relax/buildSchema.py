80cdeb8b30906466de54065984fdedd26e56c631
groves <none@none>
2004-09-23 10:08:53 -0400

    [svn r10682] moved from schemaBuilderScriptBuilder, added run script functionality, made work
    --HG--
    branch : sod

---
+++ relax/buildSchema.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python -O
+import sys, os
+from optparse import OptionParser
+sys.path.append('../../devTools/maven')
+import scriptBuilder, ProjectParser, depCopy
+class buildSchemaParams(scriptBuilder.scriptParameters):
+    homeloc = '.'
+    def __init__(self, mods, proj, name, mainclass):
+        scriptBuilder.scriptParameters.__init__(self)
+        for mod in mods: self.update(mod)
+        self.name = name
+        homevar = self.add('SOD_HOME', self.homeloc, 'initial', 1, True)
+        libvar = self.getVar('LIB', 'initial')
+        if os.environ.has_key('OS') and os.environ['OS'] == 'Windows_NT':
+            stdin, stdout = os.popen2('cygpath -w ' + proj.repo)
+            libvar.setValue(stdout.read())
+        else:
+            libvar.setValue(proj.repo)
+        self.mainclass = mainclass
+        self.xOptions['mx']='mx256m'
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-c", "--clean", dest="clean",
+                      help="remove scripts after running",
+                      default=False,
+                      action="store_true")
+    options = parser.parse_args()[0]
+    proj = ProjectParser.ProjectParser('../project.xml')
+    scripts = [('buildSchema', 'com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng'),
+               ('buildGrouperSchema', 'com.sun.msv.writer.relaxng.Driver network/grouper.rng > ../src/edu/sc/seis/sod/data/grouper.rng'),
+               ('serializeGrammar', 'edu.sc.seis.sod.editor.SchemaGrammar')]#,
+              # ('validateExamples', 'edu.sc.seis.sod.validator.example.ExampleValidator')]
+    for name, mainclass in scripts:
+        print 'running ' + name
+        if os.environ.has_key('OS') and os.environ['OS'] == 'Windows_NT':
+            scriptBuilder.setVarWindows()
+            scriptBuilder.build(buildSchemaParams([scriptBuilder.windowsParameters()], proj, name, mainclass), proj, useMavenJars=True)
+            os.spawnlp(os.P_WAIT, name + '.bat', 'cmd', name + '.bat')
+            if options.clean: os.remove(name + '.bat')
+        else:
+            scriptBuilder.setVarSh()
+            scriptBuilder.build(buildSchemaParams([], proj, name, mainclass), proj, useMavenJars=True)
+            os.spawnlp(os.P_WAIT, './' + name + '.sh', 'sh', './' + name + '.sh')
+            if options.clean: os.remove(name + '.sh')
