0acae0d54a02201b22af9c000678662fcb2cd23b
groves <none@none>
2004-09-23 10:21:11 -0400

    [svn r10683] made work on mac, added slightly psychotic reminder to commit things
    --HG--
    branch : sod

--- relax/buildSchema.py
+++ relax/buildSchema.py
@@ -41,5 +41,11 @@ if __name__ == "__main__":
         else:
             scriptBuilder.setVarSh()
             scriptBuilder.build(buildSchemaParams([], proj, name, mainclass), proj, useMavenJars=True)
-            os.spawnlp(os.P_WAIT, './' + name + '.sh', 'sh', './' + name + '.sh')
+            os.spawnlp(os.P_WAIT, './' + name + '.sh', 'sh', '')
             if options.clean: os.remove(name + '.sh')
+    print """Done. If you're not going to monkey with the schema any more you'll want
+to commit elementNode.ser, sod.rng, and grouper.rng in
+src/edu/sc/seis/sod/data.  If not, I'll come complain to you.  If I'm
+reading this message at a later date I'll want to commit elementNode.ser,
+sod.rng and grouper.rng in src/edu/sc/seis/sod/data so that someone else
+doesn't come to complain to me."""
