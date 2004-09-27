6e0bfdd555a55c7a79bfa97a2922f54f6484a8e3
groves <none@none>
2004-09-27 12:46:59 -0400

    [svn r10714] added event channel pairs for each channel
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/ChannelGroup.java
+++ src/edu/sc/seis/sod/ChannelGroup.java
@@ -1,41 +1,66 @@
 /**
  * ChannelGroup.java
- *
+ * 
  * @author Jagadeesh Danala
  * @version
  */
-
 package edu.sc.seis.sod;
+
 import org.apache.log4j.Logger;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.network.ChannelIdUtil;
 
 public class ChannelGroup {
+
     public ChannelGroup(Channel[] channels) {
         this.channels = channels;
+        ecps = new EventChannelPair[channels.length];
+    }
+
+    public void addEventChannelPair(EventChannelPair ecp) {
+        int index = getIndex(ecp.getChannel());
+        if(index != -1) {
+            ecps[index] = ecp;
+        } else {
+            throw new IllegalArgumentException("Tried to add event channel pair to channel group with channel not in channel group");
+        }
+    }
+
+    public EventChannelPair getEventChannelPair(Channel chan) {
+        int index = getIndex(chan);
+        if(index != -1) {
+            return ecps[index];
+        } else {
+            throw new IllegalArgumentException("Tried to get event channel pair for channel not in channel group");
+        }
     }
 
     public Channel[] getChannels() {
         return channels;
     }
+
     public boolean contains(Channel c) {
-        for(int i=0;i<channels.length;i++) {
-            if(channels[i].equals(c)) return true;
+        return getIndex(c) != -1;
+    }
+
+    private int getIndex(Channel chan) {
+        for(int i = 0; i < channels.length; i++) {
+            if(channels[i].equals(chan)) return i;
         }
         // didn't find by object equals, check for ids
-        for(int i=0;i<channels.length;i++) {
-            if(ChannelIdUtil.areEqual(channels[i].get_id(), c.get_id())) {
-                logger.warn("Found two channels with same id that are not equals()"+ChannelIdUtil.toString(c.get_id()));
-                return true;
+        for(int i = 0; i < channels.length; i++) {
+            if(ChannelIdUtil.areEqual(channels[i].get_id(), chan.get_id())) {
+                logger.warn("Found two channels with same id that are not equals()"
+                        + ChannelIdUtil.toString(chan.get_id()));
+                return i;
             }
         }
-        return false;
+        return -1;
     }
+
     private Channel[] channels;
 
+    private EventChannelPair[] ecps;
+
     private static final Logger logger = Logger.getLogger(ChannelGroup.class);
-
 }
-
-
-
