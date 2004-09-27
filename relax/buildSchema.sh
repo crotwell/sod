9c377eec156097485d41f2e098b944a2327453a9
groves <none@none>
2004-09-27 11:01:26 -0400

    [svn r10700] replaced by buildSchema.py
    --HG--
    branch : sod

--- relax/buildSchema.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python schemaBuilderScriptBuilder.py
-sh bigSchemaBuilder.sh
-echo Done.  If you're not going to monkey with the schema any more you'll want to commit elementNode.ser, sod.rng, and grouper.rng in src/edu/sc/seis/sod/data.  If not, I'll come complain to you.  If I'm reading this message at a later date I'll want to commit elementNode.ser, sod.rng and grouper.rng in src/edu/sc/seis/sod/data so that someone else doesn't come to complain to me.  
