92f69c2795056b64b3b638dc268422b318688196
groves <none@none>
2004-10-19 15:24:32 -0400

    [svn r10929] scads of options for your quick schema editing pleasure
    --HG--
    branch : sod

--- relax/buildSchema.py
+++ relax/buildSchema.py
@@ -2,7 +2,9 @@
 import sys, os
 from optparse import OptionParser
 sys.path.append('../../devTools/maven')
-import scriptBuilder, ProjectParser, depCopy
+import scriptBuilder, ProjectParser, depCopy, utils
+sys.path.append('..')
+import build
 class buildSchemaParams(scriptBuilder.scriptParameters):
     homeloc = '.'
     def __init__(self, mods, proj, name, mainclass):
@@ -18,15 +20,32 @@ class buildSchemaParams(scriptBuilder.scriptParameters):
             libvar.setValue(proj.repo)
         self.mainclass = mainclass
         self.xOptions['mx']='mx256m'
+        
+def run(name, mainclass, clean):
+    print 'running ' + name
+    if os.environ.has_key('OS') and os.environ['OS'] == 'Windows_NT':
+        scriptBuilder.setVarWindows()
+        scriptBuilder.build(buildSchemaParams([scriptBuilder.windowsParameters()], proj, name, mainclass), proj, useMavenJars=True)
+        os.spawnlp(os.P_WAIT, name + '.bat', 'cmd', name + '.bat')
+        if clean: os.remove(name + '.bat')
+    else:
+        scriptBuilder.setVarSh()
+        scriptBuilder.build(buildSchemaParams([], proj, name, mainclass), proj, useMavenJars=True)
+        os.spawnlp(os.P_WAIT, './' + name + '.sh', 'sh', '')
+        if clean: os.remove(name + '.sh')
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-c", "--clean", dest="clean",
-                      help="remove scripts after running",
+    parser.add_option("-f", "--filthy", dest="filthy",
+                      help="leave scripts after running",
                       default=False,
                       action="store_true")
     parser.add_option("-v", "--validate", dest="validate",
                       help="validate examples",
+                      default=False,
+                      action="store_true")
+    parser.add_option("-b", "--build", dest="build",
+                      help="build jar after the schemas are built but before the examples are validated.  The jars will only be built if the big schema is older than the rng files",
                       default=False,
                       action="store_true")
     options = parser.parse_args()[0]
@@ -34,21 +53,20 @@ if __name__ == "__main__":
     scripts = [('buildSchema', 'com.sun.msv.writer.relaxng.Driver sod.rng > ../src/edu/sc/seis/sod/data/sod.rng'),
                ('buildGrouperSchema', 'com.sun.msv.writer.relaxng.Driver network/grouper.rng > ../src/edu/sc/seis/sod/data/grouper.rng'),
                ('serializeGrammar', 'edu.sc.seis.sod.editor.SchemaGrammar')]
-    if validate:
-        scripts = [('validateExamples', 'edu.sc.seis.sod.validator.example.ExampleValidator')]
-    for name, mainclass in scripts:
-        print 'running ' + name
-        if os.environ.has_key('OS') and os.environ['OS'] == 'Windows_NT':
-            scriptBuilder.setVarWindows()
-            scriptBuilder.build(buildSchemaParams([scriptBuilder.windowsParameters()], proj, name, mainclass), proj, useMavenJars=True)
-            os.spawnlp(os.P_WAIT, name + '.bat', 'cmd', name + '.bat')
-            if options.clean: os.remove(name + '.bat')
-        else:
-            scriptBuilder.setVarSh()
-            scriptBuilder.build(buildSchemaParams([], proj, name, mainclass), proj, useMavenJars=True)
-            os.spawnlp(os.P_WAIT, './' + name + '.sh', 'sh', '')
-            if options.clean: os.remove(name + '.sh')
-    print """Done. If you're not going to monkey with the schema any more you'll want
+    bigRngModtime = os.stat('../src/edu/sc/seis/sod/data/sod.rng').st_mtime
+    buildschema = utils.filesModifiedAfter(bigRngModtime, '.')
+    if buildschema:
+        for name, mainclass in scripts: run(name, mainclass, not options.filthy)
+    else:
+        print 'No changes to relax files!'
+    if options.build: build.buildJars(proj)
+    if options.validate:
+        run('validateExamples', 
+            'edu.sc.seis.sod.validator.example.ExampleValidator',
+            not options.filthy)
+    
+    if buildschema:    
+        print """Done. If you're not going to monkey with the schema any more you'll want
 to commit elementNode.ser, sod.rng, and grouper.rng in
 src/edu/sc/seis/sod/data.  If not, I'll come complain to you.  If I'm
 reading this message at a later date I'll want to commit elementNode.ser,
