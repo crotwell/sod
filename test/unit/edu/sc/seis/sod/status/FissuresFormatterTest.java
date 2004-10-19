a00473255ef7fec6cdba93827c63fcf8629978ab
groves <none@none>
2004-10-19 14:16:24 -0400

    [svn r10922] change test to formatNetwork from removed formatNetworkYear method
    --HG--
    branch : sod

--- test/unit/edu/sc/seis/sod/status/FissuresFormatterTest.java
+++ test/unit/edu/sc/seis/sod/status/FissuresFormatterTest.java
@@ -1,27 +1,28 @@
 /**
  * FissuresFormatterTest.java
- *
+ * 
  * @author Philip Crotwell
  */
-
 package edu.sc.seis.sod.status;
 
 import junit.framework.TestCase;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.sc.seis.fissuresUtil.mockFissures.IfNetwork.MockChannel;
 
-public class FissuresFormatterTest extends TestCase{
+public class FissuresFormatterTest extends TestCase {
 
-    public FissuresFormatterTest(String name){ super(name); }
+    public FissuresFormatterTest(String name) {
+        super(name);
+    }
 
-    public void setUp(){
+    public void setUp() {
         chan = MockChannel.createChannel();
     }
 
-    public void testNetworkYear(){
-        assertEquals("XX1970", FissuresFormatter.formatNetworkYear(chan.get_id().network_id));
+    public void testFormatNetwork() {
+        assertEquals("XX70",
+                     FissuresFormatter.formatNetwork(chan.get_id().network_id));
     }
 
     private Channel chan;
 }
-
