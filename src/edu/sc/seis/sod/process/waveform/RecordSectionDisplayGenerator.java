61507e1e0898349345cdfbf16e4fb0023ef35f3c
danala <none@none>
2004-10-20 13:38:37 -0400

    [svn r10942] uses id to associate with a particular SaveSeismogramToFile process
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/RecordSectionDisplayGenerator.java
+++ src/edu/sc/seis/sod/process/waveform/RecordSectionDisplayGenerator.java
@@ -10,6 +10,7 @@ import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 import edu.sc.seis.fissuresUtil.display.RecordSectionDisplay;
 import org.apache.log4j.BasicConfigurator;
 import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.SodUtil;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.iris.Fissures.IfNetwork.Channel;
@@ -25,48 +26,73 @@ import edu.sc.seis.sod.Start;
 import edu.sc.seis.sod.process.waveform.vector.WaveformVectorProcess;
 import edu.sc.seis.sod.status.waveformArm.WaveformMonitor;
 
-public class RecordSectionDisplayGenerator implements WaveformProcess{
-	
-	public RecordSectionDisplayGenerator(Element config) throws ConfigurationException {
-	
-	}
-	public SaveSeismogramToFile getSaveSeismogramToFile() throws ConfigurationException {
-		WaveformProcess[] waveformProcesses = Start.getWaveformArm().getLocalSeismogramArm().getProcesses();
-		for(int i=0;i<waveformProcesses.length;i++) {
-			if(waveformProcesses[i] instanceof SaveSeismogramToFile) {
-				return (SaveSeismogramToFile) waveformProcesses[i];
-			}
-		}
-		throw new ConfigurationException("RecordSectionDisplayGenerator needs a SaveSeismogramToFile process");
-	}
-	public WaveformResult process(EventAccessOperations event, Channel channel,
-								  RequestFilter[] original, RequestFilter[] available,
-								  LocalSeismogramImpl[] seismograms,
-								  CookieJar cookieJar) throws ParserConfigurationException,
-								  IOException, IncomprehensibleDSMLException,
-								  UnsupportedFileTypeException, ConfigurationException   {
-		try {
-				saveSeisToFile = getSaveSeismogramToFile();
-			DataSet ds = DataSetToXML.load(saveSeisToFile.getDSMLFile(event).toURI().toURL());
-			String[] dataSeisNames = ds.getDataSetSeismogramNames();
-			int numDSSeismograms = dataSeisNames.length;
-			dss = new DataSetSeismogram[numDSSeismograms];
-			for(int i=0;i<numDSSeismograms;i++){
-				dss[i] = ds.getDataSetSeismogram(dataSeisNames[i]);
-			}
-			RecordSectionDisplay recordSectiondisplay = new RecordSectionDisplay();
-			recordSectiondisplay.add(dss);
-			try {
-				File outPNG = new File(saveSeisToFile.getEventDirectory(event),"recordSection.png");
-				recordSectiondisplay.outputToPNG(outPNG,new Dimension(500,500));
-			} catch (IOException e) {
-				throw new IOException("Problem writing recordSection output to PNG " +e);
-			}
-		}catch(IOException e) {
-			throw new IOException("Problem opening dsml file in RecordSectionDisplayGenerator" + e);
-		}
-		return new WaveformResult(seismograms, new StringTreeLeaf(this, true));
-	}
-	public DataSetSeismogram[] dss;
-	private SaveSeismogramToFile saveSeisToFile;
+public class RecordSectionDisplayGenerator implements WaveformProcess {
+
+    public RecordSectionDisplayGenerator(Element config)
+            throws ConfigurationException {
+        this.config = config;
+        this.id = SodUtil.getText(SodUtil.getElement(config, "id"));
+    }
+
+    public SaveSeismogramToFile getSaveSeismogramToFile()
+            throws ConfigurationException {
+        WaveformProcess[] waveformProcesses = Start.getWaveformArm()
+                .getLocalSeismogramArm()
+                .getProcesses();
+        for(int i = 0; i < waveformProcesses.length; i++) {
+            if(waveformProcesses[i] instanceof SaveSeismogramToFile) {
+                 SaveSeismogramToFile saveSeis = (SaveSeismogramToFile)waveformProcesses[i];
+                 if(id.equals(saveSeis.getId())){
+                     return saveSeis;
+                 }
+            }
+        }
+        throw new ConfigurationException("RecordSectionDisplayGenerator needs a SaveSeismogramToFile process");
+    }
+
+    public WaveformResult process(EventAccessOperations event,
+                                  Channel channel,
+                                  RequestFilter[] original,
+                                  RequestFilter[] available,
+                                  LocalSeismogramImpl[] seismograms,
+                                  CookieJar cookieJar)
+            throws ParserConfigurationException, IOException,
+            IncomprehensibleDSMLException, UnsupportedFileTypeException,
+            ConfigurationException {
+        try {
+            saveSeisToFile = getSaveSeismogramToFile();
+            DataSet ds = DataSetToXML.load(saveSeisToFile.getDSMLFile(event)
+                    .toURI()
+                    .toURL());
+            String[] dataSeisNames = ds.getDataSetSeismogramNames();
+            int numDSSeismograms = dataSeisNames.length;
+            dss = new DataSetSeismogram[numDSSeismograms];
+            for(int i = 0; i < numDSSeismograms; i++) {
+                dss[i] = ds.getDataSetSeismogram(dataSeisNames[i]);
+            }
+            RecordSectionDisplay recordSectiondisplay = new RecordSectionDisplay();
+            recordSectiondisplay.add(dss);
+            try {
+                File outPNG = new File(saveSeisToFile.getEventDirectory(event),
+                                       "recordSection.png");
+                recordSectiondisplay.outputToPNG(outPNG,
+                                                 new Dimension(500, 500));
+            } catch(IOException e) {
+                throw new IOException("Problem writing recordSection output to PNG "
+                        + e);
+            }
+        } catch(IOException e) {
+            throw new IOException("Problem opening dsml file in RecordSectionDisplayGenerator"
+                    + e);
+        }
+        return new WaveformResult(seismograms, new StringTreeLeaf(this, true));
+    }
+
+    public DataSetSeismogram[] dss;
+
+    private SaveSeismogramToFile saveSeisToFile;
+
+    private Element config;
+
+    private String id;
 }
