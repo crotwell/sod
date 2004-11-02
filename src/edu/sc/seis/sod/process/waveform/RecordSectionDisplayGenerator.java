c1d47ca8ade658c35384e18d3501fd32f11fc2af
danala <none@none>
2004-11-02 14:15:37 -0500

    [svn r11080] changed the algorithm to get better recordsections
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/RecordSectionDisplayGenerator.java
+++ src/edu/sc/seis/sod/process/waveform/RecordSectionDisplayGenerator.java
@@ -7,11 +7,14 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.*;
 import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
+import edu.sc.seis.fissuresUtil.display.DisplayUtils;
 import edu.sc.seis.fissuresUtil.display.RecordSectionDisplay;
 import org.apache.log4j.BasicConfigurator;
 import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.SodUtil;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.model.QuantityImpl;
+import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
@@ -41,10 +44,8 @@ public class RecordSectionDisplayGenerator implements WaveformProcess {
                 .getProcesses();
         for(int i = 0; i < waveformProcesses.length; i++) {
             if(waveformProcesses[i] instanceof SaveSeismogramToFile) {
-                 SaveSeismogramToFile saveSeis = (SaveSeismogramToFile)waveformProcesses[i];
-                 if(id.equals(saveSeis.getId())){
-                     return saveSeis;
-                 }
+                SaveSeismogramToFile saveSeis = (SaveSeismogramToFile)waveformProcesses[i];
+                if(id.equals(saveSeis.getId())) { return saveSeis; }
             }
         }
         throw new ConfigurationException("RecordSectionDisplayGenerator needs a SaveSeismogramToFile process");
@@ -70,22 +71,72 @@ public class RecordSectionDisplayGenerator implements WaveformProcess {
             for(int i = 0; i < numDSSeismograms; i++) {
                 dss[i] = ds.getDataSetSeismogram(dataSeisNames[i]);
             }
-            RecordSectionDisplay recordSectiondisplay = new RecordSectionDisplay();
-            recordSectiondisplay.add(dss);
-            try {
-                File outPNG = new File(saveSeisToFile.getEventDirectory(event),
-                                       "recordSection.png");
-                recordSectiondisplay.outputToPNG(outPNG,
-                                                 new Dimension(500, 500));
-            } catch(IOException e) {
-                throw new IOException("Problem writing recordSection output to PNG "
-                        + e);
-            }
+            outputRecordSections(event, dss);
         } catch(IOException e) {
             throw new IOException("Problem opening dsml file in RecordSectionDisplayGenerator"
                     + e);
         }
         return new WaveformResult(seismograms, new StringTreeLeaf(this, true));
+    }
+
+    public void sort(DataSetSeismogram[] dataSeis) {
+        for(int i = 0; i < dataSeis.length; i++) {
+            QuantityImpl distance = DisplayUtils.calculateDistance(dataSeis[i]);
+            for(int j = 0; j < dataSeis.length; j++) {
+                if(distance.greaterThanEqual(DisplayUtils.calculateDistance(dataSeis[j]))) {
+                    DataSetSeismogram tempSeis = dataSeis[i];
+                    dataSeis[i] = dataSeis[j];
+                    dataSeis[j] = tempSeis;
+                }
+            }
+        }
+    }
+
+    private void outputRecordSections(EventAccessOperations event,
+                                      DataSetSeismogram[] dataSeis)
+            throws IOException {
+        sort(dataSeis);
+        RecordSectionDisplay recordSectionDisplay = new RecordSectionDisplay();
+        if(dataSeis.length <= 6) {
+            writeImage(dss, event);
+            return;
+        } else {
+            DataSetSeismogram[] tempDSS = new DataSetSeismogram[6];
+            int maxIndex = 0;
+            int length = dataSeis.length;
+            int count = length / 6;
+            for(int i = 0; i < count; i++) {
+                int seisCount = 0;
+                while(seisCount < 6) {
+                    tempDSS[seisCount] = dataSeis[i + count];
+                    maxIndex = i + count;
+                    seisCount++;
+                }
+                writeImage(tempDSS, event);
+            }
+            if((length % 6) != 0) {
+                tempDSS = new DataSetSeismogram[length - maxIndex - 1];
+                for(int j = 0; j < length - maxIndex - 1; j++) {
+                    tempDSS[j] = dataSeis[maxIndex + j];
+                }
+                writeImage(tempDSS, event);
+            }
+        }
+    }
+
+    private void writeImage(DataSetSeismogram[] dataSeis,
+                            EventAccessOperations event) throws IOException {
+        File parentDir = saveSeisToFile.getEventDirectory(event);
+        RecordSectionDisplay rsDisplay = new RecordSectionDisplay();
+        rsDisplay.add(dataSeis);
+        try {
+            File outPNG = new File(parentDir, fileBase+fileNameCounter+fileExtension);
+            rsDisplay.outputToPNG(outPNG, new Dimension(500, 500));
+            fileNameCounter++;
+        } catch(IOException e) {
+            throw new IOException("Problem writing recordSection output to PNG "
+                    + e);
+        }
     }
 
     public DataSetSeismogram[] dss;
@@ -95,4 +146,12 @@ public class RecordSectionDisplayGenerator implements WaveformProcess {
     private Element config;
 
     private String id;
+
+    private int counter;
+
+    public static int fileNameCounter=1;
+    
+    private final static String fileBase = "recordSection";
+    
+    private final static String fileExtension = ".png";
 }
