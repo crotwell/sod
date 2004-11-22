d97418b5e579713829edc7d67bd359cbe8852644
danala <none@none>
2004-11-22 00:33:20 -0500

    [svn r11212] added a method to check if image already exists
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/database/waveform/JDBCEventRecordSection.java
+++ src/edu/sc/seis/sod/database/waveform/JDBCEventRecordSection.java
@@ -9,7 +9,6 @@ import edu.sc.seis.fissuresUtil.database.ConnMgr;
 import edu.sc.seis.fissuresUtil.database.DBUtil;
 import edu.sc.seis.fissuresUtil.database.JDBCSequence;
 import edu.sc.seis.fissuresUtil.database.JDBCTable;
-import edu.sc.seis.sod.database.SodJDBC;
 
 public class JDBCEventRecordSection extends JDBCTable {
 
@@ -30,12 +29,14 @@ public class JDBCEventRecordSection extends JDBCTable {
         seq = new JDBCSequence(conn, "RecordSectionSeq");
         prepareStatements();
     }
-    public void insert(int eventid,String imageName,boolean bestForEvent) throws SQLException {
-        insert.setInt(1, seq.next());
+    public int insert(int eventid,String imageName,boolean bestForEvent) throws SQLException {
+        int recSecId=seq.next();
+        insert.setInt(1, recSecId);
         insert.setInt(2, eventid);
         insert.setString(3,imageName);
         insert.setBoolean(4,bestForEvent);
         insert.executeUpdate();
+        return recSecId;
     }
     public String getBestImageforEvent(int eventid) throws SQLException{
         getBestImageforEvent.setInt(1,eventid);
@@ -58,9 +59,35 @@ public class JDBCEventRecordSection extends JDBCTable {
             throw new RuntimeException("Running the SQL statement " + getImageforEventChannel.toString(), e);
         }
     }
+    public int getRecSecId(int eventid,String fileName) throws SQLException{
+        getRecSecId.setInt(1,eventid);
+        getRecSecId.setString(2,fileName);
+        try {
+            ResultSet rs = getRecSecId.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch(SQLException e) {
+            throw new RuntimeException("Running the SQL statement" + getRecSecId.toString(), e);
+        }
+}
+    public boolean imageExists(int eventid,String imageName) throws SQLException {
+        imageNameExists.setInt(1,eventid);
+        imageNameExists.setString(2,imageName);
+        try{
+            ResultSet rs = imageNameExists.executeQuery();
+            rs.next();
+            int count=rs.getInt(1);
+            if(count !=0){
+                return true;
+            }
+        } catch(SQLException e) {
+            throw new RuntimeException("Running the SQL statement " + imageNameExists.toString(), e);
+        }
+        return false;
+    }
 
     private PreparedStatement getBestImageforEvent, insert,
-            getImageforEventChannel;
+            getImageforEventChannel,imageNameExists,getRecSecId;
     
     private JDBCSequence seq;
 }
