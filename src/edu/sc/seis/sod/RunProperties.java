19c15ebe9a5330174818a5f9d6ba00fb257336e5
groves <none@none>
2005-01-18 10:21:16 -0500

    [svn r11711] fixed doIndex to be true on no doIndex property
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/RunProperties.java
+++ src/edu/sc/seis/sod/RunProperties.java
@@ -1,122 +1,153 @@
 /**
  * RunProperties.java
- *
+ * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod;
 
 import org.w3c.dom.Element;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 
-public class RunProperties{
-    public RunProperties(Element el) throws ConfigurationException{
+public class RunProperties {
+
+    public RunProperties(Element el) throws ConfigurationException {
         Element runNameChild = SodUtil.getElement(el, "runName");
-        if(runNameChild != null ){
+        if(runNameChild != null) {
             runName = SodUtil.getText(runNameChild);
             CookieJar.getCommonContext().put("runName", runName);
         }
-
         Element statusBaseChild = SodUtil.getElement(el, "statusBase");
-        if(statusBaseChild != null){
+        if(statusBaseChild != null) {
             statusDir = SodUtil.getText(statusBaseChild);
         }
-
         Element eventQueryChild = SodUtil.getElement(el, "eventQueryIncrement");
-        if(eventQueryChild != null){
+        if(eventQueryChild != null) {
             eventQueryIncrement = SodUtil.loadTimeInterval(eventQueryChild);
         }
-
         Element eventLagChild = SodUtil.getElement(el, "eventLag");
-        if(eventLagChild != null){
+        if(eventLagChild != null) {
             eventLag = SodUtil.loadTimeInterval(eventLagChild);
         }
-
-        Element eventRefreshChild = SodUtil.getElement(el, "eventRefreshInterval");
-        if(eventRefreshChild != null){
+        Element eventRefreshChild = SodUtil.getElement(el,
+                                                       "eventRefreshInterval");
+        if(eventRefreshChild != null) {
             eventRefresh = SodUtil.loadTimeInterval(eventRefreshChild);
         }
-
         Element maxRetryChild = SodUtil.getElement(el, "maxRetryDelay");
-        if(maxRetryChild != null){
+        if(maxRetryChild != null) {
             maxRetry = SodUtil.loadTimeInterval(maxRetryChild);
         }
-
         Element serverRetryChild = SodUtil.getElement(el, "serverRetryDelay");
-        if(serverRetryChild != null){
+        if(serverRetryChild != null) {
             serverRetryDelay = SodUtil.loadTimeInterval(serverRetryChild);
         }
-
-        Element numWorkersChild = SodUtil.getElement(el, "waveformWorkerThreads");
-        if(numWorkersChild != null){
+        Element numWorkersChild = SodUtil.getElement(el,
+                                                     "waveformWorkerThreads");
+        if(numWorkersChild != null) {
             numWorkers = Integer.parseInt(SodUtil.getText(numWorkersChild));
         }
-
-        Element evChanPairProcChild = SodUtil.getElement(el, "eventChannelPairProcessing");
-        if(evChanPairProcChild != null){
+        Element evChanPairProcChild = SodUtil.getElement(el,
+                                                         "eventChannelPairProcessing");
+        if(evChanPairProcChild != null) {
             evChanPairProc = SodUtil.getText(evChanPairProcChild);
         }
-
-        if(SodUtil.isTrue(el, "reopenEvents")){
+        if(SodUtil.isTrue(el, "reopenEvents")) {
             reopenEvents = true;
         }
-
-        if(SodUtil.isTrue(el, "removeDatabase")){
+        if(SodUtil.isTrue(el, "removeDatabase")) {
             removeDatabase = true;
         }
-        if(!SodUtil.isTrue(el, "makeIndexPage")){
+        if(SodUtil.getElement(el, "makeIndexPage") != null
+                && !SodUtil.isTrue(el, "makeIndexPage")) {
             doIndex = false;
         }
     }
 
-    public TimeInterval getMaxRetryDelay() { return maxRetry; }
+    public TimeInterval getMaxRetryDelay() {
+        return maxRetry;
+    }
 
-    public TimeInterval getServerRetryDelay(){ return serverRetryDelay; }
+    public TimeInterval getServerRetryDelay() {
+        return serverRetryDelay;
+    }
 
-    public TimeInterval getEventQueryIncrement() { return eventQueryIncrement; }
+    public TimeInterval getEventQueryIncrement() {
+        return eventQueryIncrement;
+    }
 
-    public TimeInterval getEventLag() { return eventLag; }
+    public TimeInterval getEventLag() {
+        return eventLag;
+    }
 
-    public TimeInterval getEventRefreshInterval() { return eventRefresh; }
+    public TimeInterval getEventRefreshInterval() {
+        return eventRefresh;
+    }
 
-    public String getRunName(){ return runName; }
+    public String getRunName() {
+        return runName;
+    }
 
-    public String getStatusBaseDir(){ return statusDir; }
+    public String getStatusBaseDir() {
+        return statusDir;
+    }
 
-    public int getNumWaveformWorkerThreads(){ return numWorkers; }
+    public int getNumWaveformWorkerThreads() {
+        return numWorkers;
+    }
 
-    public boolean reopenEvents(){ return reopenEvents; }
+    public boolean reopenEvents() {
+        return reopenEvents;
+    }
 
-    public boolean removeDatabase(){ return removeDatabase; }
+    public boolean removeDatabase() {
+        return removeDatabase;
+    }
 
-    public String getEventChannelPairProcessing(){
+    public String getEventChannelPairProcessing() {
         return evChanPairProc;
     }
-    
-    public boolean doIndex(){ return doIndex; }
 
-    public static final TimeInterval NO_TIME = new TimeInterval(0, UnitImpl.SECOND);
-    public static final TimeInterval ONE_WEEK = new TimeInterval(7, UnitImpl.DAY);
-    public static final TimeInterval TEN_MIN = new TimeInterval(10, UnitImpl.MINUTE);
-    public static final TimeInterval DAYS_180 = new TimeInterval(180, UnitImpl.DAY);
+    public boolean doIndex() {
+        return doIndex;
+    }
+
+    public static final TimeInterval NO_TIME = new TimeInterval(0,
+                                                                UnitImpl.SECOND);
+
+    public static final TimeInterval ONE_WEEK = new TimeInterval(7,
+                                                                 UnitImpl.DAY);
+
+    public static final TimeInterval TEN_MIN = new TimeInterval(10,
+                                                                UnitImpl.MINUTE);
+
+    public static final TimeInterval DAYS_180 = new TimeInterval(180,
+                                                                 UnitImpl.DAY);
 
     private TimeInterval eventQueryIncrement = ONE_WEEK;
+
     private TimeInterval eventLag = ONE_WEEK;
+
     private TimeInterval eventRefresh = TEN_MIN;
+
     private TimeInterval maxRetry = DAYS_180;
+
     private TimeInterval serverRetryDelay = NO_TIME;
 
     private String runName = "Your Sod";
+
     private String statusDir = "status";
 
     private int numWorkers = 1;
 
     private boolean reopenEvents = false;
+
     private boolean removeDatabase = false;
+
     private boolean doIndex = true;
 
     public static final String AT_LEAST_ONCE = "atLeastOnce";
+
     public static final String AT_MOST_ONCE = "atMostOnce";
 
     private String evChanPairProc = AT_LEAST_ONCE;
