47cb6e34d24a1c64d126dc0243908aef56b6fb4a
groves <none@none>
2005-02-02 11:34:06 -0500

    [svn r11890] try to load a file from the current loc
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/TemplateFileLoader.java
+++ src/edu/sc/seis/sod/status/TemplateFileLoader.java
@@ -1,8 +1,6 @@
 package edu.sc.seis.sod.status;
 
-import edu.sc.seis.sod.CommonAccess;
-import edu.sc.seis.sod.SodUtil;
-import edu.sc.seis.sod.Start;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -12,32 +10,41 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import edu.sc.seis.sod.CommonAccess;
+import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.Start;
 
-public class TemplateFileLoader{
+public class TemplateFileLoader {
+
     public static Element getTemplate(Element el) throws MalformedURLException,
-        IOException, SAXException, ParserConfigurationException {
+            IOException, SAXException, ParserConfigurationException {
         Element templateEl = SodUtil.getElement(el, "template");
-        if(templateEl == null || SodUtil.getNestedText(templateEl).equals("")) {
-            throw new IllegalArgumentException("Expected the passed in element '" + el.getNodeName() + "' to have a template element, but none found");
-        }
-        return getTemplate(CommonAccess.getLoader(), SodUtil.getNestedText(templateEl));
+        if(templateEl == null || SodUtil.getNestedText(templateEl).equals("")) { throw new IllegalArgumentException("Expected the passed in element '"
+                + el.getNodeName()
+                + "' to have a template element, but none found"); }
+        return getTemplate(CommonAccess.getLoader(),
+                           SodUtil.getNestedText(templateEl));
     }
-    
-    public static Element getTemplate(ClassLoader cl, String loc) throws MalformedURLException, SAXException, ParserConfigurationException, IOException{
+
+    public static Element getTemplate(ClassLoader cl, String loc)
+            throws MalformedURLException, SAXException,
+            ParserConfigurationException, IOException {
         URL url = getUrl(cl, loc);
         InputStream in = url.openStream();
         Document doc = Start.createDoc(new InputSource(in), loc);
         return (Element)doc.getFirstChild();
-
     }
 
-    public static URL getUrl(ClassLoader cl, String loc) throws MalformedURLException{
-        URL url = null;
+    public static URL getUrl(ClassLoader cl, String loc)
+            throws MalformedURLException {
         if(loc.startsWith("jar:")) {
-            url =cl.getResource(loc.substring(4));
+            return cl.getResource(loc.substring(4));
         } else {
-            url = new URL(loc);
+            try {
+                return new URL(loc);
+            } catch(MalformedURLException e) {
+                return new File(loc).toURL();
+            }
         }
-        return url;
     }
 }
