29a5db278a6e09d8d2fddda9bf8e58c32981a806
groves <none@none>
2005-02-04 15:58:32 -0500

    [svn r11965] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/network/FilterNetworkFinder.java
@@ -0,0 +1,34 @@
+package edu.sc.seis.sod.subsetter.network;
+
+import java.util.regex.Pattern;
+import org.w3c.dom.Element;
+import edu.sc.seis.fissuresUtil.cache.BulletproofVestFactory;
+import edu.sc.seis.fissuresUtil.cache.FilterNetworkDC;
+import edu.sc.seis.fissuresUtil.cache.ProxyNetworkDC;
+import edu.sc.seis.sod.SodUtil;
+
+/**
+ * @author groves Created on Feb 4, 2005
+ */
+public class FilterNetworkFinder extends NetworkFinder {
+
+    public FilterNetworkFinder(Element el) throws Exception {
+        super(el);
+        String url = SodUtil.nodeValueOfXPath(el, "filterURL/text()");
+        patterns = FilterNetworkDC.readPattern(url);
+    }
+
+    public synchronized ProxyNetworkDC getNetworkDC() {
+        if(netDC == null) {
+            netDC = BulletproofVestFactory.vestNetworkDC(getDNSName(),
+                                                         getSourceName(),
+                                                         getFissuresNamingService());
+            netDC = new FilterNetworkDC(netDC, patterns);
+        }
+        return netDC;
+    }
+
+    private Pattern[] patterns;
+
+    private ProxyNetworkDC netDC;
+}
