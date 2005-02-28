3055982204426454dd972aef16c3daf7ef4499e0
groves <none@none>
2005-02-28 16:10:40 -0500

    [svn r12294] removed self registration from mapEventStatus
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/IndexTemplate.java
+++ src/edu/sc/seis/sod/status/IndexTemplate.java
@@ -1,4 +1,5 @@
 package edu.sc.seis.sod.status;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -19,13 +20,14 @@ import edu.sc.seis.sod.Start;
 import edu.sc.seis.sod.status.eventArm.MapEventStatus;
 import edu.sc.seis.sod.status.waveformArm.WaveformMonitor;
 
+public class IndexTemplate extends FileWritingTemplate implements
+        WaveformMonitor {
 
-public class IndexTemplate extends FileWritingTemplate implements WaveformMonitor{
     public IndexTemplate() throws IOException {
         this(FileWritingTemplate.getBaseDirectoryName());
     }
 
-    public IndexTemplate(String dirName) throws IOException{
+    public IndexTemplate(String dirName) throws IOException {
         super(dirName, "index.html");
         try {
             initExceptionHandler();
@@ -33,32 +35,40 @@ public class IndexTemplate extends FileWritingTemplate implements WaveformMonito
                                                               indexLoc);
             parse(template);
             write();
-            FileWritingTemplate help = new FileWritingTemplate(dirName + "/help/", "eventPageHelp.html");
+            FileWritingTemplate help = new FileWritingTemplate(dirName
+                    + "/help/", "eventPageHelp.html");
             help.parse(TemplateFileLoader.getTemplate(getClass().getClassLoader(),
                                                       eventPageHelp));
             help.write();
-            SodUtil.copyFile(cssLoc, dirName+"/main.css");
-            SodUtil.copyFile(sortLoc, dirName+"/sorttable.js");
-            SodUtil.copyFile(rulLoc, dirName +"/tableRuler.js");
-            SodUtil.copyFile(footPosLoc, dirName +"/footerPositioner.js");
+            SodUtil.copyFile(cssLoc, dirName + "/main.css");
+            SodUtil.copyFile(sortLoc, dirName + "/sorttable.js");
+            SodUtil.copyFile(rulLoc, dirName + "/tableRuler.js");
+            SodUtil.copyFile(footPosLoc, dirName + "/footerPositioner.js");
             SodUtil.copyFile(helpMark, dirName + "/images/helpmark.png");
             SodUtil.copyFile(up, dirName + "/images/up.gif");
             SodUtil.copyFile(down, dirName + "/images/down.gif");
             SodUtil.copyFile(none, dirName + "/images/none.gif");
             SodUtil.copyFile(key, dirName + "/images/mapkey.gif");
-            SodUtil.copyFile(individualKey, dirName + "/images/individualEventMapKey.gif");
-            /* To avoid problems during rendering of XML by some of the browsers like Mac Safari*/
+            SodUtil.copyFile(individualKey, dirName
+                    + "/images/individualEventMapKey.gif");
+            /*
+             * To avoid problems during rendering of XML by some of the browsers
+             * like Mac Safari
+             */
             convertToHTML(dirName);
-        } catch (Exception e) {
-            GlobalExceptionHandler.handle("unexpected problem creating index.html page", e);
+        } catch(Exception e) {
+            GlobalExceptionHandler.handle("unexpected problem creating index.html page",
+                                          e);
         }
     }
 
-    public static void setConfigFileLoc() throws FileNotFoundException{
+    public static void setConfigFileLoc() throws FileNotFoundException {
         String configFileLoc = Start.getConfigFileName();
         String configFileName = new File(configFileLoc).getName();
         configFile = configFileName;
-        SodUtil.copyFile(Start.getConfigFileName(), FileWritingTemplate.getBaseDirectoryName() + "/" + configFile);
+        SodUtil.copyFile(Start.getConfigFileName(),
+                         FileWritingTemplate.getBaseDirectoryName() + "/"
+                                 + configFile);
     }
 
     public static String getCopiedConfigFileLocation() {
@@ -66,24 +76,29 @@ public class IndexTemplate extends FileWritingTemplate implements WaveformMonito
     }
 
     public static String getHtmlConfigFileName() {
-        String configFileLoc  = getCopiedConfigFileLocation();
-        String fileName = configFileLoc.substring(0, configFileLoc.indexOf(".xml"));
+        String configFileLoc = getCopiedConfigFileLocation();
+        String fileName = configFileLoc.substring(0,
+                                                  configFileLoc.indexOf(".xml"));
         return fileName += ".html";
     }
 
-    public void update(EventChannelPair ecp) { write(); }
+    public void update(EventChannelPair ecp) {
+        write();
+    }
 
-
-    /** Exists so IndexTemplate can be created before arms, in order for exceptions
-     * in initialization to be in the status pages.*/
+    /**
+     * Exists so IndexTemplate can be created before arms, in order for
+     * exceptions in initialization to be in the status pages.
+     */
     public void performRegistration() {
         Start.getEventArm().add(mapEventStatus);
         Start.getWaveformArm().addStatusMonitor(this);
     }
 
-    protected Object getTemplate(String tagName, Element el) throws ConfigurationException  {
-        if(tagName.equals("eventMap")){
-            mapEventStatus = new MapEventStatus(el, false);
+    protected Object getTemplate(String tagName, Element el)
+            throws ConfigurationException {
+        if(tagName.equals("eventMap")) {
+            mapEventStatus = new MapEventStatus(el);
             return new RelativeLocationTemplate(getOutputLocation(),
                                                 mapEventStatus.getLocation());
         }
@@ -95,12 +110,15 @@ public class IndexTemplate extends FileWritingTemplate implements WaveformMonito
         errorDir.mkdirs();
         GlobalExceptionHandler.add(new HTMLReporter(errorDir));
     }
-    private void convertToHTML(String statusDir) throws TransformerException, FileNotFoundException, TransformerConfigurationException, IOException {
+
+    private void convertToHTML(String statusDir) throws TransformerException,
+            FileNotFoundException, TransformerConfigurationException,
+            IOException {
         String wrapperFile = statusDir + "/xmlverbatimwrapper.xsl";
         String mainXSL = statusDir + "/xmlverbatim.xsl";
-        SodUtil.copyFile(xslWrapperFileLoc,wrapperFile);
-        SodUtil.copyFile(supportXslFileLoc,mainXSL);
-        SodUtil.copyFile(cssFileLoc,statusDir+"/xmlverbatim.css");
+        SodUtil.copyFile(xslWrapperFileLoc, wrapperFile);
+        SodUtil.copyFile(supportXslFileLoc, mainXSL);
+        SodUtil.copyFile(cssFileLoc, statusDir + "/xmlverbatim.css");
         String htmlFile = statusDir + "/" + getHtmlConfigFileName();
         TransformerFactory tFactory = TransformerFactory.newInstance();
         Transformer t = tFactory.newTransformer(new StreamSource(wrapperFile));
@@ -110,24 +128,37 @@ public class IndexTemplate extends FileWritingTemplate implements WaveformMonito
         new File(mainXSL).delete();
     }
 
-
     private MapEventStatus mapEventStatus;
 
     private static String indexLoc = "jar:edu/sc/seis/sod/data/templates/index.xml";
+
     private static String cssLoc = "jar:edu/sc/seis/sod/data/templates/main.css";
+
     private static String sortLoc = "jar:edu/sc/seis/sod/data/templates/sorttable.js";
+
     private static String rulLoc = "jar:edu/sc/seis/sod/data/templates/tableRuler.js";
+
     private static String footPosLoc = "jar:edu/sc/seis/sod/data/templates/footerPositioner.js";
+
     private static String helpMark = "jar:edu/sc/seis/sod/data/templates/defaults/helpmark.png";
+
     private static String up = "jar:edu/sc/seis/sod/data/templates/defaults/up.gif";
+
     private static String down = "jar:edu/sc/seis/sod/data/templates/defaults/down.gif";
+
     private static String none = "jar:edu/sc/seis/sod/data/templates/defaults/none.gif";
+
     private static String key = "jar:edu/sc/seis/sod/data/templates/defaults/mapkey.gif";
+
     private static String individualKey = "jar:edu/sc/seis/sod/data/templates/defaults/individualEventMapKey.gif";
+
     private static String eventPageHelp = "jar:edu/sc/seis/sod/data/templates/defaults/eventPageHelp.xml";
+
     private static String xslWrapperFileLoc = "jar:edu/sc/seis/sod/data/xmlverbatimwrapper.xsl";
+
     private static String supportXslFileLoc = "jar:edu/sc/seis/sod/data/xmlverbatim.xsl";
+
     private static String cssFileLoc = "jar:edu/sc/seis/sod/data/xmlverbatim.css";
+
     private static String configFile;
 }
-
