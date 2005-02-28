52e7a5eed94238f36635b541ed1495c679b1c444
groves <none@none>
2005-02-28 16:10:57 -0500

    [svn r12295] use db to get last event
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/eventArm/LastEventTemplate.java
+++ src/edu/sc/seis/sod/status/eventArm/LastEventTemplate.java
@@ -1,21 +1,40 @@
 package edu.sc.seis.sod.status.eventArm;
 
-import edu.sc.seis.sod.Start;
+import java.sql.SQLException;
+import org.w3c.dom.Element;
+import edu.sc.seis.fissuresUtil.cache.CacheEvent;
+import edu.sc.seis.fissuresUtil.database.NotFound;
+import edu.sc.seis.fissuresUtil.database.event.JDBCEventAccess;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.status.AllTypeTemplate;
 import edu.sc.seis.sod.status.EventFormatter;
-import org.w3c.dom.Element;
-import edu.sc.seis.sod.ConfigurationException;
 
-public class LastEventTemplate extends AllTypeTemplate{
-    public LastEventTemplate(Element el) throws ConfigurationException{
+public class LastEventTemplate extends AllTypeTemplate {
+
+    public LastEventTemplate(Element el) throws ConfigurationException {
         ef = new EventFormatter(el);
+        try {
+            evAcc = new JDBCEventAccess();
+        } catch(SQLException e) {
+            throw new ConfigurationException("Couldn't create database to get last event",
+                                             e);
+        }
     }
 
     public String getResult() {
-        if(Start.getEventArm() != null && Start.getEventArm().getLastEvent() != null){
-            return ef.getResult(Start.getEventArm().getLastEvent());
-        }else{ return "None"; }
+        try {
+            CacheEvent ev = evAcc.getLastEvent();
+            return ef.getResult(ev);
+        } catch(NotFound e) {
+            return "None";
+        } catch(SQLException e) {
+            GlobalExceptionHandler.handle(e);
+            return "Database Error!";
+        }
     }
+
+    private JDBCEventAccess evAcc;
 
     private EventFormatter ef;
 }
