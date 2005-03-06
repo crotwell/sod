cbf6e37d58988547e66d1f937301ca691ee5426f
groves <none@none>
2005-03-05 22:33:45 -0500

    [svn r12375] return contained subsettrs
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/network/NetworkLogicalSubsetter.java
+++ src/edu/sc/seis/sod/subsetter/network/NetworkLogicalSubsetter.java
@@ -1,13 +1,20 @@
 package edu.sc.seis.sod.subsetter.network;
+
 import org.w3c.dom.Element;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.subsetter.LogicalSubsetter;
 
-public class NetworkLogicalSubsetter extends LogicalSubsetter{
-    public NetworkLogicalSubsetter (Element config) throws ConfigurationException{
+public class NetworkLogicalSubsetter extends LogicalSubsetter {
+
+    public NetworkLogicalSubsetter(Element config)
+            throws ConfigurationException {
         super(config);
     }
-    
+
+    public NetworkSubsetter[] getSubsetters() {
+        return (NetworkSubsetter[])filterList.toArray(new NetworkSubsetter[0]);
+    }
+
     public String getArmName() {
         return "network";
     }
