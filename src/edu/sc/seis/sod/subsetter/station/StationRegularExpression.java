04b1000866e82ca29229b0bfc170ef5b9be43660
groves <none@none>
2005-03-06 01:15:17 -0500

    [svn r12380] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/station/StationRegularExpression.java
@@ -0,0 +1,30 @@
+package edu.sc.seis.sod.subsetter.station;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import org.w3c.dom.Element;
+import edu.iris.Fissures.IfNetwork.Station;
+import edu.sc.seis.fissuresUtil.cache.FilterNetworkAccess;
+import edu.sc.seis.fissuresUtil.cache.FilterNetworkDC;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
+
+/**
+ * @author groves Created on Mar 4, 2005
+ */
+public class StationRegularExpression implements StationSubsetter {
+
+    public StationRegularExpression(Element el) throws IOException {
+        String url = DOMHelper.extractText(el, "url");
+        patterns = FilterNetworkDC.readPattern(url);
+    }
+
+    public boolean accept(Station station) throws Exception {
+        for(int i = 0; i < patterns.length; i++) {
+            if(patterns[i].matcher(FilterNetworkAccess.getStationString(station.get_id()))
+                    .matches()) { return true; }
+        }
+        return false;
+    }
+
+    private Pattern[] patterns;
+}
