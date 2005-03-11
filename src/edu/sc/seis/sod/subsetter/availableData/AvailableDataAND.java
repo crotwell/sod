271eb957dbc46f802397813af757c89cf2172297
groves <none@none>
2005-03-11 13:56:28 -0500

    [svn r12417] fixed in the case of success
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/availableData/AvailableDataAND.java
+++ src/edu/sc/seis/sod/subsetter/availableData/AvailableDataAND.java
@@ -1,6 +1,5 @@
 package edu.sc.seis.sod.subsetter.availableData;
 
-import java.util.Iterator;
 import org.apache.log4j.Category;
 import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
@@ -19,20 +18,19 @@ public final class AvailableDataAND extends AvailableDataLogicalSubsetter
     }
 
     public StringTree accept(EventAccessOperations event,
-                          Channel channel,
-                          RequestFilter[] original,
-                          RequestFilter[] available,
-                          CookieJar cookieJar) throws Exception {
-        Iterator it = filterList.iterator();
+                             Channel channel,
+                             RequestFilter[] original,
+                             RequestFilter[] available,
+                             CookieJar cookieJar) throws Exception {
         StringTree[] result = new StringTree[filterList.size()];
-        int i=0;
-        while(it.hasNext()) {
-            AvailableDataSubsetter filter = (AvailableDataSubsetter)it.next();
-            result[i] = filter.accept(event, channel, original, available, cookieJar);
-            if(!result[i].isSuccess()) { break; }
-            i++;
+        for(int i = 0; i < filterList.size(); i++) {
+            AvailableDataSubsetter f = (AvailableDataSubsetter)filterList.get(i);
+            result[i] = f.accept(event, channel, original, available, cookieJar);
+            if(!result[i].isSuccess()) { return new StringTreeBranch(this,
+                                                                     false,
+                                                                     result); }
         }
-        return new StringTreeBranch(this, result[i].isSuccess(), result);
+        return new StringTreeBranch(this, true, result);
     }
 
     static Category logger = Category.getInstance(AvailableDataAND.class.getName());
