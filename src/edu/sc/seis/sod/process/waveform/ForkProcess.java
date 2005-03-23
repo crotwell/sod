270b5ce6a3f026ac4fa152777ca6a3775cc817a2
groves <none@none>
2005-03-23 17:13:57 -0500

    [svn r12554] removed unused stuff
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/ForkProcess.java
+++ src/edu/sc/seis/sod/process/waveform/ForkProcess.java
@@ -5,6 +5,12 @@
  */
 package edu.sc.seis.sod.process.waveform;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
@@ -15,12 +21,6 @@ import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.status.StringTreeBranch;
 import edu.sc.seis.sod.status.StringTreeLeaf;
-import java.util.Iterator;
-import java.util.LinkedList;
-import org.apache.log4j.Logger;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class ForkProcess implements WaveformProcess {
 
@@ -31,17 +31,8 @@ public class ForkProcess implements WaveformProcess {
         for(int i = 0; i < children.getLength(); i++) {
             node = children.item(i);
             if(node instanceof Element) {
-                if(((Element)node).getTagName().equals("description")) {
-                    // skip description element
-                    continue;
-                }
                 Object sodElement = SodUtil.load((Element)node, "waveform");
-                if(sodElement instanceof WaveformProcess) {
-                    localSeisProcessList.add(sodElement);
-                } else {
-                    logger.warn("Unknown tag in LocalSeismogramArm config. "
-                            + sodElement);
-                } // end of else
+                localSeisProcessList.add(sodElement);
             } // end of if (node instanceof Element)
         } // end of for (int i=0; i<children.getSize(); i++)
     }
