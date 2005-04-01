db927d8204b855f8110ba055a241165e44674749
groves <none@none>
2005-04-01 16:25:40 -0500

    [svn r12721] use sod.py
    --HG--
    branch : sod

--- site/buildSite.sh
+++ site/buildSite.sh
@@ -5,6 +5,6 @@ echo 'Copying over included stuff'
 cp -r include/* generatedSite
 rm -r generatedSite/CVS
 cd schemaDocs
-./buildSchemaDocs.py
+../../../devTools/maven/sod.py --docs
 cd ..
 echo 'Site now complete in generatedSite'
