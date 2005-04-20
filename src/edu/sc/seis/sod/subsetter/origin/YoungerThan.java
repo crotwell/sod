5b86d7880c49e4cab732ecfbc765c6aed90a26df
groves <none@none>
2005-04-20 09:21:18 -0400

    [svn r12959] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/origin/YoungerThan.java
@@ -0,0 +1,31 @@
+package edu.sc.seis.sod.subsetter.origin;
+
+import org.w3c.dom.Element;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfEvent.EventAttr;
+import edu.iris.Fissures.IfEvent.Origin;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
+import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.SodUtil;
+
+/**
+ * @author groves Created on Apr 19, 2005
+ */
+public class YoungerThan implements OriginSubsetter {
+
+    public YoungerThan(Element config) throws ConfigurationException {
+        expirationAge = SodUtil.loadTimeInterval(config);
+    }
+
+    public boolean accept(EventAccessOperations ev,
+                          EventAttr eventAttr,
+                          Origin preferred_origin) throws Exception {
+        MicroSecondDate originTime = new MicroSecondDate(preferred_origin.origin_time);
+        MicroSecondDate expirationDate = originTime.add(expirationAge);
+        return expirationDate.after(ClockUtil.now());
+    }
+
+    private TimeInterval expirationAge;
+}
