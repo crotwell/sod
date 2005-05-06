861d8c61c4a44136835a93e6d5ecce43d525b473
groves <none@none>
2005-05-06 15:17:37 -0400

    [svn r13266] reformat
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/origin/OriginTimeRange.java
+++ src/edu/sc/seis/sod/subsetter/origin/OriginTimeRange.java
@@ -7,12 +7,20 @@ import edu.iris.Fissures.model.MicroSecondDate;
 import edu.sc.seis.sod.ConfigurationException;
 import org.w3c.dom.Element;
 
-public class OriginTimeRange extends edu.sc.seis.sod.subsetter.TimeRange implements OriginSubsetter {
-    public OriginTimeRange (Element config) throws ConfigurationException{ super(config); }
+public class OriginTimeRange extends edu.sc.seis.sod.subsetter.TimeRange
+        implements OriginSubsetter {
 
-    public boolean accept(EventAccessOperations event, EventAttr eventAttr, Origin origin) {
+    public OriginTimeRange(Element config) throws ConfigurationException {
+        super(config);
+    }
+
+    public boolean accept(EventAccessOperations event,
+                          EventAttr eventAttr,
+                          Origin origin) {
         MicroSecondDate actualDate = new MicroSecondDate(origin.origin_time);
-        if( getMSTR().intersects(actualDate)) return true;
-        else return false;
+        if(getMSTR().intersects(actualDate))
+            return true;
+        else
+            return false;
     }
 }// EventTimeRange
