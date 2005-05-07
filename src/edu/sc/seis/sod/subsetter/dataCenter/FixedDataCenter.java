d7342fbba7398b18ac0cf78962bd0bf1a65ae111
groves <none@none>
2005-05-07 01:27:17 -0400

    [svn r13280] added getDataCenter method
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/dataCenter/FixedDataCenter.java
+++ src/edu/sc/seis/sod/subsetter/dataCenter/FixedDataCenter.java
@@ -11,32 +11,36 @@ import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.SodElement;
 import edu.sc.seis.sod.subsetter.AbstractSource;
 import org.w3c.dom.Element;
+
 /**
- * FixedDataCenter.java
- *
- *
- * Created: Wed Mar 20 14:27:42 2002
- *
- * @author <a href="mailto:">Srinivasa Telukutla</a>
+ * FixedDataCenter.java Created: Wed Mar 20 14:27:42 2002
+ * 
+ * @author <a href="mailto:">Srinivasa Telukutla </a>
  * @version
  */
-
 public class FixedDataCenter extends AbstractSource implements SodElement,
-    SeismogramDCLocator {
+        SeismogramDCLocator {
 
-    public FixedDataCenter (Element element) throws Exception{
+    public FixedDataCenter(Element element) throws Exception {
         super(element);
         CommonAccess commonAccess = CommonAccess.getCommonAccess();
         fissuresNamingService = commonAccess.getFissuresNamingService();
-
         dns = getDNSName();
         objectName = getSourceName();
-        dataCenter = BulletproofVestFactory.vestSeismogramDC(dns, objectName, fissuresNamingService);
+        dataCenter = BulletproofVestFactory.vestSeismogramDC(dns,
+                                                             objectName,
+                                                             fissuresNamingService);
     }
 
     public ProxySeismogramDC getSeismogramDC(EventAccessOperations event,
                                              Channel channel,
-                                             RequestFilter[] infilters, CookieJar cookieJar) throws Exception{
+                                             RequestFilter[] infilters,
+                                             CookieJar cookieJar)
+            throws Exception {
+        return dataCenter;
+    }
+
+    public ProxySeismogramDC getDataCenter() {
         return dataCenter;
     }
 
@@ -47,5 +51,4 @@ public class FixedDataCenter extends AbstractSource implements SodElement,
     private String dns = null;
 
     private String objectName = null;
-
 }// FixedDataCenter
