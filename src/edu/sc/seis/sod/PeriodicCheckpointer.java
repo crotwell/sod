0a5ff3afdf323dc8fe341c6104422c343cf91db9
groves <none@none>
2005-05-09 18:05:22 -0400

    [svn r13311] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/PeriodicCheckpointer.java
@@ -0,0 +1,41 @@
+package edu.sc.seis.sod;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Timer;
+import java.util.TimerTask;
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.database.ConnMgr;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+
+/**
+ * does a checkpoint on the database every hour
+ * 
+ * @author groves Created on May 9, 2005
+ */
+public class PeriodicCheckpointer extends TimerTask {
+
+    public PeriodicCheckpointer() {
+        Timer t = new Timer(true);
+        t.schedule(this,
+                   new MicroSecondDate().add(new TimeInterval(1, UnitImpl.HOUR)),
+                   ONE_HOUR);
+    }
+
+    public void run() {
+        try {
+            Statement stmt = ConnMgr.createConnection().createStatement();
+            logger.info("Checkpointing db");
+            stmt.executeUpdate("CHECKPOINT");
+            logger.info("Done checkpointing db");
+        } catch(SQLException e) {
+            GlobalExceptionHandler.handle(e);
+        }
+    }
+
+    private static final int ONE_HOUR = 60 * 60 * 1000;
+
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(PeriodicCheckpointer.class);
+}
