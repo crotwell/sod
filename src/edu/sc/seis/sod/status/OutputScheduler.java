3442ca54b4ab8c6722f1726644cb998701bf8167
groves <none@none>
2005-05-17 15:31:09 -0400

    [svn r13452] run last outputters when network event and waveform arms are done
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/OutputScheduler.java
+++ src/edu/sc/seis/sod/status/OutputScheduler.java
@@ -1,9 +1,8 @@
 /**
  * TimerWrapper.java
- *
+ * 
  * @author Charles Groves
  */
-
 package edu.sc.seis.sod.status;
 
 import java.util.Collections;
@@ -15,18 +14,18 @@ import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.sod.Start;
 
-public class OutputScheduler extends Thread{
-    protected OutputScheduler(){
-    }
+public class OutputScheduler extends Thread {
 
-    public void schedule(Runnable a){
-        synchronized(runnables){
+    protected OutputScheduler() {}
+
+    public void schedule(Runnable a) {
+        synchronized(runnables) {
             runnables.add(a);
         }
     }
 
     public void scheduleForExit(Runnable a) {
-        synchronized(onExitRunnables){
+        synchronized(onExitRunnables) {
             onExitRunnables.add(a);
         }
     }
@@ -35,19 +34,21 @@ public class OutputScheduler extends Thread{
         // initial sleep before first set of status pages
         try {
             Thread.sleep(INITIAL_SLEEP);
-        } catch (InterruptedException e) {}
-
+        } catch(InterruptedException e) {}
         while(true) {
             runAll(runnables);
-            if(Start.getWaveformArm() != null && Start.getWaveformArm().isFinished()){
+            if((Start.getWaveformArm() == null || Start.getWaveformArm()
+                    .isFinished())
+                    && (Start.getEventArm() == null || !Start.getEventArm()
+                            .isAlive())
+                    && (Start.getNetworkArm() == null || Start.getNetworkArm()
+                            .isFinished())) {
                 runAll(runnables);
                 runAll(onExitRunnables);
                 logger.debug("Output Scheduler done.");
-
-
                 String lcOSName = System.getProperty("os.name").toLowerCase();
                 boolean MAC_OS_X = lcOSName.startsWith("mac os x");
-                if (MAC_OS_X) {
+                if(MAC_OS_X) {
                     // hopefully everything is done!
                     System.out.println("Using System.exit(0) only on the mac due to AWT thread not exiting.");
                     logger.info("Using System.exit(0) only on the mac due to AWT thread not exiting.");
@@ -55,44 +56,48 @@ public class OutputScheduler extends Thread{
                 }
                 return;
             }
-
             try {
                 sleep(ACTION_INTERVAL_MILLIS);
-            } catch (InterruptedException e) {}
+            } catch(InterruptedException e) {}
         }
     }
 
-    private void runAll(Set runnables){
+    private void runAll(Set runnables) {
         Runnable[] currentRunnables = new Runnable[0];
-        synchronized(runnables){
+        synchronized(runnables) {
             currentRunnables = (Runnable[])runnables.toArray(currentRunnables);
             runnables.clear();
         }
-        for (int i = 0; i < currentRunnables.length; i++) {
-            try{
+        for(int i = 0; i < currentRunnables.length; i++) {
+            try {
                 currentRunnables[i].run();
-            }catch(Throwable t){
+            } catch(Throwable t) {
                 GlobalExceptionHandler.handle(t);
             }
         }
     }
 
     public synchronized static OutputScheduler getDefault() {
-        if (DEFAULT == null) {
+        if(DEFAULT == null) {
             DEFAULT = new OutputScheduler();
             DEFAULT.start();
         }
         return DEFAULT;
     }
 
-    private static final TimeInterval ACTION_INTERVAL = new TimeInterval(2, UnitImpl.MINUTE);
-    private static final long ACTION_INTERVAL_MILLIS = (long)ACTION_INTERVAL.convertTo(UnitImpl.MILLISECOND).get_value();
+    private static final TimeInterval ACTION_INTERVAL = new TimeInterval(2,
+                                                                         UnitImpl.MINUTE);
+
+    private static final long ACTION_INTERVAL_MILLIS = (long)ACTION_INTERVAL.convertTo(UnitImpl.MILLISECOND)
+            .get_value();
+
     private static final long INITIAL_SLEEP = 10000l;
+
     private static OutputScheduler DEFAULT = null;
 
     private Set runnables = Collections.synchronizedSet(new HashSet());
+
     private Set onExitRunnables = Collections.synchronizedSet(new HashSet());
+
     private static final Logger logger = Logger.getLogger(OutputScheduler.class);
-
 }
-
