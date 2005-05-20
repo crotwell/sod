981fe23a26e2a6d1b847238782decbe7c24fc718
crotwell <none@none>
2005-05-20 10:42:43 -0400

    [svn r13505] first cut at a rotateGCP processor
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/ChannelGroup.java
+++ src/edu/sc/seis/sod/ChannelGroup.java
@@ -42,6 +42,64 @@ public class ChannelGroup {
     public boolean contains(Channel c) {
         return getIndex(c) != -1;
     }
+    
+    /**
+     * Finds the vertical channel. If no channel has a dip of -90 then
+     * null is returned.
+     */
+    public Channel getVertical() {
+        for(int i = 0; i < channels.length; i++) {
+            if (channels[i].an_orientation.dip == -90) {
+                return channels[i];
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Finds the 2 horizontal channels.
+     */
+    public Channel[] getHorizontal() {
+        Channel[] out = new Channel[2];
+        for(int i = 0; i < channels.length; i++) {
+            if (channels[i].an_orientation.dip == 0) {
+                if (out[0] == null) {
+                    out[0] = channels[i];
+                } else {
+                    out[1] = channels[i];
+                }
+            }
+        }
+        if (out[1] == null) {
+            if (out[0] == null) {
+                return new Channel[0];
+            } else {
+                return new Channel[] {out[0]};
+            }
+        }
+        return out;
+    }
+    
+    /**
+     * Gets the horizontals as X and Y, so that the second channel's azimmuth
+     * is the first's + 90 degrees. If this is not possible, then a zero length
+     * array is returned.
+     */
+    public Channel[] getHorizontalXY() {
+        Channel[] out = getHorizontal();
+        if (out.length != 2) {
+            out = new Channel[0];
+        } else if((out[0].an_orientation.azimuth+90) % 360 == out[1].an_orientation.azimuth % 360) {
+            // in right order
+        } else if((out[1].an_orientation.azimuth+90) % 360 == out[0].an_orientation.azimuth % 360) {
+            Channel tmp = out[0];
+            out[0] = out[1];
+            out[1] = tmp;
+        } else {
+            out = new Channel[0];
+        }
+        return out;
+    }
 
     private int getIndex(Channel chan) {
         for(int i = 0; i < channels.length; i++) {
---
+++ src/edu/sc/seis/sod/process/waveform/vector/RotateGCP.java
@@ -0,0 +1,100 @@
+package edu.sc.seis.sod.process.waveform.vector;
+
+import org.apache.xerces.util.DOMUtil;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import edu.iris.Fissures.Location;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.network.ChannelIdUtil;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.bag.Rotate;
+import edu.sc.seis.fissuresUtil.cache.EventUtil;
+import edu.sc.seis.sod.ChannelGroup;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.status.StringTreeBranch;
+import edu.sc.seis.sod.status.StringTreeLeaf;
+
+
+/**
+ * @author crotwell
+ * Created on May 20, 2005
+ */
+public class RotateGCP implements WaveformVectorProcess {
+
+    /**
+     *
+     */
+    public RotateGCP(Element el) {
+        NodeList nl = el.getChildNodes();
+        for(int i = 0; i < nl.getLength(); i++) {
+            Node n = nl.item(i);
+            if(n.getNodeName().equals("radialOrientationCode")) {
+                radialOrientationCode = DOMUtil.getChildText(n);
+            } else if(n.getNodeName().equals("transverseOrientationCode")) {
+                transverseOrientationCode = DOMUtil.getChildText(n);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public WaveformVectorResult process(EventAccessOperations event,
+                                        ChannelGroup channelGroup,
+                                        RequestFilter[][] original,
+                                        RequestFilter[][] available,
+                                        LocalSeismogramImpl[][] seismograms,
+                                        CookieJar cookieJar) throws Exception {
+        // find x & y channel, y should be x+90 degrees and horizontal
+        Channel[] horizontal = channelGroup.getHorizontalXY();
+        if (horizontal.length == 0) {
+            return new WaveformVectorResult(seismograms,
+                                            new StringTreeLeaf(this,
+                                                               false,
+                                                               "Channels not rotatable."));
+        }
+        int xIndex = -1, yIndex = -1;
+        for(int i = 0; i < seismograms.length; i++) {
+            if(seismograms[i].length != 0) {
+                if (ChannelIdUtil.areEqual(seismograms[i][0].channel_id, horizontal[0].get_id())) {
+                    xIndex = i;
+                }
+                if(ChannelIdUtil.areEqual(seismograms[i][0].channel_id, horizontal[1].get_id())) {
+                    yIndex = i;
+                }
+            }
+        }
+        if (xIndex == -1 || yIndex == -1) {
+            return new WaveformVectorResult(seismograms,
+                                            new StringTreeLeaf(this,
+                                                               false,
+                                                               "Can't find seismograms to match horizontal channels: xIndex="+xIndex+" yIndex="+yIndex));
+        }
+        if (seismograms[xIndex].length != seismograms[yIndex].length) {
+            return new WaveformVectorResult(seismograms,
+                                            new StringTreeLeaf(this,
+                                                               false,
+                                                               "Seismogram lengths for horizontal channels don't match: "+seismograms[xIndex].length+" != "+seismograms[yIndex].length)); 
+        }
+        Location staLoc = horizontal[0].my_site.my_location;
+        Location eventLoc = EventUtil.extractOrigin(event).my_location;
+        LocalSeismogramImpl[][] out = new LocalSeismogramImpl[seismograms.length][];
+        for(int i = 0; i < out.length; i++) {
+            out[i] = new LocalSeismogramImpl[seismograms[i].length];
+            System.arraycopy(seismograms[i], 0, out[i], 0, out[i].length);
+        }
+        for(int i = 0; i < seismograms[xIndex].length; i++) {
+            LocalSeismogramImpl[] rot = Rotate.rotateGCP(seismograms[xIndex][i], seismograms[yIndex][i], staLoc, eventLoc, transverseOrientationCode, radialOrientationCode);
+            out[xIndex][i] = rot[0];
+            out[yIndex][i] = rot[1];
+        }
+        return new WaveformVectorResult(out,
+                                        new StringTreeLeaf(this,
+                                                             true));
+    }
+    
+    String radialOrientationCode, transverseOrientationCode;
+}
