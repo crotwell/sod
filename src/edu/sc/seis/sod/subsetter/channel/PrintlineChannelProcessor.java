bb33fede329f3430f4cf997e0400b8e9a583aa2f
groves <none@none>
2005-05-27 16:56:12 -0400

    [svn r13625] added output to an file template
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/channel/PrintlineChannelProcessor.java
+++ src/edu/sc/seis/sod/subsetter/channel/PrintlineChannelProcessor.java
@@ -1,12 +1,10 @@
 package edu.sc.seis.sod.subsetter.channel;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import org.w3c.dom.Element;
 import edu.iris.Fissures.IfNetwork.Channel;
-import edu.iris.Fissures.network.ChannelIdUtil;
-import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
+import edu.sc.seis.sod.velocity.SimpleVelocitizer;
 
 /**
  * @author <a href="mailto:crotwell@pooh">Philip Crotwell </a>
@@ -14,24 +12,18 @@ import edu.sc.seis.sod.SodUtil;
 public class PrintlineChannelProcessor implements ChannelSubsetter {
 
     public PrintlineChannelProcessor(Element config) {
-        filename = SodUtil.getNestedText(config);
+        filename = DOMHelper.extractText(config, "filename", "");
+        template = DOMHelper.extractText(config, "template", DEFAULT_TEMPLATE);
     }
 
     public boolean accept(Channel channel) throws IOException {
-        if(filename != null && filename.length() != 0) {
-            FileWriter fwriter = new FileWriter(filename, true);
-            BufferedWriter bwriter = new BufferedWriter(fwriter);
-            bwriter.write(ChannelIdUtil.toString(channel.get_id()),
-                          0,
-                          ChannelIdUtil.toString(channel.get_id()).length());
-            bwriter.newLine();
-            bwriter.close();
-        } else {
-            System.out.println("Channel: "
-                    + ChannelIdUtil.toString(channel.get_id()));
-        } // end of else
+        velocitizer.evaluate(filename, template, channel);
         return true;
     }
 
-    String filename = null;
+    private static final String DEFAULT_TEMPLATE = "Channel: $channel";
+
+    private SimpleVelocitizer velocitizer = new SimpleVelocitizer();
+
+    private String filename, template;
 }// PrintlineChannelProcessor
