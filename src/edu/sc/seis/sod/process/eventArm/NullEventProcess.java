dd9c5dc505335aa19ee5704cbc8be6307072c082
groves <none@none>
2005-05-27 14:40:22 -0400

    [svn r13611] folded into subsetters
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/eventArm/NullEventProcess.java
+++
@@ -1,9 +0,0 @@
-package edu.sc.seis.sod.process.eventArm;
-
-import edu.iris.Fissures.IfEvent.EventAccessOperations;
-
-public class NullEventProcess implements EventProcess {
-    public NullEventProcess (){}
-
-    public void process(EventAccessOperations event) {}
-}// NullEventProcess
