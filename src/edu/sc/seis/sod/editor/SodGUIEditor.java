33358c4b9f4ad5d6d61c36ad29e2e30a267b4b0f
groves <none@none>
2005-05-30 16:36:05 -0400

    [svn r13639] added printline stuff
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/SodGUIEditor.java
+++ src/edu/sc/seis/sod/editor/SodGUIEditor.java
@@ -1,9 +1,8 @@
 /**
  * SodGUIEditor.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.sod.editor;
 
 import java.io.IOException;
@@ -19,13 +18,13 @@ import org.xml.sax.SAXException;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.sod.UpdateChecker;
 
-
-
 public class SodGUIEditor extends SimpleGUIEditor {
 
-    public SodGUIEditor(String[] args) throws IOException, ParserConfigurationException, TransformerException, DOMException, SAXException, Exception {
+    public SodGUIEditor(String[] args) throws IOException,
+            ParserConfigurationException, TransformerException, DOMException,
+            SAXException, Exception {
         super(args);
-        UpdateChecker check = new UpdateChecker(true);
+        new UpdateChecker(true);
         frameName = "SOD Editor";
         setTabbed(true);
         initEditors();
@@ -34,20 +33,22 @@ public class SodGUIEditor extends SimpleGUIEditor {
     JComponent getCompForElement(Element element) {
         try {
             String tag = element.getTagName();
-            if (editors.containsKey(tag)){
+            if(editors.containsKey(tag)) {
                 EditorPlugin ed = (EditorPlugin)editors.get(tag);
                 return ed.getGUI(element);
             } else {
                 return super.getCompForElement(element);
             }
-        } catch (Exception e) {
-            System.err.println("Can't get component for "+element.getTagName()+", using default. "+e);
-            logger.warn("Can't get component for "+element.getTagName()+", using default. ",e);
+        } catch(Exception e) {
+            System.err.println("Can't get component for "
+                    + element.getTagName() + ", using default. " + e);
+            logger.warn("Can't get component for " + element.getTagName()
+                    + ", using default. ", e);
             return super.getCompForElement(element);
         }
     }
 
-    /** gets a custom added editor for the tagName, null if there isn't one*/
+    /** gets a custom added editor for the tagName, null if there isn't one */
     public EditorPlugin getCustomEditor(String tagName) {
         return (EditorPlugin)editors.get(tagName);
     }
@@ -69,116 +70,138 @@ public class SodGUIEditor extends SimpleGUIEditor {
         editors.put("gainCode", netCodeEdit);
         editors.put("orientationCode", netCodeEdit);
         editors.put("boxArea", new BoxAreaEditor(this));
-
         editors.put("stationEffectiveTimeOverlap", timeRangeEdit);
         editors.put("siteEffectiveTimeOverlap", timeRangeEdit);
         editors.put("channelEffectiveTimeOverlap", timeRangeEdit);
-
         editors.put("magnitudeRange", new MagnitudeEditor());
-        editors.put("distanceRange", new UnitRangeEditor(ANGLE_UNITS, 0, 180, 5, true));
+        editors.put("distanceRange", new UnitRangeEditor(ANGLE_UNITS,
+                                                         0,
+                                                         180,
+                                                         5,
+                                                         true));
         editors.put("phaseRequest", new PhaseRequestEditor());
         editors.put("originPointDistance", new OriginPointDistanceEditor());
         editors.put("originPointAzimuth", new OriginPointAzimuthEditor());
-        editors.put("originPointBackAzimuth", new OriginPointBackAzimuthEditor());
+        editors.put("originPointBackAzimuth",
+                    new OriginPointBackAzimuthEditor());
         editors.put("seismicRegion", new SeismicRegionEditor());
         editors.put("geographicRegion", new GeographicRegionEditor());
         editors.put("eventStatusTemplate", new EventStatusTemplateEditor());
         editors.put("eventFinder", new EventFinderEditor(this));
         editors.put("networkFinder", new NetworkFinderEditor(this));
+        editors.put("printlineEventProcess", new PrintlineEventEditor());
         editors.put("catalog", new CatalogEditor());
         editors.put("contributor", new ContributorEditor());
         editors.put("unitRange", new UnitRangeEditor(DISTANCE_UNITS));
-        editors.put("originDepthRange", new UnitRangeEditor(DISTANCE_UNITS, true));
-        editors.put("linearDistanceMagnitudeRange", new LinearDistanceMagnitudeEditor(this, DISTANCE_UNITS, true));
+        editors.put("originDepthRange", new UnitRangeEditor(DISTANCE_UNITS,
+                                                            true));
+        editors.put("linearDistanceMagnitudeRange",
+                    new LinearDistanceMagnitudeEditor(this,
+                                                      DISTANCE_UNITS,
+                                                      true));
         editors.put("midPoint", new MidPointEditor(this));
-        editors.put("latitudeRange", new  UnitRangeEditor(ANGLE_UNITS, -90, 90, 5, false));
-        editors.put("longitudeRange", new UnitRangeEditor(ANGLE_UNITS, -180, 180, 5, false));
-        editors.put("azimuthRange", new UnitRangeEditor(ANGLE_UNITS, 0, 360, 5, true));
-        editors.put("backAzimuthRange", new UnitRangeEditor(ANGLE_UNITS, 0, 360, 5, true));
+        editors.put("latitudeRange", new UnitRangeEditor(ANGLE_UNITS,
+                                                         -90,
+                                                         90,
+                                                         5,
+                                                         false));
+        editors.put("longitudeRange", new UnitRangeEditor(ANGLE_UNITS,
+                                                          -180,
+                                                          180,
+                                                          5,
+                                                          false));
+        editors.put("azimuthRange", new UnitRangeEditor(ANGLE_UNITS,
+                                                        0,
+                                                        360,
+                                                        5,
+                                                        true));
+        editors.put("backAzimuthRange", new UnitRangeEditor(ANGLE_UNITS,
+                                                            0,
+                                                            360,
+                                                            5,
+                                                            true));
         editors.put("refreshInterval", new RefreshIntervalEditor());
         editors.put("saveSeismogramToFile", new SaveSeismogramToFileEditor());
         editors.put("responseWriter", new ResponseWriterEditor());
         editors.put("waveformNetworkStatus", new WaveformStatusEditor());
         editors.put("waveformStationStatus", new WaveformStatusEditor());
-
         SubelementEater se = new SubelementEater();
         editors.put("networkInfoTemplateGenerator", se);
         editors.put("waveformEventTemplateGenerator", se);
         editors.put("localSeismogramTemplateGenerator", se);
-
         editors.put("fixedDataCenter", new FixedDataCenterEditor(this));
-
         editors.put("legacyExecute", new LegacyExecuteEditor());
         editors.put("legacyVectorExecute", new LegacyExecuteEditor());
-
         editors.put("breqFastRequest", new BreqFastRequestSubsetterEditor());
-
         editors.put("phaseSignalToNoise", new PhaseSignalToNoiseEditor());
-        
         editors.put("removeEventDuplicate", new RemoveEventDuplicateEditor());
-
+        editors.put("printlineChannelProcess", new PrintlineChannelEditor());
+        editors.put("printlineSeismogramProcess",
+                    new PrintlineSeismogramEditor());
         BooleanEditor bool = new BooleanEditor(this);
-        String[] switchTypes = { "origin", "network", "station", "site", "channel", "eventStation", "eventChannel", "availableData", "request", "seismogram", "waveformVector"};
-        String[] logicals = { "AND", "OR", "NOT" }; // what about XOR?
-        for (int i = 0; i < switchTypes.length; i++) {
-            for (int j = 0; j < logicals.length; j++) {
-                editors.put(switchTypes[i]+logicals[j], bool);
+        String[] switchTypes = {"origin",
+                                "network",
+                                "station",
+                                "site",
+                                "channel",
+                                "eventStation",
+                                "eventChannel",
+                                "availableData",
+                                "request",
+                                "seismogram",
+                                "waveformVector"};
+        String[] logicals = {"AND", "OR", "NOT"}; // what about XOR?
+        for(int i = 0; i < switchTypes.length; i++) {
+            for(int j = 0; j < logicals.length; j++) {
+                editors.put(switchTypes[i] + logicals[j], bool);
             }
         }
-
         editors.put("requestGeneratorWrapper", new WrapperEditor(this));
-
-        String[] wrapperTypes = { "AvailableDataWrapper", "RequestWrapper", "WaveformProcessWrapper", "EventChannelWrapper" };
-        String[] wrapperLogicals = { "AND", "OR" };
-        for (int i = 0; i < wrapperTypes.length; i++) {
-            for (int j = 0; j < wrapperLogicals.length; j++) {
-                editors.put(wrapperLogicals[j]+wrapperTypes[i], bool);
+        String[] wrapperTypes = {"AvailableDataWrapper",
+                                 "RequestWrapper",
+                                 "WaveformProcessWrapper",
+                                 "EventChannelWrapper"};
+        String[] wrapperLogicals = {"AND", "OR"};
+        for(int i = 0; i < wrapperTypes.length; i++) {
+            for(int j = 0; j < wrapperLogicals.length; j++) {
+                editors.put(wrapperLogicals[j] + wrapperTypes[i], bool);
             }
         }
-
         EmbeddedEditor embedded = new EmbeddedEditor(this);
         editors.put("embeddedOriginSubsetter", embedded);
-
-        // this puts "switchers" in place of the editors, which are stored with the suffix
-        // the switchers contian the origins, plus a popup menu for the types that could
+        // this puts "switchers" in place of the editors, which are stored with
+        // the suffix
+        // the switchers contian the origins, plus a popup menu for the types
+        // that could
         // go in this place
-            
-        /* COMMENTED OUT 2005/05/20 because switchers don't work
-         for (int i = 0; i < switchTypes.length; i++) {
-            Switcher originSwitcher = new Switcher(switchTypes[i], this);
-            List subTypes = originSwitcher.getSubTypes();
-            Iterator it = subTypes.iterator();
-            while(it.hasNext()) {
-                String tagName = (String)it.next();
-                if (editors.containsKey(tagName)) {
-                    // save original with _tagChooser appended
-                    if (editors.containsKey(tagName+TagChooser.PLUGIN_SUFFIX)) {
-                        // already visited this editor plugin
-                    } else {
-                        if ( ! (editors.get(tagName) instanceof Switcher)) {
-                            // only add switch if current plugin isn't a switcher
-                            editors.put(tagName+TagChooser.PLUGIN_SUFFIX, editors.get(tagName));
-                            editors.put(tagName, originSwitcher);
-                        }
-                    }
-                } else {
-                    // no custom editor
-                    editors.put(tagName, originSwitcher);
-                }
-            }
-        }*/
-
+        /*
+         * COMMENTED OUT 2005/05/20 because switchers don't work for (int i = 0;
+         * i < switchTypes.length; i++) { Switcher originSwitcher = new
+         * Switcher(switchTypes[i], this); List subTypes =
+         * originSwitcher.getSubTypes(); Iterator it = subTypes.iterator();
+         * while(it.hasNext()) { String tagName = (String)it.next(); if
+         * (editors.containsKey(tagName)) { // save original with _tagChooser
+         * appended if (editors.containsKey(tagName+TagChooser.PLUGIN_SUFFIX)) { //
+         * already visited this editor plugin } else { if ( !
+         * (editors.get(tagName) instanceof Switcher)) { // only add switch if
+         * current plugin isn't a switcher
+         * editors.put(tagName+TagChooser.PLUGIN_SUFFIX, editors.get(tagName));
+         * editors.put(tagName, originSwitcher); } } } else { // no custom
+         * editor editors.put(tagName, originSwitcher); } } }
+         */
     }
 
     protected HashMap editors = new HashMap();
 
-    public static final UnitImpl[] DISTANCE_UNITS = {  UnitImpl.KILOMETER };
+    public static final UnitImpl[] DISTANCE_UNITS = {UnitImpl.KILOMETER};
 
-    public static final UnitImpl[] ANGLE_UNITS = { UnitImpl.DEGREE };
+    public static final UnitImpl[] ANGLE_UNITS = {UnitImpl.DEGREE};
 
-    public static final UnitImpl[] TIME_UNITS = { UnitImpl.SECOND, UnitImpl.MINUTE,
-            UnitImpl.HOUR, UnitImpl.DAY, UnitImpl.WEEK, };
-
+    public static final UnitImpl[] TIME_UNITS = {UnitImpl.SECOND,
+                                                 UnitImpl.MINUTE,
+                                                 UnitImpl.HOUR,
+                                                 UnitImpl.DAY,
+                                                 UnitImpl.WEEK,};
 
     public static void main(String[] args) throws Exception {
         BasicConfigurator.configure();
