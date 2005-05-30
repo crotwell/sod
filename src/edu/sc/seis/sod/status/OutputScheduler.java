ca8c868309ebcc78c0c32649437e73c3609d4bca
oliverpa <none@none>
2005-05-30 12:04:03 -0400

    [svn r13630] added registerArms method. don't exit while any arm is still active
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/OutputScheduler.java
+++ src/edu/sc/seis/sod/status/OutputScheduler.java
@@ -12,11 +12,17 @@ import org.apache.log4j.Logger;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
-import edu.sc.seis.sod.Start;
+import edu.sc.seis.sod.Arm;
 
 public class OutputScheduler extends Thread {
 
     protected OutputScheduler() {}
+    
+    public void registerArm(Arm arm) {
+        synchronized(arms) {
+            arms.add(arm);
+        }
+    }
 
     public void schedule(Runnable a) {
         synchronized(runnables) {
@@ -37,12 +43,7 @@ public class OutputScheduler extends Thread {
         } catch(InterruptedException e) {}
         while(true) {
             runAll(runnables);
-            if((Start.getWaveformArm() == null || Start.getWaveformArm()
-                    .isFinished())
-                    && (Start.getEventArm() == null || !Start.getEventArm()
-                            .isAlive())
-                    && (Start.getNetworkArm() == null || Start.getNetworkArm()
-                            .isFinished())) {
+            if(!anyArmsActive()) {
                 runAll(runnables);
                 runAll(onExitRunnables);
                 logger.debug("Output Scheduler done.");
@@ -79,6 +80,21 @@ public class OutputScheduler extends Thread {
         }
     }
 
+    private boolean anyArmsActive() {
+        Arm[] curArms = new Arm[0];
+        synchronized(arms) {
+            curArms = (Arm[])arms.toArray(curArms);
+        }
+        boolean active = false;
+        for(int i = 0; i < curArms.length; i++) {
+            if (curArms[i].isActive()) {
+                active = true;
+                break;
+            }
+        }
+        return active;
+    }
+    
     public synchronized static OutputScheduler getDefault() {
         if(DEFAULT == null) {
             DEFAULT = new OutputScheduler();
@@ -96,6 +112,8 @@ public class OutputScheduler extends Thread {
     private static final long INITIAL_SLEEP = 10000l;
 
     private static OutputScheduler DEFAULT = null;
+    
+    private Set arms = Collections.synchronizedSet(new HashSet());
 
     private Set runnables = Collections.synchronizedSet(new HashSet());
 
