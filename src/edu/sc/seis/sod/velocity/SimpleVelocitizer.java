2147e357ca91d748d2dcd8c74036a61cd717209f
groves <none@none>
2005-05-30 16:36:54 -0400

    [svn r13642] broke file stuff out into PrintlineVelocitizer
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/velocity/SimpleVelocitizer.java
+++ src/edu/sc/seis/sod/velocity/SimpleVelocitizer.java
@@ -1,98 +1,31 @@
 package edu.sc.seis.sod.velocity;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Properties;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.runtime.RuntimeConstants;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
-import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.sod.CookieJar;
-import edu.sc.seis.sod.velocity.event.VelocityEvent;
-import edu.sc.seis.sod.velocity.network.VelocityChannel;
 
 /**
- * @author groves Created on May 25, 2005
+ * @author groves
+ * 
+ * Created on May 25, 2005
  */
 public class SimpleVelocitizer {
 
     public String evaluate(String template, Channel chan) {
-        try {
-            return evaluate("", template, chan);
-        } catch(IOException e) {
-            throw new RuntimeException("Shouldn't throw an IOException because it shouldn't be doing IO with an empty string fileTemplate");
-        }
-    }
-
-    public String evaluate(String fileTemplate, String template, Channel chan)
-            throws IOException {
-        VelocityContext ctx = new VelocityContext();
-        new VelocityChannel(chan).insertIntoContext(ctx);
-        return evalulate(fileTemplate, template, ctx);
+        return evaluate(template, ContextWrangler.createContext(chan));
     }
 
     public String evaluate(String template, EventAccessOperations event) {
-        try {
-            return evaluate("", template, event);
-        } catch(IOException e) {
-            throw new RuntimeException("Shouldn't throw an IOException because it shouldn't be doing IO with an empty string fileTemplate");
-        }
-    }
-
-    public String evaluate(String fileTemplate,
-                           String template,
-                           EventAccessOperations event) throws IOException {
-        VelocityContext ctx = createEventContext(event);
-        return evalulate(fileTemplate, template, ctx);
-    }
-
-    public String evalulate(String fileTemplate,
-                            String template,
-                            VelocityContext ctx) throws IOException {
-        String result = evaluate(template, ctx);
-        if(fileTemplate.equals("")) {
-            System.out.println(result);
-        } else {
-            appendToFile(fileTemplate, result, ctx);
-        }
-        return result;
-    }
-
-    private void appendToFile(String fileTemplate,
-                              String toAppend,
-                              VelocityContext ctx) throws IOException {
-        String filename = evaluate(fileTemplate, ctx);
-        File file = new File(filename);
-        file.getParentFile().mkdirs();
-        FileWriter fwriter = new FileWriter(file, true);
-        BufferedWriter bwriter = null;
-        try {
-            bwriter = new BufferedWriter(fwriter);
-            bwriter.write(toAppend);
-            bwriter.newLine();
-        } finally {
-            if(bwriter != null) {
-                bwriter.close();
-            }
-        }
-    }
-
-    private VelocityContext createEventContext(EventAccessOperations event) {
-        VelocityContext ctx = new VelocityContext();
-        if(event instanceof CacheEvent) {
-            ctx.put("event", new VelocityEvent((CacheEvent)event));
-        } else {
-            ctx.put("event", new VelocityEvent(new CacheEvent(event)));
-        }
-        return ctx;
+        return evaluate(template, ContextWrangler.createContext(event));
     }
 
     public String evaluate(String template,
@@ -102,41 +35,22 @@ public class SimpleVelocitizer {
                            RequestFilter[] available,
                            LocalSeismogramImpl[] seismograms,
                            CookieJar cookieJar) {
-        try {
-            return evaluate(template,
-                            "",
-                            event,
-                            channel,
-                            original,
-                            available,
-                            seismograms,
-                            cookieJar);
-        } catch(IOException e) {
-            throw new RuntimeException("Shouldn't throw an IOException because it shouldn't be doing IO with an empty string fileTemplate");
-        }
+        return evaluate(template, ContextWrangler.createContext(event,
+                                                                channel,
+                                                                original,
+                                                                available,
+                                                                seismograms,
+                                                                cookieJar));
     }
 
-    public String evaluate(String fileTemplate,
-                           String template,
-                           EventAccessOperations event,
-                           Channel channel,
-                           RequestFilter[] original,
-                           RequestFilter[] available,
-                           LocalSeismogramImpl[] seismograms,
-                           CookieJar cookieJar) throws IOException {
-        VelocityContext ctx = new WaveformProcessContext(event,
-                                                         channel,
-                                                         original,
-                                                         available,
-                                                         seismograms,
-                                                         cookieJar);
-        return evalulate(fileTemplate, template, ctx);
-    }
-
-    private String evaluate(String template, VelocityContext ctx) {
+    public String evaluate(String template, VelocityContext ctx) {
         StringWriter writer = new StringWriter();
         try {
-            Velocity.evaluate(ctx, writer, "SimpleVelocitizer", template);
+            try {
+                Velocity.evaluate(ctx, writer, "SimpleVelocitizer", template);
+            } catch(ParseErrorException parseError) {
+                return parseError.getMessage();
+            }
             return writer.toString();
         } catch(Exception e) {
             GlobalExceptionHandler.handle(e);
