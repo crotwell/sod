2b70f2bd32775a5e26eac6e2c17e8db4520f274c
groves <none@none>
2005-06-01 23:16:35 -0400

    [svn r13700] merged with seiswww
    --HG--
    branch : sod

--- site/include/footerPositioner.js
+++
@@ -1,8 +0,0 @@
-function positionFooter(){
-    var menu = document.getElementById('menu');
-    var content = document.getElementById('content');
-    if(menu.offsetHeight > content.offsetHeight){ 
-        var footer = document.getElementById('footer'); 
-        footer.style.position = 'absolute'; 
-    } 
-} 
