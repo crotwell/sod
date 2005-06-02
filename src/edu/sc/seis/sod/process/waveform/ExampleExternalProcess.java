b404e5d8702de1d0460d00389fa5ab5d6199f184
crotwell <none@none>
2005-06-02 16:15:30 -0400

    [svn r13725] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/process/waveform/ExampleExternalProcess.java
@@ -0,0 +1,31 @@
+package edu.sc.seis.sod.process.waveform;
+
+import org.w3c.dom.Element;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.status.StringTreeLeaf;
+
+public class ExampleExternalProcess implements WaveformProcess {
+
+    public ExampleExternalProcess(Element config) throws ConfigurationException {}
+
+    public WaveformResult process(EventAccessOperations event,
+                                  Channel channel,
+                                  RequestFilter[] original,
+                                  RequestFilter[] available,
+                                  LocalSeismogramImpl[] seismograms,
+                                  CookieJar cookieJar) throws Exception {
+        if(seismograms.length == 1) {
+            return new WaveformResult(true, seismograms);
+        } else {
+            return new WaveformResult(seismograms,
+                                      new StringTreeLeaf("seismograms does not have length one, "
+                                                                 + seismograms.length,
+                                                         false));
+        }
+    }
+}
