3893c93fc024c29c787262f298f6424580ef60c9
groves <none@none>
2005-06-01 23:14:18 -0400

    [svn r13698] merged into seiswww
    --HG--
    branch : sod

--- site/deploySite.sh
+++
@@ -1,8 +0,0 @@
-buildSite.sh
-cd generatedSite
-tar cjvf site.tar.bz2 *
-echo 'copying site to pooh'
-scp -r site.tar.bz2 sac@pooh:/seis/raid1/Apache/htdocs/sod
-ssh sac@pooh 'cd /seis/raid1/Apache/htdocs/sod ; bunzip2 site.tar.bz2 ; tar vxf site.tar; rm site.tar'
-rm site.tar.bz2
-echo done
