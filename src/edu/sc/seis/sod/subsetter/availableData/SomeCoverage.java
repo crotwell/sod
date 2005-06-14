f4bd0836a64da2075ded9464d98ca34df7e8fb0f
groves <none@none>
2005-06-14 16:26:05 -0400

    [svn r13865] removed unused constructors
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/availableData/SomeCoverage.java
+++ src/edu/sc/seis/sod/subsetter/availableData/SomeCoverage.java
@@ -1,7 +1,6 @@
 package edu.sc.seis.sod.subsetter.availableData;
 
 import org.apache.log4j.Category;
-import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
@@ -11,15 +10,14 @@ import edu.sc.seis.sod.status.StringTreeLeaf;
 
 public class SomeCoverage implements AvailableDataSubsetter {
 
-    public SomeCoverage(Element config) {}
-
     public StringTree accept(EventAccessOperations event,
-                          Channel channel,
-                          RequestFilter[] original,
-                          RequestFilter[] available,
-                          CookieJar cookieJar) {
+                             Channel channel,
+                             RequestFilter[] original,
+                             RequestFilter[] available,
+                             CookieJar cookieJar) {
         // simple impl, probably need more robust
-        return new StringTreeLeaf(this, available != null && available.length != 0);
+        return new StringTreeLeaf(this, available.length != 0, available.length
+                + " filters returned by available data");
     }
 
     static Category logger = Category.getInstance(SomeCoverage.class.getName());
