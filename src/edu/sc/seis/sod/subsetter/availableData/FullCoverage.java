2d29a2e26ae66a3198e0b5d0b9e422f5511c643f
groves <none@none>
2005-06-14 16:25:41 -0400

    [svn r13864] use CoverageTool to get an accurate answer
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/availableData/FullCoverage.java
+++ src/edu/sc/seis/sod/subsetter/availableData/FullCoverage.java
@@ -1,10 +1,10 @@
 package edu.sc.seis.sod.subsetter.availableData;
 
 import org.apache.log4j.Category;
-import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.sc.seis.fissuresUtil.time.CoverageTool;
 import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.SodElement;
 import edu.sc.seis.sod.status.StringTree;
@@ -12,18 +12,14 @@ import edu.sc.seis.sod.status.StringTreeLeaf;
 
 public class FullCoverage implements AvailableDataSubsetter, SodElement {
 
-    public FullCoverage(Element config) {}
-
     public StringTree accept(EventAccessOperations event,
-                          Channel channel,
-                          RequestFilter[] original,
-                          RequestFilter[] available,
-                          CookieJar cookieJar) {
-        // simple impl, probably need more robust
-        if(original.length == available.length) { 
-            return new StringTreeLeaf(this, true);
-        } 
-        return new StringTreeLeaf(this, false, "filter lengths are not the same "+original.length+" "+available.length);
+                             Channel channel,
+                             RequestFilter[] original,
+                             RequestFilter[] available,
+                             CookieJar cookieJar) {
+        RequestFilter[] uncovered = CoverageTool.notCovered(original, available);
+        return new StringTreeLeaf(this, uncovered.length == 0, uncovered.length
+                + " uncovered segments");
     }
 
     static Category logger = Category.getInstance(FullCoverage.class.getName());
