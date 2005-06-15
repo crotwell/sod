e1683c83c496481581cddcc1d55f360f497aa27d
groves <none@none>
2005-06-15 11:44:16 -0400

    [svn r13873] dont allow for empty not case, simplify logic
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/availableData/AvailableDataNOT.java
+++ src/edu/sc/seis/sod/subsetter/availableData/AvailableDataNOT.java
@@ -1,6 +1,5 @@
 package edu.sc.seis.sod.subsetter.availableData;
 
-import java.util.Iterator;
 import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfNetwork.Channel;
@@ -9,13 +8,13 @@ import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.status.StringTreeBranch;
-import edu.sc.seis.sod.status.StringTreeLeaf;
 
 public final class AvailableDataNOT extends AvailableDataLogicalSubsetter
         implements AvailableDataSubsetter {
 
     public AvailableDataNOT(Element config) throws ConfigurationException {
         super(config);
+        filter = (AvailableDataSubsetter)filterList.get(0);
     }
 
     public StringTree accept(EventAccessOperations event,
@@ -23,17 +22,13 @@ public final class AvailableDataNOT extends AvailableDataLogicalSubsetter
                              RequestFilter[] original,
                              RequestFilter[] available,
                              CookieJar cookieJar) throws Exception {
-        Iterator it = filterList.iterator();
-        StringTree result;
-        if(it.hasNext()) {
-            AvailableDataSubsetter filter = (AvailableDataSubsetter)it.next();
-            result = filter.accept(event,
-                                   channel,
-                                   original,
-                                   available,
-                                   cookieJar);
-            return new StringTreeBranch(this, !result.isSuccess(), result);
-        }
-        return new StringTreeLeaf(this, false, "Empty NOT");
+        StringTree result = filter.accept(event,
+                                          channel,
+                                          original,
+                                          available,
+                                          cookieJar);
+        return new StringTreeBranch(this, !result.isSuccess(), result);
     }
+
+    private AvailableDataSubsetter filter;
 }// AvailableDataNOT
