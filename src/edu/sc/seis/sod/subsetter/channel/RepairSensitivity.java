e862a4c8737231dbf8ce9bf7c07d99dfff0eec9d
groves <none@none>
2005-06-17 15:28:18 -0400

    [svn r13934] check for channel not found exception, dont check for null, print out messages indicating why failing
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/channel/RepairSensitivity.java
+++ src/edu/sc/seis/sod/subsetter/channel/RepairSensitivity.java
@@ -1,19 +1,26 @@
 package edu.sc.seis.sod.subsetter.channel;
 
+import org.apache.log4j.Logger;
 import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.ChannelNotFound;
 import edu.iris.Fissures.IfNetwork.Instrumentation;
 import edu.iris.Fissures.IfNetwork.NetworkAccess;
 import edu.iris.Fissures.IfNetwork.Response;
 import edu.iris.Fissures.IfNetwork.Stage;
+import edu.iris.Fissures.network.ChannelIdUtil;
 import edu.sc.seis.fissuresUtil.bag.ResponseGain;
 
 public class RepairSensitivity implements ChannelSubsetter {
 
     public boolean accept(Channel channel, NetworkAccess network)
             throws Exception {
-        Instrumentation instrumentation = network.retrieve_instrumentation(channel.get_id(),
-                                                                           channel.get_id().begin_time);
-        if(instrumentation == null) {
+        Instrumentation instrumentation;
+        try {
+            instrumentation = network.retrieve_instrumentation(channel.get_id(),
+                                                               channel.get_id().begin_time);
+        } catch(ChannelNotFound e) {
+            logger.debug("No instrumentation for "
+                    + ChannelIdUtil.toString(channel.get_id()));
             return false;
         }
         if(ResponseGain.isValid(instrumentation.the_response.the_sensitivity)) {
@@ -22,11 +29,15 @@ public class RepairSensitivity implements ChannelSubsetter {
         Response resp = instrumentation.the_response;
         Stage[] stages = resp.stages;
         if(stages.length == 0) {
+            logger.debug("No stages in the response of "
+                    + ChannelIdUtil.toString(channel.get_id()));
             return false;
         }
         float sensitivity = stages[0].the_gain.gain_factor;
         for(int i = 1; i < stages.length; i++) {
             if(stages[i - 1].the_gain.frequency != stages[i].the_gain.frequency) {
+                logger.debug("Different frequencies in the stages of the response of "
+                        + ChannelIdUtil.toString(channel.get_id()));
                 return false;
             }
             sensitivity *= stages[i].the_gain.gain_factor;
@@ -35,4 +46,6 @@ public class RepairSensitivity implements ChannelSubsetter {
         resp.the_sensitivity.frequency = stages[0].the_gain.frequency;
         return true;
     }
+
+    private Logger logger = Logger.getLogger(RepairSensitivity.class);
 }
