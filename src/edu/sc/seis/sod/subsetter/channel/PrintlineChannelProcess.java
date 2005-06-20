152e44ebd70d7fcdd15421ec872f5c29438ab55d
groves <none@none>
2005-06-20 10:47:00 -0400

    [svn r13945] renaming from processor
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/channel/PrintlineChannelProcess.java
@@ -0,0 +1,31 @@
+package edu.sc.seis.sod.subsetter.channel;
+
+import java.io.IOException;
+import org.w3c.dom.Element;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
+import edu.sc.seis.sod.velocity.PrintlineVelocitizer;
+
+/**
+ * @author <a href="mailto:crotwell@pooh">Philip Crotwell </a>
+ */
+public class PrintlineChannelProcess implements ChannelSubsetter {
+
+    public PrintlineChannelProcess(Element config) {
+        filename = DOMHelper.extractText(config, "filename", "");
+        template = DOMHelper.extractText(config, "template", DEFAULT_TEMPLATE);
+    }
+
+    public boolean accept(Channel channel, NetworkAccess network)
+            throws IOException {
+        velocitizer.evaluate(filename, template, channel);
+        return true;
+    }
+
+    public static final String DEFAULT_TEMPLATE = "Channel: $channel";
+
+    private PrintlineVelocitizer velocitizer = new PrintlineVelocitizer();
+
+    private String filename, template;
+}// PrintlineChannelProcessor
