762ad016e24539c58a3981bfa292f4fb4c5e43c2
groves <none@none>
2005-07-05 10:21:49 -0400

    [svn r14139] initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/source/event/SimpleEventSource.java
@@ -0,0 +1,46 @@
+package edu.sc.seis.sod.source.event;
+
+import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.cache.CacheEvent;
+import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
+
+public abstract class SimpleEventSource implements EventSource {
+
+    public abstract CacheEvent[] getEvents();
+
+    public boolean hasNext() {
+        return !hasNextBeenCalled;
+    }
+
+    public CacheEvent[] next() {
+        hasNextBeenCalled = true;
+        return getEvents();
+    }
+
+    public TimeInterval getWaitBeforeNext() {
+        return new TimeInterval(0, UnitImpl.SECOND);
+    }
+
+    public MicroSecondTimeRange getEventTimeRange() {
+        CacheEvent[] events = getEvents();
+        MicroSecondDate earliest = extractBeginTime(events[0]);
+        MicroSecondDate latest = earliest;
+        for(int i = 0; i < events.length; i++) {
+            MicroSecondDate eventTime = extractBeginTime(events[i]);
+            if(eventTime.before(earliest)) {
+                earliest = eventTime;
+            } else if(eventTime.after(latest)) {
+                latest = eventTime;
+            }
+        }
+        return new MicroSecondTimeRange(earliest, latest);
+    }
+
+    public MicroSecondDate extractBeginTime(CacheEvent ev) {
+        return new MicroSecondDate(ev.getOrigin().origin_time);
+    }
+
+    public boolean hasNextBeenCalled = false;
+}
