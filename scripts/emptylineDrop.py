6157f3d060c08713328b2cdd1279adfefd5cb47e
groves <none@none>
2005-07-15 14:37:06 -0400

    [svn r14262] moved into regenerateRecipes.py
    --HG--
    branch : sod

--- scripts/emptylineDrop.py
+++
@@ -1,7 +0,0 @@
-import sys
-import re
-blanks = re.compile('^\s*$')
-
-for line in sys.stdin:
-    if not blanks.match(line):
-        print line,
--- scripts/generateDropperXSLT.py
+++
@@ -1,7 +0,0 @@
-import sys
-print """<?xml version="1.0"?>
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-    <xsl:output method="xml"/>
-    <xsl:include href="copyAll.xslt"/>
-    <xsl:template match="%s"/>
-</xsl:stylesheet>""" % ' | '.join(sys.argv[1:])
