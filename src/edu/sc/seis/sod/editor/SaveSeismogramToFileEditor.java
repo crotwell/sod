7991874cbdda69cea20e9149109a52d7b3d08e76
oliverpa <none@none>
2005-07-22 17:07:18 -0400

    [svn r14380] no longer broken.  still needs TauP stuff
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/SaveSeismogramToFileEditor.java
+++ src/edu/sc/seis/sod/editor/SaveSeismogramToFileEditor.java
@@ -1,10 +1,10 @@
 /**
  * SaveSeismogramToFileEditor.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.sod.editor;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Box;
@@ -15,64 +15,68 @@ import javax.swing.border.TitledBorder;
 import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Element;
 import org.w3c.dom.Text;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
 import edu.sc.seis.sod.process.waveform.SaveSeismogramToFile;
 
-public class SaveSeismogramToFileEditor extends PrintlineEventEditor{
+public class SaveSeismogramToFileEditor extends PrintlineEventEditor {
 
-    public SaveSeismogramToFileEditor(){
+    public SaveSeismogramToFileEditor() {}
 
-    }
-    
     protected String getTitle() {
         return "Event Directory";
     }
-    
+
     protected String getDefaultTemplateValue() {
         return SaveSeismogramToFile.DEFAULT_TEMPLATE;
     }
-    
+
     protected String evaluate(String template) {
         return super.evaluate(template);
     }
 
     public JComponent getGUI(Element element) throws Exception {
-        Box vBox= Box.createVerticalBox();
+        Box vBox = Box.createVerticalBox();
         vBox.setBorder(new TitledBorder(SimpleGUIEditor.getDisplayName(element.getNodeName())));
-
         Box b = Box.createHorizontalBox();
         b.add(Box.createHorizontalGlue());
         b.add(new JLabel("File Type"));
         b.add(Box.createHorizontalStrut(10));
-        final Text text = (Text)XPathAPI.selectSingleNode(element, "fileType/text()");
+        final Text text = getTextChildFromPossiblyNonexistentElement(element,
+                                                                     "fileType",
+                                                                     fileTypes[1]);
         final JComboBox fileTypeBox = new JComboBox(fileTypes);
         fileTypeBox.setSelectedItem(text.getNodeValue());
-        fileTypeBox.addActionListener(new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        text.setNodeValue(fileTypeBox.getSelectedItem().toString());
-                    }
-                });
+        fileTypeBox.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                text.setNodeValue(fileTypeBox.getSelectedItem().toString());
+            }
+        });
         b.add(fileTypeBox);
         b.add(Box.createHorizontalGlue());
         vBox.add(b);
-
         b = Box.createHorizontalBox();
         b.add(Box.createHorizontalGlue());
         b.add(new JLabel("Data Directory"));
         b.add(Box.createHorizontalStrut(10));
-        Text text2 = (Text)XPathAPI.selectSingleNode(element, "dataDirectory/text()");
+        Text text2 = getTextChildFromPossiblyNonexistentElement(element,
+                                                                "dataDirectory",
+                                                                SaveSeismogramToFile.DEFAULT_DATA_DIRECTORY);
+        text2 = (Text)XPathAPI.selectSingleNode(element, "dataDirectory/text()");
         b.add(EditorUtil.getTextField(text2));
         b.add(Box.createHorizontalGlue());
         vBox.add(b);
-
         b = Box.createHorizontalBox();
         b.setBorder(new TitledBorder(getTitle()));
-        JComponent comp = createVelocityFilenameEditor(element, getDefaultTemplateValue(), "eventDirLabel", "Label", false);
+        JComponent comp = createVelocityFilenameEditor(element,
+                                                       getDefaultTemplateValue(),
+                                                       "eventDirLabel",
+                                                       "Label",
+                                                       false);
         b.add(comp);
         vBox.add(b);
-
         return vBox;
     }
 
     public static String[] fileTypes = {"sac", "mseed"};
 }
-
