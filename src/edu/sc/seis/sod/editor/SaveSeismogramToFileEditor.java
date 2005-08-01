3abb9c2775fb094d950e9fd3fa83308d7643573c
oliverpa <none@none>
2005-08-01 14:32:56 -0400

    [svn r14392] from the 'this took entirely too long' department: edit existing tauP sac headers
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/editor/SaveSeismogramToFileEditor.java
+++ src/edu/sc/seis/sod/editor/SaveSeismogramToFileEditor.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.border.TitledBorder;
 import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
 import edu.sc.seis.sod.process.waveform.SaveSeismogramToFile;
@@ -55,6 +56,29 @@ public class SaveSeismogramToFileEditor extends PrintlineEventEditor {
         b.add(fileTypeBox);
         b.add(Box.createHorizontalGlue());
         vBox.add(b);
+                
+        if (text.getNodeValue().equals("sac")) {
+            if(DOMHelper.hasElement(element, "sacHeader")) {
+                Element sacHeader = DOMHelper.extractElement(element,
+                                                             "sacHeader");
+                NodeList nl = DOMHelper.getElements(sacHeader, "phaseTime");
+                for(int i = 0; i < nl.getLength(); i++) {
+                    Element phaseEl = (Element)nl.item(i);
+                    Box hBox = Box.createHorizontalBox();
+                    hBox.setBorder(new TitledBorder("Phase Time"));
+                    hBox.add(Box.createHorizontalStrut(10));
+                    hBox.add(EditorUtil.getLabeledTextField(DOMHelper.getElement(phaseEl, "model"), "Model"));
+                    hBox.add(Box.createHorizontalStrut(10));
+                    hBox.add(EditorUtil.getLabeledTextField(DOMHelper.getElement(phaseEl, "phaseName"), "Phase Name"));
+                    hBox.add(Box.createHorizontalStrut(10));
+                    hBox.add(new JLabel("tHeader:"));
+                    hBox.add(EditorUtil.createNumberSpinner(getTextChildFromPossiblyNonexistentElement(phaseEl, "tHeader", "0"), 0, 9, 1));
+                    hBox.add(Box.createHorizontalGlue());
+                    vBox.add(hBox);
+                }
+            }
+        }
+        
         b = Box.createHorizontalBox();
         b.add(Box.createHorizontalGlue());
         b.add(new JLabel("Data Directory"));
