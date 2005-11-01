6b2d30495debe7517571a0e1ce7d0db312206329
groves <none@none>
2005-10-31 23:14:28 -0500

    [svn r15083] removed abortive attempt at putting retries in here
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/WaveformResult.java
+++ src/edu/sc/seis/sod/process/waveform/WaveformResult.java
@@ -24,14 +24,6 @@ public class WaveformResult {
         return reason.isSuccess();
     }
 
-    public boolean isRetry() {
-        return retry;
-    }
-
-    public void setRetry(boolean retry) {
-        this.retry = retry;
-    }
-
     public LocalSeismogramImpl[] getSeismograms() {
         return seismograms;
     }
@@ -43,6 +35,4 @@ public class WaveformResult {
     private LocalSeismogramImpl[] seismograms;
 
     private StringTree reason;
-
-    private boolean retry;
 }
