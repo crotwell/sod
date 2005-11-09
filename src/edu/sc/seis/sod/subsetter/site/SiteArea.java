bf797e8b199d419baa00a17e08857899a72fca27
fenner <none@none>
2005-11-09 17:02:48 -0500

    [svn r15196] send SiteArea queries to parent (AreaSubsetter)
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/site/SiteArea.java
+++ src/edu/sc/seis/sod/subsetter/site/SiteArea.java
@@ -1,14 +1,11 @@
 package edu.sc.seis.sod.subsetter.site;
 
-import edu.iris.Fissures.GlobalArea;
 import edu.iris.Fissures.IfNetwork.NetworkAccess;
 import edu.iris.Fissures.IfNetwork.Site;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodElement;
-import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.subsetter.AreaSubsetter;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  * SiteArea.java Created: Thu Mar 14 14:02:33 2002
@@ -18,31 +15,13 @@ import org.w3c.dom.NodeList;
  *          implements SiteSubsetter and can be any one of GlobalArea or BoxArea
  *          or PointDistanceArea or FlinneEngdahlArea.
  */
-public class SiteArea implements SiteSubsetter, SodElement {
+public class SiteArea extends AreaSubsetter implements SiteSubsetter, SodElement {
 
     public SiteArea(Element config) throws ConfigurationException {
-        NodeList children = config.getChildNodes();
-        for(int i = 0; i < children.getLength(); i++) {
-            Node node = children.item(i);
-            if(node instanceof Element) {
-                area = (edu.iris.Fissures.Area)SodUtil.load((Element)node, "");
-                break;
-            }
-        }
+        super(config);
     }
 
     public boolean accept(Site e, NetworkAccess network) {
-        if(area instanceof edu.iris.Fissures.BoxArea) {
-            edu.iris.Fissures.BoxArea boxArea = (edu.iris.Fissures.BoxArea)area;
-            if(e.my_location.latitude >= boxArea.min_latitude
-                    && e.my_location.latitude <= boxArea.max_latitude
-                    && e.my_location.longitude >= boxArea.min_longitude
-                    && e.my_location.longitude <= boxArea.max_longitude) {
-                return true;
-            } else return false;
-        } else if(area instanceof GlobalArea) return true;
-        return true;
+        return super.accept(e.my_location);
     }
-
-    private edu.iris.Fissures.Area area = null;
-}// SiteArea
+}
