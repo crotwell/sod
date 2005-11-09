7bdebac8577c442b7b9dc3683265bdef5bc18af4
fenner <none@none>
2005-11-09 17:03:38 -0500

    [svn r15197] send StationArea queries to parent (AreaSubsetter)
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/station/StationArea.java
+++ src/edu/sc/seis/sod/subsetter/station/StationArea.java
@@ -1,40 +1,20 @@
 package edu.sc.seis.sod.subsetter.station;
 
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import edu.iris.Fissures.GlobalArea;
 import edu.iris.Fissures.IfNetwork.NetworkAccess;
 import edu.iris.Fissures.IfNetwork.Station;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodElement;
-import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.subsetter.AreaSubsetter;
 
-public class StationArea implements StationSubsetter, SodElement {
+public class StationArea extends AreaSubsetter implements StationSubsetter, SodElement {
 
     public StationArea(Element config) throws ConfigurationException {
-        NodeList children = config.getChildNodes();
-        for(int i = 0; i < children.getLength(); i++) {
-            Node node = children.item(i);
-            if(node instanceof Element) {
-                area = (edu.iris.Fissures.Area)SodUtil.load((Element)node, "");
-                break;
-            }
-        }
+        super(config);
     }
 
     public boolean accept(Station e, NetworkAccess network) {
-        if(area instanceof edu.iris.Fissures.BoxArea) {
-            edu.iris.Fissures.BoxArea boxArea = (edu.iris.Fissures.BoxArea)area;
-            if(e.my_location.latitude >= boxArea.min_latitude
-                    && e.my_location.latitude <= boxArea.max_latitude
-                    && e.my_location.longitude >= boxArea.min_longitude
-                    && e.my_location.longitude <= boxArea.max_longitude) {
-                return true;
-            } else return false;
-        } else if(area instanceof GlobalArea) return true;
-        return true;
+        return super.accept(e.my_location);
     }
 
-    private edu.iris.Fissures.Area area = null;
-}// StationArea
+}
