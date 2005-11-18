dc8c942349de7d6b7e8e74079eeb07e11808abbc
crotwell <none@none>
2005-11-18 13:58:28 -0500

    [svn r15277] do not catch exceptions and then keep going as if nothing happened, also general cleanup
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/AreaSubsetter.java
+++ src/edu/sc/seis/sod/subsetter/AreaSubsetter.java
@@ -1,22 +1,19 @@
 package edu.sc.seis.sod.subsetter;
 
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import edu.iris.Fissures.BoxArea;
 import edu.iris.Fissures.GlobalArea;
 import edu.iris.Fissures.Location;
 import edu.sc.seis.fissuresUtil.bag.AreaUtil;
 import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodUtil;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 
 /**
  * SiteArea.java Created: Thu Mar 14 14:02:33 2002
@@ -30,7 +27,7 @@ public class AreaSubsetter {
 
     public AreaSubsetter(Element config) throws ConfigurationException {
         String fileLocation = DOMHelper.extractText(config, "polygonFile", "");
-        if(fileLocation.equals("")){
+        if(fileLocation.equals("")) {
             NodeList children = config.getChildNodes();
             for(int i = 0; i < children.getLength(); i++) {
                 Node node = children.item(i);
@@ -40,35 +37,27 @@ public class AreaSubsetter {
                 }
             }
         } else {
-            File file = new File(fileLocation);
-            FileInputStream fis = null;
             try {
-                fis = new FileInputStream(file);
-            } catch(FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader reader = new BufferedReader(isr);
-            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileLocation)));
                 locationArray = AreaUtil.loadPolygon(reader);
             } catch(IOException e) {
-                e.printStackTrace();
+                throw new ConfigurationException("Problem reading from file " + fileLocation, e);
             }
         }
     }
 
     public boolean accept(Location loc) {
-        if(area instanceof edu.iris.Fissures.BoxArea) {
-            edu.iris.Fissures.BoxArea boxArea = (edu.iris.Fissures.BoxArea)area;
-            return AreaUtil.inArea(boxArea, loc);
+        if(area instanceof BoxArea) {
+            return AreaUtil.inArea((BoxArea)area, loc);
         } else if(area instanceof GlobalArea) {
             return true;
-        } else if (locationArray != null) {
+        } else if(locationArray != null) {
             return AreaUtil.inArea(locationArray, loc);
         }
         return true;
-        
     }
+
     protected edu.iris.Fissures.Area area = null;
+
     protected Location[] locationArray;
 }
