c9fb04d47c6f594b232b03ae6f6e9e951f9de536
crotwell <none@none>
2005-11-29 13:54:18 -0500

    [svn r15341] needs to extend subsetter for logicals to work
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/request/vector/VectorRequest.java
+++ src/edu/sc/seis/sod/subsetter/request/vector/VectorRequest.java
@@ -9,8 +9,9 @@ import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.sc.seis.sod.ChannelGroup;
 import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.subsetter.Subsetter;
 
-public interface VectorRequest {
+public interface VectorRequest extends Subsetter {
 
     public boolean accept(EventAccessOperations event,
                           ChannelGroup channel,
