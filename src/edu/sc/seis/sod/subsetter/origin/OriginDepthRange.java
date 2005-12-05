141d021a1fcb56711f5b14acf17893d32adeb538
groves <none@none>
2005-12-05 18:13:26 -0500

    [svn r15446] removed incorrect comment
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/origin/OriginDepthRange.java
+++ src/edu/sc/seis/sod/subsetter/origin/OriginDepthRange.java
@@ -6,55 +6,18 @@ import edu.iris.Fissures.IfEvent.Origin;
 import edu.iris.Fissures.model.QuantityImpl;
 import org.w3c.dom.Element;
 
-/**
- * This subsetter specifies the depthRange for the origin
- *
- *<pre>
- *  &lt;originDepthRange&gt;
- *      &lt;unitRange&gt;
- *           &lt;unit&gt;KILOMETER&lt;/unit&gt;
- *           &lt;min&gt;10&lt;/min&gt;
- *           &lt;max&gt;100&lt;/max&gt;
- *      &lt;/unitRange&gt;
- *  &lt;/originDepthRange&gt;
- *
- *                  (or)
- *
- *  &lt;originDepthRange&gt;
- *      &lt;unitRange&gt;
- *           &lt;unit&gt;KILOMETER&lt;/unit&gt;
- *           &lt;min&gt;10&lt;/min&gt;
- *      &lt;/unitRange&gt;
- *  &lt;/originDepthRange&gt;
- *
- *                  (or)
- *
- *  &lt;originDepthRange&gt;
- *      &lt;unitRange&gt;
- *           &lt;unit&gt;KILOMETER&lt;/unit&gt;
- *           &lt;max&gt;100&lt;/max&gt;
- *      &lt;/unitRange&gt;
- *  &lt;/originDepthRange&gt;
- *
- *                  (or)
- *
- *  &lt;originDepthRange&gt;
- *      &lt;unitRange&gt;
- *           &lt;unit&gt;KILOMETER&lt;/unit&gt;
- *      &lt;/unitRange&gt;
- *  &lt;/originDepthRange&gt;
- *</pre>
- */
+public class OriginDepthRange extends edu.sc.seis.sod.subsetter.DepthRange
+        implements OriginSubsetter {
 
-public class OriginDepthRange extends edu.sc.seis.sod.subsetter.DepthRange implements OriginSubsetter{
-    public OriginDepthRange (Element config) throws Exception{
+    public OriginDepthRange(Element config) throws Exception {
         super(config);
     }
 
-    public boolean accept(EventAccessOperations event, EventAttr eventAttr, Origin origin) {
+    public boolean accept(EventAccessOperations event,
+                          EventAttr eventAttr,
+                          Origin origin) {
         QuantityImpl actualDepth = (QuantityImpl)origin.my_location.depth;
-        if(actualDepth.greaterThanEqual(getMinDepth()) && actualDepth.lessThanEqual(getMaxDepth())) {
-            return true;
-        } else return false;
+        return actualDepth.greaterThanEqual(getMinDepth())
+                && actualDepth.lessThanEqual(getMaxDepth());
     }
 }// OriginDepthRange
