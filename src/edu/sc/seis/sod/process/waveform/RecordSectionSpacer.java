bf6d473b7d4f58e02eb1caa80f98256683b9371e
groves <none@none>
2005-12-07 15:29:59 -0500

    [svn r15474] fix dumb bug where I throw out and log events without distance and then try to put them in the record section anyway
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/RecordSectionSpacer.java
+++ src/edu/sc/seis/sod/process/waveform/RecordSectionSpacer.java
@@ -1,7 +1,7 @@
 package edu.sc.seis.sod.process.waveform;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -33,20 +33,31 @@ public class RecordSectionSpacer {
     }
 
     public DataSetSeismogram[] spaceOut(DataSetSeismogram[] dataSeis) {
-        Map distMap = new HashMap();
+        final Map dists = new HashMap();
+        List remaining = new ArrayList();
         for(int i = 0; i < dataSeis.length; i++) {
             QuantityImpl dist = DisplayUtils.calculateDistance(dataSeis[i]);
             if(dist != null) {
-                distMap.put(dataSeis[i], dist);
-            }else{
+                remaining.add(dataSeis[i]);
+                dists.put(dataSeis[i], dist);
+            } else {
                 logger.debug("Unable to calculate distance for " + dataSeis[i]);
             }
         }
-        sortByDistance(dataSeis, distMap);
-        List remaining = new ArrayList();
-        for(int i = 0; i < dataSeis.length; i++) {
-            remaining.add(dataSeis[i]);
-        }
+        Collections.sort(remaining, new Comparator() {
+
+            public int compare(Object o1, Object o2) {
+                QuantityImpl dist1 = (QuantityImpl)dists.get(o1);
+                QuantityImpl dist2 = (QuantityImpl)dists.get(o2);
+                if(dist1.lessThan(dist2)) {
+                    return -1;
+                } else if(dist1.greaterThan(dist2)) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
         List accepted = new ArrayList();
         double nextSlot = 0;
         while(remaining.size() > 0) {
@@ -58,7 +69,7 @@ public class RecordSectionSpacer {
             double closestSToN = Double.MAX_VALUE;
             while(it.hasNext()) {
                 DataSetSeismogram cur = (DataSetSeismogram)it.next();
-                double dist = ((QuantityImpl)distMap.get(cur)).getValue();
+                double dist = ((QuantityImpl)dists.get(cur)).getValue();
                 double sToN = Double.MAX_VALUE;
                 if(cur.getAuxillaryData(SaveSeismogramToFile.SVN_PARAM) != null) {
                     sToN = Double.parseDouble((String)cur.getAuxillaryData(SaveSeismogramToFile.SVN_PARAM));
@@ -96,24 +107,6 @@ public class RecordSectionSpacer {
             }
         }
         return (DataSetSeismogram[])accepted.toArray(new DataSetSeismogram[accepted.size()]);
-    }
-
-    public static void sortByDistance(DataSetSeismogram[] seismograms,
-                                      final Map dists) {
-        Arrays.sort(seismograms, new Comparator() {
-
-            public int compare(Object o1, Object o2) {
-                QuantityImpl dist1 = (QuantityImpl)dists.get(o1);
-                QuantityImpl dist2 = (QuantityImpl)dists.get(o2);
-                if(dist1.lessThan(dist2)) {
-                    return -1;
-                } else if(dist1.greaterThan(dist2)) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });
     }
 
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(RecordSectionSpacer.class);
