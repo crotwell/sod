e1c380b7124f93e4b850fcec9f8936b98e9bb4a6
groves <none@none>
2006-05-03 15:23:54 -0400

    [svn r17098] initital
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/availableData/PrintlineAvailableDataProcess.java
@@ -0,0 +1,43 @@
+package edu.sc.seis.sod.subsetter.availableData;
+
+import org.w3c.dom.Element;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
+import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.status.StringTree;
+import edu.sc.seis.sod.status.StringTreeLeaf;
+import edu.sc.seis.sod.velocity.PrintlineVelocitizer;
+
+public class PrintlineAvailableDataProcess implements AvailableDataSubsetter {
+
+    public PrintlineAvailableDataProcess(Element config)
+            throws ConfigurationException {
+        filename = DOMHelper.extractText(config, "filename", "");
+        template = DOMHelper.extractText(config, "template", DEFAULT_TEMPLATE);
+        velocitizer = new PrintlineVelocitizer(new String[] {filename, template});
+    }
+
+    private PrintlineVelocitizer velocitizer;
+
+    private String template, filename;
+
+    public static final String DEFAULT_TEMPLATE = "Got $availableRequests.size()";
+
+    public StringTree accept(EventAccessOperations event,
+                             Channel channel,
+                             RequestFilter[] original,
+                             RequestFilter[] available,
+                             CookieJar cookieJar) throws Exception {
+        velocitizer.evaluate(filename,
+                             template,
+                             event,
+                             channel,
+                             original,
+                             available,
+                             cookieJar);
+        return new StringTreeLeaf(this, true);
+    }
+}
