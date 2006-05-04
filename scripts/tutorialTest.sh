204257e6afcab67bbcb3c6a484e38b071d1095b1
groves <none@none>
2006-05-04 17:01:22 -0400

    [svn r17128] autogenerate doc stuff
    --HG--
    branch : sod

--- scripts/tutorialTest.sh
+++ scripts/tutorialTest.sh
@@ -11,6 +11,23 @@ runrecipe(){
     run $1 "-f ${SOD_HOME}/recipes/tutorial/$1.xml"
 }
 
+generateDemoDoc(){
+mkdir documentation/demo/
+head -10 demo.out > documentation/demo/head
+echo '...' >> documentation/demo/head
+head -1 demo.out > documentation/demo/events
+grep Channel demo.out | head -1 > documentation/demo/channels
+grep seismograms demo.out | head -1 > documentation/demo/seismograms
+}
+
+generateNetworkDoc(){
+DOCDIR=documentation/network
+mkdir $DOCDIR
+cat simpleNetwork.out | wc -l > ${DOCDIR}/all
+cat subsettingNetwork.out | wc -l > ${DOCDIR}/subset
+cat network.out | wc -l > ${DOCDIR}/completeSubset
+}
+
 VERSION=2.1.2beta
 DIR=sod-${VERSION}
 sod.py -o . --tar
@@ -18,8 +35,9 @@ tar xzf ${DIR}.tar.gz
 export SOD_HOME=`pwd`/${DIR}
 echo $SOD_HOME
 export PATH=${SOD_HOME}/bin:$PATH
-runrecipe waveform
+
 run demo -demo
+runrecipe waveform
 runrecipe network
 runrecipe event
 runrecipe simpleNetwork
@@ -27,3 +45,7 @@ runrecipe subsettingNetwork
 runrecipe simpleEvent
 runrecipe simpleWaveform
 runrecipe subsettingWaveform
+
+mkdir documentation
+generateDemoDoc
+generateNetworkDoc
