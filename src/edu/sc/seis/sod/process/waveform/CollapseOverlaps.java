e0d0d35b8c8bea7f0dee268aba4bf0ef6b1a9df8
crotwell <none@none>
2006-06-09 00:12:26 -0400

    [svn r17408] implement collapseOverlaps to get rid of annoying overlapping seismograms from Pond
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/process/waveform/CollapseOverlaps.java
@@ -0,0 +1,22 @@
+package edu.sc.seis.sod.process.waveform;
+
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.time.ReduceTool;
+import edu.sc.seis.sod.CookieJar;
+
+
+public class CollapseOverlaps implements WaveformProcess {
+
+    public WaveformResult process(EventAccessOperations event,
+                                  Channel channel,
+                                  RequestFilter[] original,
+                                  RequestFilter[] available,
+                                  LocalSeismogramImpl[] seismograms,
+                                  CookieJar cookieJar) throws Exception {
+        return new WaveformResult(true, ReduceTool.cutOverlap(seismograms), this);
+    }
+
+}
