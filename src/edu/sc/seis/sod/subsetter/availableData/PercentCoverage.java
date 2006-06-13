a3136786e973daddfce5c65bc51fa73aeb26909b
groves <none@none>
2006-06-13 15:25:55 -0400

    [svn r17431] added percentCoverage
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/subsetter/availableData/PercentCoverage.java
@@ -0,0 +1,65 @@
+package edu.sc.seis.sod.subsetter.availableData;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.w3c.dom.Element;
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.display.MicroSecondTimeRange;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
+import edu.sc.seis.fissuresUtil.time.CoverageTool;
+import edu.sc.seis.fissuresUtil.time.ReduceTool;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.status.StringTree;
+import edu.sc.seis.sod.status.StringTreeLeaf;
+
+public class PercentCoverage implements AvailableDataSubsetter {
+
+    private double percentage;
+    
+    public PercentCoverage(Element content){
+        DOMHelper.extractDouble(content, ".", 100);
+    }
+
+    public PercentCoverage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public StringTree accept(EventAccessOperations event,
+                             Channel channel,
+                             RequestFilter[] original,
+                             RequestFilter[] available,
+                             CookieJar cookieJar) {
+        return new StringTreeLeaf(this, accept(original, available));
+    }
+
+    public boolean accept(RequestFilter[] original, RequestFilter[] available) {
+        RequestFilter[] uncovered = CoverageTool.notCovered(original, available);
+        TimeInterval totalOriginalTime = sum(toMSTR(original));
+        TimeInterval totalUncoveredTime = sum(toMSTR(uncovered));
+        return (1 - totalUncoveredTime.divideBy(totalOriginalTime).getValue()) * 100 >= percentage;
+    }
+
+    private TimeInterval sum(List microSecondTimeRanges) {
+        TimeInterval total = new TimeInterval(0, UnitImpl.SECOND);
+        for(Iterator iter = microSecondTimeRanges.iterator(); iter.hasNext();) {
+            MicroSecondTimeRange time = (MicroSecondTimeRange)iter.next();
+            total = total.add(time.getInterval());
+        }
+        return total;
+    }
+
+    private List toMSTR(RequestFilter[] filters) {
+        // Ensure that there are no overlaps in the request filters
+        filters = ReduceTool.merge(filters);
+        List mstrs = new ArrayList(filters.length);
+        for(int i = 0; i < filters.length; i++) {
+            mstrs.add(new MicroSecondTimeRange(filters[i]));
+        }
+        return mstrs;
+    }
+}
