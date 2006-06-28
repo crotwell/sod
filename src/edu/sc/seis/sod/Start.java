8bb0c0954d4851b89263687df07e78f2afe63f10
groves <none@none>
2006-06-28 11:37:24 -0400

    [svn r17621] cut number of retries down from 1000 to the default of 3.
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/Start.java
+++ src/edu/sc/seis/sod/Start.java
@@ -67,7 +67,6 @@ public class Start {
             }
         });
         GlobalExceptionHandler.registerWithAWTThread();
-        BulletproofVestFactory.setDefaultNumRetry(1000);
     }
 
     /**
