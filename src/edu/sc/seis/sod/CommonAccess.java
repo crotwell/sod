307cfb978d4c7268219538126c594c75a1ad4575
groves <none@none>
2006-10-03 11:38:45 -0400

    [svn r18304] use the set props in orb creation since we were using it already
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/CommonAccess.java
+++ src/edu/sc/seis/sod/CommonAccess.java
@@ -2,49 +2,49 @@ package edu.sc.seis.sod;
 
 import java.util.Properties;
 import org.apache.log4j.Logger;
-import org.apache.log4j.lf5.util.LogMonitorAdapter;
 import edu.iris.Fissures.model.AllVTFactory;
 import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
 
-
 /**
  * CommonAccess.java
- *
- *
+ * 
+ * 
  * Created: Wed Mar 20 14:00:32 2002
- *
+ * 
  * @author <a href="mailto:">Srinivasa Telukutla</a>
  * @version
  */
-
 public class CommonAccess {
-    private CommonAccess (){}
-    
-    public static ClassLoader getLoader(){
+
+    private CommonAccess() {}
+
+    public static ClassLoader getLoader() {
         return getCommonAccess().getClass().getClassLoader();
     }
 
-    public static CommonAccess getCommonAccess() { return commonAccess; }
-
-    public FissuresNamingService getFissuresNamingService(){
-        if (fissuresNamingService == null) {
-            fissuresNamingService = new FissuresNamingService(getORB());
-            if ( props.containsKey(FissuresNamingService.CORBALOC_PROP)) {
-                fissuresNamingService.setNameServiceCorbaLoc((String)props.get(FissuresNamingService.CORBALOC_PROP));
-            } else if ( System.getProperties().containsKey(FissuresNamingService.CORBALOC_PROP)) {
-                fissuresNamingService.setNameServiceCorbaLoc((String)System.getProperties().get(FissuresNamingService.CORBALOC_PROP));
-            } // end of if ()
-        } // end of if (fissuresNamingService == null)
-
-        return fissuresNamingService;
-
+    public static CommonAccess getCommonAccess() {
+        return commonAccess;
     }
 
-    protected void initORB(String[] args, java.util.Properties props) {
-        if (orb == null) {
+    public FissuresNamingService getFissuresNamingService() {
+        if(fissuresNamingService == null) {
+            fissuresNamingService = new FissuresNamingService(getORB());
+            if(props.containsKey(FissuresNamingService.CORBALOC_PROP)) {
+                fissuresNamingService.setNameServiceCorbaLoc((String)props.get(FissuresNamingService.CORBALOC_PROP));
+            } else if(System.getProperties()
+                    .containsKey(FissuresNamingService.CORBALOC_PROP)) {
+                fissuresNamingService.setNameServiceCorbaLoc((String)System.getProperties()
+                        .get(FissuresNamingService.CORBALOC_PROP));
+            } // end of if ()
+        } // end of if (fissuresNamingService == null)
+        return fissuresNamingService;
+    }
+
+    protected void initORB(String[] args) {
+        if(orb == null) {
             // Initialize the ORB.
             orb = (org.omg.CORBA_2_3.ORB)org.omg.CORBA.ORB.init(args, props);
-            logger.info("ORB class is "+orb.getClass().getName());
+            logger.info("ORB class is " + orb.getClass().getName());
             // register valuetype factories
             AllVTFactory vt = new AllVTFactory();
             vt.register(orb);
@@ -52,8 +52,8 @@ public class CommonAccess {
     }
 
     public org.omg.CORBA_2_3.ORB getORB() {
-        if (orb == null) {
-            initORB(null, null);
+        if(orb == null) {
+            initORB(null);
         } // end of if (orb == null)
         return orb;
     }
@@ -61,19 +61,14 @@ public class CommonAccess {
     public void setProps(Properties props) {
         this.props = props;
     }
-    
+
     private static CommonAccess commonAccess = new CommonAccess();
 
-    private LogMonitorAdapter adapter;
-
-    private org.omg.CORBA_2_3.ORB orb = null;
+    private org.omg.CORBA_2_3.ORB orb;
 
     FissuresNamingService fissuresNamingService;
 
     Properties props;
-    
+
     private static Logger logger = Logger.getLogger(CommonAccess.class);
-
-
 }// CommonAccess
-
