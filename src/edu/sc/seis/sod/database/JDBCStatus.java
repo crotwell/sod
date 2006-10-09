e122a50fa183b3a6e52ee2809a7ae3e5131c797a
groves <none@none>
2006-10-09 13:43:37 -0400

    [svn r18336] Only insert status rows when creating table
    Throw any SQLExceptions generated instead of just logging them.  The system
    will be pretty useless without a status table.
    
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/database/JDBCStatus.java
+++ src/edu/sc/seis/sod/database/JDBCStatus.java
@@ -1,9 +1,8 @@
 /**
  * JDBCStatus.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.sod.database;
 
 import java.sql.Connection;
@@ -11,27 +10,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import edu.sc.seis.fissuresUtil.database.ConnMgr;
 import edu.sc.seis.fissuresUtil.database.DBUtil;
-import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.sod.Status;
 
-public class JDBCStatus{
-    public JDBCStatus(){
-        try {
-            Connection conn = ConnMgr.createConnection();
-            Statement stmt = conn.createStatement();
-            if(!DBUtil.tableExists("status", conn)){
-                stmt.executeUpdate("CREATE TABLE status ( id int, name varchar )");
-            }
+public class JDBCStatus {
+
+    public JDBCStatus() throws SQLException {
+        Connection conn = ConnMgr.createConnection();
+        Statement stmt = conn.createStatement();
+        if(!DBUtil.tableExists("status", conn)) {
+            stmt.executeUpdate("CREATE TABLE status ( id int, name varchar )");
             Status[][] all = Status.ALL;
-            for (int i = 0; i < all.length; i++) {
-                for (int j = 0; j < all[i].length; j++) {
-                    stmt.executeUpdate("INSERT INTO status( id, name ) values ( " + all[i][j].getAsShort() + ", '" + all[i][j].toString() + "' )");
+            for(int i = 0; i < all.length; i++) {
+                for(int j = 0; j < all[i].length; j++) {
+                    stmt.executeUpdate("INSERT INTO status( id, name ) values ( "
+                            + all[i][j].getAsShort()
+                            + ", '"
+                            + all[i][j].toString() + "' )");
                 }
             }
-        } catch (SQLException e) {
-            GlobalExceptionHandler.handle(e);
         }
-
     }
 }
-
