26b7687eb21cab8918a4a9bbdf4b89aadc6db6b5
groves <none@none>
2006-11-09 11:04:47 -0500

    [svn r18458] whoops.  Actually remove the old accept code so the super code will be used
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/channel/ChannelNOT.java
+++ src/edu/sc/seis/sod/subsetter/channel/ChannelNOT.java
@@ -1,29 +1,14 @@
 package edu.sc.seis.sod.subsetter.channel;
 
-import java.util.Iterator;
 import org.w3c.dom.Element;
-import edu.iris.Fissures.IfNetwork.Channel;
-import edu.sc.seis.fissuresUtil.cache.ProxyNetworkAccess;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.status.StringTree;
-import edu.sc.seis.sod.status.StringTreeBranch;
-import edu.sc.seis.sod.status.StringTreeLeaf;
 
 public final class ChannelNOT extends  ChannelLogicalSubsetter
     implements ChannelSubsetter {
 
     public ChannelNOT (Element config) throws ConfigurationException {
         super(config);
-    }
-
-    public StringTree accept(Channel e, ProxyNetworkAccess network) throws Exception{
-        Iterator it = subsetters.iterator();
-        if (it.hasNext()) {
-            ChannelSubsetter filter = (ChannelSubsetter)it.next();
-            StringTree ans = filter.accept(e, network);
-            return new StringTreeBranch(this, ! ans.isSuccess(), ans); 
-        }
-        return new StringTreeLeaf(this, true);
     }
 
     public boolean isSuccess(StringTree[] reasons) {
