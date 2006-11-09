384b76c0d594f85222d304f25af81f7e3010f07b
groves <none@none>
2006-11-09 18:10:16 -0500

    [svn r18462] stringtreeeeeee
    --HG--
    branch : sod

--- externalExample/src/edu/sc/seis/sod/example/ChannelSubsetterExample.java
+++ externalExample/src/edu/sc/seis/sod/example/ChannelSubsetterExample.java
@@ -2,12 +2,14 @@ package edu.sc.seis.sod.example;
 
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.sc.seis.fissuresUtil.cache.ProxyNetworkAccess;
+import edu.sc.seis.sod.status.Fail;
+import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.subsetter.channel.ChannelSubsetter;
 
 public class ChannelSubsetterExample implements ChannelSubsetter {
 
-    public boolean accept(Channel channel, ProxyNetworkAccess network)
+    public StringTree accept(Channel channel, ProxyNetworkAccess network)
             throws Exception {
-        return false;
+        return new Fail(this);
     }
 }
