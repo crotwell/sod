4e37255239baa708b9e5878694411568b631e5a2
groves <none@none>
2007-01-31 17:33:34 -0500

    [svn r18781] added globs to channelcode
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/channel/ChannelCode.java
+++ src/edu/sc/seis/sod/subsetter/channel/ChannelCode.java
@@ -3,18 +3,51 @@ package edu.sc.seis.sod.subsetter.channel;
 import edu.iris.Fissures.IfNetwork.Channel;
 import edu.sc.seis.fissuresUtil.cache.ProxyNetworkAccess;
 import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.UserConfigurationException;
 import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.status.StringTreeLeaf;
 import org.w3c.dom.Element;
 
 public class ChannelCode implements ChannelSubsetter {
-    public ChannelCode(Element config) {
-        acceptedCode = SodUtil.getNestedText(config);
+
+    public ChannelCode(Element config) throws UserConfigurationException {
+        this(SodUtil.getNestedText(config));
     }
 
-    public StringTree accept(Channel channel, ProxyNetworkAccess network){
-        return new StringTreeLeaf(this, channel.get_id().channel_code.equals(acceptedCode));
+    public ChannelCode(String string) throws UserConfigurationException {
+        acceptedCode = string.toUpperCase();
+        if(acceptedCode.length() == 0
+                || (acceptedCode.length() == 1 && acceptedCode.charAt(0) == '*')) {
+            required = "***";
+        } else if(acceptedCode.length() == 2) {
+            if(acceptedCode.charAt(0) == '*') {
+                required = "**" + acceptedCode.charAt(1);
+            } else if(acceptedCode.charAt(1) == '*') {
+                required = acceptedCode.charAt(0) + "**";
+            } else {
+                throw new UserConfigurationException("A channelCode must be of length 3 if it doesn't contain any '*' characters.  Yours was '"
+                        + string + "'");
+            }
+        } else if(acceptedCode.length() == 3) {
+            required = acceptedCode;
+        } else {
+            throw new UserConfigurationException("A channelCode can be at most of length 3.  Yours was '"
+                    + string + "'");
+        }
     }
+
+    public StringTree accept(Channel channel, ProxyNetworkAccess network) {
+        String code = channel.get_id().channel_code;
+        for(int i = 0; i < required.length(); i++) {
+            if(required.charAt(i) != '*'
+                    && code.charAt(i) != required.charAt(i)) {
+                return new StringTreeLeaf(this, false);
+            }
+        }
+        return new StringTreeLeaf(this, true);
+    }
+
+    String required;
 
     private String acceptedCode;
-}//GainCode
+}// GainCode
---
+++ test/unit/edu/sc/seis/sod/subsetter/channel/TestChannelCode.java
@@ -0,0 +1,96 @@
+package edu.sc.seis.sod.subsetter.channel;
+
+import junit.framework.TestCase;
+import edu.iris.Fissures.IfNetwork.Channel;
+import edu.sc.seis.fissuresUtil.mockFissures.IfNetwork.MockChannel;
+import edu.sc.seis.sod.UserConfigurationException;
+
+public class TestChannelCode extends TestCase {
+
+    public void testStraightUpBHZ() throws UserConfigurationException {
+        cc = new ChannelCode("BHZ");
+        assertAccepted(BHZ);
+        assertRejected(BHE);
+        assertRejected(LHZ);
+        assertRejected(LLN);
+    }
+
+    public void testLowerCaseBHZ() throws UserConfigurationException {
+        cc = new ChannelCode("bhz");
+        assertAccepted(BHZ);
+        assertRejected(BHE);
+        assertRejected(LHZ);
+        assertRejected(LLN);
+    }
+
+    public void testBHStar() throws UserConfigurationException {
+        cc = new ChannelCode("BH*");
+        assertAccepted(BHZ);
+        assertAccepted(BHE);
+        assertRejected(LHZ);
+        assertRejected(LLN);
+    }
+
+    public void testStarhStar() throws UserConfigurationException {
+        cc = new ChannelCode("*h*");
+        assertAccepted(BHZ);
+        assertAccepted(BHE);
+        assertAccepted(LHZ);
+        assertRejected(LLN);
+    }
+
+    public void testStar() throws UserConfigurationException {
+        cc = new ChannelCode("*");
+        assertAccepted(BHZ);
+        assertAccepted(BHE);
+        assertAccepted(LHZ);
+        assertAccepted(LLN);
+    }
+
+    public void testBStar() throws UserConfigurationException {
+        cc = new ChannelCode("B*");
+        assertAccepted(BHZ);
+        assertAccepted(BHE);
+        assertRejected(LHZ);
+        assertRejected(LLN);
+    }
+
+    public void testStarLStar() throws UserConfigurationException {
+        cc = new ChannelCode("*L*");
+        assertRejected(BHZ);
+        assertRejected(BHE);
+        assertRejected(LHZ);
+        assertAccepted(LLN);
+    }
+
+    public void testTooFewCharsNotEnoughStars() {
+        try {
+            cc = new ChannelCode("AB");
+            fail("'AB' doesn't completly specify a channel code so it should provke a UserConfigurationException");
+        } catch(UserConfigurationException uce) {}
+    }
+
+    private void assertAccepted(Channel chan) {
+        assertTrue(cc.accept(chan, null).isSuccess());
+    }
+
+    private void assertRejected(Channel chan) {
+        assertFalse(cc.accept(chan, null).isSuccess());
+    }
+
+    private static final Channel BHZ = MockChannel.createChannel();
+
+    private static final Channel BHE = MockChannel.createEastChannel();
+
+    private static final Channel LHZ = MockChannel.createChannel();
+    static {
+        LHZ.get_id().channel_code = "LHZ";
+    }
+
+    private static final Channel LLN = MockChannel.createChannel();
+    static {
+        LLN.get_id().channel_code = "LLN";
+    }
+
+    private ChannelCode cc;
+}
