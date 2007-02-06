34721c52f5675cf956845059c7b10be6ebedcb72
groves <none@none>
2007-02-05 19:32:42 -0500

    [svn r18791] ytp
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/database/JDBCConfig.java
+++ src/edu/sc/seis/sod/database/JDBCConfig.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
