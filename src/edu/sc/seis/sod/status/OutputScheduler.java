a4ccd7615be725b11a3fb968cd1392cc6ccb7dde
groves <none@none>
2007-03-30 01:08:20 -0400

    [svn r18936] rather than sleeping INITIAL_WAIT before starting to chug away, wait for the first arm to come through and notify at that point.  Keeps us from idling and holding sod open for really short runs
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/status/OutputScheduler.java
+++ src/edu/sc/seis/sod/status/OutputScheduler.java
@@ -3,9 +3,7 @@ package edu.sc.seis.sod.status;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
 import org.apache.log4j.Logger;
-
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
@@ -17,126 +15,127 @@ import edu.sc.seis.sod.Start;
 
 public class OutputScheduler extends Thread implements ArmListener {
 
-	private OutputScheduler() {
-		super("OutputScheduler");
-		Start.add(this);
-	}
+    private OutputScheduler() {
+        super("OutputScheduler");
+        Start.add(this);
+    }
 
-	public void starting(Arm arm) {
-		synchronized (arms) {
-			arms.add(arm);
-		}
-		if (arm instanceof NetworkArm) {
-			((NetworkArm) arm).add(this);
-		}
-	}
+    public void starting(Arm arm) {
+        synchronized(arms) {
+            arms.add(arm);
+            // Wake up when we get our first arm
+            if(arms.size() == 1) {
+                synchronized(this) {
+                    notify();
+                }
+            }
+        }
+        if(arm instanceof NetworkArm) {
+            ((NetworkArm)arm).add(this);
+        }
+    }
 
-	public void finished(Arm arm) {
-		synchronized (this) {
-			notify();
-		}
-	}
+    public void finished(Arm arm) {
+        synchronized(this) {
+            notify();
+        }
+    }
 
-	public void started() throws ConfigurationException {
-	}
+    public void started() throws ConfigurationException {}
 
-	public void schedule(Runnable a) {
-		synchronized (runnables) {
-			runnables.add(a);
-		}
-	}
+    public void schedule(Runnable a) {
+        synchronized(runnables) {
+            runnables.add(a);
+        }
+    }
 
-	public void scheduleForExit(Runnable a) {
-		synchronized (onExitRunnables) {
-			onExitRunnables.add(a);
-		}
-	}
+    public void scheduleForExit(Runnable a) {
+        synchronized(onExitRunnables) {
+            onExitRunnables.add(a);
+        }
+    }
 
-	public void run() {
-		// initial sleep before first set of status pages
-		try {
-			Thread.sleep(INITIAL_SLEEP);
-		} catch (InterruptedException e) {
-		}
-		while (true) {
-			runAll(runnables);
-			if (!anyArmsActive()) {
-				runAll(runnables);
-				runAll(onExitRunnables);
-				logger.debug("Output Scheduler done.");
-				String lcOSName = System.getProperty("os.name").toLowerCase();
-				boolean MAC_OS_X = lcOSName.startsWith("mac os x");
-				if (MAC_OS_X) {
-					// hopefully everything is done!
-					logger
-							.debug("Using System.exit(0) only on the mac due to AWT thread not exiting.");
-					System.exit(0);
-				}
-				return;
-			}
-			try {
-				synchronized (this) {
-					wait(ACTION_INTERVAL_MILLIS);
-				}
-			} catch (InterruptedException e) {
-			}
-		}
-	}
+    public void run() {
+        // Waiting for the initial arm to come into starting and wake us
+        try {
+            synchronized(this) {
+                wait();
+            }
+        } catch(InterruptedException e) {}
+        while(true) {
+            runAll(runnables);
+            if(!anyArmsActive()) {
+                runAll(runnables);
+                runAll(onExitRunnables);
+                logger.debug("Output Scheduler done.");
+                String lcOSName = System.getProperty("os.name").toLowerCase();
+                boolean MAC_OS_X = lcOSName.startsWith("mac os x");
+                if(MAC_OS_X) {
+                    // hopefully everything is done!
+                    logger.debug("Using System.exit(0) only on the mac due to AWT thread not exiting.");
+                    System.exit(0);
+                }
+                return;
+            }
+            try {
+                synchronized(this) {
+                    wait(ACTION_INTERVAL_MILLIS);
+                }
+            } catch(InterruptedException e) {}
+        }
+    }
 
-	private void runAll(Set toRun) {
-		Runnable[] currentRunnables = new Runnable[0];
-		synchronized (toRun) {
-			currentRunnables = (Runnable[]) toRun.toArray(currentRunnables);
-			toRun.clear();
-		}
-		for (int i = 0; i < currentRunnables.length; i++) {
-			try {
-				currentRunnables[i].run();
-			} catch (Throwable t) {
-				GlobalExceptionHandler.handle(t);
-			}
-		}
-	}
+    private void runAll(Set toRun) {
+        Runnable[] currentRunnables = new Runnable[0];
+        synchronized(toRun) {
+            currentRunnables = (Runnable[])toRun.toArray(currentRunnables);
+            toRun.clear();
+        }
+        for(int i = 0; i < currentRunnables.length; i++) {
+            try {
+                currentRunnables[i].run();
+            } catch(Throwable t) {
+                GlobalExceptionHandler.handle(t);
+            }
+        }
+    }
 
-	private boolean anyArmsActive() {
-		Arm[] curArms = new Arm[0];
-		synchronized (arms) {
-			curArms = (Arm[]) arms.toArray(curArms);
-		}
-		boolean active = false;
-		for (int i = 0; i < curArms.length; i++) {
-			if (curArms[i].isActive()) {
-				active = true;
-				break;
-			}
-		}
-		return active;
-	}
+    private boolean anyArmsActive() {
+        Arm[] curArms = new Arm[0];
+        synchronized(arms) {
+            curArms = (Arm[])arms.toArray(curArms);
+        }
+        boolean active = false;
+        for(int i = 0; i < curArms.length; i++) {
+            if(curArms[i].isActive()) {
+                active = true;
+                break;
+            }
+        }
+        return active;
+    }
 
-	public synchronized static OutputScheduler getDefault() {
-		if (DEFAULT == null) {
-			DEFAULT = new OutputScheduler();
-			DEFAULT.start();
-		}
-		return DEFAULT;
-	}
+    public synchronized static OutputScheduler getDefault() {
+        if(DEFAULT == null) {
+            DEFAULT = new OutputScheduler();
+            DEFAULT.start();
+        }
+        return DEFAULT;
+    }
 
-	private static final TimeInterval ACTION_INTERVAL = new TimeInterval(2,
-			UnitImpl.MINUTE);
+    private static final TimeInterval ACTION_INTERVAL = new TimeInterval(2,
+                                                                         UnitImpl.MINUTE);
 
-	private static final long ACTION_INTERVAL_MILLIS = (long) ACTION_INTERVAL
-			.convertTo(UnitImpl.MILLISECOND).get_value();
+    private static final long ACTION_INTERVAL_MILLIS = (long)ACTION_INTERVAL.convertTo(UnitImpl.MILLISECOND)
+            .get_value();
 
-	private static final long INITIAL_SLEEP = 10000l;
+    private static OutputScheduler DEFAULT = null;
 
-	private static OutputScheduler DEFAULT = null;
+    private Set arms = Collections.synchronizedSet(new HashSet());
 
-	private Set arms = Collections.synchronizedSet(new HashSet());
+    private Set runnables = Collections.synchronizedSet(new HashSet());
 
-	private Set runnables = Collections.synchronizedSet(new HashSet());
+    private Set onExitRunnables = Collections.synchronizedSet(new HashSet());
 
-	private Set onExitRunnables = Collections.synchronizedSet(new HashSet());
-
-	private static final Logger logger = Logger
-			.getLogger(OutputScheduler.class);
+    private static final Logger logger = Logger.getLogger(OutputScheduler.class);
 }
