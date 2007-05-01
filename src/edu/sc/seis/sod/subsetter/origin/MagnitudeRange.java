f43ed027b110bb301e8c76c0cd02c1230e42391b
oliverpa <none@none>
2007-05-01 16:54:28 -0400

    [svn r19023] added <largest>, <smallest>, <any>, and <contributor> tags
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/origin/MagnitudeRange.java
+++ src/edu/sc/seis/sod/subsetter/origin/MagnitudeRange.java
@@ -7,10 +7,11 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import edu.iris.Fissures.IfEvent.EventAccessOperations;
 import edu.iris.Fissures.IfEvent.EventAttr;
+import edu.iris.Fissures.IfEvent.Magnitude;
 import edu.iris.Fissures.IfEvent.Origin;
+import edu.sc.seis.fissuresUtil.cache.EventUtil;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.SodUtil;
-import edu.sc.seis.sod.source.event.EventDCQuerier;
 import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.status.StringTreeLeaf;
 import edu.sc.seis.sod.subsetter.MagType;
@@ -24,45 +25,128 @@ public class MagnitudeRange extends RangeSubsetter implements OriginSubsetter {
 
     public MagnitudeRange(Element config) throws ConfigurationException {
         super(config);
-        parseSearchTypes(config);
+        searchTypes = parseSearchElements(config, "magType");
+        contributors = parseSearchElements(config, "contributor");
+        parseValueTolerance(config);
     }
 
     public StringTree accept(EventAccessOperations event,
                              EventAttr eventAttr,
                              Origin origin) {
-        for(int i = 0; i < origin.magnitudes.length; i++) {
-            if(accept(origin.magnitudes[i].value)) {
-                if(getSearchTypes().length == 0) {
-                    // don't care about search types
-                    return new StringTreeLeaf(this, true);
-                }
-                for(int j = 0; j < searchTypes.length; j++) {
-                    if(origin.magnitudes[i].type.equals(searchTypes[j])) { return new StringTreeLeaf(this, true); }
-                }
-            }
-        }
-        return new StringTreeLeaf(this, false);
+        return new StringTreeLeaf(this, accept(origin.magnitudes));
     }
 
     public String[] getSearchTypes() {
         return searchTypes;
     }
+    
+    public String[] getContributors() {
+        return contributors;
+    }
 
-    private void parseSearchTypes(Element config) throws ConfigurationException {
-        List types = new ArrayList();
+    public boolean accept(Magnitude[] mags) {
+        List matchList = new ArrayList();
+        for(int i = 0; i < mags.length; i++) {
+            if((contributors.length == 0 || accept(contribAcceptor,
+                                                   mags[i],
+                                                   contributors))
+                    && (searchTypes.length == 0 || accept(typeAcceptor,
+                                                          mags[i],
+                                                          searchTypes))) {
+                matchList.add(mags[i]);
+            }
+        }
+        Magnitude[] matches = (Magnitude[])matchList.toArray(new Magnitude[0]);
+        if(matches.length > 0) {
+            if(valueTolerance == LARGEST) {
+                matches = new Magnitude[] {EventUtil.getLargest(matches)};
+            } else if(valueTolerance == SMALLEST) {
+                matches = new Magnitude[] {EventUtil.getSmallest(matches)};
+            }
+        }
+        return matches.length > 0 && acceptValues(matches);
+    }
+
+    private boolean accept(StringValueAcceptor acceptor,
+                           Magnitude mag,
+                           String[] values) {
+        for(int i = 0; i < values.length; i++) {
+            if(acceptor.accept(mag, values[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean acceptValues(Magnitude[] mags) {
+        for(int i = 0; i < mags.length; i++) {
+            if(accept(mags[i].value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void parseValueTolerance(Element config) {
+        if(SodUtil.getElement(config, "largest") != null) {
+            valueTolerance = LARGEST;
+        } else if(SodUtil.getElement(config, "smallest") != null) {
+            valueTolerance = SMALLEST;
+        } else {
+            valueTolerance = ANY;
+        }
+    }
+
+    private String[] parseSearchElements(Element config, String elementName)
+            throws ConfigurationException {
+        List values = new ArrayList();
         NodeList childNodes = config.getChildNodes();
         for(int counter = 0; counter < childNodes.getLength(); counter++) {
             Node node = childNodes.item(counter);
             if(node instanceof Element) {
                 String tagName = ((Element)node).getTagName();
-                if(tagName.equals("magType")) {
-                    MagType magType = (MagType)SodUtil.load((Element)node, "");
-                    types.add(magType.getType());
+                if(tagName.equals(elementName)) {
+                    if(tagName.equals("magType")) {
+                        MagType magType = (MagType)SodUtil.load((Element)node,
+                                                                "");
+                        values.add(magType.getType());
+                    } else if(tagName.equals("contributor")) {
+                        values.add(SodUtil.getNestedText((Element)node));
+                    }
                 }
             }
         }
-        searchTypes = (String[])types.toArray(new String[types.size()]);
+        return (String[])values.toArray(new String[values.size()]);
     }
 
+    private interface StringValueAcceptor {
+
+        boolean accept(Magnitude mag, String fieldValue);
+    }
+
+    private StringValueAcceptor typeAcceptor = new StringValueAcceptor() {
+
+        public boolean accept(Magnitude mag, String value) {
+            return mag.type != null && mag.type.equals(value);
+        }
+    };
+
+    private StringValueAcceptor contribAcceptor = new StringValueAcceptor() {
+
+        public boolean accept(Magnitude mag, String value) {
+            return mag.contributor != null && mag.contributor.equals(value);
+        }
+    };
+
     private String[] searchTypes = {};
+
+    private String[] contributors = {};
+
+    private int valueTolerance = ANY;
+
+    private static final int ANY = 0;
+
+    private static final int LARGEST = 1;
+
+    private static final int SMALLEST = 2;
 }// MagnitudeRange
--- test/unit/edu/sc/seis/sod/subsetter/origin/MagnitudeRangeTest.java
+++ test/unit/edu/sc/seis/sod/subsetter/origin/MagnitudeRangeTest.java
@@ -5,6 +5,8 @@
  */
 package edu.sc.seis.sod.subsetter.origin;
 
+import junit.framework.TestCase;
+import org.w3c.dom.Element;
 import edu.iris.Fissures.IfEvent.EventAttr;
 import edu.iris.Fissures.IfEvent.Magnitude;
 import edu.iris.Fissures.IfEvent.Origin;
@@ -12,9 +14,6 @@ import edu.iris.Fissures.event.EventAttrImpl;
 import edu.sc.seis.fissuresUtil.display.ParseRegions;
 import edu.sc.seis.fissuresUtil.mockFissures.IfEvent.MockOrigin;
 import edu.sc.seis.sod.XMLConfigUtil;
-import edu.sc.seis.sod.subsetter.origin.MagnitudeRange;
-import junit.framework.TestCase;
-import org.w3c.dom.Element;
 
 public class MagnitudeRangeTest extends TestCase {
 
@@ -50,5 +49,44 @@ public class MagnitudeRangeTest extends TestCase {
         assertFalse(mag.value + " " + mag.type + "  " + range.getMinValue()
                             + "<" + range.getMaxValue() + " " + searchTypes,
                     range.accept(null, eventAttr, origin).isSuccess());
+        element = XMLConfigUtil.parse("<magnitudeRange>"
+                + "<contributor>FAKE</contributor>" + "<min>5.0</min>"
+                + "<max>6.0</max>" + "</magnitudeRange>");
+        range = new MagnitudeRange(element);
+        String contributors = " contributors=";
+        for(int i = 0; i < range.getContributors().length; i++) {
+            contributors += ", " + range.getContributors()[i];
+        }
+        mag.value = 5.0f;
+        assertFalse(mag.value + " " + mag.type + "  " + range.getMinValue()
+                            + "<" + range.getMaxValue() + " " + contributors,
+                    range.accept(null, eventAttr, origin).isSuccess());
+        mag.contributor = "FAKE";
+        assertTrue(mag.value + " " + mag.type + "  " + range.getMinValue()
+                           + "<" + range.getMaxValue() + " " + contributors,
+                   range.accept(null, eventAttr, origin).isSuccess());
+        element = XMLConfigUtil.parse("<magnitudeRange>" + "<largest/>"
+                + "<min>5.0</min>" + "<max>6.0</max>" + "</magnitudeRange>");
+        range = new MagnitudeRange(element);
+        Magnitude mag2 = new Magnitude("Ms", 6.1f, "FAKE");
+        origin.magnitudes = new Magnitude[] {mag, mag2};
+        assertFalse(mag.value + " " + mag.type + "  " + range.getMinValue()
+                            + "<" + range.getMaxValue() + " " + contributors,
+                    range.accept(null, eventAttr, origin).isSuccess());
+        mag2.value = 6.0f;
+        assertTrue(mag.value + " " + mag.type + "  " + range.getMinValue()
+                           + "<" + range.getMaxValue() + " " + contributors,
+                   range.accept(null, eventAttr, origin).isSuccess());
+        element = XMLConfigUtil.parse("<magnitudeRange>" + "<smallest/>"
+                + "<min>5.0</min>" + "<max>6.0</max>" + "</magnitudeRange>");
+        range = new MagnitudeRange(element);
+        mag.value = 4.9f;
+        assertFalse(mag.value + " " + mag.type + "  " + range.getMinValue()
+                            + "<" + range.getMaxValue() + " " + contributors,
+                    range.accept(null, eventAttr, origin).isSuccess());
+        mag.value = 5.0f;
+        assertTrue(mag.value + " " + mag.type + "  " + range.getMinValue()
+                           + "<" + range.getMaxValue() + " " + contributors,
+                   range.accept(null, eventAttr, origin).isSuccess());
     }
 }
