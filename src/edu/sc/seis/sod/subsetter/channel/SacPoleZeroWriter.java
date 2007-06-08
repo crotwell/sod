e76172b0c1d7140e6c64b260fd4c555b7b4413ad
crotwell <none@none>
2007-06-08 13:38:27 -0400

    [svn r19053] messages on fail
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/subsetter/channel/SacPoleZeroWriter.java
+++ src/edu/sc/seis/sod/subsetter/channel/SacPoleZeroWriter.java
@@ -42,10 +42,10 @@ public class SacPoleZeroWriter implements ChannelSubsetter {
         } catch(ChannelNotFound e) {
             return new Fail(this, "No instrumentation");
         } catch(InstrumentationInvalid e) {
-            return new Fail(this, "Invalid instrumentation");
+            return new Fail(this, "Invalid instrumentation", e);
         }
         if(inst.the_response.stages[0].filters[0].discriminator().value() != FilterType._POLEZERO) {
-            return new Fail(this);
+            return new Fail(this, "stage zero is not a PoleZero filter");
         }
         String response = FissuresToSac.getPoleZero(inst.the_response)
                 .toString();
