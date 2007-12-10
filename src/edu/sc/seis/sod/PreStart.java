73a84e1b3af054910be6b6e26f1332210c76515f
crotwell <none@none>
2007-12-10 13:50:44 -0500

    [svn r19469] catch throwable
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/PreStart.java
+++ src/edu/sc/seis/sod/PreStart.java
@@ -2,18 +2,25 @@ package edu.sc.seis.sod;
 
 import java.lang.reflect.Method;
 
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+
 public class PreStart {
 
-    public static void main(String[] args) throws Exception {
-        if(System.getProperty("java.vm.name").equals("GNU libgcj")) {
-            System.err.println("You are running GNU's version of Java, gcj, which doesn't have all the features SOD requires.  Instead, use Sun's Java from http://java.sun.com.");
-            System.exit(-1);
-        }
-        
-        Class realStart = Class.forName(args[0]);
-        String[] realArgs = new String[args.length - 1];
-        System.arraycopy(args, 1, realArgs, 0, realArgs.length);
-        Method main = realStart.getMethod("main", new Class[] {String[].class});
-        main.invoke(null, new Object[] {realArgs});
-    }
+	public static void main(String[] args) throws Exception {
+		if (System.getProperty("java.vm.name").equals("GNU libgcj")) {
+			System.err
+					.println("You are running GNU's version of Java, gcj, which doesn't have all the features SOD requires.  Instead, use Sun's Java from http://java.sun.com.");
+			System.exit(-1);
+		}
+		try {
+			Class realStart = Class.forName(args[0]);
+			String[] realArgs = new String[args.length - 1];
+			System.arraycopy(args, 1, realArgs, 0, realArgs.length);
+			Method main = realStart.getMethod("main",
+					new Class[] { String[].class });
+			main.invoke(null, new Object[] { realArgs });
+		} catch (Throwable t) {
+			GlobalExceptionHandler.handle(t);
+		}
+	}
 }
