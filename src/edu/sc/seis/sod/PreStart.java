1dc366303c494c345c9f5f11003ec79a2bb17e3d
crotwell <none@none>
2008-03-14 08:37:05 -0400

    [svn r19673] configure logger so do not loose errors in PreStart
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/PreStart.java
+++ src/edu/sc/seis/sod/PreStart.java
@@ -2,11 +2,14 @@ package edu.sc.seis.sod;
 
 import java.lang.reflect.Method;
 
+import org.apache.log4j.BasicConfigurator;
+
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 
 public class PreStart {
 
 	public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
 		if (System.getProperty("java.vm.name").equals("GNU libgcj")) {
 			System.err
 					.println("You are running GNU's version of Java, gcj, which doesn't have all the features SOD requires.  Instead, use Sun's Java from http://java.sun.com.");
