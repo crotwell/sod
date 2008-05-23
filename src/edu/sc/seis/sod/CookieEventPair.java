dc38f0ed24739ee6703cd46180a367fb02c465cc
crotwell <none@none>
2008-05-23 06:37:01 -0400

    [svn r19798] not needed and initial
    --HG--
    branch : sod

---
+++ src/edu/sc/seis/sod/CookieEventPair.java
@@ -0,0 +1,38 @@
+package edu.sc.seis.sod;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.sc.seis.sod.hibernate.StatefulEvent;
+
+public abstract class CookieEventPair extends AbstractEventPair {
+
+    public CookieEventPair() {
+        super();
+    }
+
+    public CookieEventPair(StatefulEvent event, Status status) {
+        super(event, status);
+    }
+
+    public CookieEventPair(StatefulEvent event) {
+        super(event);
+    }
+
+    public void setStatus(Status status) {
+        super.setStatus(status);
+        getCookies().put("status", status);
+        Start.getWaveformArm().setStatus(this);
+    }
+    // hibernate
+    protected void setCookies(Map<String, Serializable> cookies) {
+        this.cookies = cookies;
+    }
+
+    public Map<String, Serializable> getCookies() {
+        return cookies;
+    }
+
+    private Map<String, Serializable> cookies = new HashMap<String, Serializable>();
+}
