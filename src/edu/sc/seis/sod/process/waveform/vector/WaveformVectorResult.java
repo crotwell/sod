d55c997554749ef70a848a0db8a6c6be40e7e7b4
crotwell <none@none>
2009-01-08 13:19:02 -0500

    [svn r20147] more construtors to match WaveformResult
    --HG--
    branch : sod

--- src/edu/sc/seis/sod/process/waveform/vector/WaveformVectorResult.java
+++ src/edu/sc/seis/sod/process/waveform/vector/WaveformVectorResult.java
@@ -8,9 +8,17 @@ package edu.sc.seis.sod.process.waveform.vector;
 
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.sod.status.StringTree;
+import edu.sc.seis.sod.status.StringTreeLeaf;
 
 public class WaveformVectorResult {
 
+    public WaveformVectorResult(boolean success, LocalSeismogramImpl[][] seismograms, Object actor) {
+        this(seismograms, new StringTreeLeaf(actor, success));
+    }
+    
+    public WaveformVectorResult(boolean success, LocalSeismogramImpl[][] seismograms, Object actor, String reason) {
+        this(seismograms, new StringTreeLeaf(actor, success, reason));
+    }
     public WaveformVectorResult(LocalSeismogramImpl[][] seismograms, StringTree reason) {
         this.seismograms = seismograms;
         this.reason = reason;
