178c65d46db6f5a137cdfecab7175a4c2d0edb3f
crotwell <none@none>
2009-09-24 15:55:11 -0400

    [svn r20746] version 3.0beta3
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/Version.java
+++ src/main/java/edu/sc/seis/sod/Version.java
@@ -73,8 +73,8 @@ public class Version{
 
     public static String getCVSVersion() { return cvsversion; }
 
-    private static String cvsversion = "$Revision: 20684 $";
-    private static String datetime = "$Date: 2009-08-06 14:41:24 -0400 (Thu, 06 Aug 2009) $";
+    private static String cvsversion = "$Revision: 20746 $";
+    private static String datetime = "$Date: 2009-09-24 15:55:11 -0400 (Thu, 24 Sep 2009) $";
 
     //versionsToDate stores all of the versions that have been released,
     //and is a multidimensional array of width 2.  The first String in
@@ -102,7 +102,8 @@ public class Version{
       new Version("2.2.2", false),
       new Version("3.0alpha", true),
       new Version("3.0beta1", true),
-      new Version("3.0beta2", true)
+      new Version("3.0beta2", true),
+      new Version("3.0beta3", true)
     };
 
 }
