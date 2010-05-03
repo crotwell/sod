38e322267529bacbcd57362c89899d272074700a
crotwell <none@none>
2010-05-03 14:45:06 -0400

    [svn r21273] fix gap fill, was not joining some seismograms
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/process/waveform/GapFill.java
+++ src/main/java/edu/sc/seis/sod/process/waveform/GapFill.java
@@ -63,41 +63,59 @@ public class GapFill extends Merge {
     }
         
     public List<LocalSeismogramImpl> reduce(List<LocalSeismogramImpl> inList, MicroSecondTimeRange mstr) throws FissuresException {
+        System.out.println("reduce size=:"+inList.size());
+        if (inList.size() == 1) {
+            return inList;
+        }
         LocalSeismogramImpl first = inList.remove(0);
-        LocalSeismogramImpl second = inList.remove(0);
+        // reduce remainder 
+        List<LocalSeismogramImpl> remaining = reduce(inList, mstr);
+        LocalSeismogramImpl second = remaining.remove(0);
         List<LocalSeismogramImpl> outList = new LinkedList<LocalSeismogramImpl>();
         if (mstr.contains(first.getEndTime()) && mstr.contains(second.getBeginTime())) {
             // have overlap in a request window, so fill the gap
-            TimeSeriesDataSel fillData = filler.fill(first, second);
-            LocalSeismogramImpl fillSeis = new LocalSeismogramImpl(first.get_id()+"_gapFill",
-                                                                   first.getProperties(),
-                                                                   first.getEndTime().add(first.getSampling().getPeriod()).getFissuresTime(),
-                                                                   calcNumGapPoints(first, second),
-                                                                   first.getSampling(),
-                                                                   first.getUnit(), first.getChannelID(),
-                                                                   first.getParameterRefs(), 
-                                                                   first.time_corrections, 
-                                                                   first.sample_rate_history, 
-                                                                   fillData);
-            LocalSeismogramImpl[] merged = ReduceTool.merge(new LocalSeismogramImpl[] {first, fillSeis, second});
-            
+            LocalSeismogramImpl[] merged = gapFill(first, second);
+            System.out.println("gap in window, merge.size="+merged.length);
             for (int i = 0; i < merged.length; i++) {
                 outList.add(merged[i]);
             }
         } else {
             // gap not in window, do not fill
+            System.out.println("gap not in window");
             outList.add(first);
             outList.add(second);
-            outList.addAll(reduce(inList, mstr));
         }
+        outList.addAll(remaining);
+        System.out.println("reduce finish: size="+outList.size());
         return outList;
+    }
+    
+    public LocalSeismogramImpl[] gapFill(LocalSeismogramImpl first, LocalSeismogramImpl second) throws FissuresException {
+        TimeSeriesDataSel fillData = filler.fill(first, second);
+        LocalSeismogramImpl fillSeis = new LocalSeismogramImpl(first.get_id()+"_gapFill",
+                                                               first.getProperties(),
+                                                               first.getEndTime().add(first.getSampling().getPeriod()).getFissuresTime(),
+                                                               calcNumGapPoints(first, second),
+                                                               first.getSampling(),
+                                                               first.getUnit(), first.getChannelID(),
+                                                               first.getParameterRefs(), 
+                                                               first.time_corrections, 
+                                                               first.sample_rate_history, 
+                                                               fillData);
+        System.out.println("Before ReduceTool.merge");
+        System.out.println("First end: "+first.getEndTime());
+        System.out.println("middle begin: "+fillSeis.getBeginTime());
+        System.out.println("middle end: "+fillSeis.getEndTime());
+        System.out.println("last begin: "+second.getBeginTime());
+        LocalSeismogramImpl[] merged = ReduceTool.merge(new LocalSeismogramImpl[] {first, fillSeis, second});
+        return merged;
     }
 
     public static int calcNumGapPoints(LocalSeismogramImpl first, LocalSeismogramImpl second) {
         MicroSecondDate firstEnd = first.getEndTime();
         MicroSecondDate secondBegin = second.getBeginTime();
         QuantityImpl numSamplePeriods = secondBegin.subtract(firstEnd).divideBy(first.getSampling().getPeriod());
-        return (int)Math.round(numSamplePeriods.getValue(UnitImpl.DIMENSIONLESS)) -1; // one less
+        return (int)Math.ceil(numSamplePeriods.getValue(UnitImpl.DIMENSIONLESS)) -1; // one less than ceiling
     }
     
     FillStyle filler;
