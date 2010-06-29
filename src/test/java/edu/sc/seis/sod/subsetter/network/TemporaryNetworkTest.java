42f4c5222e0177ce7b27b9c5d910bea0e11ae428
crotwell <none@none>
2010-06-29 12:18:19 -0400

    [svn r21428] use Impl
    --HG--
    branch : sod

--- src/test/java/edu/sc/seis/sod/subsetter/network/TemporaryNetworkTest.java
+++ src/test/java/edu/sc/seis/sod/subsetter/network/TemporaryNetworkTest.java
@@ -2,6 +2,7 @@ package edu.sc.seis.sod.subsetter.network;
 
 import junit.framework.TestCase;
 import edu.iris.Fissures.IfNetwork.NetworkAttr;
+import edu.iris.Fissures.network.NetworkAttrImpl;
 import edu.sc.seis.fissuresUtil.mockFissures.IfNetwork.MockNetworkAttr;
 
 
@@ -9,16 +10,16 @@ public class TemporaryNetworkTest extends TestCase {
     
     public void testTempNet() {
         TemporaryNetwork subsetter = new TemporaryNetwork();
-        NetworkAttr x = MockNetworkAttr.createNetworkAttr();
+        NetworkAttrImpl x = MockNetworkAttr.createNetworkAttr();
         x.get_id().network_code = "X";
         assertTrue(subsetter.accept(x).isSuccess());
-        NetworkAttr xa = MockNetworkAttr.createNetworkAttr();
+        NetworkAttrImpl xa = MockNetworkAttr.createNetworkAttr();
         xa.get_id().network_code = "XA";
         assertTrue(subsetter.accept(xa).isSuccess());
-        NetworkAttr ax = MockNetworkAttr.createNetworkAttr();
+        NetworkAttrImpl ax = MockNetworkAttr.createNetworkAttr();
         ax.get_id().network_code = "AX";
         assertFalse(subsetter.accept(ax).isSuccess());
-        NetworkAttr other = MockNetworkAttr.createOtherNetworkAttr();
+        NetworkAttrImpl other = MockNetworkAttr.createOtherNetworkAttr();
         assertFalse(subsetter.accept(other).isSuccess());
     }
 }
