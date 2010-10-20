9061a9129bc382358530134a86d13ecb987a4c95
crotwell <none@none>
2010-10-20 12:02:55 -0400

    [svn r21780] use seisFile impl
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/source/seismogram/DataSelectWebService.java
+++ src/main/java/edu/sc/seis/sod/source/seismogram/DataSelectWebService.java
@@ -1,11 +1,7 @@
 package edu.sc.seis.sod.source.seismogram;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.net.Authenticator;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.PasswordAuthentication;
 import java.net.URL;
@@ -18,19 +14,20 @@ import org.w3c.dom.Element;
 import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.model.MicroSecondDate;
-import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.network.ChannelImpl;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
 import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
 import edu.sc.seis.fissuresUtil.mseed.FissuresConvert;
+import edu.sc.seis.seisFile.dataSelectWS.DataSelectException;
+import edu.sc.seis.seisFile.dataSelectWS.DataSelectReader;
 import edu.sc.seis.seisFile.mseed.DataRecord;
 import edu.sc.seis.seisFile.mseed.SeedFormatException;
 import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.SodUtil;
 
-public class DataSelectWebService implements SeismogramSource, SeismogramSourceLocator {
+public class DataSelectWebService implements SeismogramSourceLocator {
 
     public DataSelectWebService(Element config) throws MalformedURLException {
         this(config, DEFAULT_WS_URL);
@@ -45,64 +42,46 @@ public class DataSelectWebService implements SeismogramSource, SeismogramSourceL
         }
     }
 
-    @Override
     public SeismogramSource getSeismogramSource(CacheEvent event,
                                                 ChannelImpl channel,
                                                 RequestFilter[] infilters,
                                                 CookieJar cookieJar) throws Exception {
-        return this;
-    }
-
-    @Override
-    public List<RequestFilter> available_data(List<RequestFilter> request) {
-//bad fix this...
-        return request;
-    }
-
-    @Override
-    public List<LocalSeismogramImpl> retrieveData(List<RequestFilter> request) throws FissuresException {
-        try {
-            List<LocalSeismogramImpl> out = new ArrayList<LocalSeismogramImpl>();
-            for (RequestFilter rf : request) {
-                String query = "net="+ rf.channel_id.network_id.network_code;
-                query += "&sta=" + rf.channel_id.station_code;
-                query += "&loc=" + rf.channel_id.site_code;
-                query += "&cha=" + rf.channel_id.channel_code;
-                query += "&start=" + longFormat.format(new MicroSecondDate(rf.start_time));
-                query += "&end=" + longFormat.format(new MicroSecondDate(rf.end_time).add(new TimeInterval(1, UnitImpl.SECOND)));
-                URL requestURL = new URL(url + "?"+query);
-                HttpURLConnection conn = (HttpURLConnection)requestURL.openConnection();
-                conn.connect();
-                if (conn.getResponseCode() != 200) {
-                    if (conn.getResponseCode() == 404) {
-                        logger.debug("no data: "+requestURL);
-                        return out;
-                    } else {
-                        throw new FissuresException("Did not get an OK repsonse code:"+conn.getResponseCode(), null);
-                    }
-                }
-                BufferedInputStream bif = new BufferedInputStream(conn.getInputStream());
-                DataInputStream in = new DataInputStream(bif);
-                List<DataRecord> records = new ArrayList<DataRecord>();
-                while (true) {
-                    try {
-                    records.add(DataRecord.read(in));
-                    } catch (EOFException e) {
-                        // end of data?
-                        break;
-                    }
-                }
-                LocalSeismogramImpl seis = FissuresConvert.toFissures(records.toArray(new DataRecord[0]));
-                out.add(seis);
+        return new SeismogramSource() {
+    
+            public List<RequestFilter> available_data(List<RequestFilter> request) {
+//              bad fix this...
+                return request;
             }
-            return out;
-        } catch(IOException e) {
-            GlobalExceptionHandler.handle(e);
-            throw new FissuresException(e.getMessage(), new edu.iris.Fissures.Error(1, "IOException"));
-       } catch(SeedFormatException e) {
-            GlobalExceptionHandler.handle(e);
-            throw new FissuresException(e.getMessage(), new edu.iris.Fissures.Error(1, "SeedFormatException")); 
-        }
+
+            public List<LocalSeismogramImpl> retrieveData(List<RequestFilter> request) throws FissuresException {
+                try {
+                    List<LocalSeismogramImpl> out = new ArrayList<LocalSeismogramImpl>();
+                    DataSelectReader dsReader = new DataSelectReader(url);
+                    for (RequestFilter rf : request) {
+                        MicroSecondDate start = new MicroSecondDate(rf.start_time);
+                        URL requestURL = dsReader.createQuery(rf.channel_id.network_id.network_code,
+                                                              rf.channel_id.station_code,
+                                                              rf.channel_id.site_code, 
+                                                              rf.channel_id.channel_code, 
+                                                              start,
+                                                              (float)new MicroSecondDate(rf.end_time).subtract(start).getValue(UnitImpl.SECOND));
+                        List<DataRecord> records = dsReader.read(requestURL);
+                        LocalSeismogramImpl seis = FissuresConvert.toFissures(records.toArray(new DataRecord[0]));
+                        out.add(seis);
+                    }
+                    return out;
+                } catch(IOException e) {
+                    GlobalExceptionHandler.handle(e);
+                    throw new FissuresException(e.getMessage(), new edu.iris.Fissures.Error(1, "IOException"));
+                } catch(SeedFormatException e) {
+                    GlobalExceptionHandler.handle(e);
+                    throw new FissuresException(e.getMessage(), new edu.iris.Fissures.Error(1, "SeedFormatException")); 
+                } catch(DataSelectException e) {
+                    GlobalExceptionHandler.handle(e);
+                    throw new FissuresException(e.getMessage(), new edu.iris.Fissures.Error(1, "DataSelectException")); 
+                }
+            }
+        };
     }
     
     protected String url;
