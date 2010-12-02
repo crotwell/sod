5e7ff22fbfa9a58afaa26bdc1e910c1b8686dac8
crotwell <none@none>
2010-12-02 17:44:25 -0500

    [svn r21876] use List
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/process/waveform/SacWriter.java
+++ src/main/java/edu/sc/seis/sod/process/waveform/SacWriter.java
@@ -1,6 +1,8 @@
 package edu.sc.seis.sod.process.waveform;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -14,6 +16,7 @@ import edu.sc.seis.fissuresUtil.sac.FissuresToSac;
 import edu.sc.seis.fissuresUtil.xml.SeismogramFileTypes;
 import edu.sc.seis.seisFile.sac.SacTimeSeries;
 import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.SodUtil;
 
 public class SacWriter extends AbstractSeismogramWriter {
 
@@ -28,13 +31,15 @@ public class SacWriter extends AbstractSeismogramWriter {
              DOMHelper.hasElement(el, "littleEndian"));
     }
 
-    private static SacProcess[] extractProcessors(Element el) {
+    private static List<SacProcess> extractProcessors(Element el) {
         NodeList nl = DOMHelper.getElements(el, "phaseTimeHeader");
-        SacProcess[] processes = new SacProcess[nl.getLength()];
-        for(int i = 0; i < processes.length; i++) {
-            processes[i] = new PhaseHeaderProcess((Element)nl.item(i));
+        ArrayList<SacProcess> out = new ArrayList<SacProcess>();
+        for(int i = 0; i < nl.getLength(); i++) {
+            out.add( new PhaseHeaderProcess((Element)nl.item(i)));
         }
-        return processes;
+        Element sacScript = SodUtil.getElement(el, "sacHeaderScript");
+        out.add( new SacHeaderScript(sacScript));
+        return out;
     }
 
     public SacWriter() throws ConfigurationException {
@@ -46,14 +51,14 @@ public class SacWriter extends AbstractSeismogramWriter {
     }
 
     public SacWriter(String workingDir, String fileTemplate) throws ConfigurationException {
-        this(workingDir, fileTemplate, DEFAULT_PREFIX, new SacProcess[0], false, false);
+        this(workingDir, fileTemplate, DEFAULT_PREFIX, new ArrayList<SacProcess>(), false, false);
     }
 
-    public SacWriter(SacProcess[] processes) throws ConfigurationException {
+    public SacWriter(List<SacProcess> processes) throws ConfigurationException {
         this(DEFAULT_WORKING_DIR, DEFAULT_FILE_TEMPLATE, DEFAULT_PREFIX, processes, false, false);
     }
 
-    public SacWriter(String workingDir, String fileTemplate, String prefix, SacProcess[] processes, boolean storeSeismogramsInDB, boolean littleEndian) throws ConfigurationException {
+    public SacWriter(String workingDir, String fileTemplate, String prefix, List<SacProcess> processes, boolean storeSeismogramsInDB, boolean littleEndian) throws ConfigurationException {
         super(workingDir, fileTemplate, prefix, storeSeismogramsInDB);
         this.processors = processes;
         this.littleEndian=littleEndian;
@@ -78,8 +83,8 @@ public class SacWriter extends AbstractSeismogramWriter {
     public void applyProcessors(SacTimeSeries writer,
                                 CacheEvent ev,
                                 ChannelImpl chan) throws Exception {
-        for(int i = 0; i < processors.length; i++) {
-            processors[i].process(writer, ev, chan);
+        for (SacProcess processor : processors) {
+            processor.process(writer, ev, chan);
         }
     }
 
@@ -90,5 +95,5 @@ public class SacWriter extends AbstractSeismogramWriter {
     
     boolean littleEndian;
 
-    private SacProcess[] processors;
+    private List<SacProcess> processors = new ArrayList<SacProcess>();
 }
