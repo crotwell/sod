2b1821639a107a916b68efeacdc682d940805782
crotwell <none@none>
2011-02-10 13:25:10 -0500

    [svn r22020] rename
    --HG--
    branch : sod
    rename : src/main/java/edu/sc/seis/sod/process/waveform/vector/VectorForkProcess.java => src/main/java/edu/sc/seis/sod/process/waveform/vector/WaveformVectorFork.java

--- src/main/java/edu/sc/seis/sod/process/waveform/vector/WaveformVectorFork.java
+++ src/main/java/edu/sc/seis/sod/process/waveform/vector/WaveformVectorFork.java
@@ -16,28 +16,82 @@ import edu.sc.seis.fissuresUtil.hibernate.ChannelGroup;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.CookieJar;
 import edu.sc.seis.sod.MotionVectorArm;
+import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.Threadable;
 import edu.sc.seis.sod.process.waveform.ForkProcess;
+import edu.sc.seis.sod.process.waveform.WaveformProcess;
 import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.status.StringTreeBranch;
 import edu.sc.seis.sod.status.StringTreeLeaf;
+import edu.sc.seis.sod.subsetter.Subsetter;
+import edu.sc.seis.sod.subsetter.availableData.AvailableDataSubsetter;
+import edu.sc.seis.sod.subsetter.availableData.vector.ANDAvailableDataWrapper;
+import edu.sc.seis.sod.subsetter.availableData.vector.VectorAvailableDataLogicalSubsetter;
+import edu.sc.seis.sod.subsetter.availableData.vector.VectorAvailableDataSubsetter;
+import edu.sc.seis.sod.subsetter.request.vector.VectorRequestLogical;
+import edu.sc.seis.sod.subsetter.request.vector.VectorRequestSubsetter;
 
 /**
  * @author groves Created on Mar 23, 2005
  */
-public class VectorForkProcess implements WaveformVectorProcess, Threadable {
+public class WaveformVectorFork implements WaveformVectorProcess, Threadable {
 
-    public VectorForkProcess(Element config) throws ConfigurationException {
+    public WaveformVectorFork(Element config) throws ConfigurationException {
         NodeList children = config.getChildNodes();
         for(int i = 0; i < children.getLength(); i++) {
             Node node = children.item(i);
             if(node instanceof Element) {
-                WaveformVectorProcess sodElement = MotionVectorArm.loadAndWrap((Element)node);
+                WaveformVectorProcess sodElement = load((Element)node);
                 processes.add(sodElement);
             } // end of if (node instanceof Element)
         } // end of for (int i=0; i<children.getSize(); i++)
     }
 
+    public static final List<String> packages;
+    
+    static {
+        packages = new LinkedList<String>();
+        packages.add("waveform.vector");
+        packages.addAll(VectorAvailableDataLogicalSubsetter.packages);
+        packages.addAll(ForkProcess.packages);
+    }
+    
+    public List<String> getPackages() {
+        return packages;
+    }
+
+    protected Subsetter getSubsetter(final Subsetter s) throws ConfigurationException {
+        return createSubsetter(s);
+    }
+    
+    public static WaveformVectorProcess load(Element el) throws ConfigurationException {
+        Object o = SodUtil.load(el, packages);
+        if (o instanceof Subsetter) {
+        return createSubsetter((Subsetter)o);
+        } else {
+            throw new ConfigurationException("Not an Element: "+o.getClass().getName());
+        }
+    }
+    public static WaveformVectorProcess createSubsetter(final Subsetter s) throws ConfigurationException {
+        if (s instanceof WaveformVectorProcess) {
+            return (WaveformVectorProcess)s;
+        } else if (s instanceof WaveformProcess) {
+            return new ANDWaveformProcessWrapper((WaveformProcess)s);
+        } else {
+            return new WaveformVectorProcess() {
+                VectorAvailableDataSubsetter ecs = VectorAvailableDataLogicalSubsetter.createSubsetter(s);
+                public WaveformVectorResult accept(CacheEvent event,
+                                                   ChannelGroup channelGroup,
+                                                   RequestFilter[][] request,
+                                                   RequestFilter[][] available,
+                                                   LocalSeismogramImpl[][] seismograms,
+                                                   CookieJar cookieJar) throws Exception {
+                    return new WaveformVectorResult(seismograms,
+                                                    ecs.accept(event, channelGroup, request, available, cookieJar));
+                }
+            };
+        }
+    }
     public WaveformVectorResult accept(CacheEvent event,
                                         ChannelGroup channelGroup,
                                         RequestFilter[][] original,
@@ -86,7 +140,7 @@ public class VectorForkProcess implements WaveformVectorProcess, Threadable {
         return true;
     }
     
-    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(VectorForkProcess.class);
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(WaveformVectorFork.class);
 
     private List processes = new ArrayList();
 }
