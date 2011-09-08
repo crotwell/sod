63cb28ad4540379b8c6ac11c89faaa788213809e
Philip Crotwell <crotwell@seis.sc.edu>
2011-09-08 08:55:04 -0400

    use RecordSectionItem instead of DSS
    --HG--
    branch : sod

--- src/test/java/edu/sc/seis/sod/process/waveform/RecordSectionSpacerTest.java
+++ src/test/java/edu/sc/seis/sod/process/waveform/RecordSectionSpacerTest.java
@@ -21,6 +21,7 @@ import edu.sc.seis.fissuresUtil.xml.DataSetSeismogram;
 import edu.sc.seis.fissuresUtil.xml.MemoryDataSet;
 import edu.sc.seis.fissuresUtil.xml.MemoryDataSetSeismogram;
 import edu.sc.seis.fissuresUtil.xml.StdDataSetParamNames;
+import edu.sc.seis.sod.hibernate.RecordSectionItem;
 
 /**
  * @author groves Created on Apr 8, 2005
@@ -31,28 +32,28 @@ public class RecordSectionSpacerTest extends TestCase {
     private static final AuditInfo[] NO_AUDIT = new AuditInfo[0];
 
     public void testNoSeis() {
-        List<DataSetSeismogram> empty = new ArrayList<DataSetSeismogram>();
+        List<RecordSectionItem> empty = new ArrayList<RecordSectionItem>();
         assertEquals(empty.size(), spacer.spaceOut(empty).size());
     }
 
     public void testOneSeis() {
-        List<DataSetSeismogram> seis = create(new Channel[] {MockChannel.createChannel()});
+        List<RecordSectionItem> seis = create(new Channel[] {MockChannel.createChannel()});
         assertEquals(seis.size(), spacer.spaceOut(seis).size());
     }
 
     public void testTwoSeis() {
-        List<DataSetSeismogram> seis = create(new Channel[] {MockChannel.createChannel(),
+        List<RecordSectionItem> seis = create(new Channel[] {MockChannel.createChannel(),
                                                              MockChannel.createOtherNetChan()});
         assertEquals(seis.size(), spacer.spaceOut(seis).size());
     }
 
     public void testSimpleCluster() {
-        List<DataSetSeismogram> seis = create(new Channel[] {MockChannel.createChannel(), MockChannel.createChannel()});
+        List<RecordSectionItem> seis = create(new Channel[] {MockChannel.createChannel(), MockChannel.createChannel()});
         assertEquals(1, spacer.spaceOut(seis).size());
     }
 
     public void testDoubleCluster() {
-        List<DataSetSeismogram> seis = create(new Channel[] {MockChannel.createChannel(),
+        List<RecordSectionItem> seis = create(new Channel[] {MockChannel.createChannel(),
                                                              MockChannel.createOtherNetChan(),
                                                              MockChannel.createOtherNetChan(),
                                                              MockChannel.createChannel()});
@@ -63,7 +64,7 @@ public class RecordSectionSpacerTest extends TestCase {
         for (int i = 30; i < 90; i++) {
             Location[] locs = MockLocation.create(1, i, 0, 0, 0, 180);
             Channel[] chans = MockChannel.createChannelsAtLocs(locs);
-            List<DataSetSeismogram> seis = create(chans);
+            List<RecordSectionItem> seis = create(chans);
             int results = spacer.spaceOut(seis).size();
             int fewest = (int)Math.floor(180/spacer.getIdealDegreesBetweenSeis())-1;
             int most = (int)Math.ceil(180/spacer.getMinimumDegreesBetweenSeis())+1;
@@ -85,9 +86,9 @@ public class RecordSectionSpacerTest extends TestCase {
                                           MockLocation.create(42.7654, -111.1004)}; //US.AHID
         CacheEvent event = MockEventAccessOperations.createEvent(Defaults.WALL_FALL, -33.8f, -72.07f);
         Channel[] chans = MockChannel.createChannelsAtLocs(locs);
-        List<DataSetSeismogram> seis = create(chans, event);
-        List<DataSetSeismogram> best = spacer.spaceOut(seis);
-        for (DataSetSeismogram curr : best) {
+        List<RecordSectionItem> seis = create(chans, event);
+        List<RecordSectionItem> best = spacer.spaceOut(seis);
+        for (RecordSectionItem curr : best) {
             System.out.println(""+DisplayUtils.calculateDistance(curr));
         }
         assertEquals("There were " + best.size() + " seismograms with " + locs.length
@@ -103,32 +104,24 @@ public class RecordSectionSpacerTest extends TestCase {
                                           }; //US.AHID
         CacheEvent event = MockEventAccessOperations.createEvent(Defaults.WALL_FALL, 7.4f, 126.44f);
         Channel[] chans = MockChannel.createChannelsAtLocs(locs);
-        List<DataSetSeismogram> seis = create(chans, event);
-        List<DataSetSeismogram> best = spacer.spaceOut(seis);
-        for (DataSetSeismogram curr : best) {
+        List<RecordSectionItem> seis = create(chans, event);
+        List<RecordSectionItem> best = spacer.spaceOut(seis);
+        for (RecordSectionItem curr : best) {
             System.out.println(""+DisplayUtils.calculateDistance(curr));
         }
         assertEquals("There were " + best.size() + " seismograms with " + locs.length
                 + " given to it in the spacers results but 2 should be in the result.", 2, best.size());
     }
 
-    public List<DataSetSeismogram> create(Channel[] chans) {
+    public List<RecordSectionItem> create(Channel[] chans) {
         return create(chans, MockEventAccessOperations.createEvent());
     }
     
-    public List<DataSetSeismogram> create(Channel[] chans, CacheEvent event) {
-        List<DataSetSeismogram> seis = new ArrayList<DataSetSeismogram>(chans.length);
-        MemoryDataSet ds = new MemoryDataSet("Test Id", "Test Data Set", "The Tester", NO_AUDIT);
-        ds.addParameter(StdDataSetParamNames.EVENT, event, NO_AUDIT);
+    public List<RecordSectionItem> create(Channel[] chans, CacheEvent event) {
+        List<RecordSectionItem> seis = new ArrayList<RecordSectionItem>(chans.length);
         for (int i = 0; i < chans.length; i++) {
             Channel chan = chans[i];
-            String chanIdStr = ChannelIdUtil.toString(chan.get_id());
-            ds.addParameter(StdDataSetParamNames.CHANNEL + chanIdStr, chan, NO_AUDIT);
-            RequestFilter rf = new RequestFilter();
-            rf.channel_id = chan.get_id();
-            DataSetSeismogram dss = new MemoryDataSetSeismogram(rf, chanIdStr);
-            ds.addDataSetSeismogram(dss, NO_AUDIT);
-            seis.add(dss);
+            seis.add(new RecordSectionItem("vertical", "global", event, chan, 3.0f, false));
         }
         return seis;
     }
