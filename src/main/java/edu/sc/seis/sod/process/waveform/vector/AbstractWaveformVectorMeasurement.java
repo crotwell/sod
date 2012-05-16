a1f4187408a03677f3810f8423a4042c12eca987
Philip Crotwell <crotwell@seis.sc.edu>
2012-05-16 18:02:19 -0400

    initial
    --HG--
    branch : sod

---
+++ src/main/java/edu/sc/seis/sod/process/waveform/vector/AbstractWaveformVectorMeasurement.java
@@ -0,0 +1,51 @@
+package edu.sc.seis.sod.process.waveform.vector;
+
+import org.w3c.dom.Element;
+
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.cache.CacheEvent;
+import edu.sc.seis.fissuresUtil.hibernate.ChannelGroup;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.SodUtil;
+import edu.sc.seis.sod.measure.Measurement;
+import edu.sc.seis.sod.status.Fail;
+import edu.sc.seis.sod.status.Pass;
+
+
+public abstract class AbstractWaveformVectorMeasurement implements WaveformVectorProcess {
+    
+
+    protected String name;
+
+    public AbstractWaveformVectorMeasurement(Element config) {
+        name = SodUtil.loadText(config, "name", SodUtil.getSimpleName(getClass()));
+    }
+    
+    @Override
+    public WaveformVectorResult accept(CacheEvent event,
+                                                     ChannelGroup channelGroup,
+                                                     RequestFilter[][] original,
+                                                     RequestFilter[][] available,
+                                                     LocalSeismogramImpl[][] seismograms,
+                                                     CookieJar cookieJar)
+            throws Exception {
+        if (seismograms.length != 0) {
+            Measurement m = calculate(event, channelGroup, original, available, seismograms, cookieJar);
+            cookieJar.put(m.getName(), m);
+            return new WaveformVectorResult(seismograms, new Pass(this));
+        }
+        return new WaveformVectorResult(seismograms, new Fail(this));
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    abstract Measurement calculate(CacheEvent event,
+                                   ChannelGroup channelGroup,
+                                   RequestFilter[][] original,
+                                   RequestFilter[][] available,
+                                   LocalSeismogramImpl[][] seismograms,
+                                   CookieJar cookieJar) throws Exception;
+}
