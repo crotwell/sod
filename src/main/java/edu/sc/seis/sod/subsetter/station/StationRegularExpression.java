43edc7992d6065064df86fb970df0dbee80d5d14
Philip Crotwell <crotwell@seis.sc.edu>
2013-04-12 13:36:44 -0400

    allow pattern directly in recipe
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/subsetter/station/StationRegularExpression.java
+++ src/main/java/edu/sc/seis/sod/subsetter/station/StationRegularExpression.java
@@ -9,6 +9,7 @@ import edu.iris.Fissures.network.StationImpl;
 import edu.sc.seis.fissuresUtil.cache.FilterNetworkAccess;
 import edu.sc.seis.fissuresUtil.cache.FilterNetworkDC;
 import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
+import edu.sc.seis.sod.SodUtil;
 import edu.sc.seis.sod.source.network.NetworkSource;
 import edu.sc.seis.sod.status.Fail;
 import edu.sc.seis.sod.status.Pass;
@@ -20,8 +21,12 @@ import edu.sc.seis.sod.status.StringTree;
 public class StationRegularExpression implements StationSubsetter {
 
     public StationRegularExpression(Element el) throws IOException {
-        String url = DOMHelper.extractText(el, "url");
-        patterns = FilterNetworkDC.readPattern(url);
+        if (DOMHelper.hasElement(el, "url")) {
+            String url = DOMHelper.extractText(el, "url");
+            patterns = FilterNetworkDC.readPattern(url);
+        } else if (DOMHelper.hasElement(el, "code")) {
+            patterns = new Pattern[] { Pattern.compile(SodUtil.getNestedText(DOMHelper.getElement(el, "code"))) };
+        }
     }
 
     public StringTree accept(StationImpl station, NetworkSource network) throws Exception {
