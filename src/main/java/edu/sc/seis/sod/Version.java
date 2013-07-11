2a0ffc0dc9ca2c3af94b47bfd53bf81d27e49ac3
Philip Crotwell <crotwell@seis.sc.edu>
2013-07-11 16:17:47 -0400

    version 3.2.1
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/Version.java
+++ src/main/java/edu/sc/seis/sod/Version.java
@@ -118,7 +118,8 @@ public class Version{
       new Version("3.0.1rc1", false),
       new Version("3.1.0", false),
       new Version("3.1.1beta1", false),
-      new Version("3.2.0", false)
+      new Version("3.2.0", false),
+      new Version("3.2.1", false)
     };
 
 }
