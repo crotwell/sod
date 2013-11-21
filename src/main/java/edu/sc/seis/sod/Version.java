4170e0cf96527dc22b89cb71a3e3df42bb78ac3e
Philip Crotwell <crotwell@seis.sc.edu>
2013-11-21 10:37:59 -0500

    add SNAPSHOT version
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/Version.java
+++ src/main/java/edu/sc/seis/sod/Version.java
@@ -121,7 +121,8 @@ public class Version{
       new Version("3.2.0", false),
       new Version("3.2.1", false),
       new Version("3.2.2", false),
-      new Version("3.2.3", false)
+      new Version("3.2.3", false),
+      new Version("3.2.4-SNAPSHOT", false)
     };
 
 }
