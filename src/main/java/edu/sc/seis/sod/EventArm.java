bd543dfbd8744ececa031c5f70664d51a8327175
Philip Crotwell <crotwell@seis.sc.edu>
2014-04-11 10:36:25 -0400

    add to logger
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/EventArm.java
+++ src/main/java/edu/sc/seis/sod/EventArm.java
@@ -121,7 +121,7 @@ public class EventArm implements Arm {
             if ((lastTime.get(source) == null || lastTime.get(source).add(wait).before(ClockUtil.now()))
                     && source.hasNext()) {
                 CacheEvent[] next = source.next();
-                logger.info("Handling " + next.length + " events from " + source.getDescription());
+                logger.info("Handling " + next.length + " events from source " + source.getDescription());
                 handle(next);
                 lastTime.put(source, ClockUtil.now());
                 if (source.hasNext()) {
