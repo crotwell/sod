b3f1a172a2081720749ff4c9a917f55d19f331ac
Philip Crotwell <crotwell@seis.sc.edu>
2014-04-17 11:23:07 -0400

    add timerange comment to description
    --HG--
    branch : sod

--- src/main/java/edu/sc/seis/sod/source/event/FdsnEvent.java
+++ src/main/java/edu/sc/seis/sod/source/event/FdsnEvent.java
@@ -310,7 +310,7 @@ public class FdsnEvent extends AbstractEventSource implements EventSource {
     @Override
     public String getDescription() {
         try {
-            return queryParams.formURI().toString();
+            return queryParams.formURI().toString()+" with time range appended later.";
         } catch(URISyntaxException e) {
             throw new RuntimeException("Unable to for URL for description.", e);
         }
