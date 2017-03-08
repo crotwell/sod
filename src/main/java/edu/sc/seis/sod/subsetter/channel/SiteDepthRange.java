127494f71b5fc9983866b8fbf1336b60b36e69c9
Philip Crotwell <crotwell@seis.sc.edu>
2017-03-08 17:10:50 -0500

    add message for depthrange fail

--- src/main/java/edu/sc/seis/sod/subsetter/channel/SiteDepthRange.java
+++ src/main/java/edu/sc/seis/sod/subsetter/channel/SiteDepthRange.java
@@ -5,6 +5,7 @@ import org.w3c.dom.Element;
 import edu.iris.Fissures.model.QuantityImpl;
 import edu.iris.Fissures.network.ChannelImpl;
 import edu.sc.seis.sod.source.network.NetworkSource;
+import edu.sc.seis.sod.status.Fail;
 import edu.sc.seis.sod.status.StringTree;
 import edu.sc.seis.sod.status.StringTreeLeaf;
 
@@ -18,7 +19,12 @@ public class SiteDepthRange extends edu.sc.seis.sod.subsetter.DepthRange
     public StringTree accept(ChannelImpl channel, NetworkSource network)
             throws Exception {
         QuantityImpl actualDepth = (QuantityImpl)channel.getSite().getLocation().depth;
-        return new StringTreeLeaf(this, actualDepth.greaterThanEqual(getMinDepth())
-                && actualDepth.lessThanEqual(getMaxDepth()));
+        boolean inDepthRange = actualDepth.greaterThanEqual(getMinDepth())
+                && actualDepth.lessThanEqual(getMaxDepth());
+        if (inDepthRange) {
+            return new StringTreeLeaf(this, true);
+        } else {
+            return new Fail(this, getMinDepth()+" <= "+actualDepth+" <= "+getMaxDepth());
+        }
     }
 }// SiteDepthRange
