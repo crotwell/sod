ce327462411805f027a5e23dd4f458e8842370f8
Philip Crotwell <crotwell@seis.sc.edu>
2017-06-19 14:12:40 -0400

    remove unneeded test

--- src/test/java/edu/sc/seis/sod/subsetter/station/BelongsToVirtualTest.java
+++ src/test/java/edu/sc/seis/sod/subsetter/station/BelongsToVirtualTest.java
@@ -2,36 +2,17 @@ package edu.sc.seis.sod.subsetter.station;
 
 import java.util.List;
 
-import edu.sc.seis.sod.UserConfigurationException;
-import edu.sc.seis.sod.hibernate.NetworkNotFound;
 import edu.sc.seis.sod.mock.MockNetworkSource;
 import edu.sc.seis.sod.mock.station.MockNetworkAttr;
 import edu.sc.seis.sod.mock.station.MockStation;
 import edu.sc.seis.sod.model.common.TimeInterval;
 import edu.sc.seis.sod.model.common.UnitImpl;
-import edu.sc.seis.sod.model.station.CacheNetworkAccess;
 import edu.sc.seis.sod.model.station.NetworkAttrImpl;
 import edu.sc.seis.sod.model.station.StationImpl;
 import junit.framework.TestCase;
 
 public class BelongsToVirtualTest extends TestCase {
-
-    private final class ExplodesOnRetByName  extends NetworkFinder {
-
-        public ExplodesOnRetByName() {
-            super(FissuresNamingService.MOCK_DNS, NamedNetDC.EVERYBODY, 1);
-            this.netDC = new VestingNetworkDC(FissuresNamingService.MOCK_DNS, NamedNetDC.EVERYBODY, new FissuresNamingService());
-        }
-        
-        @Override
-        public List<CacheNetworkAccess> getNetworkByName(String name)
-                throws NetworkNotFound {
-            throw new NetworkNotFound("You lose!");
-        }
-    }
-
-
-    
+ 
     private final class CountRetrieveStations extends MockNetworkSource {
 
         public int callCount;
@@ -84,16 +65,5 @@ public class BelongsToVirtualTest extends TestCase {
         BelongsToVirtual btv = new BelongsToVirtual(mockNetName, FORTNIGHT);
         assertFalse(btv.accept(MockStation.createStation(), na).isSuccess());
         assertFalse(btv.accept(MockStation.createOtherStation(), na).isSuccess());
-    }
-
-    public void testBadVirtualName() throws Exception {
-        try {
-            ExplodesOnRetByName na = new ExplodesOnRetByName();
-            BelongsToVirtual btv = new BelongsToVirtual(mockNetName, FORTNIGHT);
-            btv.accept(MockStation.createStation(), na);
-            fail();
-        } catch(UserConfigurationException uce) {
-            assertTrue(true);
-        }
     }
 }
