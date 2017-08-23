0c1e4fe148a5e11a11f47498a161d479d3abec64
Philip Crotwell <crotwell@seis.sc.edu>
2017-08-23 14:25:59 -0400

    coarse cut seismograms before passing to processors

--- src/main/java/edu/sc/seis/sod/source/seismogram/PromiseSeismogramList.java
+++ src/main/java/edu/sc/seis/sod/source/seismogram/PromiseSeismogramList.java
@@ -1,12 +1,16 @@
 package edu.sc.seis.sod.source.seismogram;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
 import edu.iris.Fissures.network.ChannelIdUtil;
 import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.bag.Cut;
 import edu.sc.seis.fissuresUtil.time.MicroSecondTimeRange;
+import edu.sc.seis.sod.process.waveform.PhaseCut;
 
 
 public class PromiseSeismogramList {
@@ -32,7 +36,8 @@ public class PromiseSeismogramList {
         return matching;
     }
     
-    synchronized void finishRequest(List<LocalSeismogramImpl> seisList) {
+    synchronized void finishRequest(List<LocalSeismogramImpl> seisList) throws SeismogramSourceException {
+        try {
         matching = new ArrayList<LocalSeismogramImpl>();
         for (LocalSeismogramImpl seis : seisList) {
             for (RequestFilter rf : request) {
@@ -40,13 +45,17 @@ public class PromiseSeismogramList {
                     MicroSecondTimeRange rfRange = new MicroSecondTimeRange(rf);
                     MicroSecondTimeRange seisRange = new MicroSecondTimeRange(seis);
                     if (rfRange.intersects(seisRange)) {
-                        matching.add(seis);
+                        Cut c = new Cut(rf);
+                        matching.add(c.applyEncoded(seis));
                         break;
                     }
                 }
             }
         }
         notifyAll();
+        } catch (FissuresException e) {
+          throw new SeismogramSourceException(e);
+        }
     }
     
     synchronized void seismogramSourceException(SeismogramSourceException e) {
