fdb8574395f19b349c15646974241ef1dbe72661
Philip Crotwell <crotwell@seis.sc.edu>
2020-04-29 11:34:39 -0400

    cherry pick from 0c1e4fe148a5e11a11f47498a161d479d3abec64

--- src/main/java/edu/sc/seis/sod/source/seismogram/PromiseSeismogramList.java
+++ src/main/java/edu/sc/seis/sod/source/seismogram/PromiseSeismogramList.java
@@ -3,6 +3,8 @@ package edu.sc.seis.sod.source.seismogram;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.sc.seis.sod.bag.Cut;
+import edu.sc.seis.sod.model.common.FissuresException;
 import edu.sc.seis.sod.model.common.TimeRange;
 import edu.sc.seis.sod.model.seismogram.LocalSeismogramImpl;
 import edu.sc.seis.sod.model.seismogram.RequestFilter;
@@ -32,21 +34,26 @@ public class PromiseSeismogramList {
         return matching;
     }
     
-    synchronized void finishRequest(List<LocalSeismogramImpl> seisList) {
-        matching = new ArrayList<LocalSeismogramImpl>();
-        for (LocalSeismogramImpl seis : seisList) {
-            for (RequestFilter rf : request) {
-                if (ChannelIdUtil.areEqualExceptForBeginTime(rf.channelId, seis.getChannelID())) {
-                    TimeRange rfRange = new TimeRange(rf);
-                    TimeRange seisRange = new TimeRange(seis);
-                    if (rfRange.intersects(seisRange)) {
-                        matching.add(seis);
-                        break;
-                    }
-                }
-            }
-        }
-        notifyAll();
+    synchronized void finishRequest(List<LocalSeismogramImpl> seisList) throws SeismogramSourceException {
+        try {
+        	matching = new ArrayList<LocalSeismogramImpl>();
+			for (LocalSeismogramImpl seis : seisList) {
+			    for (RequestFilter rf : request) {
+			        if (ChannelIdUtil.areEqualExceptForBeginTime(rf.channelId, seis.getChannelID())) {
+			            TimeRange rfRange = new TimeRange(rf);
+			            TimeRange seisRange = new TimeRange(seis);
+			            if (rfRange.intersects(seisRange)) {
+			                Cut c = new Cut(rf);
+			                matching.add(c.applyEncoded(seis));
+			                break;
+			            }
+			        }
+			    }
+			}
+			notifyAll();
+		} catch (FissuresException e) {
+			throw new SeismogramSourceException(e);
+		}
     }
     
     synchronized void seismogramSourceException(SeismogramSourceException e) {
