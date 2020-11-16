cd4a728597580f339a72d0ef2054551e563a872c
Philip Crotwell <crotwell@seis.sc.edu>
2020-11-16 16:24:37 -0500

    version bump to 3.2.11-SNAPSHOT4

--- src/main/java/edu/sc/seis/sod/Version.java
+++ src/main/java/edu/sc/seis/sod/Version.java
@@ -139,7 +139,8 @@ public class Version{
       new Version("3.2.10", false),
       new Version("3.2.11-SNAPSHOT", false),
       new Version("3.2.11-SNAPSHOT2", false),
-      new Version("3.2.11-SNAPSHOT3", false)
+      new Version("3.2.11-SNAPSHOT3", false),
+      new Version("3.2.11-SNAPSHOT4", false)
     };
 
 }
