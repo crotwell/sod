8946ae355ff258872d323db249afc07e96eb80f6
Philip Crotwell <crotwell@seis.sc.edu>
2023-01-26 22:34:53 +0000

    version in source

--- src/main/java/edu/sc/seis/sod/Version.java
+++ src/main/java/edu/sc/seis/sod/Version.java
@@ -140,7 +140,9 @@ public class Version{
       new Version("3.2.11-SNAPSHOT", false),
       new Version("3.2.11-SNAPSHOT2", false),
       new Version("3.2.11-SNAPSHOT3", false),
-      new Version("3.2.11-SNAPSHOT5", false)
+      new Version("3.2.11-SNAPSHOT4", false),
+      new Version("3.2.11-SNAPSHOT5", false),
+      new Version("3.2.11-SNAPSHOT6", false)
     };
 
 }
