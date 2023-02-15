d34079ecbe5b2b0fc07526cce86cee05f8d8411d
Philip Crotwell <crotwell@seis.sc.edu>
2023-02-15 17:24:10 -0500

    ver to just snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ application {
 }
 
 group = "edu.sc.seis"
-version = "4.0.0-SNAPSHOT-WEBSTATUS"
+version = "4.0.0-SNAPSHOT"
 // also remember to change in sod.Version class
 // site/velocity/VM_library.vm
 // site/velocity/previousReleases.vm
