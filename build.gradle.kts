f94b9d931e4bdbc1c2421c4a03b64810b1d69a19
Philip Crotwell <crotwell@seis.sc.edu>
2023-04-21 12:53:43 -0400

    snap4

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ application {
 }
 
 group = "edu.sc.seis"
-version = "4.0.0-SNAPSHOT3"
+version = "4.0.0-SNAPSHOT4"
 // also remember to change in sod.Version class
 // site/velocity/VM_library.vm
 // site/velocity/previousReleases.vm
